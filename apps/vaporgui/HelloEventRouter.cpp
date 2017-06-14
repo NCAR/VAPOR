c11d8db7808b0568c7a1fe94ad5e129a4dcaf651
Scott Pearse <pearse@ucar.edu>
2017-06-14 16:35:43 -0600

    Removal of subeventrouter

--- apps/vaporgui/HelloEventRouter.cpp
+++ apps/vaporgui/HelloEventRouter.cpp
@@ -118,8 +118,14 @@ void HelloEventRouter::_updateTab() {
         _controlExec->GetDataMgr(),
         _controlExec->GetParamsMgr(),
         GetActiveParams());
-    _appearance->updateTab();
-    _geometry->updateTab();
+    _appearance->Update(
+        _controlExec->GetDataMgr(),
+        _controlExec->GetParamsMgr(),
+        GetActiveParams());
+    _geometry->Update(
+        _controlExec->GetDataMgr(),
+        _controlExec->GetParamsMgr(),
+        GetActiveParams());
 }
 
 // Whenever the user presses enter, call confirmText
@@ -213,8 +219,12 @@ void HelloEventRouter::
     //Set the color in the params.  Note that the UndoRedo automatically captures this event.
     aParams->SetConstantColor(rgbf);
 }
-void HelloEventRouter::HelloLayoutGUI::_updateTab() {
-    HelloParams *hParams = (HelloParams *)GetActiveParams();
+void HelloEventRouter::HelloLayoutGUI::Update(
+    VAPoR::DataMgr *dataMgr,
+    VAPoR::ParamsMgr *paramsMgr,
+    VAPoR::RenderParams *rParams) {
+
+    HelloParams *hParams = (HelloParams *)rParams;
 
     //Apply the start and end point coordinates to the text boxes in the geometry sub-tab
     //First obtain the point coordinates from the Params:
@@ -229,8 +239,13 @@ void HelloEventRouter::HelloLayoutGUI::_updateTab() {
     yCoord2Edit->setText(QString::number(endPoint[1]));
     zCoord2Edit->setText(QString::number(endPoint[2]));
 }
-void HelloEventRouter::HelloAppearanceGUI::_updateTab() {
-    HelloParams *hParams = (HelloParams *)GetActiveParams();
+
+void HelloEventRouter::HelloAppearanceGUI::Update(
+    VAPoR::DataMgr *dataMgr,
+    VAPoR::ParamsMgr *paramsMgr,
+    VAPoR::RenderParams *rParams) {
+
+    HelloParams *hParams = (HelloParams *)rParams;
 
     //Setup the constant color box.  Obtain the color from the params
     float rgb[3];
