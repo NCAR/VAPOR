e665e0d9e1b0940244e778650daebf57b1d2127e
John Clyne <clyne@ucar.edu>
2017-06-19 14:45:30 -0600

    Incremental commit - work on DataMgrUtils

--- apps/vaporgui/VariablesWidget.cpp
+++ apps/vaporgui/VariablesWidget.cpp
@@ -293,8 +293,7 @@ void VariablesWidget::getCmpFactors(
 	// Now get the "levels of detail" compression factors
 	//
 	vector <size_t> cratios;
-	int rc = _dataMgr->GetCRatios(varname, cratios);
-	assert(rc >= 0);
+	cratios = _dataMgr->GetCRatios(varname);
 
 	for (int i=0; i<cratios.size(); i++) {
 		ostringstream oss;
@@ -325,8 +324,7 @@ void VariablesWidget::updateFidelity(
 	}
 	fidelityFrame->show();
 
-	vector <size_t> cratios;
-	int rc = _dataMgr->GetCRatios(varname, cratios);
+	vector <size_t> cratios = _dataMgr->GetCRatios(varname);
 	assert(rc >= 0);
 
 
