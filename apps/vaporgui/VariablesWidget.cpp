abddd86d5b65ff8f163054e78a9f51ac1151e3f8
John Clyne <clyne@ucar.edu>
2017-06-20 09:52:43 -0600

    Fidelity buttons were not being properly cleaned up during update

--- apps/vaporgui/VariablesWidget.cpp
+++ apps/vaporgui/VariablesWidget.cpp
@@ -36,8 +36,11 @@ VariablesWidget::VariablesWidget(QWidget *parent)
     setupUi(this);
 
     _fidelityButtons = new QButtonGroup(fidelityBox);
+    _fidelityButtons->setExclusive(true);
+
     QHBoxLayout *hlay = new QHBoxLayout(fidelityBox);
     hlay->setAlignment(Qt::AlignHCenter);
+    fidelityBox->setLayout(hlay);
 
     connect(
         refinementCombo, SIGNAL(activated(int)), this,
@@ -373,19 +376,26 @@ void VariablesWidget::updateFidelity(
         }
     } while (l < lodCFs.size() && m < multiresCFs.size());
 
-    QHBoxLayout *hlay = (QHBoxLayout *)fidelityBox->layout();
-
+    // Remove buttons from the group
+    //
     QList<QAbstractButton *> btns = _fidelityButtons->buttons();
     for (int i = 0; i < btns.size(); i++) {
         _fidelityButtons->removeButton(btns[i]);
-        hlay->removeWidget(btns[i]);
-        delete btns[i];
+    }
+
+    // Remove and delete buttons from the layout
+    //
+    QHBoxLayout *hlay = (QHBoxLayout *)fidelityBox->layout();
+    QLayoutItem *child;
+    while ((child = hlay->takeAt(0)) != 0) {
+        delete child;
     }
 
     int numButtons = _fidelityLodStrs.size();
     for (int i = 0; i < numButtons; i++) {
-        QRadioButton *rd = new QRadioButton(fidelityBox);
+        QRadioButton *rd = new QRadioButton();
         hlay->addWidget(rd);
+
         _fidelityButtons->addButton(rd, i);
         QString qs = "Refinement " +
                      QString(_fidelityMultiresStrs[i].c_str()) + "\nLOD " +
@@ -397,6 +407,7 @@ void VariablesWidget::updateFidelity(
             rd->setChecked(true);
         }
     }
+    fidelityBox->setLayout(hlay);
 }
 
 void VariablesWidget::uncheckFidelity() {
@@ -407,13 +418,11 @@ void VariablesWidget::uncheckFidelity() {
         return;
 
     QList<QAbstractButton *> btns = _fidelityButtons->buttons();
-    _fidelityButtons->setExclusive(false);
     for (int i = 0; i < btns.size(); i++) {
         if (btns[i]->isChecked()) {
             btns[i]->setChecked(false);
         }
     }
-    _fidelityButtons->setExclusive(true);
 }
 
 void VariablesWidget::setCompRatio(int num) {
