009f0d6132d9a4f5ba9ffd8a4faa76cb285b0190
Scott Pearse <pearse@ucar.edu>
2017-07-06 11:57:00 -0600

    Updates for undo/redo in MappingFrame

--- apps/vaporgui/MappingFrame.cpp
+++ apps/vaporgui/MappingFrame.cpp
@@ -1648,6 +1648,7 @@ void MappingFrame::resize() {
 // Handle mouse press events
 //----------------------------------------------------------------------------
 void MappingFrame::mousePressEvent(QMouseEvent *event) {
+    _paramsMgr->BeginSaveStateGroup("MappingFrame mousePressEvent");
     select(event->x(), event->y(), event->modifiers());
 
     _lastx = xViewToWorld(event->x());
@@ -1715,7 +1716,7 @@ void MappingFrame::mouseReleaseEvent(QMouseEvent *event) {
         emit updateParams();
     }
 
-    cout << "MappingFrame mouseReleaseEvent" << endl;
+    _paramsMgr->EndSaveStateGroup();
 }
 
 //----------------------------------------------------------------------------
