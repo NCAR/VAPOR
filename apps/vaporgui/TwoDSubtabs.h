735cdd80a4ba5d1844cf7ca46a7206caff8ea104
Scott Pearse <pearse@ucar.edu>
2017-07-06 10:17:07 -0600

    Incremental commit

--- apps/vaporgui/TwoDSubtabs.h
+++ apps/vaporgui/TwoDSubtabs.h
@@ -38,6 +38,7 @@ class TwoDAppearanceSubtab : public QWidget, public Ui_TwoDAppearanceGUI {
   public:
     TwoDAppearanceSubtab(QWidget *parent) {
         setupUi(this);
+        //_TFWidget->Reinit((TFWidget::Flags)(0));
         _TFWidget->setEventRouter(dynamic_cast<RenderEventRouter *>(parent));
     }
 
