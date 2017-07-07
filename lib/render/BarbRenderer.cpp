982af115c585743cd0ee9574b03dc2c85e9c5095
Scott Pearse <pearse@ucar.edu>
2017-07-07 09:05:49 -0600

    Changed diagnostic print

--- lib/render/BarbRenderer.cpp
+++ lib/render/BarbRenderer.cpp
@@ -122,7 +122,6 @@ int BarbRenderer::_paintGL() {
     }
 
     float vectorLengthScale = bParams->GetLengthScale() * _vectorScaleFactor;
-    cout << "calculated vectorLengthScale" << endl;
 
     //
     //Perform OpenGL rendering of barbs
@@ -308,7 +307,7 @@ int BarbRenderer::performRendering(
     float thickness = bParams->GetLineThickness();
     //float rad =(float)( 0.001*vpParams->GetCurrentViewDiameter()*thickness);
     float rad = (float)(1000 * thickness);
-    cout << "fudging barb line thickness.  Fix me!" << endl;
+    cout << "fudging barb line thickness.  Need stretch factors.  Fix me!" << endl;
     //Set up lighting and color
     int nLights = vpParams->getNumLights();
     float fcolor[3];
