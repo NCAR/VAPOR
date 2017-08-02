f57795de28cd0b8fcd2f496091002d08a742e307
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-08-02 16:49:01 -0600

    Changed error save output from Unicode to ASCII. Added log to file. Updated to conform to VAPOR coding standards. Added doxygen documentation

--- apps/vaporgui/ErrorReporter.h
+++ apps/vaporgui/ErrorReporter.h
@@ -1,18 +1,18 @@
 //************************************************************************
-//									*
-//		     Copyright (C)  2015				*
-//     University Corporation for Atmospheric Research			*
-//		     All Rights Reserved				*
-//									*
-//************************************************************************/
+//                                                                       *
+//                        Copyright (C)  2017                            *
+//           University Corporation for Atmospheric Research             *
+//                        All Rights Reserved                            *
+//                                                                       *
+//************************************************************************
 //
-//	File:		ErrorReporter.h
+//	File:			ErrorReporter.h
 //
-//	Author:		Stas Jaroszynski
-//				National Center for Atmospheric Research
-//				PO 3000, Boulder, Colorado
+//	Author:			Stas Jaroszynski (stasj@ucar.edu)
+//					National Center for Atmospheric Research
+//					PO 3000, Boulder, Colorado
 //
-//	Date:		July 2017
+//	Date:			July 2017
 //
 //	Description:	Defines the ErrorReporting class.  This is used for
 //	posting various messages that can occur during the operation of the Vapor GUI
@@ -30,6 +30,18 @@
 #include <string>
 #include <vector>
 
+#define ERRORREPORTER_DEFAULT_MESSAGE "The action failed"
+
+//! \class ErrorReporter
+//! \ingroup Public_GUI
+//! \brief A utility singleton class that provides error reporting functinality
+//! \author Stas Jaroszynski
+//! \version 3.0
+//! \date July 2017
+
+//! ErrorReporter class provides error reporting functionality. Registers error
+//! callbacks with MyBase and registers signal handler for SIGSEGV
+
 class ErrorReporter {
 
 	public:
@@ -44,23 +56,40 @@ class ErrorReporter {
 				: type(type_), value(value_), err_code(err_code_) {}
 		};
 
-		static ErrorReporter *getInstance();
-		static void showErrors();
-		static void report(std::string msg, Type severity = Diagnostic, std::string details = "");
-		static std::string getSystemInformation();
+		//! Returns the singleton instance of this class with lazy initialization
+		//! \retval ErrorReporter instance
+		static ErrorReporter *GetInstance();
+
+		//! Displays the current log of errors with the default message ERRORREPORTER_DEFAULT_MESSAGE
+		static void ShowErrors();
+
+		//! Displays an error message with the log of errors and outputs the message to the log file
+		//! \param string msg to display explaining error cause/implications
+		//! \param Type severity of message
+		//! \param string details of error. Default to current erros in log
+		static void Report(std::string msg, Type severity = Diagnostic, std::string details = "");
+
+		//! Returns basic system OS information
+		//! \retval string containing OS information
+		static std::string GetSystemInformation();
+
+		//! Opens log file and begins logging error and diagnostic messages
+		//! \retval int returns -1 on failure
+		static int OpenLogFile(std::string path);
 
 	protected:
 		ErrorReporter();
 		~ErrorReporter();
 
 	private:
-		static ErrorReporter *instance;
-		std::vector<Message> log;
-		std::vector<Message> fullLog;
-		FILE *logFile;
+		static ErrorReporter *_instance;
+		std::vector<Message> _log;
+		std::vector<Message> _fullLog;
+		std::string _logFilePath;
+		FILE *_logFile;
 
-		friend void MyBaseErrorCallback(const char *msg, int err_code);
-		friend void MyBaseDiagCallback(const char *msg);
+		friend void _myBaseErrorCallback(const char *msg, int err_code);
+		friend void _myBaseDiagCallback(const char *msg);
 };
 
 #endif
