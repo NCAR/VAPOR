5c10baa674dd65e02a6b1b79f38f8174e0f16036
Samuel Li <shaomenglee@gmail.com>
2017-08-07 15:22:14 -0600

    sync

--- apps/vaporgui/ImageSubtabs.h
+++ apps/vaporgui/ImageSubtabs.h
@@ -12,61 +12,76 @@ namespace VAPoR {
 	class DataMgr;
 }
 
+
+//
+// ImageVariablesSubtab class
+//
 class ImageVariablesSubtab : public QWidget, public Ui_ImageVariablesGUI {
 
 	Q_OBJECT
 
 public:
-	ImageVariablesSubtab(QWidget* parent) {
-		setupUi(this);
+	ImageVariablesSubtab(QWidget* parent) 
+  { 
+    setupUi(this);
 		_variablesWidget->Reinit( VariablesWidget::HGT, VariablesWidget::TWOD );
 	}
 
-	void Update(
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::RenderParams *rParams
-	) {
+	void Update(  VAPoR::DataMgr *dataMgr,
+		            VAPoR::ParamsMgr *paramsMgr,
+		            VAPoR::RenderParams *rParams) 
+  {
 		_variablesWidget->Update(dataMgr, paramsMgr, rParams);
 	}
 };
 
+
+//
+// ImageAppearanceSubtab class
+//
 class ImageAppearanceSubtab : public QWidget, public Ui_ImageAppearanceGUI {
 
 	Q_OBJECT
 
 public:
-	ImageAppearanceSubtab(QWidget* parent) {
+	ImageAppearanceSubtab(QWidget* parent) 
+  {
 		setupUi(this);
-		//_TFWidget->Reinit((TFWidget::Flags)(0));
+    connect( GeoreferenceCheckbox, SIGNAL(clicked()), this, SLOT( GeoreferenceClicked() ) );
 	}
 
-	void Update(
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::RenderParams *rParams
-	) {
-		//_TFWidget->Update(dataMgr, paramsMgr, rParams);
-		//_ColorbarWidget->Update(dataMgr, paramsMgr, rParams);
+	void Update(  VAPoR::DataMgr*       dataMgr,
+		            VAPoR::ParamsMgr*     paramsMgr,
+		            VAPoR::RenderParams*  rParams) 
+  {
 	}
+
+private slots:
+  void GeoreferenceClicked()
+  {
+    std::cout << "I'm clicked" << std::endl;
+  }
 };
 
+
+//
+// ImageGeometrySubtab class
+//
 class ImageGeometrySubtab : public QWidget, public Ui_ImageGeometryGUI {
 
 	Q_OBJECT
 
 public:
-	ImageGeometrySubtab(QWidget* parent) {
+	ImageGeometrySubtab(QWidget* parent) 
+  {
 		setupUi(this);
-		_geometryWidget->Reinit(
-			GeometryWidget::TWOD);
+		_geometryWidget->Reinit( GeometryWidget::TWOD);
 	}
 	
-	void Update(
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::RenderParams *rParams
-	) {
+	void Update(  VAPoR::ParamsMgr *paramsMgr,
+		            VAPoR::DataMgr *dataMgr,
+		            VAPoR::RenderParams *rParams) 
+  {
 		_geometryWidget->Update(paramsMgr, dataMgr, rParams);
 	}
 
