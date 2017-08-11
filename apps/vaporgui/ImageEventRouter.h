d1dd68960a28d430803b1ddcfc5a8e4e571034d4
Samuel Li <shaomenglee@gmail.com>
2017-08-11 15:46:42 -0600

    it paints something

--- apps/vaporgui/ImageEventRouter.h
+++ apps/vaporgui/ImageEventRouter.h
@@ -14,7 +14,6 @@ QT_USE_NAMESPACE
 
 namespace VAPoR {
 class ControlExec;
-}
 
 class ImageEventRouter : public QTabWidget, public RenderEventRouter {
 
@@ -47,5 +46,7 @@ class ImageEventRouter : public QTabWidget, public RenderEventRouter {
     ImageGeometrySubtab *_geometry;
     ImageAppearanceSubtab *_appearance;
 };
+
+} // namespace VAPoR
 
 #endif
