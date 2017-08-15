6abea3256ae5d7178ca170ed7e0d2ca7bc6c3a75
Samuel Li <shaomenglee@gmail.com>
2017-08-15 14:23:58 -0600

    ImageTab now loads NaturalEarth image by default

--- include/vapor/ImageParams.h
+++ include/vapor/ImageParams.h
@@ -3,6 +3,7 @@
 
 #include <vapor/RenderParams.h>
 #include <vapor/DataMgr.h>
+#include <vapor/GetAppPath.h>
 
 namespace VAPoR 
 {
@@ -33,7 +34,11 @@ public:
   }
   std::string GetImagePath( ) const
   {
-    return GetValueString( _fileNameTag, "File name not found!" );
+    std::vector<std::string> paths;
+    paths.push_back("images/NaturalEarth.tms");
+    std::string defaultImage = Wasp::GetAppPath("VAPOR", "share", paths);
+
+    return GetValueString( _fileNameTag, defaultImage );
   }
 
 
