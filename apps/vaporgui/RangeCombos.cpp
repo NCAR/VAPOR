9bf4d82b4bc981d5d32fe6950de458778ec0aded
Samuel Li <shaomenglee@gmail.com>
2017-08-18 12:08:32 -0600

    ImageSubtabs use combo

--- apps/vaporgui/RangeCombos.cpp
+++ apps/vaporgui/RangeCombos.cpp
@@ -25,9 +25,11 @@ Combo::Combo(QLineEdit* edit, QSlider* slider, bool intType)
 	_lineEditValidator = NULL;
 	_slider = NULL;
 
-//	_lineEdit = new QLineEdit;
 	_lineEdit = edit;
 
+  /*
+   * validators without specifications don't validate anything, so better not having them.
+   *
 	if (_intType) {
 		_lineEditValidator = new QIntValidator(_lineEdit);
 		_lineEdit->setValidator((QIntValidator *) _lineEditValidator);
@@ -35,7 +37,7 @@ Combo::Combo(QLineEdit* edit, QSlider* slider, bool intType)
 	else {
 		_lineEditValidator = new QDoubleValidator(_lineEdit);
 		_lineEdit->setValidator((QDoubleValidator *) _lineEditValidator);
-	} 
+	}*/ 
 
 
 	// Set up slot for changes to _lineEdit
