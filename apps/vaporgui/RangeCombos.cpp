fdfe2fe5350fe9fc4273b25c3401704dc8d69d30
Samuel Li <shaomenglee@gmail.com>
2017-08-21 11:08:46 -0600

    revert deleting QDoubleValidator

--- apps/vaporgui/RangeCombos.cpp
+++ apps/vaporgui/RangeCombos.cpp
@@ -25,17 +25,13 @@ Combo::Combo(QLineEdit *edit, QSlider *slider, bool intType) {
 
     _lineEdit = edit;
 
-    /*
-   * validators without specifications don't validate anything, so better not having them.
-   *
-	if (_intType) {
-		_lineEditValidator = new QIntValidator(_lineEdit);
-		_lineEdit->setValidator((QIntValidator *) _lineEditValidator);
-	}
-	else {
-		_lineEditValidator = new QDoubleValidator(_lineEdit);
-		_lineEdit->setValidator((QDoubleValidator *) _lineEditValidator);
-	}*/
+    if (_intType) {
+        _lineEditValidator = new QIntValidator(_lineEdit);
+        _lineEdit->setValidator((QIntValidator *)_lineEditValidator);
+    } else {
+        _lineEditValidator = new QDoubleValidator(_lineEdit);
+        _lineEdit->setValidator((QDoubleValidator *)_lineEditValidator);
+    }
 
     // Set up slot for changes to _lineEdit
     //
