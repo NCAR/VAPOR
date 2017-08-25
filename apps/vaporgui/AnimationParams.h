ecfe23abea067df6034729be76623ff2f1aa3e14
Stas Jaroszynski <stasj@ucar.edu>
2017-08-25 13:55:02 -0600

    Updated rebase to comply with VC++

--- apps/vaporgui/AnimationParams.h
+++ apps/vaporgui/AnimationParams.h
@@ -37,7 +37,7 @@
 //! When this class is local, it controls the time-steps in one visualizer.
 //! The global (shared) AnimationParams controls the animation in any number of visualizers.
 
-class PARAMS_API AnimationParams : public VAPoR::ParamsBase {
+class AnimationParams : public VAPoR::ParamsBase {
 	
 public: 
 
