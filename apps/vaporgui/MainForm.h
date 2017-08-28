1631a11cf623d4da1a7618b760443c24a9c1ed97
Scott Pearse <pearse@ucar.edu>
2017-08-28 09:15:06 -0600

    Updates to contour renderer

--- apps/vaporgui/MainForm.h
+++ apps/vaporgui/MainForm.h
@@ -213,6 +213,7 @@ private:
 	QAction* _dataImportWRF_Action;
 	QAction* _dataImportCF_Action;
 	QAction* _dataLoad_MetafileAction;
+	QAction* _dataClose_MetafileAction;
 	QAction* _fileNew_SessionAction;
 	QAction* _plotAction;
 	QAction* _statsAction;
@@ -265,6 +266,7 @@ private slots:
 	void helpContents();
 	void helpAbout();
 	void loadData(string fileName="");
+	void closeData(string fileName="");
 	void importWRFData();
 	void importCFData();
 	void sessionNew();
