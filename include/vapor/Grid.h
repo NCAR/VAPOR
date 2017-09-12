a2cf1b11bf28e972e72092e1484e0bb6e2e554aa
John Clyne <clyne@ucar.edu>
2017-09-12 15:50:14 -0600

    incremental commit - making all iterators polymorphic (virtual)

--- include/vapor/Grid.h
+++ include/vapor/Grid.h
@@ -490,6 +490,36 @@ class VDF_API Grid {
 
     VDF_API friend std::ostream &operator<<(std::ostream &o, const Grid &g);
 
+    /////////////////////////////////////////////////////////////////////////////
+    //
+    // Iterators
+    //
+    /////////////////////////////////////////////////////////////////////////////
+
+    // Inside a box functor
+    //
+    // operator() returns true if point pt is on or inside the axis-aligned
+    // box defined by min and max
+    //
+    class InsideBox {
+      public:
+        InsideBox(
+            const std::vector<double> &min, const std::vector<double> &max) : _min(min), _max(max) {}
+        InsideBox() {}
+
+        bool operator()(const std::vector<double> &pt) const {
+            for (int i = 0; i < _min.size(); i++) {
+                if (pt[i] < _min[i] || pt[i] > _max[i])
+                    return (false);
+            }
+            return (true);
+        }
+
+      private:
+        std::vector<double> _min;
+        std::vector<double> _max;
+    };
+
     //
     // Define polymorphic iterator that can be used with any
     // class derived from this class
@@ -550,7 +580,7 @@ class VDF_API Grid {
             return (PolyIterator());
         };
 
-        const std::vector<double> &operator*() const {
+        const T &operator*() const {
             return _impl->deref();
         }
 
@@ -566,6 +596,8 @@ class VDF_API Grid {
         std::unique_ptr<AbstractIterator<T>> _impl;
     };
 
+    // Coordinate iterator. Iterates over grid node/cell coordinates
+    //
     typedef const std::vector<double> ConstCoordType;
     typedef Grid::PolyIterator<ConstCoordType> ConstCoordItr;
     typedef Grid::AbstractIterator<ConstCoordType> ConstCoordItrAbstract;
@@ -573,26 +605,14 @@ class VDF_API Grid {
     virtual ConstCoordItr ConstCoordBegin() const = 0;
     virtual ConstCoordItr ConstCoordEnd() const = 0;
 
-    // Inside a box functor
-    //
-    class InsideBox {
-      public:
-        InsideBox(
-            const std::vector<double> &min, const std::vector<double> &max) : _min(min), _max(max) {}
-        InsideBox() {}
+    typedef const std::vector<size_t> ConstIndexType;
+    typedef Grid::PolyIterator<ConstIndexType> ConstNodeIterator;
+    typedef Grid::AbstractIterator<ConstIndexType> ConstNodeIteratorAbstract;
 
-        bool operator()(const std::vector<double> &pt) const {
-            for (int i = 0; i < _min.size(); i++) {
-                if (pt[i] < _min[i] || pt[i] > _max[i])
-                    return (false);
-            }
-            return (true);
-        }
-
-      private:
-        std::vector<double> _min;
-        std::vector<double> _max;
-    };
+    virtual ConstNodeIterator ConstNodeBegin() const = 0;
+    virtual ConstNodeIterator ConstNodeBegin(
+        const std::vector<double> &minu, const std::vector<double> &maxu) const = 0;
+    virtual ConstNodeIterator ConstNodeEnd() const = 0;
 
     //! A forward iterator for accessing the data elements of the
     //! structured grid.
--- test_apps/grid_iter/test_grid_iter.cpp
+++ test_apps/grid_iter/test_grid_iter.cpp
@@ -218,7 +218,7 @@ void init_grid(StructuredGrid *sg) {
     }
 }
 
-void test_node_iterator(const StructuredGrid *sg) {
+void test_iterator(const StructuredGrid *sg) {
 
     double t0 = Wasp::GetTime();
 
@@ -237,6 +237,7 @@ void test_node_iterator(const StructuredGrid *sg) {
 
 void test_cell_iterator(const StructuredGrid *sg) {
 
+#ifdef DEAD
     double t0 = Wasp::GetTime();
 
     StructuredGrid::ConstCellIterator itr;
@@ -259,6 +260,20 @@ void test_cell_iterator(const StructuredGrid *sg) {
     }
     cout << "Iteration time : " << Wasp::GetTime() - t0 << endl;
     cout << "Cell count : " << count << endl;
+#endif
+}
+
+void test_node_iterator(const StructuredGrid *sg) {
+
+    double t0 = Wasp::GetTime();
+
+    Grid::ConstNodeIterator newitr;
+    size_t count = 0;
+    for (newitr = sg->ConstNodeBegin(); newitr != sg->ConstNodeEnd(); ++newitr) {
+        out_container((*newitr).cbegin(), (*newitr).cend());
+        cout << endl;
+    }
+    cout << "Iteration time : " << Wasp::GetTime() - t0 << endl;
 }
 
 int main(int argc, char **argv) {
@@ -313,9 +328,11 @@ int main(int argc, char **argv) {
     cout << *sg;
     cout << endl;
 
-    test_node_iterator(sg);
+    //	test_iterator(sg);
 
-    test_cell_iterator(sg);
+    //	test_cell_iterator(sg);
+
+    test_node_iterator(sg);
 
     delete sg;
 
