1cb40e1d0b2ecd878499cec1e322dc4d2eb3a452
Scott Pearse <pearse@ucar.edu>
2017-09-15 16:44:05 -0600

    Cleanup for Box and BarbSubtabs

--- lib/params/Box.cpp
+++ lib/params/Box.cpp
@@ -88,7 +88,6 @@ void Box::SetExtents(
         exts.push_back(0.0);
 
     SetValueDoubleVec(m_extentsTag, "Set box extents", exts);
-    //cout << "Box::SetExts: " << exts[0] << " " << exts[1] << " " << exts[2] << " " << exts[3] << " " << exts[4] << " " << exts[5] << endl;
 }
 
 void Box::GetExtents(
