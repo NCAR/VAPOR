86a65b39003df3d68a24294027f20dfc81e6044c
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-09-18 00:59:38 -0600

    Added help menu item on OSX that installs CLI utils

--- apps/vaporgui/MainForm.h
+++ apps/vaporgui/MainForm.h
@@ -204,6 +204,7 @@ private:
     QAction *_helpIndexAction;
     QAction *_helpAboutAction;
     QAction *_whatsThisAction;
+    QAction *_installCLIToolsAction;
 
     // Data menu
     QAction *_dataImportWRF_Action;
@@ -270,6 +271,7 @@ private slots:
     void endAnimCapture();
     void captureSingleJpeg();
     void launchSeedMe();
+    void installCLITools();
     void launchStats();
     void launchPlotUtility();
     void batchSetup();
