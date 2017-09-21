531d81ec093ea32433e0c3012327933d4d65fb46
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-09-21 14:15:54 -0600

    Linux launchers v.9

---
+++ apps/linuxlauncher/launcher.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char path[PATH_MAX];
+	readlink("/proc/self/exe", path, PATH_MAX);
+	char *p = &path[strlen(path)-1];
+	int up = 2;
+	while (p != path && up) {
+		if (*p == '/') {
+			*p = 0;
+			up--;
+		}
+		p--;
+	}
+
+	setenv("VAPOR_HOME", path, 1);
+
+	// TODO Append to path instead of overriding
+	strcat(path, "/lib");
+	setenv("LD_LIBRARY_PATH", path, 1);
+
+	argv[0] = "@TARGET_NAME@";
+
+	strcat(path, "/@TARGET_NAME@");
+	printf("PATH %s\n", path);
+
+	execv(path, argv);
+	return 0;
+}
