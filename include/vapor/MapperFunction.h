2b54f21eb08ee091f8087d0cc9dadf95b1336624
Scott Pearse <pearse@ucar.edu>
2017-09-21 10:04:36 -0600

    Fixed colorMapped variable transferFunction bug

--- include/vapor/MapperFunction.h
+++ include/vapor/MapperFunction.h
@@ -248,6 +248,27 @@ class PARAMS_API MapperFunction : public ParamsBase {
             _autoUpdateHistoTag, "enable/disable auto update of histogram", val);
     }
 
+    //! Method to get the state of whether the current mapper function
+    //! applies color through a primary variable or secondary variable.
+    //! For example, Barbs may have a "Color Mapped Variable" that colors
+    //! the barbs according to a "Secondary variable", independent of
+    //! the vector variables that define the Barbs.  Isosurfaces can have
+    //! Secondary Variable colorings too.
+    //!
+    bool GetSecondaryVarMapper() {
+        return ((bool)GetValueLong(_autoUpdateHistoTag, (int)false));
+    }
+
+    //! Method to set the state of whether the current mapper function
+    //! applies color through a ColorMappedVariable, or a renderer's primary
+    //! variable.
+    //! \param[in] State of the Secondary Variable color setting
+    //
+    void SetSecondaryVarMapper(bool val) {
+        SetValueLong(
+            _secondaryVarMapperTag, "Apply color through a secondary color", val);
+    }
+
   private:
     //
     // XML tags
@@ -259,6 +280,7 @@ class PARAMS_API MapperFunction : public ParamsBase {
     static const string _opacityMapsTag;
     static const string _opacityMapTag;
     static const string _autoUpdateHistoTag;
+    static const string _secondaryVarMapperTag;
 
     //
     // Size of lookup table.  Always 1<<8 currently!
