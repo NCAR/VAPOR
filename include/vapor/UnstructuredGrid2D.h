21d6a9ac2656a1df7b8af7dc7426539bb0262722
John Clyne <clyne@ucar.edu>
2017-10-05 16:38:25 -0600

    First working version of MPAS data reader and unstructured grid support

---
+++ include/vapor/UnstructuredGrid2D.h
@@ -0,0 +1,161 @@
+#ifndef _UnstructuredGrid2D_
+#define _UnstructuredGrid2D_
+
+#include <ostream>
+#include <vector>
+#include <memory>
+#include <vapor/common.h>
+#include <vapor/UnstructuredGrid.h>
+#include <vapor/UnstructuredGridCoordless.h>
+#include <vapor/KDTreeRG.h>
+
+#ifdef WIN32
+#pragma warning(disable : 4661 4251) // needed for template class
+#endif
+
+namespace VAPoR {
+
+//! \class UnstructuredGrid2D
+//! \brief class for a 2D unstructured grid.
+//! \author John Clyne
+//!
+//
+class VDF_API UnstructuredGrid2D : public UnstructuredGrid {
+  public:
+    //! Construct a unstructured grid sampling 2D scalar function
+    //!
+    //
+    UnstructuredGrid2D(
+        const std::vector<size_t> &vertexDims,
+        const std::vector<size_t> &faceDims,
+        const std::vector<size_t> &edgeDims,
+        const std::vector<size_t> &bs,
+        const std::vector<float *> &blks,
+        const int *vertexOnFace,
+        const int *faceOnVertex,
+        const int *faceOnFace,
+        Location location, // node,face, edge
+        size_t maxVertexPerFace,
+        size_t maxFacePerVertex,
+        const UnstructuredGridCoordless &xug,
+        const UnstructuredGridCoordless &yug,
+        const UnstructuredGridCoordless &zug,
+        const KDTreeRG *kdtree);
+
+    UnstructuredGrid2D() = default;
+    virtual ~UnstructuredGrid2D() = default;
+
+    virtual size_t GetNumCoordinates() const override;
+
+    virtual void GetUserExtents(
+        std::vector<double> &minu, std::vector<double> &maxu) const override;
+
+    virtual void GetBoundingBox(
+        const std::vector<size_t> &min, const std::vector<size_t> &max,
+        std::vector<double> &minu, std::vector<double> &maxu) const override;
+
+    void GetEnclosingRegion(
+        const std::vector<double> &minu, const std::vector<double> &maxu,
+        std::vector<size_t> &min, std::vector<size_t> &max) const override;
+
+    void GetUserCoordinates(
+        const std::vector<size_t> &indices,
+        std::vector<double> &coords) const override;
+
+    void GetIndices(
+        const std::vector<double> &coords,
+        std::vector<size_t> &indices) const override;
+
+    bool GetIndicesCell(
+        const std::vector<double> &coords,
+        std::vector<size_t> &indices) const override;
+
+    bool InsideGrid(const std::vector<double> &coords) const override;
+
+    float GetValueNearestNeighbor(
+        const std::vector<double> &coords) const override;
+
+    float GetValueLinear(
+        const std::vector<double> &coords) const override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    //
+    // Iterators
+    //
+    /////////////////////////////////////////////////////////////////////////////
+
+    class ConstCoordItrU2D : public Grid::ConstCoordItrAbstract {
+      public:
+        ConstCoordItrU2D(const UnstructuredGrid2D *ug, bool begin);
+        ConstCoordItrU2D(const ConstCoordItrU2D &rhs);
+
+        ConstCoordItrU2D();
+        virtual ~ConstCoordItrU2D() {}
+
+        virtual void next();
+        virtual ConstCoordType &deref() const {
+            return (_coords);
+        }
+        virtual const void *address() const { return this; };
+
+        virtual bool equal(const void *rhs) const {
+            const ConstCoordItrU2D *itrptr =
+                static_cast<const ConstCoordItrU2D *>(rhs);
+
+            return (
+                _xCoordItr == itrptr->_xCoordItr &&
+                _yCoordItr == itrptr->_yCoordItr &&
+                _zCoordItr == itrptr->_zCoordItr);
+        }
+
+        virtual std::unique_ptr<ConstCoordItrAbstract> clone() const {
+            return std::unique_ptr<ConstCoordItrAbstract>(new ConstCoordItrU2D(*this));
+        };
+
+      private:
+        int _ncoords;
+        ConstIterator _xCoordItr;
+        ConstIterator _yCoordItr;
+        ConstIterator _zCoordItr;
+        std::vector<double> _coords;
+    };
+
+    virtual ConstCoordItr ConstCoordBegin() const override {
+        return ConstCoordItr(
+            std::unique_ptr<ConstCoordItrAbstract>(new ConstCoordItrU2D(this, true)));
+    }
+    virtual ConstCoordItr ConstCoordEnd() const override {
+        return ConstCoordItr(
+            std::unique_ptr<ConstCoordItrAbstract>(new ConstCoordItrU2D(this, false)));
+    }
+
+    VDF_API friend std::ostream &operator<<(std::ostream &o, const UnstructuredGrid2D &sg);
+
+  private:
+    UnstructuredGridCoordless _xug;
+    UnstructuredGridCoordless _yug;
+    UnstructuredGridCoordless _zug;
+    const KDTreeRG *_kdtree;
+
+    bool _insideGrid(
+        const std::vector<double> &coords,
+        std::vector<size_t> &face_indices,
+        double *lambda, int &nlambda, double zwgt[2]) const;
+
+    bool _insideGridNodeCentered(
+        const std::vector<double> &coords,
+        std::vector<size_t> &face_indices,
+        double *lambda, int &nlambda, double zwgt[2]) const;
+
+    bool _insideGridFaceCentered(
+        const std::vector<double> &coords,
+        std::vector<size_t> &face_indices,
+        double *lambda, int &nlambda, double zwgt[2]) const;
+
+    bool _insideFace(
+        size_t face, double pt[2],
+        double *lambda, int &nlambda, double zwgt[2]) const;
+};
+}; // namespace VAPoR
+
+#endif
---
+++ lib/vdc/UnstructuredGrid2D.cpp
@@ -0,0 +1,385 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <cmath>
+#include <time.h>
+#ifdef Darwin
+#include <mach/mach_time.h>
+#endif
+#ifdef _WINDOWS
+#include "windows.h"
+#include "Winbase.h"
+#include <limits>
+#endif
+
+#include <vapor/utils.h>
+#include <vapor/vizutil.h>
+#include <vapor/UnstructuredGrid2D.h>
+
+using namespace std;
+using namespace VAPoR;
+
+UnstructuredGrid2D::UnstructuredGrid2D(
+    const std::vector<size_t> &vertexDims,
+    const std::vector<size_t> &faceDims,
+    const std::vector<size_t> &edgeDims,
+    const std::vector<size_t> &bs,
+    const std::vector<float *> &blks,
+    const int *vertexOnFace,
+    const int *faceOnVertex,
+    const int *faceOnFace,
+    Location location, // node,face, edge
+    size_t maxVertexPerFace,
+    size_t maxFacePerVertex,
+    const UnstructuredGridCoordless &xug,
+    const UnstructuredGridCoordless &yug,
+    const UnstructuredGridCoordless &zug,
+    const KDTreeRG *kdtree) : UnstructuredGrid(vertexDims, faceDims, edgeDims, bs, blks, 2,
+                                               vertexOnFace, faceOnVertex, faceOnFace, location,
+                                               maxVertexPerFace, maxFacePerVertex),
+                              _xug(xug), _yug(yug), _zug(zug), _kdtree(kdtree) {
+
+    assert(xug.GetDimensions() == GetDimensions());
+    assert(yug.GetDimensions() == GetDimensions());
+    assert(
+        zug.GetDimensions() == GetDimensions() ||
+        zug.GetDimensions().size() == 0);
+
+    assert(location == NODE);
+}
+
+size_t UnstructuredGrid2D::GetNumCoordinates() const {
+    return (_zug.GetDimensions().size() == 0 ? 2 : 3);
+}
+
+void UnstructuredGrid2D::GetUserExtents(
+    vector<double> &minu, vector<double> &maxu) const {
+    minu.clear();
+    maxu.clear();
+
+    float range[2];
+
+    _xug.GetRange(range);
+    minu.push_back(range[0]);
+    maxu.push_back(range[1]);
+
+    _yug.GetRange(range);
+    minu.push_back(range[0]);
+    maxu.push_back(range[1]);
+
+    if (GetNumCoordinates() < 3)
+        return;
+
+    _zug.GetRange(range);
+    minu.push_back(range[0]);
+    maxu.push_back(range[1]);
+}
+
+void UnstructuredGrid2D::GetBoundingBox(
+    const vector<size_t> &min,
+    const vector<size_t> &max,
+    vector<double> &minu,
+    vector<double> &maxu) const {
+    assert(min.size() == max.size());
+    assert(min.size() == GetDimensions().size());
+    int ncoords = GetNumCoordinates();
+    minu = vector<double>(ncoords, 0.0);
+    maxu = vector<double>(ncoords, 0.0);
+
+    size_t start = Wasp::LinearizeCoords(min, GetDimensions());
+    size_t stop = Wasp::LinearizeCoords(max, GetDimensions());
+
+    // Currently only support ++ opererator for ConstCoordItr. So random
+    // access is tricky.
+    //
+    ConstCoordItr itr = ConstCoordBegin();
+    ConstCoordItr enditr = ConstCoordBegin();
+
+    for (size_t i = 0; i < start; i++)
+        ++itr;
+    for (size_t i = 0; i < stop; i++)
+        ++enditr;
+
+    for (; itr != enditr; ++itr) {
+        for (int i = 0; i < ncoords; i++) {
+            if ((*itr)[i] < minu[i])
+                minu[i] = (*itr)[i];
+            if ((*itr)[i] > maxu[i])
+                maxu[i] = (*itr)[i];
+        }
+    }
+}
+
+void UnstructuredGrid2D::GetEnclosingRegion(
+    const vector<double> &minu, const vector<double> &maxu,
+    vector<size_t> &min, vector<size_t> &max) const {
+    assert(0 && "Not implemented");
+}
+
+void UnstructuredGrid2D::GetUserCoordinates(
+    const std::vector<size_t> &indices,
+    std::vector<double> &coords) const {
+    assert(indices.size() == 1);
+    coords.clear();
+
+    coords.push_back(_xug.AccessIndex(indices));
+    coords.push_back(_yug.AccessIndex(indices));
+    if (GetNumCoordinates() == 3) {
+        coords.push_back(_zug.AccessIndex(indices));
+    }
+}
+
+void UnstructuredGrid2D::GetIndices(
+    const std::vector<double> &coords,
+    std::vector<size_t> &indices) const {
+    assert(coords.size() == GetNumCoordinates());
+    indices.clear();
+
+    // Clamp coordinates on periodic boundaries to grid extents
+    //
+    vector<double> cCoords = coords;
+    ClampCoord(cCoords);
+
+    _kdtree->Nearest(cCoords, indices);
+}
+
+bool UnstructuredGrid2D::GetIndicesCell(
+    const std::vector<double> &coords,
+    std::vector<size_t> &indices) const {
+    assert(coords.size() == GetNumCoordinates());
+    indices.clear();
+
+    // Clamp coordinates on periodic boundaries to grid extents
+    //
+    vector<double> cCoords = coords;
+    ClampCoord(cCoords);
+
+    double lambda[_maxVertexPerFace];
+    int nlambda;
+    double zwgt[2];
+
+    // See if point is inside any cells (faces)
+    //
+    bool status = _insideGridNodeCentered(
+        cCoords, indices, lambda, nlambda, zwgt);
+
+    return (status);
+}
+
+bool UnstructuredGrid2D::InsideGrid(const std::vector<double> &coords) const {
+    assert(coords.size() == GetNumCoordinates());
+
+    // Clamp coordinates on periodic boundaries to reside within the
+    // grid extents
+    //
+    vector<double> cCoords = coords;
+    ClampCoord(cCoords);
+
+    double lambda[_maxVertexPerFace];
+    int nlambda;
+    double zwgt[2];
+    vector<size_t> indices;
+
+    // See if point is inside any cells (faces)
+    //
+    bool status = _insideGridNodeCentered(
+        cCoords, indices, lambda, nlambda, zwgt);
+
+    return (status);
+}
+
+float UnstructuredGrid2D::GetValueNearestNeighbor(
+    const std::vector<double> &coords) const {
+
+    // Clamp coordinates on periodic boundaries to reside within the
+    // grid extents
+    //
+    vector<double> cCoords = coords;
+    ClampCoord(cCoords);
+
+    vector<size_t> vertex_indices;
+    _kdtree->Nearest(cCoords, vertex_indices);
+    assert(vertex_indices.size() == 1);
+
+    return (AccessIndex(vertex_indices));
+}
+
+float UnstructuredGrid2D::GetValueLinear(
+    const std::vector<double> &coords) const {
+
+    // Clamp coordinates on periodic boundaries to reside within the
+    // grid extents
+    //
+    vector<double> cCoords = coords;
+    ClampCoord(cCoords);
+
+    double lambda[_maxVertexPerFace];
+    int nlambda;
+    double zwgt[2];
+    vector<size_t> face_indices;
+
+    // See if point is inside any cells (faces)
+    //
+    bool inside = _insideGrid(
+        cCoords, face_indices, lambda, nlambda, zwgt);
+
+    if (!inside)
+        return (GetMissingValue());
+    assert(face_indices.size() == 1);
+    assert(face_indices[0] < GetDimensions()[0]);
+
+    const int *ptr = _vertexOnFace + (face_indices[0] * _maxVertexPerFace);
+
+    double value = 0;
+    long offset = GetNodeOffset();
+    for (int i = 0; i < nlambda; i++) {
+        value += AccessIJK(*ptr + offset, 0, 0) * lambda[i];
+        ptr++;
+    }
+
+    return ((float)value);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Iterators
+//
+/////////////////////////////////////////////////////////////////////////////
+
+UnstructuredGrid2D::ConstCoordItrU2D::ConstCoordItrU2D(
+    const UnstructuredGrid2D *ug, bool begin) : ConstCoordItrAbstract() {
+    _ncoords = ug->GetNumCoordinates();
+    _coords = vector<double>(_ncoords, 0.0);
+    if (begin) {
+        _xCoordItr = ug->_xug.cbegin();
+        _yCoordItr = ug->_yug.cbegin();
+        _zCoordItr = ug->_zug.cbegin();
+    } else {
+        _xCoordItr = ug->_xug.cend();
+        _yCoordItr = ug->_yug.cend();
+        _zCoordItr = ug->_zug.cend();
+    }
+}
+
+UnstructuredGrid2D::ConstCoordItrU2D::ConstCoordItrU2D(
+    const ConstCoordItrU2D &rhs) : ConstCoordItrAbstract() {
+    _ncoords = rhs._ncoords;
+    _coords = rhs._coords;
+    _xCoordItr = rhs._xCoordItr;
+    _yCoordItr = rhs._yCoordItr;
+    _zCoordItr = rhs._zCoordItr;
+}
+
+UnstructuredGrid2D::ConstCoordItrU2D::ConstCoordItrU2D() : ConstCoordItrAbstract() {
+}
+
+void UnstructuredGrid2D::ConstCoordItrU2D::next() {
+
+    ++_xCoordItr;
+    _coords[0] = *_xCoordItr;
+
+    ++_yCoordItr;
+    _coords[1] = *_yCoordItr;
+    if (_ncoords < 3)
+        return;
+
+    ++_zCoordItr;
+    _coords[2] = *_zCoordItr;
+}
+
+namespace VAPoR {
+std::ostream &operator<<(std::ostream &o, const UnstructuredGrid2D &ug) {
+    o << "UnstructuredGrid2D " << endl;
+    o << (const Grid &)ug;
+
+    return o;
+}
+}; // namespace VAPoR
+
+// Search for a point inside the grid. If the point is inside return true,
+// and provide the Wachspress weights/coordinates for the point within
+// the face containing the point in XY, and the linear
+// interpolation weights/coordinates along Z. If the grid is 2D then
+// zwgt[0] == 1.0, and zwgt[1] == 0.0. If the point is outside of the
+// grid the values of 'lambda', and 'zwgt' are not defined
+//
+bool UnstructuredGrid2D::_insideGrid(
+    const vector<double> &coords,
+    vector<size_t> &face_indices,
+    double *lambda, int &nlambda, double zwgt[2]) const {
+    if (_location == NODE) {
+        return (_insideGridNodeCentered(
+            coords, face_indices, lambda, nlambda, zwgt));
+    } else {
+        return (_insideGridFaceCentered(
+            coords, face_indices, lambda, nlambda, zwgt));
+    }
+}
+
+bool UnstructuredGrid2D::_insideGridFaceCentered(
+    const vector<double> &coords,
+    vector<size_t> &indices,
+    double *lambda, int &nlambda, double zwgt[2]) const {
+    assert(0 && "Not supported");
+}
+
+bool UnstructuredGrid2D::_insideGridNodeCentered(
+    const vector<double> &coords,
+    vector<size_t> &face_indices,
+    double *lambda, int &nlambda, double zwgt[2]) const {
+    face_indices.clear();
+
+    assert(coords.size() == 2);
+
+    double pt[] = {coords[0], coords[1]};
+
+    // Find the indices for the nearest grid point in the plane
+    //
+    vector<size_t> vertex_indices;
+    _kdtree->Nearest(coords, vertex_indices);
+    assert(vertex_indices.size() == 1);
+
+    vector<size_t> dims = GetDimensions();
+    assert(vertex_indices[0] < dims[0]);
+
+    const int *ptr = _faceOnVertex + (vertex_indices[0] * _maxFacePerVertex);
+    long offset = GetCellOffset();
+
+    for (int i = 0; i < _maxFacePerVertex; i++) {
+        size_t face = *ptr + offset;
+        if (face == GetMissingID())
+            break;
+        if (face == GetBoundaryID())
+            continue;
+
+        if (_insideFace(face, pt, lambda, nlambda, zwgt)) {
+            face_indices.push_back(face);
+            return (true);
+        }
+    }
+
+    return (false);
+}
+
+bool UnstructuredGrid2D::_insideFace(
+    size_t face, double pt[2],
+    double *lambda, int &nlambda, double zwgt[2]) const {
+    nlambda = 0;
+
+    double verts[_maxVertexPerFace * 2];
+
+    const int *ptr = _vertexOnFace + (face * _maxVertexPerFace);
+    long offset = GetNodeOffset();
+
+    for (int i = 0; i < _maxVertexPerFace; i++) {
+        size_t vertex = *ptr + offset;
+        if (vertex == GetMissingID())
+            break;
+        verts[i * 2 + 0] = _xug.AccessIJK(vertex, 0, 0);
+        verts[i * 2 + 1] = _yug.AccessIJK(vertex, 0, 0);
+        ptr++;
+        nlambda++;
+    }
+
+    return (WachspressCoords2D(verts, pt, nlambda, lambda));
+}
