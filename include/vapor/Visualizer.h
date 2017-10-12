cd92a546047bbe13c7895dca360f7986df330238
Scott Pearse <pearse@ucar.edu>
2017-10-12 13:48:20 -0600

    Initial implementation of renderer transforms

--- include/vapor/Visualizer.h
+++ include/vapor/Visualizer.h
@@ -73,6 +73,11 @@ class RENDER_API Visualizer : public MyBase {
     //! \return zero if successful.
     int paintEvent();
 
+    //! Apply user defined transforms to the current renderer being drawn
+    //! \param[in] renIndex The index of the current renderer being drawn,
+    //! referring to the _renderer list
+    void applyTransforms(int renIndex);
+
     //! Issue the OpenGL resize call.  Must be called from an OpenGL context.
     //! \param[in] w Window width in pixels.
     //! \param[in] h Window height in pixels.
