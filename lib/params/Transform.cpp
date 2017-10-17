ebb7d90e9d82a7c9d8eafca82fc3486ee169535a
John Clyne <clyne@ucar.edu>
2017-10-17 13:29:24 -0600

    Added support for coordinate transforms to Renderer and RenderParams classes

--- lib/params/Transform.cpp
+++ lib/params/Transform.cpp
@@ -33,6 +33,7 @@ using namespace Wasp;
 const string Transform::_translationTag = "Translation";
 const string Transform::_rotationTag = "Rotation";
 const string Transform::_scaleTag = "Scale";
+const string Transform::_originTag = "Origin";
 
 //
 // Register class with object factory!!!
@@ -46,14 +47,12 @@ Transform::Transform(
 	ParamsBase::StateSave *ssave
 ) : ParamsBase(ssave, Transform::GetClassType())
 {
-	_init();
 }
 
 Transform::Transform(
 	ParamsBase::StateSave *ssave, XmlNode *node
 ) : ParamsBase(ssave, node)
 { 
-	_init();
 }
 
 //----------------------------------------------------------------------------
@@ -62,16 +61,4 @@ Transform::Transform(
 Transform::~Transform()
 {
 	MyBase::SetDiagMsg("Transform::~Transform() this=%p", this);
-}
-
-void Transform::_init() {
-	for (int i=0; i<3; i++) {
-		_defaultTranslation.push_back(0.0);
-		_defaultRotation.push_back(0.0);
-		_defaultScale.push_back(1.0);
-	}
-
-	SetTranslations(_defaultRotation);
-	SetRotations(_defaultRotation);
-	SetScales(_defaultScale);
 }
