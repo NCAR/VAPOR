a3b70f0c8be0de616831bf5d5736f50872a8f125
Samuel Li <shaomenglee@gmail.com>
2017-10-18 13:52:43 -0600

    fix #99

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -693,7 +693,7 @@ void MainForm::sessionOpen(QString qfileName)
         string          path = p->GetCurrentSessionPath();
 
         vector<string> files = myGetOpenFileNames("Choose a VAPOR session file to restore a session", path, "Vapor 3 Session Save Files (*.vs3)", false);
-        if (files.empty()) return;
+        if (files.empty()) { return; }
 
         qfileName = files[0].c_str();
     }
@@ -965,12 +965,12 @@ vector<string> MainForm::myGetOpenFileNames(string prompt, string dir, string fi
         QStringList           list = fileNames;
         QStringList::Iterator it = list.begin();
         while (it != list.end()) {
-            files.push_back((*it).toStdString());
+            if (!it->isNull()) files.push_back((*it).toStdString());
             ++it;
         }
     } else {
         QString fileName = QFileDialog::getOpenFileName(this, qPrompt, qDir, qFilter);
-        files.push_back(fileName.toStdString());
+        if (!fileName.isNull()) files.push_back(fileName.toStdString());
     }
 
     for (int i = 0; i < files.size(); i++) {
