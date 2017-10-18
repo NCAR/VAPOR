6b636c3ad0b3d548e105f3a8029c546dbc45cfa6
John Clyne <clyne@ucar.edu>
2017-10-18 16:47:37 -0600

    Implemented feature 94 - Add translation support for 2D renderers

--- apps/vaporgui/TransformTable.h
+++ apps/vaporgui/TransformTable.h
@@ -44,11 +44,13 @@ protected slots:
     void translationChanged(int row, int col);
 
 private:
+    map<string, VAPoR::Transform *> _transforms;
+
     void updateTransformTable(QTableWidget *table, string target, vector<double> values, int row);
 
-    void updateScales(const std::map<string, VAPoR::Transform *> &transforms);
-    void updateTranslations(const std::map<string, VAPoR::Transform *> &transforms);
-    void updateRotations(const std::map<string, VAPoR::Transform *> &transforms);
+    void updateScales();
+    void updateTranslations();
+    void updateRotations();
 
     void setScales(string dataset, vector<double> s);
     void setTranslations(string dataset, vector<double> t);
