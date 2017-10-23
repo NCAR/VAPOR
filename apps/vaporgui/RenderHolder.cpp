1c8384b97e3599451bc55628de69ba7a653cd4fb
Samuel Li <shaomeng@ucar.edu>
2017-10-23 17:04:36 -0600

    better logic to hide the Hello renderer

--- apps/vaporgui/RenderHolder.cpp
+++ apps/vaporgui/RenderHolder.cpp
@@ -102,8 +102,15 @@ void RenderHolder::newRenderer() {
 	ParamsMgr *paramsMgr = _controlExec->GetParamsMgr();
 	vector <string> dataSetNames = paramsMgr->GetDataMgrNames();
 
-
 	vector <string> renderClasses = _controlExec->GetAllRenderClasses();
+    for( vector<string>::iterator it = renderClasses.begin(); it != renderClasses.end(); it++ )
+    {
+        if( *it == "Hello" )
+        {
+            renderClasses.erase( it );
+            break;
+        }
+    }
 
 	// Launch a dialog to select a renderer type, visualizer, name
 	// Then insert a horizontal line with text and checkbox.
@@ -128,7 +135,6 @@ void RenderHolder::newRenderer() {
 	//
 	rDialog.rendererCombo->clear();
 
-#if DEBUG_HELLO
 	for (int i = 0; i<renderClasses.size(); i++)
     {
 		rDialog.rendererCombo->addItem( QString::fromStdString(renderClasses[i]) );
@@ -137,24 +143,6 @@ void RenderHolder::newRenderer() {
         return;
 	int selection = rDialog.rendererCombo->currentIndex();
 	string renderClass = renderClasses[selection];
-
-#else
-
-    int helloIdx = 9999;   // cannot have this many renderers...
-	for (int i = 0; i<renderClasses.size(); i++)
-    {
-        if( renderClasses[i] != "Hello" ) // Excludes the Hello renderer. 
-		    rDialog.rendererCombo->addItem( QString::fromStdString(renderClasses[i]) );
-        else
-            helloIdx = i;
-	}
-	if (nDialog.exec() != QDialog::Accepted) 
-        return;
-	int selection = rDialog.rendererCombo->currentIndex();
-    if( selection >= helloIdx )
-        selection++;
-	string renderClass = renderClasses[selection];
-#endif
 
 	selection = rDialog.dataMgrCombo->currentIndex();
 	string dataSetName = dataSetNames[selection];
