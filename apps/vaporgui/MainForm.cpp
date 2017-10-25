b280035c8c0bc65acb702211c5e1d41fb280af6e
Samuel Li <shaomeng@ucar.edu>
2017-10-25 11:24:00 -0600

    stop showing error msg when user cancels opening a session

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -758,6 +758,7 @@ void MainForm::fileSave()
         sessionPath.append("/My_Vapor_Session.vs3");
         sessionPath = QDir::toNativeSeparators(sessionPath);
         QString fileName = QFileDialog::getSaveFileName(this, "Choose the fileName to save the current session", sessionPath, "Vapor 3 Session Files (*.vs3)");
+        if (fileName.isNull()) { return; }
         path = fileName.toStdString();
     }
 
@@ -783,7 +784,8 @@ void MainForm::fileSaveAs()
     }
 
     QString fileName = QFileDialog::getSaveFileName(this, "Choose the fileName to save the current session", path, "Vapor 3 Session Files (*.vs3)");
-    string  newPath = fileName.toStdString();
+    if (fileName.isNull()) { return; }
+    string newPath = fileName.toStdString();
 
     if (_controlExec->SaveSession(newPath)) {
         MSG_ERR("Saving session file failed");
@@ -792,6 +794,7 @@ void MainForm::fileSaveAs()
         // Save to use a default for fileSave()
         //
         p->SetCurrentSessionPath(path.toStdString());
+        _stateChangeFlag = false;
     }
 }
 
