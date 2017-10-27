3fc700b7e4b257a238f53923b3ed5db4dcadb78a
Samuel Li <shaomenglee@gmail.com>
2017-10-27 16:42:37 -0600

    clean up on statisticsParams

--- apps/vaporgui/StatisticsParams.cpp
+++ apps/vaporgui/StatisticsParams.cpp
@@ -26,18 +26,10 @@
 
 using namespace VAPoR;
 
-//const string StatisticsParams::_varsTag = "Vars";
-//const string StatisticsParams::_vars3dTag = "Vars3d";
-//const string StatisticsParams::_statisticsTag = "Statistics";
 const string StatisticsParams::_dataSourceTag = "DataSource";
-//const string StatisticsParams::_refinementTag = "Refinement";
-//const string StatisticsParams::_cRatioTag = "Lod";
 const string StatisticsParams::_minTSTag = "MinTS";
 const string StatisticsParams::_maxTSTag = "MaxTS";
 const string StatisticsParams::_autoUpdateTag = "AutoUpdate";
-//const string StatisticsParams::_minExtentsTag = "MinExtents";
-//const string StatisticsParams::_maxExtentsTag = "MaxExtents";
-//const string StatisticsParams::_regionSelectTag = "RegionSelect";
 const string StatisticsParams::_minEnabledTag = "MinEnabled";
 const string StatisticsParams::_maxEnabledTag = "MaxEnabled";
 const string StatisticsParams::_meanEnabledTag = "MeanEnabled";
@@ -56,8 +48,7 @@ StatisticsParams::StatisticsParams( DataMgr* dmgr, ParamsBase::StateSave *ssave)
 StatisticsParams::StatisticsParams( DataMgr* dmgr, ParamsBase::StateSave *ssave, XmlNode *node) 
                 : RenderParams( dmgr, ssave, node)
 {
-    // If node isn't tagged correctly we correct the tag and reinitialize
-    // from scratch;
+    // If node isn't tagged correctly we correct the tag and reinitialize from scratch;
     //  
     if (node->GetTag() != StatisticsParams::GetClassType()) {
         node->SetTag(StatisticsParams::GetClassType());
@@ -79,18 +70,6 @@ void StatisticsParams::SetAutoUpdate(bool val)
     SetValueLong( _autoUpdateTag, "if we want stats auto-update", (long)val );
 }
 
-/*
-int StatisticsParams::GetRegionSelection() {
-    double state = GetValueDouble(_regionSelectTag, 0.f);
-    return (int)state;
-}
-
-void StatisticsParams::SetRegionSelection(int state) {
-    SetValueDouble(_regionSelectTag, 
-        "State of statistics region selector", (double)state);
-}
-*/
-
 int StatisticsParams::GetMinTS() 
 {
     return (int)(GetValueDouble(_minTSTag, 0.0));
@@ -110,48 +89,6 @@ void StatisticsParams::SetMaxTS(int ts)
 {
     SetValueDouble(_maxTSTag, "Maximum selected timestep for statistics", (double)ts);
 }
-
-/*
-vector<double> StatisticsParams::GetMinExtents() {
-    vector<double> extents = GetValueDoubleVec(_minExtentsTag);
-    return extents;
-}
-
-void StatisticsParams::SetMinExtents(vector<double> minExts) {
-    SetValueDoubleVec(_minExtentsTag, "Minimum extents for statistics",
-        minExts);
-}
-
-vector<double> StatisticsParams::GetMaxExtents() {
-    vector<double> extents = GetValueDoubleVec(_maxExtentsTag);
-    return extents;
-}
-
-void StatisticsParams::SetMaxExtents(vector<double> maxExts) {
-    SetValueDoubleVec(_maxExtentsTag, "Maximum extents for statistics",
-        maxExts);
-}
-
-int StatisticsParams::GetCRatio() {
-    int cRatio = (int)GetValueDouble(_cRatioTag, 0);
-    return cRatio;
-}
-
-void StatisticsParams::SetCRatio(int cRatio) {
-    SetValueDouble(_cRatioTag, "Compression ratio for statistics",
-        cRatio);
-}
-
-int StatisticsParams::GetRefinement() {
-    int refinement = (int)GetValueDouble(_refinementTag, 0);
-    return refinement;
-}
-
-void StatisticsParams::SetRefinement(int ref) {
-    SetValueDouble(_refinementTag, "Refinement level for statistics",
-        ref);
-}
-*/
 
 bool StatisticsParams::GetMinEnabled() 
 {
@@ -203,14 +140,3 @@ void StatisticsParams::SetStdDevEnabled(bool state)
     SetValueLong(_stdDevEnabledTag, "Standard deviation statistic calculation", (long)state);
 }
 
-/*
-vector<string> StatisticsParams::GetVarNames() {
-    vector<string> varNames = GetValueStringVec(_varsTag);
-    return varNames;
-}
-
-void StatisticsParams::SetVarNames(vector<string> varNames) {
-    SetValueStringVec(_varsTag, "Variable names selected for statistics",
-        varNames);
-}
-*/
