9f8b82e21ddb6ac92a80c2e8d21ee7a10c35bf9d
Samuel Li <shaomenglee@gmail.com>
2017-11-01 15:11:28 -0600

    LOD, refinement both work well

--- apps/vaporgui/Statistics.h
+++ apps/vaporgui/Statistics.h
@@ -99,6 +99,8 @@ private slots:
     void _removeVarChanged(int);
     void _newCalcChanged(int);
     void _removeCalcChanged(int);
+    void _refinementChanged(int);
+    void _lodChanged(int);
     /*
     void restoreExtents();
     void minTSChanged();
