c5de0bfc41dab274e9c04cac92da9c6e650a304a
Scott Pearse <pearse@ucar.edu>
2017-11-01 14:00:06 -0600

    Fix for bug 76

--- apps/vaporgui/TFWidget.cpp
+++ apps/vaporgui/TFWidget.cpp
@@ -92,7 +92,7 @@ void TFWidget::setCMVar(const QString &qvar) {
     _rParams->SetColorMapVariableName(var);
 
     if (_flags & CONSTCOLOR) {
-        if (var == "Default") {
+        if (var == "Map to var") {
             colorDisplay->setEnabled(false);
             colorSelectButton->setEnabled(false);
             _rParams->SetUseSingleColor(false);
