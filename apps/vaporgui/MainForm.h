541bbedb953b358d483dac2b7ea7eecf8d224a60
John Clyne <clyne@ucar.edu>
2017-11-02 11:09:07 -0600

    Fixed issue_127

--- apps/vaporgui/MainForm.h
+++ apps/vaporgui/MainForm.h
@@ -183,6 +183,7 @@ class MainForm : public QMainWindow {
     // Set the various widgets in the main window consistent with latest
     // params settings:
     //
+    void updateMenus();
     void update();
     virtual void undoRedoHelper(bool undo);
     std::vector<string> myGetOpenFileNames(
