3ca842ecd43875ba932c09df17d994b66bcc776f
Samuel Li <shaomenglee@gmail.com>
2017-11-02 16:28:32 -0600

    accessing data from a Grid seems challenging

--- apps/vaporgui/Statistics.h
+++ apps/vaporgui/Statistics.h
@@ -62,9 +62,10 @@ protected:
     //
     class ValidStats {
     public:
-        bool   AddVariable(std::string);
-        bool   RemoveVariable(std::string);
-        size_t GetVariableCount();
+        bool        AddVariable(std::string);
+        bool        RemoveVariable(std::string);
+        size_t      GetVariableCount();
+        std::string GetVariableName(int i);
 
         bool Add3MStats(std::string, const double *);    // Min, Max, Mean
         bool AddMedian(std::string, double);
@@ -103,6 +104,7 @@ private slots:
     void _lodChanged(int);
     void _minTSChanged(int);
     void _maxTSChanged(int);
+    void _updateButtonClicked();
     /*
     void restoreExtents();
     void minTSChanged();
@@ -128,9 +130,11 @@ private:
     sErrMsg *           _errMsg;
     VAPoR::ControlExec *_controlExec;
 
+    void _updateStatsTable();
+
     // calculations should put results in _validStats directly.
-    // bool                _calc3M( std::string );
-    // bool                _calcMedian( std::string );
-    // bool                _calcStddev( std::string );
+    bool _calc3M(std::string);
+    bool _calcMedian(std::string);
+    bool _calcStddev(std::string);
 };
 #endif
