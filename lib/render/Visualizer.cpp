1759f306d5c67339f2faf4b5a4061655dc5379af
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-06 16:50:01 -0700

    Changed moveRenderToFront to fail by assert instead of returning an error in case of renderer not found

--- lib/render/Visualizer.cpp
+++ lib/render/Visualizer.cpp
@@ -616,7 +616,7 @@ int Visualizer::
     return -1;
 }
 
-int Visualizer::moveRendererToFront(const Renderer *ren) {
+void Visualizer::moveRendererToFront(const Renderer *ren) {
     int renIndex = -1;
     for (int i = 0; i < _renderer.size(); i++) {
         if (_renderer[i] == ren) {
@@ -624,10 +624,8 @@ int Visualizer::moveRendererToFront(const Renderer *ren) {
             break;
         }
     }
-    if (renIndex == -1) {
-        SetErrMsg("Renderer not found.");
-        return -1;
-    }
+    assert(renIndex != -1);
+
     Renderer *save = _renderer[renIndex];
     int saveOrder = _renderOrder[renIndex];
     for (int i = _renderer.size() - 2; i >= renIndex; i--) {
@@ -638,7 +636,6 @@ int Visualizer::moveRendererToFront(const Renderer *ren) {
     }
     _renderer[_renderer.size() - 1] = save;
     _renderOrder[_renderer.size() - 1] = saveOrder;
-    return 0;
 }
 
 /*
