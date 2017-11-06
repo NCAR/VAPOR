ed493c5f437f5280d0edfd92848f6020e3490b6a
Scott Pearse <pearse@ucar.edu>
2017-11-06 11:50:05 -0700

    Fix for bug 121

--- lib/render/BarbRenderer.cpp
+++ lib/render/BarbRenderer.cpp
@@ -70,6 +70,8 @@ int BarbRenderer::_paintGL() {
     BarbParams *bParams = (BarbParams *)GetActiveParams();
     size_t ts = bParams->GetCurrentTimestep();
 
+    cout << "Current timestep " << ts << endl;
+
     int refLevel = bParams->GetRefinementLevel();
     int lod = bParams->GetCompressionLevel();
     vector<double> minExts, maxExts;
