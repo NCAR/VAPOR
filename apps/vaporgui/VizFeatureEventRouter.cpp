9cd242bddb84d860049fe40d8daa0b26ddd02cad
John Clyne <clyne@ucar.edu>
2017-11-06 13:45:26 -0700

    Fixed issue_7

--- apps/vaporgui/VizFeatureEventRouter.cpp
+++ apps/vaporgui/VizFeatureEventRouter.cpp
@@ -681,6 +681,12 @@ void VizFeatureEventRouter::timeAnnotationChanged() {
 		miscParams->SetTimeStamp(true);
 		miscParams->SetTimeStep(false);
 		_controlExec->ClearText();
+		drawTimeUser();
+	}
+	else if (index == 3) {
+		miscParams->SetTimeStamp(true);
+		miscParams->SetTimeStep(false);
+		_controlExec->ClearText();
 		drawTimeStamp();
 	}
 	else {
@@ -731,7 +737,7 @@ void VizFeatureEventRouter::drawTimeStep(string myString) {
 	_controlExec->DrawText(myString, x, y, size, color, 1);
 }
 
-void VizFeatureEventRouter::drawTimeStamp() {
+void VizFeatureEventRouter::drawTimeUser() {
 	MiscParams* mp = GetMiscParams();
 	if (mp->GetTimeStep() == true) {
 		drawTimeStep();
@@ -747,6 +753,18 @@ void VizFeatureEventRouter::drawTimeStamp() {
 	ss << myTime;
 	std::string myString = ss.str();
 	drawTimeStep(myString);
+}
+
+void VizFeatureEventRouter::drawTimeStamp() {
+	MiscParams* mp = GetMiscParams();
+	if (mp->GetTimeStep() == true) {
+		drawTimeStep();
+		return;
+	}	
+
+	size_t ts = GetCurrentTimeStep();
+	DataStatus* ds = _controlExec->getDataStatus();
+	drawTimeStep(ds->GetTimeCoordsFormatted()[ts]);
 }
 
 void VizFeatureEventRouter::setXTicOrient(int){
