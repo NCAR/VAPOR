50f863927075b8196cab555a187aad49ee719ea6
Samuel Li <shaomenglee@gmail.com>
2017-11-06 17:18:15 -0700

    emit a signal when copying a region

--- apps/vaporgui/GeometryWidget.cpp
+++ apps/vaporgui/GeometryWidget.cpp
@@ -217,6 +217,8 @@ void GeometryWidget::copyRegion() {
 
     Box *myBox = _rParams->GetBox();
     myBox->SetExtents(minExtents, maxExtents);
+
+    emit valueChanged();
 }
 
 void GeometryWidget::Update(ParamsMgr *paramsMgr,
