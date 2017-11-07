773f0ca196046a7e29aa102f803f11600b552d97
Samuel Li <shaomeng@users.noreply.github.com>
2017-11-06 17:16:55 -0700

    Issue 229 (#232)
    * fix issue #229
    
    * stylish changes

--- apps/vaporgui/GeometryWidget.cpp
+++ apps/vaporgui/GeometryWidget.cpp
@@ -218,8 +218,16 @@ void GeometryWidget::copyRegion() {
 	std::vector<double> minExtents, maxExtents;
 	copyBox->GetExtents(minExtents, maxExtents);
 
-	Box* myBox = _rParams->GetBox();
-	myBox->SetExtents(minExtents, maxExtents);
+    Box* myBox = _rParams->GetBox();
+    std::vector<double> myMin, myMax;
+    myBox->GetExtents( myMin, myMax );
+    assert( minExtents.size() == maxExtents.size() );
+    for( int i = 0; i < minExtents.size(); i++ )
+    {
+        myMin[i] = minExtents[i];
+        myMax[i] = maxExtents[i];
+    }
+    myBox->SetExtents( myMin, myMax );
 }
 
 void GeometryWidget::Update(ParamsMgr *paramsMgr,
