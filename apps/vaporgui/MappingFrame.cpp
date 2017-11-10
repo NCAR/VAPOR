c3b6c6947c5e16567966dbaf2c77fe0cc96dba31
Scott Pearse <pearse@ucar.edu>
2017-11-10 14:16:04 -0700

    Fix for issue 281

--- apps/vaporgui/MappingFrame.cpp
+++ apps/vaporgui/MappingFrame.cpp
@@ -1562,8 +1562,8 @@ void MappingFrame::select(int hits, GLuint *selectionBuffer, Qt::KeyboardModifie
   }
   else if ((int)selectionBuffer[hitOffset+3] > (int)ISO_WIDGET)  //must have selected one of the isoline widgets
   {
-	return;
 	deselectWidgets();
+	return;
 	int selectedIndex = (int)selectionBuffer[hitOffset+3] - (int)ISO_WIDGET - 1;
     _lastSelected = _isolineSliders[selectedIndex];
 	_lastSelectedIndex = selectedIndex;
