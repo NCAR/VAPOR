9420a2674244619a561990024d0bf67d07e02042
Samuel Li <shaomeng@ucar.edu>
2017-11-09 18:45:17 -0700

    walk around issue #322

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -1846,7 +1846,7 @@ void MainForm::captureSingleJpeg()
     GUIStateParams *p = GetStateParams();
     string          imageDir = p->GetCurrentImageSavePath();
 
-    QFileDialog fileDialog(this, "Specify single image capture file name", imageDir.c_str(), "Jpeg or Tiff images (*.jpg *.tif)");
+    QFileDialog fileDialog(this, "Specify single image capture file name", imageDir.c_str(), "Jpeg images (*.jpg *.jpeg)");
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.move(pos());
     fileDialog.resize(450, 450);
@@ -1859,8 +1859,8 @@ void MainForm::captureSingleJpeg()
     // Extract the path, and the root name, from the returned string.
     QFileInfo *fileInfo = new QFileInfo(fn);
     QString    suffix = fileInfo->suffix();
-    if (suffix != "jpg" && suffix != "tif") {
-        MSG_ERR("Image capture file name must end with .jpg or .tif");
+    if (suffix != "jpg" && suffix != "jpeg") {
+        MSG_ERR("Image capture file name must end with .jpg or .jpeg");
         return;
     }
 
@@ -1930,7 +1930,7 @@ void MainForm::startAnimCapture()
     showCitationReminder();
     GUIStateParams *p = GetStateParams();
     string          imageDir = p->GetCurrentImageSavePath();
-    QFileDialog     fileDialog(this, "Specify first file name for image capture sequence", imageDir.c_str(), "Jpeg or Tiff images (*.jpg *.tif )");
+    QFileDialog     fileDialog(this, "Specify first file name for image capture sequence", imageDir.c_str(), "Jpeg images (*.jpg *.jpeg )");
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.move(pos());
     fileDialog.resize(450, 450);
@@ -1943,8 +1943,7 @@ void MainForm::startAnimCapture()
     QFileInfo *fileInfo = new QFileInfo(s);
 
     QString suffix = fileInfo->suffix();
-    if (suffix != "jpg" && suffix != "tif" && suffix != "tiff") suffix = "jpg";
-    if (suffix == "tiff") suffix = "tif";
+    if (suffix != "jpg" && suffix != "jpeg") suffix = "jpeg";
     // Save the path for future captures
     p->SetCurrentImageSavePath(fileInfo->absolutePath().toStdString());
 
