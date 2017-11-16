f1d97022da0b3b4026549bc8411527cc4b935cb4
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-16 14:56:44 -0700

    Removed debug print statement

--- lib/params/DataStatus.cpp
+++ lib/params/DataStatus.cpp
@@ -260,8 +260,6 @@ void DataStatus::GetActiveExtents(const ParamsMgr *paramsMgr, size_t ts, vector<
         }
     }
     int s = minExts.size();
-    printf("GetActiveExtents [");
-    for (int i = 0; i < s * 2; i++) printf("%g%s", i < s ? minExts[i] : maxExts[i - s], i + 1 == s ? "] [" : i + 1 == 2 * s ? "]\n" : ", ");
 }
 
 size_t DataStatus::MapGlobalToLocalTimeStep(string dataSetName, size_t ts) const
