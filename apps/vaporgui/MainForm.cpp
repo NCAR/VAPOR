52da38abf9244ad378f1bd215d777ffa77f4e31e
John Clyne <clyne@ucar.edu>
2017-11-21 15:16:12 -0700

    Fixed issue_392

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -122,6 +122,15 @@ string makename(string file) {
     return (qFileInfo.fileName().toStdString());
 }
 
+string concatpath(string s1, string s2) {
+    string s;
+    if (!s1.empty()) {
+        s = s1 + "/" + s2;
+    } else {
+        s = s2;
+    }
+    return (QDir::toNativeSeparators(s.c_str()).toStdString());
+}
 }; // namespace
 
 //Only the main program should call the constructor:
@@ -258,6 +267,7 @@ MainForm::MainForm(
     app->installEventFilter(this);
 
     _controlExec->SetSaveStateEnabled(true);
+    _stateChangeFlag = false;
 }
 
 /*
@@ -827,8 +837,12 @@ void MainForm::sessionOpenHelper(string fileName) {
     // ControlExec::LoadState invalidates params state
     //
     StartupParams *sP = GetStartupParams();
-    newP->SetCurrentSessionPath(fileName);
-    newP->SetCurrentSessionPath(sP->GetSessionDir());
+    if (fileName.empty()) {
+        newP->SetCurrentSessionPath(
+            concatpath(sP->GetSessionDir(), "My_Vapor_Session.vs3"));
+    } else {
+        newP->SetCurrentSessionPath(fileName);
+    }
     newP->SetCurrentImagePath(sP->GetImageDir());
     newP->SetCurrentTFPath(sP->GetTFDir());
     newP->SetCurrentPythonPath(sP->GetPythonDir());
@@ -854,13 +868,9 @@ void MainForm::sessionOpen(QString qfileName) {
     // load that session
     //
     if (qfileName == "") {
-        GUIStateParams *p = GetStateParams();
-        string path = p->GetCurrentSessionPath();
 
-        if (path == "JustInMemory") {
-            QString sessionPath = QDir::homePath();
-            path = QDir::toNativeSeparators(sessionPath).toStdString();
-        }
+        StartupParams *sP = GetStartupParams();
+        string path = sP->GetSessionDir();
 
         vector<string> files = myGetOpenFileNames(
             "Choose a VAPOR session file to restore a session",
@@ -889,56 +899,47 @@ void MainForm::fileSave() {
     GUIStateParams *p = GetStateParams();
     string path = p->GetCurrentSessionPath();
 
-    if (path == "JustInMemory") {
-        QString sessionPath = QDir::homePath();
-        sessionPath.append("/My_Vapor_Session.vs3");
-        sessionPath = QDir::toNativeSeparators(sessionPath);
-        QString fileName = QFileDialog::getSaveFileName(this,
-                                                        "Choose the fileName to save the current session",
-                                                        sessionPath, "Vapor 3 Session Files (*.vs3)");
-        if (fileName.isNull()) {
+    if (path.empty()) {
+        vector<string> files = myGetOpenFileNames(
+            "Choose a VAPOR session file to restore a session",
+            path, "Vapor 3 Session Save Files (*.vs3)", false);
+        if (files.empty())
             return;
-        }
-        path = fileName.toStdString();
+
+        path = files[0];
     }
 
     if (_controlExec->SaveSession(path) < 0) {
         MSG_ERR("Saving session file failed");
         return;
-    } else {
-        p->SetCurrentSessionPath(path);
     }
 
+    p->SetCurrentSessionPath(path);
     _stateChangeFlag = false;
 }
 
 void MainForm::fileSaveAs() {
     GUIStateParams *p = GetStateParams();
-    QString path = QString::fromStdString(p->GetCurrentSessionPath());
+    string path = p->GetCurrentSessionPath();
 
-    if (path == "JustInMemory") {
-        QString homePath = QDir::homePath();
-        homePath.append("/My_Vapor_Session.vs3");
-        path = QDir::toNativeSeparators(homePath);
-    }
-
-    QString fileName = QFileDialog::getSaveFileName(this,
-                                                    "Choose the fileName to save the current session",
-                                                    path, "Vapor 3 Session Files (*.vs3)");
-    if (fileName.isNull()) {
+    vector<string> files = myGetOpenFileNames(
+        "Choose a VAPOR session file to restore a session",
+        path, "Vapor 3 Session Save Files (*.vs3)", false);
+    if (files.empty())
         return;
-    }
-    string newPath = fileName.toStdString();
 
-    if (_controlExec->SaveSession(newPath)) {
+    path = files[0];
+
+    if (_controlExec->SaveSession(path)) {
+
         MSG_ERR("Saving session file failed");
         return;
-    } else {
-        // Save to use a default for fileSave()
-        //
-        p->SetCurrentSessionPath(path.toStdString());
-        _stateChangeFlag = false;
     }
+
+    // Save to use a default for fileSave()
+    //
+    p->SetCurrentSessionPath(path);
+    _stateChangeFlag = false;
 }
 
 void MainForm::fileExit() {
@@ -1183,8 +1184,6 @@ void MainForm::loadDataHelper(
     enableWidgets(true);
 
     _timeStepEditValidator->setRange(0, ds->GetTimeCoordinates().size() - 1);
-
-    //	update();
 }
 
 //Load data into current session
@@ -1293,10 +1292,6 @@ void MainForm::sessionNew() {
     sessionOpenHelper("");
 
     _vizWinMgr->LaunchVisualizer();
-
-    string fileName = "JustInMemory";
-    GUIStateParams *p = GetStateParams();
-    p->SetCurrentSessionPath(fileName);
 
     _stateChangeFlag = false;
     _sessionNewFlag = true;
