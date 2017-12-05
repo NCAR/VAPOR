8aed74ce68c4af92a4d78803f3a826a73aff885f
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-12-05 11:38:03 -0700

    Removed CMakeConfig.h from gitignore

---
+++ include/vapor/CMakeConfig.h
@@ -0,0 +1,16 @@
+#ifndef CMAKECONFIG_H
+#define CMAKECONFIG_H
+
+extern const int MAJOR;
+extern const int MINOR;
+extern const int MICRO;
+
+extern const char VERSION_RC[];
+extern const char VERSION_DATE[];
+extern const char VERSION_COMMIT[];
+extern const char VERSION_STRING[];
+extern const char VERSION_FULL[];
+
+extern const char SOURCE_DIR[];
+
+#endif
