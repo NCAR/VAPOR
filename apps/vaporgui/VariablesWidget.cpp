2f36dff7ee86c89837882fd80af349c6f8e5ac19
Scott Pearse <pearse@ucar.edu>
2018-01-03 11:55:18 -0700

    Row selectors for VaporTable now working

--- apps/vaporgui/VariablesWidget.cpp
+++ apps/vaporgui/VariablesWidget.cpp
@@ -20,6 +20,8 @@
 #include <sstream>
 #include <qwidget.h>
 #include <QFileDialog>
+#include <QLineEdit>
+#include <QCheckBox>
 #include <qradiobutton.h>
 #include <qcolordialog.h>
 #include "vapor/RenderParams.h"
@@ -43,8 +45,10 @@ VariablesWidget::VariablesWidget(QWidget *parent)
     _vaporTable->Reinit((VaporTable::ValidatorFlags)(VaporTable::STRING),
                         (VaporTable::MutabilityFlags)(VaporTable::IMMUTABLE));
 
-    connect(_vaporTable, SIGNAL(valueChanged()),
-            this, SLOT(printTableContents()));
+    connect(_vaporTable, SIGNAL(cellClicked(int, int)),
+            this, SLOT(printTableContents(int, int)));
+    connect(_vaporTable, SIGNAL(valueChanged(int, int)),
+            this, SLOT(printTableContents(int, int)));
 
     connect(
         varnameCombo, SIGNAL(activated(const QString &)), this,
@@ -88,7 +92,31 @@ VariablesWidget::VariablesWidget(QWidget *parent)
 #endif
 }
 
-void VariablesWidget::printTableContents() {
+void VariablesWidget::printTableContents(int row, int col) {
+    cout << "changed r/c " << row << " " << col << endl;
+    //testTable->selectRow(row);
+
+    for (int i = 0; i < testTable->rowCount(); i++) {
+        for (int j = 0; j < testTable->columnCount(); j++) {
+            QWidget *cell = testTable->cellWidget(i, j);
+            QLineEdit *le = qobject_cast<QLineEdit *>(cell);
+            //QCheckBox *cb = qobject_cast<QCheckBox *>(cell);
+            if (le) {
+                cout << "LineEdit" << endl;
+                if (i == row)
+                    le->setStyleSheet("QLineEdit { background: rgb(0, 255, 255); selection-background-color: rgb(233, 99, 0); }");
+                else
+                    le->setStyleSheet("QLineEdit { background: rgb(255,255,255); selection-background-color: rgb(233, 99, 0); }");
+            } else {
+                cout << "Checkbox" << endl;
+                if (i == row)
+                    cell->setStyleSheet("QWidget { background: rgb(0, 255, 255); selection-background-color: rgb(233, 99, 0); }");
+                else
+                    cell->setStyleSheet("QWidget { background: rgb(255,255,255); selection-background-color: rgb(233, 99, 0); }");
+            }
+        }
+    }
+
     vector<string> vec;
     _vaporTable->GetValues(vec);
     int size = vec.size();
