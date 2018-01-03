ffc8ec882a18fff58cb192dfa79b0aa261cb29ac
Scott Pearse <pearse@ucar.edu>
2018-01-03 11:55:18 -0700

    Row selectors for VaporTable now working

--- apps/vaporgui/VaporTable.h
+++ apps/vaporgui/VaporTable.h
@@ -64,15 +64,18 @@ class VaporTable : public QWidget {
     void setCellMutability(int row, int col);
 
   public slots:
-    void emitMySignal() { emit valueChanged(); }
+    void emitValueChanged();
+    //void emitCellClicked();
 
   signals:
 
-    // This signal is emitted whenever a cell gets changed
-    //
-    void valueChanged();
+    void valueChanged(int row, int col);
+    void cellClicked(int row, int col);
 
   private:
+    void emitCellClicked(QObject *object);
+    bool eventFilter(QObject *object, QEvent *event);
+
     std::vector<std::string> convertToString(std::vector<int> values);
 
     std::vector<std::string> convertToString(std::vector<double> values);
--- apps/vaporgui/VariablesWidget.h
+++ apps/vaporgui/VariablesWidget.h
@@ -91,7 +91,7 @@ class VariablesWidget : public QWidget, public Ui_VariablesWidgetGUI {
     string getNDimsTag() { return _nDimsTag; }
 
   protected slots:
-    void printTableContents();
+    void printTableContents(int row, int col);
 
     //! Respond to selecting the single (primary) variable of field
     void setVarName(const QString &);
