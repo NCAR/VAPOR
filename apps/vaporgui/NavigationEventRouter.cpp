89dd98524f71f744beab712672161f517f1fdbbd
Scott Pearse <pearse@ucar.edu>
2018-01-09 10:12:01 -0700

    Fix for unreported bug.  Transform table was not being correctly generated by NavigationEventRouter at startup

--- apps/vaporgui/NavigationEventRouter.cpp
+++ apps/vaporgui/NavigationEventRouter.cpp
@@ -290,7 +290,7 @@ void NavigationEventRouter::updateTransforms()
         ViewpointParams *vParams = paramsMgr->GetViewpointParams(winNames[i]);
         if (!vParams) continue;
 
-        vector<string> names = vParams->GetTransformNames();
+        vector<string> names = paramsMgr->GetDataMgrNames();
 
         for (int j = 0; j < names.size(); j++) {
             Transform *t = vParams->GetTransform(names[j]);
@@ -311,11 +311,9 @@ void NavigationEventRouter::updateTransforms()
                 t->SetOrigin(origin);
                 paramsMgr->SetSaveStateEnabled(enabled);
             }
-
             transformMap[names[j]] = t;
         }
     }
-
     transformTable->Update(transformMap);
 }
 
