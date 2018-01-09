700f71d6464a6c7eb0e2a8fe93de6c6ac6eb7e4f
Scott Pearse <pearse@ucar.edu>
2018-01-09 10:21:49 -0700

    Cleanup in ContourSubtabs

--- apps/vaporgui/ContourSubtabs.h
+++ apps/vaporgui/ContourSubtabs.h
@@ -54,18 +54,11 @@ private:
 
 private slots:
     void SetContourValues(int numContours, double contourMin, double spacing);
-
     void EndTFChange();
-
     void SetLineThickness(double val) { _cParams->SetLineThickness(val); }
-
     void SetContourCount(int count);
-
     void SetContourMinimum(double min);
-
     void SetContourSpacing(double spacing);
-
-    void ContourBoundsChanged(int index);
 };
 
 class ContourGeometrySubtab : public QWidget, public Ui_ContourGeometryGUI {
