0437334fb41fe68429372681d7ab2a234b8bef95
Samuel Li <shaomenglee@gmail.com>
2018-01-25 17:29:17 -0700

    cleanup

--- apps/vaporgui/Statistics.cpp
+++ apps/vaporgui/Statistics.cpp
@@ -49,8 +49,7 @@ Statistics::Statistics(QWidget* parent) : QDialog(parent), Ui_StatsWindow()
     MyGeometryWidget->Reinit( GeometryWidget::THREED,
                               GeometryWidget::MINMAX,
                               GeometryWidget::AUXILIARY );
-    FidelityWidget::DisplayFlags dspFlags = FidelityWidget::AUXILIARY;
-    MyFidelityWidget->Reinit( dspFlags );
+    MyFidelityWidget->Reinit(FidelityWidget::AUXILIARY);
 
     Connect();
 }
