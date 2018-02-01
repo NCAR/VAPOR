3b55fc1d69829da9d73fb369e32212c6bbd7ca3f
Samuel Li <shaomeng@ucar.edu>
2018-02-01 16:46:23 -0700

    ready to move to plot

--- apps/vaporgui/QSinglePoint.cpp
+++ apps/vaporgui/QSinglePoint.cpp
@@ -11,19 +11,19 @@ QSinglePoint::QSinglePoint(QWidget *parent) : QWidget(parent),
 
     SetDimensionality(3); // default to have 3 dimensions
 
-    _ui->_xSliderEdit->SetText(QString::fromAscii("X"));
-    _ui->_ySliderEdit->SetText(QString::fromAscii("Y"));
-    _ui->_zSliderEdit->SetText(QString::fromAscii("Z"));
-    _ui->_tSliderEdit->SetText(QString::fromAscii("T"));
+    _ui->xSliderEdit->SetText(QString::fromAscii("X"));
+    _ui->ySliderEdit->SetText(QString::fromAscii("Y"));
+    _ui->zSliderEdit->SetText(QString::fromAscii("Z"));
+    _ui->tSliderEdit->SetText(QString::fromAscii("T"));
 
     // Connect signals and slots
-    connect(_ui->_xSliderEdit, SIGNAL(valueChanged(double)),
+    connect(_ui->xSliderEdit, SIGNAL(valueChanged(double)),
             this, SLOT(_coordinateChanged(double)));
-    connect(_ui->_ySliderEdit, SIGNAL(valueChanged(double)),
+    connect(_ui->ySliderEdit, SIGNAL(valueChanged(double)),
             this, SLOT(_coordinateChanged(double)));
-    connect(_ui->_zSliderEdit, SIGNAL(valueChanged(double)),
+    connect(_ui->zSliderEdit, SIGNAL(valueChanged(double)),
             this, SLOT(_coordinateChanged(double)));
-    connect(_ui->_tSliderEdit, SIGNAL(valueChanged(double)),
+    connect(_ui->tSliderEdit, SIGNAL(valueChanged(double)),
             this, SLOT(_coordinateChanged(double)));
 }
 
@@ -40,14 +40,14 @@ void QSinglePoint::SetDimensionality(int dim) {
     assert(dim >= 2);
     assert(dim <= 4);
     _dimensionality = dim;
-    _ui->_xSliderEdit->setVisible(true);
-    _ui->_ySliderEdit->setVisible(true);
-    _ui->_zSliderEdit->setVisible(false);
-    _ui->_tSliderEdit->setVisible(false);
+    _ui->xSliderEdit->setVisible(true);
+    _ui->ySliderEdit->setVisible(true);
+    _ui->zSliderEdit->setVisible(false);
+    _ui->tSliderEdit->setVisible(false);
     if (dim >= 3)
-        _ui->_zSliderEdit->setVisible(true);
+        _ui->zSliderEdit->setVisible(true);
     if (dim >= 4)
-        _ui->_tSliderEdit->setVisible(true);
+        _ui->tSliderEdit->setVisible(true);
 }
 
 int QSinglePoint::GetDimensionality() {
@@ -56,22 +56,22 @@ int QSinglePoint::GetDimensionality() {
 
 void QSinglePoint::GetCurrentPoint(std::vector<double> &point) {
     point.clear();
-    point.push_back(_ui->_xSliderEdit->GetCurrentValue());
-    point.push_back(_ui->_ySliderEdit->GetCurrentValue());
+    point.push_back(_ui->xSliderEdit->GetCurrentValue());
+    point.push_back(_ui->ySliderEdit->GetCurrentValue());
     if (_dimensionality >= 3)
-        point.push_back(_ui->_zSliderEdit->GetCurrentValue());
+        point.push_back(_ui->zSliderEdit->GetCurrentValue());
     if (_dimensionality >= 4)
-        point.push_back(_ui->_tSliderEdit->GetCurrentValue());
+        point.push_back(_ui->tSliderEdit->GetCurrentValue());
 }
 
 void QSinglePoint::SetExtents(const std::vector<double> &min, const std::vector<double> &max) {
     assert(min.size() == _dimensionality);
     assert(max.size() == _dimensionality);
 
-    _ui->_xSliderEdit->SetExtents(min[0], max[0]);
-    _ui->_ySliderEdit->SetExtents(min[1], max[1]);
+    _ui->xSliderEdit->SetExtents(min[0], max[0]);
+    _ui->ySliderEdit->SetExtents(min[1], max[1]);
     if (_dimensionality >= 3)
-        _ui->_zSliderEdit->SetExtents(min[2], max[2]);
+        _ui->zSliderEdit->SetExtents(min[2], max[2]);
     if (_dimensionality >= 4)
-        _ui->_tSliderEdit->SetExtents(min[3], max[3]);
+        _ui->tSliderEdit->SetExtents(min[3], max[3]);
 }
