8265de00b303151f64368b09208c816e251b7cd6
Samuel Li <shaomenglee@gmail.com>
2018-01-31 18:24:34 -0700

    sync up

--- apps/vaporgui/QSinglePoint.bkp.h
+++ apps/vaporgui/QSinglePoint.bkp.h
@@ -31,4 +31,4 @@ class QSinglePoint : public : QWidget {
     int _dimensionality;
     std::vector<QSlider *> _sliders;      /// _sliders.size() == _dimensionality
     std::vector<QLineEdits *> _lineEdits; /// _lineEdits.size() == _dimensionality
-}
+};
