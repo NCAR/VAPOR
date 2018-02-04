9362336f18e7d94b8aefbc6b84368c906c03b5b5
Samuel Li <shaomenglee@gmail.com>
2018-02-03 20:36:17 -0700

    many bug fixed; keep developing Plot

--- apps/vaporgui/QDoubleValidator2.h
+++ apps/vaporgui/QDoubleValidator2.h
@@ -16,14 +16,14 @@ class QDoubleValidator2 : public QDoubleValidator {
     //
     // overload fixup() from QDoubleValidator
     //
-    void fixup(QString &input) const {
+    virtual void fixup(QString &input) const override {
         double val = input.toDouble();
         if (val > top())
             val = top();
         else if (val < bottom())
             val = bottom();
 
-        input = QString::number(val);
+        input = QString::number(val, 'g', this->decimals());
     }
 };
 
