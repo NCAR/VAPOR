ba13843d20bf55fa84538e16641309f2ad08df7b
Samuel Li <shaomeng@ucar.edu>
2018-02-12 20:49:58 -0700

    There seems to be some python tricks I need to know. So let's wait till tomorrow

--- share/python/plottest.py
+++ share/python/plottest.py
@@ -1,18 +1,24 @@
-import matplotlib.pyplot as plt
-import numpy as np
+def plottest( title = "About as simple as it gets, folks" ):
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+    # Data for plotting
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
 
-# Note that using plt.subplots below is equivalent to using
-# fig = plt.figure and then ax = fig.add_subplot(111)
-fig, ax = plt.subplots()
-ax.plot(t, s)
+    # Note that using plt.subplots below is equivalent to using
+    # fig = plt.figure and then ax = fig.add_subplot(111)
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)', title=title )
+    ax.grid()
 
-# fig.savefig("test.png")
-plt.show()
+    fig.savefig("test.png")
+    # plt.show()
+
+    return 0
+
+
+if __name__ == "__main__":
+    plottest( "this is a new title" )
