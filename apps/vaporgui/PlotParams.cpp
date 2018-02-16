afb3cee37a543dd0a7e3cddecbe51ff5c1e23297
Samuel Li <shaomenglee@gmail.com>
2018-02-15 18:32:11 -0700

    rename

--- apps/vaporgui/PlotParams.cpp
+++ apps/vaporgui/PlotParams.cpp
@@ -1,1 +1,103 @@
-PlotParams.new.cpp
+//************************************************************************
+//                                                                       *
+//           Copyright (C)  2014                                         *
+//   University Corporation for Atmospheric Research                     *
+//           All Rights Reserved                                         *
+//                                                                       *
+//************************************************************************/
+//
+//  File:       PlotParams.cpp
+//
+//  Author:     Samuel Li
+//              National Center for Atmospheric Research
+//              PO 3000, Boulder, Colorado
+//
+//  Date:       January 2018
+//
+//  Description:    Implements the PlotParams class.
+//
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <PlotParams.h>
+
+using namespace VAPoR;
+
+const string PlotParams::_minMaxTSTag = "MinMaxTS";
+// const string PlotParams::_maxTSTag = "MaxTS";
+const string PlotParams::_spaceTimeTag = "SpaceTime";
+
+const string PlotParams::_p1Tag = "Point2";
+const string PlotParams::_p2Tag = "Point1";
+const string PlotParams::_singlePtTag = "SinglePoint";
+
+//
+// Register class with object factory!!!
+//
+static RenParamsRegistrar<PlotParams> registrar(PlotParams::GetClassType());
+
+PlotParams::PlotParams(DataMgr *dmgr, ParamsBase::StateSave *ssave) : RenderParams(dmgr, ssave, PlotParams::GetClassType()) {}
+
+PlotParams::PlotParams(DataMgr *dmgr, ParamsBase::StateSave *ssave, XmlNode *node) : RenderParams(dmgr, ssave, node)
+{
+    // If node isn't tagged correctly we correct the tag and reinitialize from scratch;
+    //
+    if (node->GetTag() != PlotParams::GetClassType()) node->SetTag(PlotParams::GetClassType());
+}
+
+PlotParams::~PlotParams() { MyBase::SetDiagMsg("PlotParams::~PlotParams() this=%p", this); }
+
+std::vector<long int> PlotParams::GetMinMaxTS() const
+{
+    // assert( !this->GetSpaceTimeMode() );     // make sure we're at "time" mode
+
+    std::vector<long int> vec(2, 0);
+    return GetValueLongVec(_minMaxTSTag, vec);
+}
+
+void PlotParams::SetMinMaxTS(const std::vector<long int> &minmax)
+{
+    // assert( !this->GetSpaceTimeMode() );     // make sure we're at "time" mode
+    SetValueLongVec(_minMaxTSTag, "Time range in the Time mode", minmax);
+}
+
+bool PlotParams::GetSpaceTimeMode() const { return GetValueLong(_spaceTimeTag, (long)true); }
+
+void PlotParams::SetSpaceTimeMode(bool val) { SetValueLong(_spaceTimeTag, "Set Space or Time mode", (long)val); }
+
+std::vector<double> PlotParams::GetSinglePoint() const
+{
+    // assert( !this->GetSpaceTimeMode() );     // make sure we're at "time" mode
+    return GetValueDoubleVec(_singlePtTag);
+}
+
+void PlotParams::SetSinglePoint(const std::vector<double> &point)
+{
+    // assert( !this->GetSpaceTimeMode() );     // make sure we're at "time" mode
+    SetValueDoubleVec(_singlePtTag, "Single point in the time mode", point);
+}
+
+std::vector<double> PlotParams::GetPoint1() const
+{
+    // assert( this->GetSpaceTimeMode() );     // make sure we're at "space" mode
+    return GetValueDoubleVec(_p1Tag);
+}
+
+void PlotParams::SetPoint1(const std::vector<double> &point)
+{
+    // assert( this->GetSpaceTimeMode() );     // make sure we're at "space" mode
+    SetValueDoubleVec(_p1Tag, "Point 1 in the space mode", point);
+}
+
+std::vector<double> PlotParams::GetPoint2() const
+{
+    // assert( this->GetSpaceTimeMode() );     // make sure we're at "space" mode
+    return GetValueDoubleVec(_p2Tag);
+}
+
+void PlotParams::SetPoint2(const std::vector<double> &point)
+{
+    // assert( this->GetSpaceTimeMode() );     // make sure we're at "space" mode
+    SetValueDoubleVec(_p2Tag, "Point 2 in the space mode", point);
+}
--- apps/vaporgui/PlotParams.h
+++ apps/vaporgui/PlotParams.h
@@ -1,1 +1,72 @@
-PlotParams.new.h
+//************************************************************************
+//                                                                       *
+//           Copyright (C)  2004                                         *
+//     University Corporation for Atmospheric Research                   *
+//           All Rights Reserved                                         *
+//                                                                       *
+//************************************************************************
+//
+//  File:       PlotParams.h
+//
+//  Author:     Samuel Li
+//              National Center for Atmospheric Research
+//              PO 3000, Boulder, Colorado
+//
+//  Date:       December 2017
+//
+//  Description:    Defines the PlotParams class.
+//
+
+#ifndef PLOTPARAMS_H
+#define PLOTPARAMS_H
+
+#include <vapor/RenderParams.h>
+
+namespace VAPoR {
+
+/// PlotParams inherits RenderParams.
+class PlotParams : public RenderParams {
+public:
+    /// Constructor 1
+    PlotParams(DataMgr *dmgr, ParamsBase::StateSave *ssave);
+    /// Constructor 2
+    PlotParams(DataMgr *dmgr, ParamsBase::StateSave *ssave, XmlNode *node);
+    /// Destructor
+    ~PlotParams();
+
+    /// In ``time mode,'' these 2 methods get/set the time range.
+    std::vector<long int> GetMinMaxTS() const;
+    void                  SetMinMaxTS(const std::vector<long int> &);
+
+    /// In ``time mode,'' these 2 methods get/set the single point position
+    std::vector<double> GetSinglePoint() const;
+    void                SetSinglePoint(const std::vector<double> &point);
+
+    /// In ``space mode,'' these 4 methods get/set the point 1 and point 2 positions
+    std::vector<double> GetPoint1() const;
+    std::vector<double> GetPoint2() const;
+    void                SetPoint1(const std::vector<double> &point);
+    void                SetPoint2(const std::vector<double> &point);
+
+    /// Get/set the current operational mode: space or time.
+    /// true == Space; false == time
+    bool GetSpaceTimeMode() const;
+    void SetSpaceTimeMode(bool);
+
+    static string GetClassType() { return ("PlotParams"); }
+
+    // virtual functions required by RenderParams
+    virtual bool IsOpaque() const { return true; }
+    virtual bool usingVariable(const std::string &varname) { return false; }
+
+private:
+    static const string _minMaxTSTag;
+    static const string _spaceTimeTag;    // Space=true, Time=false
+
+    static const string _p1Tag;          // point1 in space mode
+    static const string _p2Tag;          // point2 in space mode
+    static const string _singlePtTag;    // a single point in time mode
+};
+
+};    // End namespace VAPoR
+#endif
