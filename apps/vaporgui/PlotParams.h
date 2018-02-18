44d1c5c80afba4efbe025f36b2ea62d7a16be042
Samuel Li <shaomenglee@gmail.com>
2018-02-18 01:00:36 -0700

    big surgery. Need to clean up SpaceTimeMode flag

--- apps/vaporgui/PlotParams.h
+++ apps/vaporgui/PlotParams.h
@@ -50,8 +50,8 @@ class PlotParams : public RenderParams {
 
     /// Get/set the current operational mode: space or time.
     /// true == Space; false == time
-    bool GetSpaceTimeMode() const;
-    void SetSpaceTimeMode(bool);
+    //bool GetSpaceTimeMode() const;
+    //void SetSpaceTimeMode( bool );
 
     long GetNumOfSamples() const;
     void SetNumOfSamples(long);
@@ -70,7 +70,7 @@ class PlotParams : public RenderParams {
 
   private:
     static const string _minMaxTSTag;
-    static const string _spaceTimeTag; // Space=true, Time=false
+    //static const string _spaceTimeTag;  // Space=true, Time=false
 
     static const string _p1Tag;         // point1 in space mode
     static const string _p2Tag;         // point2 in space mode
