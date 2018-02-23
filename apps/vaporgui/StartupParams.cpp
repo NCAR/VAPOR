b2ed87cec951f3d8d7048bc902836502e9dd5e01
Scott Pearse <pearse@ucar.edu>
2018-02-23 08:17:16 -0700

    Incremental

--- apps/vaporgui/StartupParams.cpp
+++ apps/vaporgui/StartupParams.cpp
@@ -54,6 +54,10 @@ const string StartupParams::_fidelityDefault2DTag = "Fidelity2DDefault";
 const string StartupParams::_fidelityDefault3DTag = "Fidelity3DDefault";
 const string StartupParams::_autoStretchTag = "AutoStretch";
 const string StartupParams::_numExecutionThreads = "NumExecutionThreads";
+const string StartupParams::_jpegQualityTag = "JpegImageQuality";
+const string StartupParams::_autoSaveSessionTag = "AutoSaveSession";
+const string StartupParams::_changesPerAutoSaveTag = "ChangesPerAutoSave";
+const string StartupParams::_autoSaveFileLocation = "AutoSaveFileLocation";
 
 //
 // Register class with object factory!!!
@@ -98,6 +102,143 @@ StartupParams::StartupParams(ParamsBase::StateSave *ssave, XmlNode *node) : Para
 void StartupParams::Reinit() { _init(); }
 
 StartupParams::~StartupParams() {}
+
+bool StartupParams::GetAutoStretch() const { return (0 != GetValueLong(_autoStretchTag, (long)false)); }
+
+void StartupParams::SetAutoStretch(bool val) { SetValueLong(_autoStretchTag, "Enable Auto Stretch", val); }
+
+int StartupParams::GetJpegQuality() const
+{
+    int quality = (int)GetValueDouble(_jpegQualityTag, 100.f);
+    return quality;
+}
+
+void StartupParams::SetJpegQuality(int quality)
+{
+    string description = "Specify the quality of JPEG screen captures";
+    SetValueDouble(_jpegQualityTag, description, quality);
+}
+
+long StartupParams::GetCacheMB() const
+{
+    long val = GetValueLong(_cacheMBTag, 1000);
+    if (val < 0) val = 1000;
+    return (val);
+}
+
+void StartupParams::SetCacheMB(long val)
+{
+    if (val < 0) val = 1000;
+    SetValueLong(_cacheMBTag, "Set cache size", val);
+}
+
+long StartupParams::GetTextureSize() const
+{
+    long val = GetValueLong(_texSizeTag, 0);
+    if (val < 0) val = 0;
+    return (val);
+}
+
+void StartupParams::SetTextureSize(long val)
+{
+    if (val < 0) val = 0;
+    SetValueLong(_texSizeTag, "Set graphic tex size", val);
+}
+
+void StartupParams::SetTexSizeEnable(bool val) { SetValueLong(_texSizeEnableTag, "toggle enable texture size", (long)val); }
+
+bool StartupParams::GetTexSizeEnable() const { return (0 != GetValueLong(_texSizeEnableTag, (long)0)); }
+
+void StartupParams::SetWinSizeLock(bool val) { SetValueLong(_winSizeLockTag, "toggle lock window size", (long)val); }
+
+bool StartupParams::GetWinSizeLock() const { return (0 != GetValueLong(_winSizeLockTag, (long)false)); }
+
+string StartupParams::GetSessionDir() const
+{
+    string dir = GetValueString(_sessionDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetSessionDir(string name) { SetValueString(_sessionDirTag, "set session directory", name); }
+
+string StartupParams::GetMetadataDir() const
+{
+    string dir = GetValueString(_metadataDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetMetadataDir(string dir) { SetValueString(_metadataDirTag, "set metadata directory", dir); }
+
+string StartupParams::GetImageDir() const
+{
+    string dir = GetValueString(_imageDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetImageDir(string dir) { SetValueString(_imageDirTag, "set image directory", dir); }
+
+string StartupParams::GetTFDir() const
+{
+    string dir = GetValueString(_tfDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetTFDir(string dir) { SetValueString(_tfDirTag, "set trans function directory", dir); }
+
+string StartupParams::GetFlowDir() const
+{
+    string dir = GetValueString(_flowDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetFlowDir(string dir) { SetValueString(_flowDirTag, "set flow save directory", dir); }
+
+string StartupParams::GetPythonDir() const
+{
+    string dir = GetValueString(_pythonDirTag, string("."));
+    if (dir == "~") { dir = QDir::homePath().toStdString(); }
+    return (dir);
+}
+
+void StartupParams::SetPythonDir(string dir) { SetValueString(_pythonDirTag, "set python directory", dir); }
+
+string StartupParams::GetCurrentPrefsPath() const
+{
+    string defaultv = "/tmp/.vapor3_prefs";
+    return GetValueString(_currentPrefsPathTag, defaultv);
+}
+
+void StartupParams::SetCurrentPrefsPath(string pth) { SetValueString(_currentPrefsPathTag, "set current preference path", pth); }
+
+size_t StartupParams::GetNumExecutionThreads() const
+{
+    long val = GetValueLong(_numExecutionThreads, 0);
+    if (val < 0) val = 0;
+    return ((size_t)val);
+}
+
+void StartupParams::SetNumExecutionThreads(size_t val) { SetValueLong(_numExecutionThreads, "Number of execution threads", 0); }
+
+void StartupParams::SetFidelityDefault3D(long lodDef, long refDef)
+{
+    vector<long> val;
+    val.push_back(lodDef);
+    val.push_back(refDef);
+    SetValueLongVec(_fidelityDefault3DTag, "Set fidelity 3D default", val);
+}
+
+void StartupParams::SetFidelityDefault2D(long lodDef, long refDef)
+{
+    vector<long> val;
+    val.push_back(lodDef);
+    val.push_back(refDef);
+    SetValueLongVec(_fidelityDefault2DTag, "Set fidelity 2D default", val);
+}
 
 bool StartupParams::_loadFromStartupFile()
 {
