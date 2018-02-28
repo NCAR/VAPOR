a06fd91261ceb82f1f4ccdd028d9622328a72d21
Scott Pearse <pearse@ucar.edu>
2018-02-28 13:58:01 -0700

    Commit before renaming StartupParams to SettingsParams

--- apps/vaporgui/StartupParams.cpp
+++ apps/vaporgui/StartupParams.cpp
@@ -65,6 +65,8 @@ const string StartupParams::_jpegQualityTag = "JpegImageQuality";
 const string StartupParams::_changesPerAutoSaveTag = "ChangesPerAutoSave";
 const string StartupParams::_autoSaveFileLocationTag = "AutoSaveFileLocation";
 const string StartupParams::_sessionAutoSaveEnabledTag = "AutoSaveEnabled";
+const string StartupParams::_fontFileTag = "FontFile";
+const string StartupParams::_fontSizeTag = "FontSize";
 
 //
 // Register class with object factory!!!
@@ -72,7 +74,8 @@ const string StartupParams::_sessionAutoSaveEnabledTag = "AutoSaveEnabled";
 static ParamsRegistrar<StartupParams> registrar(StartupParams::GetClassType());
 
 namespace {
-	string StartupFile = ".vapor3_startup";
+	//string StartupFile = ".vapor3_startup";
+	string StartupFile = ".vapor3_settings";
 }
 
 
@@ -80,12 +83,9 @@ StartupParams::StartupParams(
     ParamsBase::StateSave *ssave
 ) : ParamsBase(ssave, _classType) {
 
-
 	_startupPath = QDir::homePath().toStdString();
 	_startupPath += QDir::separator().toAscii();
 	_startupPath += StartupFile;
-
-	cout << "StartupParams Constructor" << endl;
 
 	// Try to get startup params from .startup file
 	//
@@ -390,6 +390,22 @@ int StartupParams::GetNumThreads() const {
 
 void StartupParams::SetNumThreads(int val) {
 	SetValueLong(_numThreadsTag, "Number of execution threads", val);
+}
+
+int StartupParams::GetFontSize() const {
+//	return (int)GetValueDouble(_fontSizeTag, 
+}
+
+void StartupParams::SetFontSize(int size) {
+
+}
+
+string StartupParams::GetFontFile() const {
+
+}
+
+void StartupParams::SetFontFile(string file) {
+
 }
 
 void StartupParams::SetFidelityDefault3D(long lodDef, long refDef){
--- apps/vaporgui/StartupParams.h
+++ apps/vaporgui/StartupParams.h
@@ -108,8 +108,11 @@ public:
  string GetDefaultPythonDir() const;
  void SetDefaultPythonDir(string dir);
 
+ int GetFontSize() const;
+ void SetFontSize(int size);
 
-
+ string GetFontFile() const;
+ void SetFontFile(string file);
 
  string GetCurrentPrefsPath() const;
  void SetCurrentPrefsPath(string pth);
@@ -156,6 +159,8 @@ private:
  static const string _changesPerAutoSaveTag;
  static const string _autoSaveFileLocationTag;
  static const string _sessionAutoSaveEnabledTag;
+ static const string _fontFileTag;
+ static const string _fontSizeTag;
 
  void _init();
  bool _loadFromStartupFile();
