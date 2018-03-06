e332766d88d5203837dfa042f41123b7674216eb
Scott Pearse <pearse@ucar.edu>
2018-03-06 09:46:21 -0700

    Fixes for orientation arrows

--- include/vapor/VizFeatureParams.h
+++ include/vapor/VizFeatureParams.h
@@ -115,6 +115,10 @@ class PARAMS_API VizFeatureParams : public ParamsBase {
     void SetAxisArrowCoords(std::vector<double> coords);
     std::vector<double> GetAxisArrowCoords() const;
 
+    void SetXAxisArrowPosition(float pos);
+    void SetYAxisArrowPosition(float pos);
+    void SetZAxisArrowPosition(float pos);
+
     bool GetShowAxisArrows() const;
     void SetShowAxisArrows(bool val);
 
