da60187cef46e614e5f54b2bb5e03368ee458c2b
Scott Pearse <pearse@ucar.edu>
2018-03-06 12:22:28 -0700

    Incremental

--- apps/vaporgui/AnnotationEventRouter.cpp
+++ apps/vaporgui/AnnotationEventRouter.cpp
@@ -142,9 +142,6 @@ void AnnotationEventRouter::hookUpTab() {
         domainFrameCheckbox, SIGNAL(clicked()),
         this, SLOT(setDomainFrameEnabled()));
     connect(
-        regionFrameCheckbox, SIGNAL(clicked()),
-        this, SLOT(setRegionFrameEnabled()));
-    connect(
         regionColorButton, SIGNAL(clicked()),
         this, SLOT(setRegionColor()));
     connect(
@@ -187,7 +184,6 @@ void AnnotationEventRouter::_updateTab() {
     AnnotationParams *vParams = (AnnotationParams *)GetActiveParams();
 
     domainFrameCheckbox->setChecked(vParams->GetUseDomainFrame());
-    regionFrameCheckbox->setChecked(vParams->GetUseRegionFrame());
 
     // Axis arrows:
     //
@@ -910,11 +906,6 @@ void AnnotationEventRouter::setLatLonAnnot(bool val) {
 void AnnotationEventRouter::setDomainFrameEnabled() {
     AnnotationParams *vfParams = (AnnotationParams *)GetActiveParams();
     vfParams->SetUseDomainFrame(domainFrameCheckbox->isChecked());
-}
-
-void AnnotationEventRouter::setRegionFrameEnabled() {
-    AnnotationParams *vfParams = (AnnotationParams *)GetActiveParams();
-    vfParams->SetUseRegionFrame(regionFrameCheckbox->isChecked());
 }
 
 void AnnotationEventRouter::setAxisAnnotation(bool toggled) {
--- apps/vaporgui/AnnotationEventRouter.h
+++ apps/vaporgui/AnnotationEventRouter.h
@@ -78,7 +78,6 @@ class AnnotationEventRouter : public QWidget, public Ui_AnnotationGUI, public Ev
     void setDomainColor();
     void setRegionColor();
     void setBackgroundColor();
-    void setRegionFrameEnabled();
     void setDomainFrameEnabled();
     void setTimeColor();
     void setLatLonAnnot(bool);
