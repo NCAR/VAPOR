8ea2ae6846350d7430f3a8ad9a9d67cd6256af0b
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-03-15 13:24:46 -0600

    Updated vdccompare help message

--- apps/vdccompare/vdccompare.cpp
+++ apps/vdccompare/vdccompare.cpp
@@ -134,16 +134,11 @@ int main(int argc, char **argv)
 
     if (op.ParseOptions(&argc, argv, get_options) < 0) { exit(1); }
 
-    if (argc < 6) {
-        cerr << "Usage: " << ProgName << "ftype1 ftype2 ftype1files... -- ftype2files " << endl;
+    if (argc < 6 || opt.help) {
+        cerr << "Usage: " << ProgName << " ftype1 ftype2 ftype1files... -- ftype2files... " << endl;
+        cerr << "Valid file types: vdc, wrf, cf, mpas" << endl;
         op.PrintOptionHelp(stderr, 80, false);
         exit(1);
-    }
-
-    if (opt.help) {
-        cerr << "Usage: " << ProgName << " master.nc" << endl;
-        op.PrintOptionHelp(stderr, 80, false);
-        exit(0);
     }
 
     argc--;
