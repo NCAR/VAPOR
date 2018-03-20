8fdc20ee1373e4310f33ef9b06eb906fa91c3e99
Scott Pearse <pearse@ucar.edu>
2018-03-20 15:50:28 -0600

    Incremental

--- apps/vaporgui/VizWin.cpp
+++ apps/vaporgui/VizWin.cpp
@@ -691,12 +691,12 @@ void VizWin::mouseMoveEvent(QMouseEvent *e) {
         double projScreenCoords[2];
         //		if (_manip->projectPointToLine(mouseCoords,projMouseCoords)) {
         if (_manip->projectPointToLine(screenCoords, projScreenCoords)) {
-            cout << "dragging " << handle << endl;
             double dirVec[3];
             //_manip->pixelToVector(projMouseCoords, dirVec, _strHandleMid);
             _manip->pixelToVector(projScreenCoords, dirVec, _strHandleMid);
             _manip->slideHandle(handle, dirVec, false);
         }
+        //paintGL();
         return;
     }
 
@@ -741,7 +741,7 @@ void VizWin::setFocus() {
 }
 
 void VizWin::paintGL() {
-
+    cout << "paintGL()" << endl;
     if (!FrameBufferReady()) {
         return;
     }
@@ -763,13 +763,13 @@ void VizWin::paintGL() {
 
     // Only rendering if state has changed.
     //
-    if (!_controlExec->GetParamsMgr()->StateChanged()) {
-        glMatrixMode(GL_PROJECTION);
-        glPopMatrix();
-        glMatrixMode(GL_MODELVIEW);
-        glPopMatrix();
-        return;
-    }
+    //	if (! _controlExec->GetParamsMgr()->StateChanged()) {
+    //		glMatrixMode(GL_PROJECTION);
+    //		glPopMatrix();
+    //		glMatrixMode(GL_MODELVIEW);
+    //		glPopMatrix();
+    //		return;
+    //	}
 
     // Set up projection and modelview matrices
     //
@@ -851,7 +851,10 @@ void VizWin::paintGL() {
     double proj[16];
     vParams->GetProjectionMatrix(proj);
 
-    _manip->Update(minExts, maxExts, minExts, maxExts,
+    std::vector<double> llc, urc;
+    _manip->GetBox(llc, urc); // get box from active RenderParams
+
+    _manip->Update(llc, urc, minExts, maxExts,
                    vcameraPos, vrotCenter,
                    mv, proj,
                    windowSize);
