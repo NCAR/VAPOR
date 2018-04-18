f0c70d2f6b63489bce3feb9c67b534ab6c6ff874
Samuel Li <shaomenglee@gmail.com>
2018-04-17 21:04:40 -0600

    performance optimization

--- apps/vaporgui/QIntValidatorWithFixup.cpp
+++ apps/vaporgui/QIntValidatorWithFixup.cpp
@@ -4,9 +4,13 @@ void QIntValidatorWithFixup::fixup( QString& input ) const
 {
     int val = input.toInt();
     if( val > top() )
+    {
       val = top();
+      input = QString::number( val );
+    }
     else if ( val < bottom() )
+    {
       val = bottom();
-
-    input = QString::number( val );
+      input = QString::number( val );
+    }
 }
