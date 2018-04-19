89df1a6d927f7342b1ea29712835bf405f1f9b60
Samuel Li <shaomenglee@gmail.com>
2018-04-19 14:56:58 -0600

    minor fixes

--- lib/render/Visualizer.cpp
+++ lib/render/Visualizer.cpp
@@ -900,11 +900,12 @@ int Visualizer::captureImage(string filename)
     } else    // write png files
     {
         FILE *test = fopen((const char *)filename.c_str(), "wb");
-        fclose(test);
         if (!test) {
+            fclose(test);
             SetErrMsg("Image Capture Error: Error opening output PNG file: %s", (const char *)filename.c_str());
             return -1;
         }
+        fclose(test);
     }
     // Get the image buffer
     unsigned char *buf = new unsigned char[3 * width * height];
