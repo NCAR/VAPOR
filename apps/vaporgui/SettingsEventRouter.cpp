347a7345823924df11524cfa34ed301482777101
Scott Pearse <pearse@ucar.edu>
2018-04-19 14:13:22 -0600

    Fix for bug 674

--- apps/vaporgui/SettingsEventRouter.cpp
+++ apps/vaporgui/SettingsEventRouter.cpp
@@ -422,7 +422,7 @@ void SettingsEventRouter::_restoreDefaults() {
     XmlNode *parent = settingsNode->GetParent();
     XmlNode *defaultNode = _defaultParams->GetNode();
 
-    *settingsParams = *_defaultParams;
+    *settingsParams = SettingsParams(*_defaultParams);
     settingsParams->GetNode()->SetParent(parent);
 
     _saveSettings();
