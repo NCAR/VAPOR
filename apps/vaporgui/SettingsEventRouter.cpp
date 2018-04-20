cfb2a461f0d74774c0b801ceecabe13da40171f0
Scott Pearse <pearse@ucar.edu>
2018-04-20 11:33:36 -0600

    Removed print statement

--- apps/vaporgui/SettingsEventRouter.cpp
+++ apps/vaporgui/SettingsEventRouter.cpp
@@ -426,8 +426,6 @@ void SettingsEventRouter::_restoreDefaults() {
     *settingsParams = *newParams;
     settingsParams->GetNode()->SetParent(parent);
 
-    cout << settingsParams->GetCacheMB() << endl;
-
     delete newParams;
 
     _saveSettings();
