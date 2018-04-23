9390678e8fe7a042ad0500f388f1a2f110ba5b0d
Scott Pearse <pearse@ucar.edu>
2018-04-23 14:15:19 -0600

    Applied _updateTab() whenever we restore default settings

--- apps/vaporgui/SettingsEventRouter.cpp
+++ apps/vaporgui/SettingsEventRouter.cpp
@@ -487,6 +487,7 @@ void SettingsEventRouter::_restoreDefaults() {
     settingsParams->GetNode()->SetParent(parent);
 
     _saveSettings();
+    _updateTab();
 
     paramsMgr->EndSaveStateGroup();
 }
