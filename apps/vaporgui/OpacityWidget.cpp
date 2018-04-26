04b6a5b15afe709e2bf9009c1290e32d5f4f7ef3
Samuel Li <shaomeng@ucar.edu>
2018-04-26 16:16:38 -0600

    fix reorder warnings

--- apps/vaporgui/OpacityWidget.cpp
+++ apps/vaporgui/OpacityWidget.cpp
@@ -31,10 +31,10 @@ OpacityWidget::OpacityWidget(MappingFrame *parent, OpacityMap *omap) :
   GLWidget(),
   _parent(parent),
   _opacityMap(omap),
-  _handleRadius(0.045),
-  _handle(NULL),
   _minValue(0.0),
-  _maxValue(1.0)
+  _maxValue(1.0),
+  _handleRadius(0.045),
+  _handle(NULL)
 {
   _minY = -0.05;
   _maxY =  1.05;
