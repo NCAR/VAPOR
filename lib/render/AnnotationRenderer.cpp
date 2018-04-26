7a9f745d7dccf9c89760d46ceee859051f72d9ac
Samuel Li <shaomeng@ucar.edu>
2018-04-26 16:04:20 -0600

    fix bugs

--- lib/render/AnnotationRenderer.cpp
+++ lib/render/AnnotationRenderer.cpp
@@ -632,12 +632,11 @@ void AnnotationRenderer::renderText(
 	AxisAnnotation* aa)
 {
 	if (aa == NULL)
-		AxisAnnotation *aa = getCurrentAxisAnnotation();
+		aa = getCurrentAxisAnnotation();
 
 	std::vector<double> axisColor = aa->GetAxisColor();
 	std:: vector<double> txtBackground = aa->GetAxisBackgroundColor();
 	int fontSize = aa->GetAxisFontSize();
-	bool latLon = aa->GetLatLonAxesEnabled();
 	ViewpointParams *vpParams = m_paramsMgr->GetViewpointParams(m_winName);
 
 	int precision = (int)aa->GetAxisDigits();
