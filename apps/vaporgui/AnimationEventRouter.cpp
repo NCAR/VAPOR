4961dbf9d236c527ce3699069c1b3a258251ac67
Samuel Li <shaomeng@ucar.edu>
2018-04-26 16:17:51 -0600

    fix unused variables

--- apps/vaporgui/AnimationEventRouter.cpp
+++ apps/vaporgui/AnimationEventRouter.cpp
@@ -392,8 +392,6 @@ void AnimationEventRouter::AnimationStepReverse() {
 }
 
 void AnimationEventRouter::SetTimeStep(int ts) {
-    AnimationParams *aParams = (AnimationParams *)GetActiveParams();
-
     setCurrentTimestep((size_t)ts);
 }
 
