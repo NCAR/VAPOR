7d1d5d6a6a1070546922d48272a115c36a6f0c52
Samuel Li <shaomeng@ucar.edu>
2018-04-27 14:25:42 -0600

    fix a bug reported by John

--- apps/vaporgui/ErrorReporter.cpp
+++ apps/vaporgui/ErrorReporter.cpp
@@ -190,6 +190,7 @@ string ErrorReporter::GetSystemInformation() {
         while (!feof(pipe)) {
             if (fgets(buffer, 128, pipe) != 0)
                 ret += string(buffer);
+            pclose(pipe);
         }
     } else {
         fprintf(stderr, "popen failed\n");
