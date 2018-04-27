2959ebccbb4031025e0bb7a1f1436b4234dbf517
Samuel Li <shaomeng@ucar.edu>
2018-04-27 14:10:50 -0600

    finally resolve unused function warning

--- lib/wasp/WASP.cpp
+++ lib/wasp/WASP.cpp
@@ -2590,8 +2590,8 @@ int WASP::_PutVara(
 	// Ugh. Can't preserve type in thread_state, which has to be passed
 	// as a void * to thread library
 	//
-	int data_type = NetCDFType(*data);
-	int block_type = NetCDFType(*block);
+	int data_type = _NetCDFType(*data);
+	int block_type = _NetCDFType(*block);
 
 	//
 	// Set up thread state for parallel (threaded) execution
@@ -2905,8 +2905,8 @@ int WASP::_GetVara(
 	// Ugh. Can't preserve type in thread_state, which has to be passed
 	// as a void * to thread library
 	//
-	int data_type = NetCDFType(*data);
-	int block_type = NetCDFType(*block);
+	int data_type = _NetCDFType(*data);
+	int block_type = _NetCDFType(*block);
 
 	//
 	// Set up thread state for parallel (threaded) execution
