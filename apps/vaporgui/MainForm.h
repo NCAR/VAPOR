fa62affe6e7726fb4838e220f82b93609675019b
Scott Pearse <pearse@ucar.edu>
2018-04-30 11:48:47 -0600

    Removed compiler warnings in bug 751

--- apps/vaporgui/MainForm.h
+++ apps/vaporgui/MainForm.h
@@ -53,7 +53,6 @@ class QDockWindow;
 class QLabel;
 class QSpinBox;
 
-//class SeedMe;
 class VizWindow;
 class VizWinMgr;
 class VizSelectCombo;
@@ -61,10 +60,6 @@ class VizSelectCombo;
 class BannerGUI;
 class Statistics;
 class Plot;
-
-namespace VAPoR {
-	class SeedMe;
-}
 
 class MainForm : public QMainWindow {
 Q_OBJECT
@@ -174,7 +169,6 @@ private:
  QAction* _captureStartJpegCaptureAction;
  QAction* _captureEndJpegCaptureAction;
  QAction* _captureSingleJpegCaptureAction;
- QAction* _seedMeAction;
 
  // Toolbars:
  //
@@ -192,7 +186,6 @@ private:
 
  Statistics *_stats;
  Plot *_plot;
- VAPoR::SeedMe *_seedMe;
  BannerGUI* _banner;
  VizSelectCombo* _windowSelector;
  QLabel* _modeStatusWidget;
@@ -347,7 +340,6 @@ private slots:
  void startAnimCapture();
  void endAnimCapture();
  void captureSingleJpeg();
- void launchSeedMe();
  void installCLITools();
  void launchStats();
  void launchPlotUtility();
