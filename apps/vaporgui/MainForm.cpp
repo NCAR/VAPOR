06fa6b4d5c8e1723c4a52635d770853c5006e3dc
Samuel Li <shaomeng@ucar.edu>
2018-04-30 10:16:18 -0600

    minor fix

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -1893,7 +1893,7 @@ void MainForm::startAnimCapture()
     if (imageDir == "") imageDir = sP->GetDefaultImageDir();
 
     QFileDialog fileDialog(this, "Specify the base file name for image capture sequence", imageDir.c_str(), "PNG or JPEG images (*.png *.jpg *.jpeg )");
-    fileDialog.setDefaultSuffix(QString("png"));
+    fileDialog.setDefaultSuffix(QString::fromAscii("png"));
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.move(pos());
     fileDialog.resize(450, 450);
