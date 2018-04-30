841a138c877b42ba76b78dd14c2c9f593e6517f0
Samuel Li <shaomeng@ucar.edu>
2018-04-30 10:13:13 -0600

    Give default suffix property to single image capturing file dialog

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -2015,6 +2015,7 @@ void MainForm::captureSingleJpeg() {
                            "Specify single image capture file name",
                            imageDir.c_str(),
                            "PNG or JPEG images (*.png *.jpg *.jpeg)");
+    fileDialog.setDefaultSuffix(QString::fromAscii("png"));
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.move(pos());
     fileDialog.resize(450, 450);
