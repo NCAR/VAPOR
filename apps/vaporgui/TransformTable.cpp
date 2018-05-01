4810bb9c518eef4b52e3671cb0d3884895a2fce4
Scott Pearse <pearse@ucar.edu>
2018-05-01 11:07:43 -0600

    Incremental commit

--- apps/vaporgui/TransformTable.cpp
+++ apps/vaporgui/TransformTable.cpp
@@ -74,6 +74,7 @@ void TransformTable::Update(const std::map<string, Transform *> &transforms)
     UpdateTranslations();
     UpdateRotations();
     UpdateOrigins();
+    adjustSize();
 }
 
 void TransformTable::UpdateScales()
