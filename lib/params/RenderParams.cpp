129dac4d884139942073f080b633c93b63ba638c
John Clyne <clyne@ucar.edu>
2018-05-02 08:39:57 -0600

    More checks for incomplete data sets (missing variables, time steps, etc)

--- lib/params/RenderParams.cpp
+++ lib/params/RenderParams.cpp
@@ -107,6 +107,9 @@ void RenderParams::_initBox() {
     if (varname.empty())
         return;
 
+    if (!_dataMgr->VariableExists(0, varname, 0, 0))
+        return;
+
     vector<double> minExt, maxExt;
     int rc = _dataMgr->GetVariableExtents(0, varname, 0, minExt, maxExt);
 
