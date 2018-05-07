1746abf18b56b6148d66cb6117613c0f9743d0fd
Samuel Li <shaomeng@ucar.edu>
2018-05-07 17:04:53 -0600

    minor optimization

--- apps/vaporgui/Plot.cpp
+++ apps/vaporgui/Plot.cpp
@@ -541,9 +541,10 @@ void Plot::_timeTabPlotClicked()
         std::vector<float> seq;
         for (int t = minMaxTS[0]; t <= minMaxTS[1]; t++) {
             VAPoR::Grid *grid = dataMgr->GetVariable(t, enabledVars[v], refinementLevel, compressLevel);
+            float        missingVal = grid->GetMissingValue();
             if (grid) {
                 float fieldVal = grid->GetValue(singlePt);
-                if (fieldVal != grid->GetMissingValue())
+                if (fieldVal != missingVal)
                     seq.push_back(fieldVal);
                 else
                     seq.push_back(std::nanf("1"));
