a33e10d28ebce344ceb087dd9b251c347e6cd8bc
John Clyne <clyne@ucar.edu>
2018-05-18 16:45:40 -0600

    Added error checking for python initialization

--- lib/render/imagewriter.hpp
+++ lib/render/imagewriter.hpp
@@ -7,7 +7,13 @@ RENDER_API int Write_PNG(const char *file,
                          unsigned char *buffer) {
     PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
 
-    Wasp::MyPython::Instance()->Initialize();
+    int rc = Wasp::MyPython::Instance()->Initialize();
+    if (rc < 0) {
+        MyBase::SetErrMsg(
+            "Failed to initialize python : %s",
+            MyPython::Instance()->PyErr().c_str());
+        return (-1);
+    }
 
     pName = PyString_FromString("imagewriter");
     pModule = PyImport_Import(pName);
