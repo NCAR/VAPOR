2e304d511129a0dde396213a72d463ceae02affc
Samuel Li <shaomenglee@gmail.com>
2018-05-18 17:44:07 -0600

    MyPython keeps m_pyhome as static

--- include/vapor/MyPython.h
+++ include/vapor/MyPython.h
@@ -94,7 +94,7 @@ public:
 private:
  static MyPython *m_instance;
  static bool m_isInitialized;
-// static string m_pyHome;
+ static std::string m_pyHome;
 
  MyPython() {}	// Don't implement
  MyPython(MyPython const&);	// Don't Implement
