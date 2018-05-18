96c5ed988a1b2c818f3976271fd5c9a0e316629b
Samuel Li <shaomeng@ucar.edu>
2018-05-18 11:57:47 -0600

    add debug output

--- lib/render/imagewriter.hpp
+++ lib/render/imagewriter.hpp
@@ -1,8 +1,40 @@
 #include <vapor/MyPython.h>
 
 namespace VAPoR {
+// Fetch an error message genereated by Python API.
+//
+string pyErr()
+{
+    PyObject *pMain = PyImport_AddModule("__main__");
+
+    PyObject *catcher = NULL;
+    if (pMain && PyObject_HasAttrString(pMain, "catchErr")) { catcher = PyObject_GetAttrString(pMain, "catchErr"); }
+
+    // If catcher is NULL the Python message will be written
+    // to stderr. Otherwise it is writter to the catchErr object.
+    //
+    PyErr_Print();
+
+    if (!catcher) {
+        cerr << "CATCHER NULL" << endl;
+        return ("No Python error catcher");
+    }
+
+    PyObject *output = PyObject_GetAttrString(catcher, "value");
+    return (PyString_AsString(output));
+}
+
 RENDER_API int Write_PNG(const char *file, int width, int height, unsigned char *buffer)
 {
+    std::string stdErr = "import sys\n"
+                         "class CatchErr:\n"
+                         "   def __init__(self):\n"
+                         "       self.value = 'Plot: '\n"
+                         "   def write(self, txt):\n"
+                         "       self.value += txt\n"
+                         "catchErr = CatchErr()\n"
+                         "sys.stderr = catchErr\n";
+
     PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
 
     Wasp::MyPython::Instance()->Initialize();
@@ -11,6 +43,7 @@ RENDER_API int Write_PNG(const char *file, int width, int height, unsigned char 
     pModule = PyImport_Import(pName);
 
     if (pModule == NULL) {
+        MyBase::SetErrMsg("pModule (drawpng) NULL!!");
         std::cerr << "pModule (drawpng) NULL!!" << std::endl;
         PyErr_Print();
         return 1;
@@ -43,12 +76,20 @@ RENDER_API int Write_PNG(const char *file, int width, int height, unsigned char 
 
         // Call the python routine
         pValue = PyObject_CallObject(pFunc, pArgs);
+        // Catch stderr from Python to a string.
+        //
+        if (PyRun_SimpleString(stdErr.c_str()) < 0) {
+            MyBase::SetErrMsg("PyRun_SimpleString() : %s", pyErr().c_str());
+            return (1);
+        }
         if (pValue == NULL) {
-            std::cerr << "pFunc failed to execute!!" << std::endl;
+            MyBase::SetErrMsg("pFunc (drawpng) failed to execute!!");
+            std::cerr << "pFunc (drawpng) failed to execute!!" << std::endl;
             PyErr_Print();
         }
     } else {
-        std::cerr << "pFunc NULL" << std::endl;
+        MyBase::SetErrMsg("pFunc (drawpng) NULL!!");
+        std::cerr << "pFunc (drawpng) NULL!!" << std::endl;
         PyErr_Print();
         return 1;
     }
