c7ffc131e6e458e889faf55d194e4e55df49d04d
Scott Pearse <pearse@ucar.edu>
2018-06-06 11:00:20 -0600

    removed globals

--- lib/render/Manip.cpp
+++ lib/render/Manip.cpp
@@ -34,11 +34,6 @@ const float Manip::_unselectedFaceColor[4] = {0.8f, 0.2f, 0.0f, 0.5f};
     #define M_PI (3.14159265358979323846)
 #endif
 
-double winCoord1[2];
-double winCoord2[2];
-double winCoord3[2];
-double winCoord4[2];
-
 TranslateStretchManip::TranslateStretchManip() : Manip()
 {
     _buttonNum = 0;
@@ -356,7 +351,7 @@ int TranslateStretchManip::makeHandleFaces(int sortPosition, double handle[8][3]
             handle[vertex][coord] = fltCoord;
         }
     }
-    deScaleExtents(handle);
+    // deScaleExtents(handle);
     return newPosition;
 }
 
@@ -506,7 +501,7 @@ void TranslateStretchManip::makeHandleExtents(int sortPosition, double handleExt
             }
         }
     }
-    deScaleExtents(handleExtents);
+    // deScaleExtents(handleExtents);
     return;
 }
 
