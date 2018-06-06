3b73fbae26068d5de038fc86440a32d2236c5d40
Scott Pearse <pearse@ucar.edu>
2018-06-06 15:45:59 -0600

    Removed warnings and commented code

--- apps/vaporgui/VizWin.cpp
+++ apps/vaporgui/VizWin.cpp
@@ -721,10 +721,6 @@ string VizWin::getCurrentDataMgrName() const {
 	if (!exists)
 		return "";
 
-	VAPoR::RenderParams* rParams = paramsMgr->GetRenderParams(
-		_winName, dataSetName, className, inst
-	);
-
 	return dataSetName;
 }
 
@@ -771,7 +767,6 @@ void VizWin::getActiveExtents(
 	if (rParams == NULL) return;
 
 	int refLevel = rParams->GetRefinementLevel();
-	int cmpLevel = rParams->GetCompressionLevel();
 	string varName = rParams->GetVariableName();
 	vector<string> fieldVars = rParams->GetFieldVariableNames();
 
@@ -804,10 +799,9 @@ void VizWin::getCenterAndCamPos(
 	GUIStateParams *guiP = (GUIStateParams *) paramsMgr->GetParams(
 		GUIStateParams::GetClassType()
 	);
-	ViewpointParams* vParams = paramsMgr->GetViewpointParams(_winName);
 	MouseModeParams *p = guiP->GetMouseModeParams();
 	string modeName = getCurrentMouseMode();
-	double rotCenter[3], cameraPos[3], dirvec[3], upvec[3];
+	double rotCenter[3], cameraPos[3];
 	p->GetRotationCenter(rotCenter);
 	p->GetCameraPos(cameraPos);
 
@@ -845,8 +839,8 @@ VAPoR::Transform* VizWin::getDataMgrTransform() const {
 void VizWin::updateManip(bool initialize) {
 	ParamsMgr* paramsMgr = _controlExec->GetParamsMgr();
 
-	std::vector<double> minExts(3,0.f);
-	std::vector<double> maxExts(3,0.f);
+	std::vector<double> minExts(3,numeric_limits<double>::max());
+	std::vector<double> maxExts(3,numeric_limits<double>::lowest());
 	//std::vector<double> minExts;	// This dumps core...
 	//std::vector<double> maxExts;	// This dumps core...
 	getActiveExtents(minExts, maxExts);
