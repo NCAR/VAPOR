d207bd009ee781b4ff525b6d04a5901b824ff3d4
Scott Pearse <pearse@ucar.edu>
2018-06-12 14:15:54 -0600

    DVR GUI

--- apps/vaporgui/DVREventRouter.cpp
+++ apps/vaporgui/DVREventRouter.cpp
@@ -1,5 +1,21 @@
+#ifdef WIN32
+//Annoying unreferenced formal parameter warning
+#pragma warning(disable : 4100)
+#endif
+
+#include <vapor/glutil.h>
+#include <qlineedit.h>
+#include <qscrollarea.h>
+#include <qcolordialog.h>
+#include <QFileDialog>
+#include <vector>
+#include <string>
+#include "vapor/DVRParams.h"
+#include "VariablesWidget.h"
 #include "DVREventRouter.h"
-#include "ui_DVREventRouter.h"
+#include "EventRouter.h"
+
+using namespace VAPoR;
 
 //
 // Register class with object factory!!!
@@ -7,31 +23,80 @@
 static RenderEventRouterRegistrar<DVREventRouter> registrar(
     DVREventRouter::GetClassType());
 
-DVREventRouter::DVREventRouter(QWidget *parent, VAPoR::ControlExec *ce) : QWidget(parent),
-                                                                          RenderEventRouter(ce, DVREventRouter::GetClassType()),
-                                                                          ui(new Ui::DVREventRouter) {
-    ui->setupUi(this);
-    ui->myTFWidget->Reinit((TFWidget::Flags)(0)); // Cause crash if no "Reinit()"
+DVREventRouter::DVREventRouter(QWidget *parent, ControlExec *ce)
+    : QTabWidget(parent),
+      RenderEventRouter(ce, DVRParams::GetClassType()) {
+    _variables = new DVRVariablesSubtab(this);
+    QScrollArea *qsvar = new QScrollArea(this);
+    qsvar->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    _variables->adjustSize();
+    qsvar->setWidget(_variables);
+    qsvar->setWidgetResizable(true);
+    addTab(qsvar, "Variables");
+
+    _appearance = new DVRAppearanceSubtab(this);
+    QScrollArea *qsapp = new QScrollArea(this);
+    qsapp->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    qsapp->setWidget(_appearance);
+    qsapp->setWidgetResizable(true);
+    addTab(qsapp, "Appearance");
+
+    _geometry = new DVRGeometrySubtab(this);
+    QScrollArea *qsgeo = new QScrollArea(this);
+    qsgeo->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    qsgeo->setWidget(_geometry);
+    qsgeo->setWidgetResizable(true);
+    addTab(qsgeo, "Geometry");
 }
 
 DVREventRouter::~DVREventRouter() {
-    delete ui;
+    if (_variables)
+        delete _variables;
+    if (_geometry)
+        delete _geometry;
+    if (_appearance)
+        delete _appearance;
 }
 
-void DVREventRouter::_updateTab() {}
-
-void DVREventRouter::GetWebHelp(vector<pair<string, string>> &help) const {
+void DVREventRouter::GetWebHelp(
+    vector<pair<string, string>> &help) const {
     help.clear();
+
+    help.push_back(make_pair(
+        "DVR Overview",
+        "https://www.vapor.ucar.edu/docs/vapor-gui-general-guide/volume-rendering-dvr"));
+
+    help.push_back(make_pair(
+        "Renderer control",
+        "http://www.vapor.ucar.edu/docs/vapor-how-guide/renderer-instances"));
+
+    help.push_back(make_pair(
+        "Data accuracy control",
+        "http://www.vapor.ucar.edu/docs/vapor-how-guide/refinement-and-lod-control"));
 }
 
-std::string DVREventRouter::_getDescription() const {
-    return std::string("DVR Renderer");
+void DVREventRouter::_updateTab() {
+
+    // The variable tab updates itself:
+    //
+    _variables->Update(
+        GetActiveDataMgr(),
+        _controlExec->GetParamsMgr(),
+        GetActiveParams());
+
+    _appearance->Update(
+        GetActiveDataMgr(),
+        _controlExec->GetParamsMgr(),
+        GetActiveParams());
+    _geometry->Update(
+        _controlExec->GetParamsMgr(),
+        GetActiveDataMgr(),
+        GetActiveParams());
 }
 
-std::string DVREventRouter::_getSmallIconImagePath() const {
-    return std::string("DVR_small.png");
-}
-
-std::string DVREventRouter::_getIconImagePath() const {
-    return std::string("DVR.png");
+string DVREventRouter::_getDescription() const {
+    return (
+        "Displays "
+        "the user's 2D data variables along the plane described by the source data "
+        "file.\n\nThese 2D variables may be offset by a height variable.\n\n");
 }
