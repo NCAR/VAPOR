59d84600bfbcddfb9a308b93b6c79664ec949ce3
Samuel Li <shaomenglee@gmail.com>
2018-06-12 14:42:31 -0600

    retab

--- apps/vaporgui/DVRSubtabs.h
+++ apps/vaporgui/DVRSubtabs.h
@@ -6,76 +6,76 @@
 #include "ui_DVRGeometryGUI.h"
 
 namespace VAPoR {
-	class ControlExec;
-	class RenderParams;
-	class ParamsMgr;
-	class DataMgr;
+    class ControlExec;
+    class RenderParams;
+    class ParamsMgr;
+    class DataMgr;
 }
 
 class DVRVariablesSubtab : public QWidget, public Ui_DVRVariablesGUI {
 
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	DVRVariablesSubtab(QWidget* parent) {
-		setupUi(this);
-		_variablesWidget->Reinit((VariablesWidget::DisplayFlags)
-			(VariablesWidget::SCALAR),
-			(VariablesWidget::DimFlags)(VariablesWidget::THREED),
-			(VariablesWidget::ColorFlags)(0));
-	}
+    DVRVariablesSubtab(QWidget* parent) {
+        setupUi(this);
+        _variablesWidget->Reinit((VariablesWidget::DisplayFlags)
+            (VariablesWidget::SCALAR),
+            (VariablesWidget::DimFlags)(VariablesWidget::THREED),
+            (VariablesWidget::ColorFlags)(0));
+    }
 
-	void Update(
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::RenderParams *rParams
-	) {
-		_variablesWidget->Update(dataMgr, paramsMgr, rParams);
-	}
+    void Update(
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::RenderParams *rParams
+    ) {
+        _variablesWidget->Update(dataMgr, paramsMgr, rParams);
+    }
 };
 
 class DVRAppearanceSubtab : public QWidget, public Ui_DVRAppearanceGUI {
 
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	DVRAppearanceSubtab(QWidget* parent) {
-		setupUi(this);
-		_TFWidget->Reinit((TFWidget::Flags)(0));
-	}
+    DVRAppearanceSubtab(QWidget* parent) {
+        setupUi(this);
+        _TFWidget->Reinit((TFWidget::Flags)(0));
+    }
 
-	void Update(
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::RenderParams *rParams
-	) {
-		_TFWidget->Update(dataMgr, paramsMgr, rParams);
-		_ColorbarWidget->Update(dataMgr, paramsMgr, rParams);
-	}
+    void Update(
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::RenderParams *rParams
+    ) {
+        _TFWidget->Update(dataMgr, paramsMgr, rParams);
+        _ColorbarWidget->Update(dataMgr, paramsMgr, rParams);
+    }
 };
 
 class DVRGeometrySubtab : public QWidget, public Ui_DVRGeometryGUI {
 
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	DVRGeometrySubtab(QWidget* parent) {
-		setupUi(this);
-		_geometryWidget->Reinit(
-			GeometryWidget::THREED,
-			GeometryWidget::MINMAX,
-			GeometryWidget::SCALAR);
-	}
-	
-	void Update(
-		VAPoR::ParamsMgr *paramsMgr,
-		VAPoR::DataMgr *dataMgr,
-		VAPoR::RenderParams *rParams
-	) {
-		_geometryWidget->Update(paramsMgr, dataMgr, rParams);
-		_copyRegionWidget->Update(paramsMgr, rParams);
-		_transformTable->Update(rParams->GetTransform());
-	}
+    DVRGeometrySubtab(QWidget* parent) {
+        setupUi(this);
+        _geometryWidget->Reinit(
+            GeometryWidget::THREED,
+            GeometryWidget::MINMAX,
+            GeometryWidget::SCALAR);
+    }
+    
+    void Update(
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::RenderParams *rParams
+    ) {
+        _geometryWidget->Update(paramsMgr, dataMgr, rParams);
+        _copyRegionWidget->Update(paramsMgr, rParams);
+        _transformTable->Update(rParams->GetTransform());
+    }
 
 
 private:
