7733f86d9e340a72072faa4defde87082e65d558
Samuel Li <shaomenglee@gmail.com>
2018-06-12 15:32:52 -0600

    style improvement

--- apps/vaporgui/DVREventRouter.h
+++ apps/vaporgui/DVREventRouter.h
@@ -12,20 +12,6 @@
 
 QT_USE_NAMESPACE
 
-namespace VAPoR {
-class ControlExec;
-}
-
-class GLDVRImageWindow;
-
-//!
-//! \class DVREventRouter
-//! \ingroup Public_GUI
-//! \brief An EventRouter subclass that handles the DVR tab in the GUI
-//! \author Scott Pearse
-//! \version 3.0
-//! \date  April 2016
-
 //! The DVREventRouter class manages the DVR gui.  There are three sub-tabs,
 //! for variables, geometry, and appearance.
 
@@ -34,22 +20,21 @@ class DVREventRouter : public QTabWidget, public RenderEventRouter {
     Q_OBJECT
 
   public:
-    DVREventRouter(
-        QWidget *parent, VAPoR::ControlExec *ce);
+    DVREventRouter(QWidget *parent, VAPoR::ControlExec *ce);
     ~DVREventRouter();
 
-    void GetWebHelp(
-        vector<pair<string, string>> &help) const;
+    void GetWebHelp(vector<pair<string, string>> &help) const;
 
     static string GetClassType() {
         return (VAPoR::DirectVolumeRenderer::GetClassType());
     }
-    string GetType() const { return GetClassType(); }
+    string GetType() const {
+        return GetClassType();
+    }
 
   protected:
     void _updateTab();
     virtual string _getDescription() const;
-
     virtual string _getSmallIconImagePath() const {
         return ("DVR_small.png");
     }
@@ -58,8 +43,6 @@ class DVREventRouter : public QTabWidget, public RenderEventRouter {
     }
 
   private:
-    DVREventRouter() {}
-
     //! Override default wheel behavior on the tab.  This has the effect of
     //! ignoring wheel events over the tab.  This is because wheel events will always
     //! affect the combo boxes and other widgets in the tab, and it would be confusing
@@ -69,12 +52,7 @@ class DVREventRouter : public QTabWidget, public RenderEventRouter {
     //! VariablesWidget is used as Variables tab
     DVRVariablesSubtab *_variables;
     DVRGeometrySubtab *_geometry;
-    GLDVRImageWindow *_glDVRImageWindow;
     DVRAppearanceSubtab *_appearance;
-
-#ifdef VAPOR3_0_0_ALPHA
-    DVRImageGUI *_image;
-#endif
 };
 
 #endif //DVREVENTROUTER_H
