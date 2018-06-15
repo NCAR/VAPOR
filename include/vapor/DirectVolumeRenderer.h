dd869bae447d36e472fb5586e31e8acac2cfa45b
Samuel Li <shaomeng@ucar.edu>
2018-06-15 17:37:08 -0600

    triangle strip renders well

--- include/vapor/DirectVolumeRenderer.h
+++ include/vapor/DirectVolumeRenderer.h
@@ -66,8 +66,9 @@ class RENDER_API DirectVolumeRenderer : public Renderer {
     };
 
     CacheParams _cacheParams;
-    void _saveCacheParams(bool considerUserCoord);    // True: consider user coordinates too
-    bool _isCacheDirty(bool considerUserCoord) const; // False: not consider user coordinates
+    void _saveCacheParams(bool considerUserCoord); // True: consider user coordinates too
+                                                   // False: not consider user coordinates
+    bool _isCacheDirty() const;
 
     // OpenGL stuff
     GLuint _volumeTextureUnit;   // GL_TEXTURE0
@@ -85,13 +86,10 @@ class RENDER_API DirectVolumeRenderer : public Renderer {
                                   const float *leftFace,
                                   const float *topFace,
                                   const float *bottomFace,
-                                  const float *volumeMin, // array of 3 values
-                                  const float *volumeMax,
-                                  int bx,
-                                  int by,
-                                  int bz,
+                                  const double *volumeMin, // array of 3 values
+                                  const double *volumeMax,
+                                  const size_t *dims, // num. of grid points
                                   bool frontFacing);
-    void _fillUserCoordinates();
 
 }; // End of class DirectVolumeRenderer
 
