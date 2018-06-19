34061813354de7211a33023cbad7e711751fc6ec
John Clyne <clyne@ucar.edu>
2018-06-18 18:23:52 -0600

    Fixed bugs founds by reviewers

--- include/vapor/Grid.h
+++ include/vapor/Grid.h
@@ -530,8 +530,8 @@ public:
 	}
 	assert(indices.size() == dims.size());
 	for (int i=0; i<indices.size(); i++) {
-		if (indices[i] >= dims[i]-1) {
-			indices[i] = dims[i] - 2;
+		if (indices[i] >= dims[i]) {
+			indices[i] = dims[i] - 1;
 		}
 	}
  }
