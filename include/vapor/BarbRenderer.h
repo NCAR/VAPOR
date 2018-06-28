8957593df111287ec74534ef3acc8af2ac28de19
Scott Pearse <pearse@ucar.edu>
2018-06-28 11:30:35 -0600

    Broke down renderGrid() and performRendering()

--- include/vapor/BarbRenderer.h
+++ include/vapor/BarbRenderer.h
@@ -103,6 +103,18 @@ class RENDER_API BarbRenderer : public Renderer {
     float getHeightOffset(Grid *heightVar, float xCoord,
                           float yCoord, bool &missing);
 
+    bool _doColorMapping(float clut[1024]) const;
+
+    void _getDirection(
+        float direction[3],
+        vector<Grid *> varData,
+        float xCoord,
+        float yCoord,
+        float zCoord,
+        bool &missing) const;
+
+    vector<double> _getScales();
+
     void renderGrid(int rakeGrid[3], double rakeExts[6],
                     vector<Grid *> variableData, int timestep,
                     float vectorLengthScale, float rad, BarbParams *params);
