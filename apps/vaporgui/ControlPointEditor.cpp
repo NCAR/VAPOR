ce4483f546c05ec0583988dae63a8fef3d537c88
Scott Pearse <pearse@ucar.edu>
2018-08-01 09:06:04 -0600

    Improved precision in storing color values

--- apps/vaporgui/ControlPointEditor.cpp
+++ apps/vaporgui/ControlPointEditor.cpp
@@ -64,7 +64,7 @@ void ControlPointEditor::initWidgets()
     if (_cmap) {
         ColorMap::Color color = _cmap->controlPointColor(_controlPoint);
 
-        _tempColor.setHsv((int)(359 * color.hue()), (int)(255 * color.sat()), (int)(255 * color.val()));
+        _tempColor.setHsvF(color.hue(), color.sat(), color.val());
 
         QPalette pal;
         pal.setColor(QPalette::Base, _tempColor);
@@ -138,7 +138,9 @@ void ControlPointEditor::indexValueChanged()
 void ControlPointEditor::pickColor()
 {
     QPalette pal(_colorEdit->palette());
+    QColor   from = pal.color(QPalette::Base);
     QColor   newColor = QColorDialog::getColor(pal.color(QPalette::Base), this);
+    QColor   to = newColor;
     if (!newColor.isValid()) return;
     pal.setColor(QPalette::Base, newColor);
     _colorEdit->setPalette(pal);
@@ -159,11 +161,11 @@ void ControlPointEditor::okHandler()
     } else {
         float value = _dataValueField->text().toFloat();
 
-        int h, s, v;
+        double h, s, v;
 
-        _tempColor.getHsv(&h, &s, &v);
+        _tempColor.getHsvF(&h, &s, &v);
 
-        ColorMap::Color color(h / 359.0, s / 255.0, v / 255.0);
+        ColorMap::Color color(h, s, v);
 
         _cmap->controlPointValue(_controlPoint, value);
         _cmap->controlPointColor(_controlPoint, color);
