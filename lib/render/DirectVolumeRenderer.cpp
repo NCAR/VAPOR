d9e572204503606dcc5ac0f5c3681a8e96ac8fc9
Samuel Li <shaomeng@ucar.edu>
2018-08-01 12:29:09 -0600

    tweak colormap texture

--- lib/render/DirectVolumeRenderer.cpp
+++ lib/render/DirectVolumeRenderer.cpp
@@ -196,7 +196,7 @@ bool DirectVolumeRenderer::UserCoordinates::updateCoordinates( const DVRParams* 
     }
     else
     {
-        std::cout << "EXPENSIVE operation in progress!" << std::endl;
+        std::cerr << "EXPENSIVE operation in progress!" << std::endl;
 
         /* update member variables */
         grid->GetUserExtents( extMin, extMax );
@@ -297,6 +297,8 @@ bool DirectVolumeRenderer::UserCoordinates::updateCoordinates( const DVRParams* 
                 bottomFace[ idx++  ] = (float)buf[2];
             }
 
+        std::cerr << "  EXPENSIVE operation: finish retrieving faces !" << std::endl;
+
         // Save the data field values and missing values
         if( dataField )
             delete[] dataField;
@@ -342,6 +344,7 @@ bool DirectVolumeRenderer::UserCoordinates::updateCoordinates( const DVRParams* 
             std::memset( missingValueMask, 0, numOfVertices );
         }
 
+        std::cerr << "  EXPENSIVE operation: finish retrieving data field !" << std::endl;
     }
 
     delete grid;
@@ -513,7 +516,9 @@ void DirectVolumeRenderer::_initializeFramebufferTextures()
     /* Configure _colorMapTextureId */
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    float borderColor[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    glTexParameterfv(GL_TEXTURE_1D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 
     /* Bind the default textures */
     glBindTexture(GL_TEXTURE_1D, 0);
@@ -525,7 +530,7 @@ void DirectVolumeRenderer::_printGLInfo() const
 {
     std::cout << "    **** System Info ****" << std::endl;
     std::cout << "    OpenGL version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "    GLSL version   : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "    GLSL   version : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
     std::cout << "    Vendor         : " << glGetString(GL_VENDOR) << std::endl;
     std::cout << "    Renderer       : " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "    **** System Info ****" << std::endl;
