73568f1cecbe41b43e7f8772f726f86894890ad6
Samuel Li <shaomeng@ucar.edu>
2018-08-03 16:54:13 -0600

    DVR gui supports lighting parameters

--- apps/vaporgui/DVRSubtabs.h
+++ apps/vaporgui/DVRSubtabs.h
@@ -37,6 +37,8 @@ public:
         _TFWidget->Reinit((TFWidget::Flags)(0));
 
         _dvrParams = nullptr;
+
+        /* note: slider ranges are set using QTCreator */
     }
 
     void Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr, VAPoR::RenderParams *rParams)
@@ -47,10 +49,54 @@ public:
         _dvrParams = dynamic_cast<VAPoR::DVRParams *>(rParams);
         assert(_dvrParams);
         _lightingCheckBox->setChecked(_dvrParams->GetLighting());
+
+        std::vector<double> coeffs = _dvrParams->GetLightingCoeffs();
+        _ambientSlider->setValue(int(coeffs[0] * 100.0));
+        _diffuseSlider->setValue(int(coeffs[1] * 100.0));
+        _specularSlider->setValue(int(coeffs[2] * 100.0));
+        _shininessSlider->setValue(int(coeffs[3]));
     }
 
 private slots:
     void on__lightingCheckBox_toggled(bool checked) { _dvrParams->SetLighting(checked); }
+
+    void on__ambientSlider_valueChanged(int value)
+    {
+        std::vector<double> coeffs = _dvrParams->GetLightingCoeffs();
+        coeffs[0] = double(value) / 100.0;
+        _dvrParams->SetLightingCoeffs(coeffs);
+    }
+
+    void on__diffuseSlider_valueChanged(int value)
+    {
+        std::vector<double> coeffs = _dvrParams->GetLightingCoeffs();
+        coeffs[1] = double(value) / 100.0;
+        _dvrParams->SetLightingCoeffs(coeffs);
+    }
+
+    void on__specularSlider_valueChanged(int value)
+    {
+        std::vector<double> coeffs = _dvrParams->GetLightingCoeffs();
+        coeffs[2] = double(value) / 100.0;
+        _dvrParams->SetLightingCoeffs(coeffs);
+    }
+
+    void on__shininessSlider_valueChanged(int value)
+    {
+        std::vector<double> coeffs = _dvrParams->GetLightingCoeffs();
+        coeffs[3] = double(value);
+        _dvrParams->SetLightingCoeffs(coeffs);
+    }
+
+    void on__defaultLightingButton_clicked(bool checked)
+    {
+        std::vector<double> defaultLightingCoeffs(4);
+        defaultLightingCoeffs[0] = 0.5;
+        defaultLightingCoeffs[1] = 0.3;
+        defaultLightingCoeffs[2] = 0.2;
+        defaultLightingCoeffs[3] = 12.0;
+        _dvrParams->SetLightingCoeffs(defaultLightingCoeffs);
+    }
 
 private:
     VAPoR::DVRParams *_dvrParams;
