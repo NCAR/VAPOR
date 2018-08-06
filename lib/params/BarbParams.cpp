a91cc681d5d5dcda30731dcf54e2877eb4e7406c
Scott Pearse <pearse@ucar.edu>
2018-08-06 14:23:35 -0600

    reconfigured barb scale factors

--- lib/params/BarbParams.cpp
+++ lib/params/BarbParams.cpp
@@ -86,14 +86,12 @@ void BarbParams::_init() {
 	
 	std::vector<string> varnames = GetFieldVariableNames();
 	if (varnames.empty()) {
-		cout << "foo" << endl;
 		return;
 	}
 	if (varnames[X]=="" &&
 		varnames[Y]=="" &&
 		varnames[Z]==""
 	) {
-		cout << "bar" << endl;
 		return;
 	}
 
