280172f6e320e6710d146cc9c6e9a30531474fb7
Samuel Li <shaomeng@ucar.edu>
2018-08-08 00:19:43 -0600

    missing value texture uses unsigned ints

--- lib/render/DirectVolumeRenderer.cpp
+++ lib/render/DirectVolumeRenderer.cpp
@@ -303,6 +303,7 @@ bool DirectVolumeRenderer::UserCoordinates::UpdateCoordinates(const DVRParams *p
     float                         valueRange1o = 1.0f / (valueRange[1] - valueRange[0]);
 
     if (grid->HasMissingData()) {
+        std::cout << "missing value! " << std::endl;
         float missingValue = grid->GetMissingValue();
         float dataValue;
         for (size_t i = 0; i < numOfVertices; i++) {
@@ -318,6 +319,7 @@ bool DirectVolumeRenderer::UserCoordinates::UpdateCoordinates(const DVRParams *p
         }
     } else    // No missing value!
     {
+        std::cout << "no missing value! " << std::endl;
         for (size_t i = 0; i < numOfVertices; i++) {
             dataField[i] = (float(*valItr) - valueRange[0]) * valueRange1o;
             ++valItr;
@@ -372,8 +374,10 @@ int DirectVolumeRenderer::_paintGL()
         /* Also attach the new data to 3D textures: _volumeTextureId, _missingValueTextureId */
         glBindTexture(GL_TEXTURE_3D, _volumeTextureId);
         glTexImage3D(GL_TEXTURE_3D, 0, GL_R32F, _userCoordinates.dims[0], _userCoordinates.dims[1], _userCoordinates.dims[2], 0, GL_RED, GL_FLOAT, _userCoordinates.dataField);
+
         glBindTexture(GL_TEXTURE_3D, _missingValueTextureId);
-        glTexImage3D(GL_TEXTURE_3D, 0, GL_R8, _userCoordinates.dims[0], _userCoordinates.dims[1], _userCoordinates.dims[2], 0, GL_RED, GL_UNSIGNED_BYTE, _userCoordinates.missingValueMask);
+        glTexImage3D(GL_TEXTURE_3D, 0, GL_R8UI, _userCoordinates.dims[0], _userCoordinates.dims[1], _userCoordinates.dims[2], 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, _userCoordinates.missingValueMask);
+
         glBindTexture(GL_TEXTURE_3D, 0);
     }
 
@@ -402,7 +406,7 @@ int DirectVolumeRenderer::_paintGL()
     bool                insideACell = grid->GetIndicesCell(cameraUser, cameraCellIndices);
 
     if (insideACell) {
-        // std::cout << "inside a cell: " << std::endl;
+        std::cout << "inside a cell: " << std::endl;
         printf(" %f, %f, %f\n", cameraUser[0], cameraUser[1], cameraUser[2]);
         printf(" %ld, %ld, %ld\n", cameraCellIndices[0], cameraCellIndices[1], cameraCellIndices[2]);
 
@@ -421,7 +425,7 @@ int DirectVolumeRenderer::_paintGL()
 
         _drawVolumeFaces(2, true);
     } else {
-        // std::cout << "not inside a cell" << std::endl;
+        std::cout << "not inside a cell" << std::endl;
         _drawVolumeFaces(2, false);
     }
 
@@ -503,8 +507,8 @@ void DirectVolumeRenderer::_initializeFramebufferTextures()
     glBindTexture(GL_TEXTURE_3D, _missingValueTextureId);
 
     /* Configure _missingValueTextureId */
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
