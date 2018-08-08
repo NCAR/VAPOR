58d19306672d7728ee1d4e09a20b7504619046df
Scott Pearse <pearse@ucar.edu>
2018-08-08 08:36:06 -0600

    Fix for missing images on Linux (issue 615).  NewRendererDialog improvements

--- apps/vaporgui/RenderEventRouter.cpp
+++ apps/vaporgui/RenderEventRouter.cpp
@@ -70,7 +70,7 @@ string RenderEventRouter::GetSmallIconImagePath() const {
         return (imageName);
 
     vector<string> path;
-    path.push_back("Images");
+    path.push_back("images");
     path.push_back(imageName);
     return (GetAppPath("VAPOR", "share", path));
 }
