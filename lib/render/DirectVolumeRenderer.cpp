e6a653139c635c0627df4d3ca7e7daacbfb8fdc0
Samuel Li <shaomeng@ucar.edu>
2018-08-08 22:50:39 -0600

    many trials, but missingValueMask still doesn't work well

--- lib/render/DirectVolumeRenderer.cpp
+++ lib/render/DirectVolumeRenderer.cpp
@@ -387,7 +387,7 @@ bool DirectVolumeRenderer::UserCoordinates::UpdateCoordinates( const DVRParams* 
             if( dataValue == missingValue )
             {
                 dataField[ i ]        = 0.0f;
-                missingValueMask[ i ] = 255;
+                missingValueMask[ i ] = 127;
             }
             else
             {
@@ -405,7 +405,7 @@ bool DirectVolumeRenderer::UserCoordinates::UpdateCoordinates( const DVRParams* 
             dataField[ i ] = (float(*valItr) - valueRange[0]) * valueRange1o;
             ++valItr;
         }
-        std::memset( missingValueMask, 0, numOfVertices );
+        std::memset( missingValueMask, 0, sizeof(unsigned char) * numOfVertices );
     }
 
     delete grid;
@@ -460,9 +460,28 @@ int DirectVolumeRenderer::_paintGL()
                        0, GL_RED, GL_FLOAT,          _userCoordinates.dataField );
 
         glBindTexture( GL_TEXTURE_3D, _missingValueTextureId );
+        /*glTexImage3D(  GL_TEXTURE_3D, 0, GL_R8I,            _userCoordinates.dims[0], 
+                       _userCoordinates.dims[1],             _userCoordinates.dims[2], 
+                       0, GL_RED_INTEGER, GL_BYTE,  _userCoordinates.missingValueMask );*/
         glTexImage3D(  GL_TEXTURE_3D, 0, GL_R8UI,            _userCoordinates.dims[0], 
                        _userCoordinates.dims[1],             _userCoordinates.dims[2], 
                        0, GL_RED_INTEGER, GL_UNSIGNED_BYTE,  _userCoordinates.missingValueMask );
+        /*glTexImage3D(  GL_TEXTURE_3D, 0, GL_R32UI,            _userCoordinates.dims[0], 
+                       _userCoordinates.dims[1],             _userCoordinates.dims[2], 
+                       0, GL_RED_INTEGER, GL_UNSIGNED_INT,  _userCoordinates.missingValueMask );*/
+        /*glTexImage3D(  GL_TEXTURE_3D, 0, GL_R16,            _userCoordinates.dims[0], 
+                       _userCoordinates.dims[1],             _userCoordinates.dims[2], 
+                       0, GL_RED, GL_UNSIGNED_SHORT,  _userCoordinates.missingValueMask );*/
+        /*glTexImage3D(  GL_TEXTURE_3D, 0, GL_R8,            _userCoordinates.dims[0], 
+                       _userCoordinates.dims[1],             _userCoordinates.dims[2], 
+                       0, GL_RED, GL_UNSIGNED_BYTE,  _userCoordinates.missingValueMask ); */
+
+        for( size_t i = 0; i < _userCoordinates.dims[0] * _userCoordinates.dims[1] * 
+                               _userCoordinates.dims[2]; i++ )
+        {
+            if( _userCoordinates.missingValueMask[i] != 0 )
+                printf("%u\n", _userCoordinates.missingValueMask[i] );
+        }
 
         glBindTexture( GL_TEXTURE_3D, 0 );
     }
@@ -601,8 +620,8 @@ void DirectVolumeRenderer::_initializeFramebufferTextures()
     glBindTexture( GL_TEXTURE_3D, _missingValueTextureId );
 
     /* Configure _missingValueTextureId */
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
