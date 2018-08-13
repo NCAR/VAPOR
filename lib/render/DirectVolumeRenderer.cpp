3566c96a6c536e434bfba933c0749a811085ed7a
Samuel Li <shaomeng@ucar.edu>
2018-08-12 23:58:00 -0600

    minor improvement

--- lib/render/DirectVolumeRenderer.cpp
+++ lib/render/DirectVolumeRenderer.cpp
@@ -78,10 +78,6 @@ DirectVolumeRenderer::~DirectVolumeRenderer()
         glDeleteBuffers(1, &_depthBufferId);
         _depthBufferId = 0;
     }
-    if (_indexBufferId) {
-        glDeleteBuffers(1, &_indexBufferId);
-        _indexBufferId = 0;
-    }
 
     // delete vertex arrays
     if (_vertexArrayId) {
@@ -91,6 +87,10 @@ DirectVolumeRenderer::~DirectVolumeRenderer()
     if (_vertexBufferId) {
         glDeleteBuffers(1, &_vertexBufferId);
         _vertexBufferId = 0;
+    }
+    if (_indexBufferId) {
+        glDeleteBuffers(1, &_indexBufferId);
+        _indexBufferId = 0;
     }
 
     // delete shader programs
