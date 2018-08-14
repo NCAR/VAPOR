fbd00ed71ebbdfd26710eb547da07ef238f9794b
John Clyne <clyne@ucar.edu>
2018-08-14 15:02:58 -0600

    Increment - support for 3d, edge-centered data (e.g. u variable)

--- include/vapor/DCMPAS.h
+++ include/vapor/DCMPAS.h
@@ -231,6 +231,9 @@ class VDF_API DCMPAS : public VAPoR::DC {
     bool _isCoordVar(string varname) const;
     bool _isDataVar(string varname) const;
 
+    template <class T>
+    int _getVar(size_t ts, string varname, T *buf);
+
     int _read_nEdgesOnCell(size_t ts);
     void _addMissingFlag(int *data) const;
     int _readVarToSmartBuf(
@@ -240,6 +243,10 @@ class VDF_API DCMPAS : public VAPoR::DC {
     void _splitOnBoundary(string varname, int *connData) const;
 
     int _readRegionTransposed(
+        MPASFileObject *w,
+        const vector<size_t> &min, const vector<size_t> &max, float *region);
+
+    int _readRegionEdgeVariable(
         MPASFileObject *w,
         const vector<size_t> &min, const vector<size_t> &max, float *region);
 
