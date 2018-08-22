226e8ceafe61260118240965b6270b18918191e6
sgpearse <pearse@ucar.edu>
2018-08-22 16:12:43 -0600

    Another unreported bug

--- apps/vaporgui/FidelityWidget.cpp
+++ apps/vaporgui/FidelityWidget.cpp
@@ -239,8 +239,6 @@ void FidelityWidget::Update(const DataMgr *dataMgr,
         varname = _rParams->GetColorMapVariableName();
     }
 
-    cout << "FidelityWidget " << varname << endl;
-
     if (varname.empty()) {
         fidelityTab->setEnabled(false);
         return;
