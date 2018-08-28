73ce56eac58559c9c0d56debe809bcfe99a8d358
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-08-28 16:40:26 -0600

    Created manager interface

--- lib/render/GLManager.cpp
+++ lib/render/GLManager.cpp
@@ -1,15 +1,16 @@
 #include "vapor/GLManager.h"
-#include "vapor/LegacyGL.h"
 
 using namespace VAPoR;
 
 GLManager::GLManager()
     : shaderManager(new ShaderManager),
+      fontManager(new FontManager(this)),
       matrixManager(new MatrixManager),
       legacy(new LegacyGL(this)) {}
 
 GLManager::~GLManager() {
     delete shaderManager;
+    delete fontManager;
     delete matrixManager;
     delete legacy;
 }
