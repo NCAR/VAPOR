c1d1e9b782fac3220dcda02febec36949feb7e47
Scott Pearse <pearse@ucar.edu>
2018-09-10 14:14:49 -0600

    Added files

--- apps/vaporgui/SliceSubtabs.cpp
+++ apps/vaporgui/SliceSubtabs.cpp
@@ -3,6 +3,10 @@
 #define MIN_SAMPLES 25
 #define MAX_SAMPLES 8000
 
+#define X 0
+#define Y 1
+#define Z 2
+
 SliceVariablesSubtab::SliceVariablesSubtab(QWidget* parent) {
     setupUi(this);
     _variablesWidget->Reinit(
@@ -35,7 +39,12 @@ SliceAppearanceSubtab::SliceAppearanceSubtab(QWidget* parent) {
     _zSampleRate->SetIntType(true);
     _zSampleRate->SetExtents(MIN_SAMPLES, MAX_SAMPLES);
 
-    //connect(_xSamplingRate
+    connect(_xSamplingRate, SIGNAL(valueChanged(int)), this,
+        SLOT(xSamplingRateChanged(int)));
+    connect(_ySamplingRate, SIGNAL(valueChanged(int)), this,
+        SLOT(ySamplingRateChanged(int)));
+    connect(_zSamplingRate, SIGNAL(valueChanged(int)), this,
+        SLOT(zSamplingRateChanged(int)));
 }
 
 void SliceAppearanceSubtab::Update(
@@ -43,7 +52,24 @@ void SliceAppearanceSubtab::Update(
     VAPoR::ParamsMgr *paramsMgr,
     VAPoR::RenderParams *rParams
 ) {
-      _TFWidget->Update(dataMgr, paramsMgr, rParams);
+    _TFWidget->Update(dataMgr, paramsMgr, rParams);
+
+    Box* box = rParams->GetBox();
+    std::vector<double> minExt, maxExt;
+    box->GetExtents(minExt, maxExt);
+
+    if (minExt[X]==maxExt[X])
+        _xSamplingRate->setEnabled(false);
+    else
+        _xSamplingRage->setEnabled(true);
+    if (minExt[Y]==maxExt[Y])
+        _ySamplingRate->setEnabled(false);
+    else
+        _ySamplingRage->setEnabled(true);
+    if (minExt[Z]==maxExt[Z])
+        _zSamplingRate->setEnabled(false);
+    else
+        _zSamplingRage->setEnabled(true);
 }
 
 SliceGeometrySubtab::SliceGeometrySubtab(QWidget* paremt) {
