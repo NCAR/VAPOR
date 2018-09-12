a44c7bbc37528ab6eb60b9d92c47757cd51efa31
John Clyne <clyne@ucar.edu>
2018-09-12 14:27:17 -0600

    Fixed regression that resulted in data and coordinate variables not having their dimensions associated correctly.

--- include/vapor/DataMgr.h
+++ include/vapor/DataMgr.h
@@ -941,8 +941,10 @@ UnstructuredGrid2D *_make_grid_unstructured2d(
 	const vector <size_t> &data_bmin,
 	const vector <size_t> &data_bmax,
 	string coord_varname,
+	int order,
 	vector <size_t> &coord_bmin,
-	vector <size_t> &coord_bmax
+	vector <size_t> &coord_bmax,
+	bool structured
  ) const;
 
  int _setupCoordVecs(
@@ -958,7 +960,8 @@ UnstructuredGrid2D *_make_grid_unstructured2d(
 	vector < vector <size_t > > &bsvec,
 	vector < vector <size_t > > &bs_at_levelvec,
 	vector < vector <size_t > > &bminvec,
-	vector < vector <size_t > > &bmaxvec
+	vector < vector <size_t > > &bmaxvec,
+	bool structured
  ) const;
 
  int _setupConnVecs(
