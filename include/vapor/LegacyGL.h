11c7cbdf550ac35791a20316c4ebe2945ab153eb
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-09-13 15:48:19 -0600

    Added lighting to LegacyGL

--- include/vapor/LegacyGL.h
+++ include/vapor/LegacyGL.h
@@ -3,6 +3,22 @@
 #include <vector>
 #include "vapor/ShaderProgram2.h"
 
+/*
+#ifdef GL_CURRENT_BIT
+#define LGL_CURRENT_BIT GL_CURRENT_BIT
+#else
+#define LGL_CURRENT_BIT 1
+#endif
+ */
+
+#ifdef GL_QUADS
+    #define LGL_QUADS GL_QUADS
+#else
+    #define LGL_QUADS 105999
+#endif
+
+#define LEGACY_TODO(x) printf("TODO %s\n", #x)
+
 namespace VAPoR {
 
 struct GLManager;
@@ -11,6 +27,7 @@ class LegacyGL {
 #pragma pack(push, 4)
     struct VertexData {
         float x, y, z;
+        float nx, ny, nz;
         float r, g, b, a;
     };
 #pragma pack(pop)
@@ -18,19 +35,44 @@ class LegacyGL {
     GLManager *             _glManager;
     std::vector<VertexData> _vertices;
     unsigned int            _mode;
+    bool                    _emulateQuads;
+    bool                    _firstQuadTriangle;
     unsigned int            _VAO, _VBO;
-    ShaderProgram2          _shader;
+    float                   _nx, _ny, _nz;
     float                   _r, _g, _b, _a;
-    bool                    _initialized, _insideBeginEndBlock;
+    bool                    _initialized, _insideBeginEndBlock, _lightingEnabled;
+    float                   _lightDir[3];
 
 public:
     LegacyGL(GLManager *glManager);
+    ~LegacyGL();
     void Initialize();
     void Begin(unsigned int mode);
     void End();
+    void Vertex(glm::vec2);
+    void Vertex(glm::vec3);
+    void Vertex2f(float x, float y);
     void Vertex3f(float x, float y, float z);
+    void Vertex3fv(const float *v);
+    void Vertex3dv(const double *v);
+    void Normal3f(float x, float y, float z);
+    void Normal3fv(const float *n);
+    void Color(glm::vec3);
+    void Color(glm::vec4);
     void Color3f(float r, float g, float b);
+    void Color3fv(const float *f);
     void Color4f(float r, float g, float b, float a);
+    void Color4fv(const float *f);
+
+    void EnableLighting();
+    void DisableLighting();
+    void LightDirectionfv(const float *f);
+
+#ifndef NDEBUG
+    void TestSquare();
+#endif
+
+    // void PushAttrib(int flag);
 };
 
 }    // namespace VAPoR
--- lib/render/LegacyGL.cpp
+++ lib/render/LegacyGL.cpp
@@ -4,41 +4,58 @@
 #include <glm/glm.hpp>
 #include "vapor/GLManager.h"
 // #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 using namespace VAPoR;
 using std::vector;
 
-extern const char *SHADER_SOURCE_VERTEX;
-extern const char *SHADER_SOURCE_FRAGMENT;
+LegacyGL::LegacyGL(GLManager *glManager)
+: _glManager(glManager), _mode(0), _emulateQuads(false), _firstQuadTriangle(true), _VAO(0), _VBO(0), _nx(0), _ny(0), _nz(0), _r(1), _g(1), _b(1), _a(1), _initialized(false),
+  _insideBeginEndBlock(false), _lightingEnabled(false), _lightDir{0}
+{
+}
 
-LegacyGL::LegacyGL(GLManager *glManager) : _glManager(glManager), _mode(0), _VAO(0), _VBO(0), _r(1), _g(1), _b(1), _a(1), _initialized(false), _insideBeginEndBlock(false) {}
+LegacyGL::~LegacyGL()
+{
+    if (_VAO) glDeleteVertexArrays(1, &_VAO);
+    if (_VBO) glDeleteBuffers(1, &_VBO);
+}
 
 void LegacyGL::Initialize()
 {
+    GL_ERR_BREAK();
     assert(!_initialized);
+
     glGenVertexArrays(1, &_VAO);
     glGenBuffers(1, &_VBO);
+
     assert(_VAO);
     assert(_VBO);
-    assert(_shader.AddShaderFromSource(GL_VERTEX_SHADER, SHADER_SOURCE_VERTEX));
-    assert(_shader.AddShaderFromSource(GL_FRAGMENT_SHADER, SHADER_SOURCE_FRAGMENT));
-    assert(_shader.Link());
 
     glBindVertexArray(_VAO);
     glBindBuffer(GL_ARRAY_BUFFER, _VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), NULL);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void *)offsetof(struct VertexData, nx));
+    glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void *)offsetof(struct VertexData, r));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void *)offsetof(struct VertexData, r));
     glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     _initialized = true;
+    GL_ERR_BREAK();
 }
 
 void LegacyGL::Begin(unsigned int mode)
 {
+    assert(!_insideBeginEndBlock);
+
+    if (mode == LGL_QUADS) {
+        _emulateQuads = true;
+        mode = GL_TRIANGLES;
+    }
+
     _mode = mode;
     _insideBeginEndBlock = true;
 }
@@ -53,22 +70,62 @@ void LegacyGL::End()
     glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData) * _vertices.size(), _vertices.data(), GL_STREAM_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    _shader.Bind();
-    _shader.SetUniform("MVP", _glManager->matrixManager->GetModelViewProjectionMatrix());
+    ShaderProgram2 *_shader = _glManager->shaderManager->GetShader("Legacy");
+    _shader->Bind();
+    _shader->SetUniform("P", _glManager->matrixManager->GetProjectionMatrix());
+    _shader->SetUniform("MV", _glManager->matrixManager->GetModelViewMatrix());
+    _shader->SetUniform("lightingEnabled", _lightingEnabled);
+    _shader->SetUniform("lightDir", glm::make_vec3(_lightDir));
     glDrawArrays(_mode, 0, _vertices.size());
 
     glBindVertexArray(0);
-    _shader.UnBind();
+    _shader->UnBind();
 
+    _emulateQuads = false;
     _insideBeginEndBlock = false;
     _vertices.clear();
 }
 
+void LegacyGL::Vertex(glm::vec2 v) { Vertex2f(v.x, v.y); }
+
+void LegacyGL::Vertex(glm::vec3 v) { Vertex3f(v.x, v.y, v.z); }
+
+void LegacyGL::Vertex2f(float x, float y) { Vertex3f(x, y, 0); }
+
 void LegacyGL::Vertex3f(float x, float y, float z)
 {
     assert(_insideBeginEndBlock);
-    _vertices.push_back({x, y, z, _r, _g, _b, _a});
+    _vertices.push_back({x, y, z, _nx, _ny, _nz, _r, _g, _b, _a});
+
+    if (_emulateQuads && _vertices.size() % 3 == 0) {
+        if (_firstQuadTriangle) {
+            VertexData v1 = _vertices[_vertices.size() - 3];
+            VertexData v3 = _vertices[_vertices.size() - 1];
+            _vertices.push_back(v1);
+            _vertices.push_back(v3);
+            _firstQuadTriangle = false;
+        } else {
+            _firstQuadTriangle = true;
+        }
+    }
 }
+
+void LegacyGL::Vertex3fv(const float *v) { Vertex3f(v[0], v[1], v[2]); }
+
+void LegacyGL::Vertex3dv(const double *v) { Vertex3f((float)v[0], (float)v[1], (float)v[2]); }
+
+void LegacyGL::Normal3f(float x, float y, float z)
+{
+    _nx = x;
+    _ny = y;
+    _nz = z;
+}
+
+void LegacyGL::Normal3fv(const float *n) { Normal3f(n[0], n[1], n[2]); }
+
+void LegacyGL::Color(glm::vec3 v) { Color3f(v.r, v.g, v.b); }
+
+void LegacyGL::Color(glm::vec4 v) { Color4f(v.r, v.g, v.b, v.a); }
 
 void LegacyGL::Color3f(float r, float g, float b)
 {
@@ -78,6 +135,8 @@ void LegacyGL::Color3f(float r, float g, float b)
     _a = 1.0f;
 }
 
+void LegacyGL::Color3fv(const float *f) { Color3f(f[0], f[1], f[2]); }
+
 void LegacyGL::Color4f(float r, float g, float b, float a)
 {
     _r = r;
@@ -86,29 +145,35 @@ void LegacyGL::Color4f(float r, float g, float b, float a)
     _a = a;
 }
 
-const char *SHADER_SOURCE_VERTEX = R"(
-#version 330 core
+void LegacyGL::Color4fv(const float *f) { Color4f(f[0], f[1], f[2], f[3]); }
 
-layout (location = 0) in vec3 vPos;
-layout (location = 1) in vec4 vColor;
+void LegacyGL::EnableLighting() { _lightingEnabled = true; }
 
-out vec4 fColor;
+void LegacyGL::DisableLighting() { _lightingEnabled = false; }
 
-uniform mat4 MVP;
-
-void main() {
-    gl_Position = MVP * vec4(vPos, 1.0f);
-    fColor = vColor;
+void LegacyGL::LightDirectionfv(const float *f)
+{
+    glm::vec3 dir = glm::make_vec3(f);
+    dir = _glManager->matrixManager->GetModelViewMatrix() * glm::vec4(dir, 0.f);
+    _lightDir[0] = dir.x;
+    _lightDir[1] = dir.y;
+    _lightDir[2] = dir.z;
 }
-)";
 
-const char *SHADER_SOURCE_FRAGMENT = R"(
-#version 330 core
+#ifndef NDEBUG
+void LegacyGL::TestSquare()
+{
+    GL_ERR_BREAK();
+    Normal3f(0, 0, 1);
+    Begin(GL_TRIANGLES);
+    Vertex3f(-1, -1, 0);
+    Vertex3f(1, -1, 0);
+    Vertex3f(-1, 1, 0);
 
-in  vec4 fColor;
-out vec4 fragment;
-
-void main() {
-    fragment = fColor;
+    Vertex3f(-1, 1, 0);
+    Vertex3f(1, -1, 0);
+    Vertex3f(1, 1, 0);
+    End();
+    GL_ERR_BREAK();
 }
-)";
+#endif
