49488b45862268c3c10c40d8d6f435fcb8642156
clyne <clyne@ucar.edu>
2018-09-20 11:26:24 -0600

    Revert "Fixed 1004"

--- lib/vdc/UnstructuredGrid2D.cpp
+++ lib/vdc/UnstructuredGrid2D.cpp
@@ -107,7 +107,7 @@ void UnstructuredGrid2D::GetBoundingBox(
 	// access is tricky.
 	//
 	ConstCoordItr itr = ConstCoordBegin();
-	ConstCoordItr enditr = ConstCoordEnd();
+	ConstCoordItr enditr = ConstCoordBegin();
 
 	for (size_t i = 0; i<start; i++) ++itr;
 	for (size_t i = 0; i<stop; i++) ++enditr;
