751834bf4a527e58f8b2596577e81732d18009bf
Samuel Li <shaomeng@ucar.edu>
2018-09-22 15:11:07 -0600

    tiny optimization...

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -532,6 +532,8 @@ int RayCaster::_paintGL(bool fast) {
     glBindVertexArray(_vertexArrayId);
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferId);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBufferId);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
     RayCasterParams *params = dynamic_cast<RayCasterParams *>(GetActiveParams());
     if (!params) {
@@ -689,6 +691,8 @@ int RayCaster::_paintGL(bool fast) {
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glDisableVertexAttribArray(0);
 
     return 0;
 }
@@ -858,10 +862,6 @@ void RayCaster::_drawVolumeFaces(int whichPass,
         glDepthMask(GL_FALSE);
     }
 
-    // Let's use our VAO indices here
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-
     if (insideACell) {
         glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat),
                      _userCoordinates.nearCoords, GL_STREAM_DRAW);
@@ -869,8 +869,6 @@ void RayCaster::_drawVolumeFaces(int whichPass,
     } else {
         _renderTriangleStrips(castingMode);
     }
-
-    glDisableVertexAttribArray(0);
 
     glDisable(GL_CULL_FACE);
     glDisable(GL_DEPTH_TEST);
