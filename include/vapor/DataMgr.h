e1d69d1e27d38406770ad388692a07e84e3c18b5
John Clyne <clyne@ucar.edu>
2018-09-25 20:26:02 -0600

    Increment. File blocking is not hidden from most of the application

--- include/vapor/DataMgr.h
+++ include/vapor/DataMgr.h
@@ -452,15 +452,13 @@ std::vector <size_t> GetCRatios(string varname) const;
  
  //! \copydoc DC::GetDimLensAtLevel()
  //!
- //! \param[in] spatial A boolean, if true, indicates that only the variable's
- //! spatial dimensions and block size should be returned. A time varying 
- //! dimension, if one exists, is not included 
- //!
  virtual int GetDimLensAtLevel(
 	string varname, int level, 
-	std::vector <size_t> &dims_at_level,
-	std::vector <size_t> &bs_at_level
- ) const; 
+	std::vector <size_t> &dims_at_level
+ ) const {
+	std::vector <size_t> dummy;
+	return(GetDimLensAtLevel(varname, level, dims_at_level, dummy));
+ }
 
  //! Unlock a floating-point region of memory
  //!
@@ -1029,6 +1027,7 @@ UnstructuredGrid2D *_make_grid_unstructured2d(
  int _get_blocked_region_from_fs(
 	size_t ts, string varname, int level, int lod,
 	const vector <size_t> &file_bs,
+	const vector <size_t> &grid_dims,
 	const vector <size_t> &grid_bs,
 	const vector <size_t> &grid_min,
 	const vector <size_t> &grid_max,
@@ -1190,6 +1189,14 @@ UnstructuredGrid2D *_make_grid_unstructured2d(
 	string dummy;
 	return(_hasVerticalXForm(meshname, dummy, dummy));
  }
+
+ // Hide public DC::GetDimLensAtLevel by making it private
+ //
+ virtual int GetDimLensAtLevel(
+	string varname, int level, 
+	std::vector <size_t> &dims_at_level,
+	std::vector <size_t> &bs_at_level
+ ) const; 
 
 
 };
