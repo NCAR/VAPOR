1c0660b41805d3c85a5955ef503ddd34b57546d1
sgpearse <pearse@ucar.edu>
2018-10-08 09:49:23 -0600

    Removed console outputs

--- apps/vaporgui/SliceSubtabs.cpp
+++ apps/vaporgui/SliceSubtabs.cpp
@@ -53,7 +53,6 @@ SliceAppearanceSubtab::SliceAppearanceSubtab(QWidget *parent) {
 
 void SliceAppearanceSubtab::_sampleRateChanged(int rate) {
     _params->SetSampleRate(rate);
-    cout << "sampleRateChanged" << endl;
     _TFWidget->SetAutoUpdateParamChanged(true);
     //_TFWidget->mappingFrame->RefreshHistogram(true);
 }
