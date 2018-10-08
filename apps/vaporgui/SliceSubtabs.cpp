e279faa54408448b2e435335dbd32ef850d259d2
sgpearse <pearse@ucar.edu>
2018-10-08 10:43:38 -0600

    Testing

--- apps/vaporgui/SliceSubtabs.cpp
+++ apps/vaporgui/SliceSubtabs.cpp
@@ -20,6 +20,9 @@ SliceVariablesSubtab::SliceVariablesSubtab(QWidget* parent) {
     QButtonGroup* fidelityButtons = _variablesWidget->_fidelityWidget->GetFidelityButtons();
     connect(fidelityButtons, SIGNAL(buttonClicked(int)),
         this, SLOT(_setDefaultSampleRate()));
+	QComboBox* refinementCombo = _variablesWidget->_fidelityWidget->refinementCombo;
+	connect(refinementCombo, SIGNAL(currentIndexChanged(int)),
+		this, SLOT(_setDefaultSampleRate()));
 }
 
 void SliceVariablesSubtab::Update(
@@ -34,6 +37,7 @@ void SliceVariablesSubtab::Update(
 
 void SliceVariablesSubtab::_setDefaultSampleRate() {
     int rate = _params->GetDefaultSampleRate();
+	cout << "setting default sample rate to " << rate << endl;
     _params->SetSampleRate(rate);
 }
 
