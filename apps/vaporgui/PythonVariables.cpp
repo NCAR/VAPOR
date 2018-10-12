cf7a0b1a96b12f299f7ff576f154545013406dd4
Scott Pearse <pearse@ucar.edu>
2018-10-12 14:24:45 -0600

    Added new files

---
+++ apps/vaporgui/PythonVariables.cpp
@@ -0,0 +1,14 @@
+#include "PythonVariables.h"
+#include "ui_PythonVariablesgui.h"
+
+PythonVariablesGUI::PythonVariablesGUI(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PythonVariablesGUI)
+{
+    ui->setupUi(this);
+}
+
+PythonVariablesGUI::~PythonVariablesGUI()
+{
+    delete ui;
+}
