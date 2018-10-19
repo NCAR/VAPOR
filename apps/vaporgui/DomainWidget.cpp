289ff4ecac78d1e42786578abce75ca6d36ba7fd
Scott Pearse <pearse@ucar.edu>
2018-10-19 10:15:14 -0600

    Fixed isoline bug on OSX

--- apps/vaporgui/DomainWidget.cpp
+++ apps/vaporgui/DomainWidget.cpp
@@ -299,7 +299,8 @@ IsoSlider::IsoSlider(QWidget *parent, float min, float max) : DomainWidget(paren
     _maxValue = 0.53;
     _minY = -.10;
     _maxY = 0.06;
-    _lineWidth = 0.02;
+    //_lineWidth = 0.02;
+    _lineWidth = 0.04;
 }
 
 //----------------------------------------------------------------------------
@@ -322,7 +323,6 @@ int IsoSlider::paintGL() {
     glPushMatrix();
     glPushName(VERTLINE);
     {
-        cout << " at " << mid() << " lw " << _lineWidth << " rc ";
         glColor3f(1., 1., 1.);
 
         glBegin(GL_QUADS);
@@ -337,7 +337,6 @@ int IsoSlider::paintGL() {
     glPopMatrix();
     glPopName();
     int rc = printOpenGLError();
-    cout << rc << endl;
     if (rc < 0)
         return -1;
     return 0;
