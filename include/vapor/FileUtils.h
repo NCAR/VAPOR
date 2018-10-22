cb0576e875701b8065ba20b3867fc87ca4d1d22e
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-10-22 16:10:14 -0600

    Added to FileUtils

--- include/vapor/FileUtils.h
+++ include/vapor/FileUtils.h
@@ -12,8 +12,11 @@ enum class FileType {
     Does_Not_Exist
 };
 
+extern const char Separator;
+
 std::string ReadFileToString(const std::string &path);
 std::string Basename(const std::string &path);
+std::string Extension(const std::string &path);
 long GetFileModifiedTime(const std::string &path);
 bool FileExists(const std::string &path);
 bool IsRegularFile(const std::string &path);
