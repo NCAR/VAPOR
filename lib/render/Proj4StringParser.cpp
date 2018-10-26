6e42993e14ba0dc05c03b69dfd73375ff054271b
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-10-26 14:25:03 -0600

    Geotiff functionality v1.0a

--- lib/render/Proj4StringParser.cpp
+++ lib/render/Proj4StringParser.cpp
@@ -33,6 +33,10 @@ map<string, string> Proj4StringParser::Proj4StringToParameterMap(string proj) {
     return tokens;
 }
 
+bool Proj4StringParser::HasKey(const std::string &key) const {
+    return _tokens.find(key) != _tokens.end();
+}
+
 std::string Proj4StringParser::GetString(const std::string &key, const std::string &defaultValue) const {
     auto it = _tokens.find(key);
     if (it == _tokens.end())
@@ -74,6 +78,6 @@ int Proj4StringParser::Proj4EllipseStringToGeoTIFEnum(const std::string &proj) {
         return Ellipse_Plessis_1817;
     else if (proj == "sphere")
         return Ellipse_Sphere;
-    SetErrMsg("Invalid Proj4 ellipse parameter \"%s\"", proj.c_str());
+    SetErrMsg("Unsupported Proj4 ellipse parameter \"%s\"", proj.c_str());
     return -1;
 }
