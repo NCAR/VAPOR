ab8ae3778cbe8b315a303bc90bd9dd354c53a2e7
John Clyne <clyne@ucar.edu>
2018-10-30 12:35:40 -0600

    Increment. Bug in memory allocation

--- lib/render/PyEngine.cpp
+++ lib/render/PyEngine.cpp
@@ -17,8 +17,9 @@ namespace {
 
 void free_arrays(vector<float *> &arrays) {
     for (int i = 0; i < arrays.size(); i++) {
-        if (arrays[i])
+        if (arrays[i]) {
             delete[] arrays[i];
+        }
     }
 }
 
@@ -32,20 +33,18 @@ void free_arrays(
 int alloc_arrays(
     const vector<vector<size_t>> &dimsVectors,
     vector<float *> &arrays) {
+    arrays.clear();
 
     size_t nElements = 1;
     for (int i = 0; i < dimsVectors.size(); i++) {
         nElements *= VProduct(dimsVectors[i]);
-    }
 
-    float *buf = new (nothrow) float[nElements];
-    if (!buf)
-        return (-1);
-
-    float *bufptr = buf;
-    for (int i = 0; i < dimsVectors.size(); i++) {
-        arrays.push_back(bufptr);
-        bufptr += VProduct(dimsVectors[i]);
+        float *buf = new (nothrow) float[nElements];
+        if (!buf) {
+            free_arrays(arrays);
+            return (-1);
+        }
+        arrays.push_back(buf);
     }
 
     return (0);
@@ -624,12 +623,12 @@ int PyEngine::DerivedPythonVar::_readRegionSubset(
         outputVarNames, outputVarDims, outputVarArrays);
     if (rc < 0) {
         DataMgrUtils::UnlockGrids(_dataMgr, variables);
-        free_arrays(inputVarArrays, outputVarArrays);
+        free_arrays(inputVarArrays); // output arrays aren't owned by us
         return (-1);
     }
 
     DataMgrUtils::UnlockGrids(_dataMgr, variables);
-    free_arrays(inputVarArrays);
+    free_arrays(inputVarArrays); // output arrays aren't owned by us
 
     return (0);
 }
