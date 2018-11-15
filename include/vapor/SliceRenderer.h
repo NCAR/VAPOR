11042c15bf10f3504db656f054cdf4300e3dff5b
Scott Pearse <pearse@ucar.edu>
2018-11-15 10:28:10 -0700

    better handling of geometry changes

--- include/vapor/SliceRenderer.h
+++ include/vapor/SliceRenderer.h
@@ -46,6 +46,7 @@ class RENDER_API SliceRenderer : public Renderer {
         std::vector<float> tf_lut;
         std::vector<double> tf_minMax;
         std::vector<double> boxMin, boxMax;
+        std::vector<double> domainMin, domainMax;
     } _cacheParams;
 
     void _initVAO();
@@ -54,8 +55,10 @@ class RENDER_API SliceRenderer : public Renderer {
 
     bool _isColormapCacheDirty() const;
     bool _isDataCacheDirty() const;
+    bool _isBoxCacheDirty() const;
     int _saveCacheParams();
     void _resetColormapCache();
+    int _resetBoxCache();
     int _resetDataCache();
     void _initTextures();
     int _saveTextureData();
@@ -67,6 +70,7 @@ class RENDER_API SliceRenderer : public Renderer {
     void _configureShader();
     void _resetState();
     void _initializeState();
+    void _resetTextureCoordinates();
 
     void _setVertexPositions();
     void _setXYVertexPositions(
@@ -87,7 +91,8 @@ class RENDER_API SliceRenderer : public Renderer {
     int _textureWidth;
     int _textureHeight;
     float *_dataValues;
-    std::vector<double> _vertexPositions;
+    std::vector<double> _vertexCoords;
+    std::vector<float> _texCoords;
 
     GLuint _VAO;
     GLuint _vertexVBO;
