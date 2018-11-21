9bd3e5c3ca0dfaae4493f4a7af8b7687d0bea104
Samuel Li <shaomeng@ucar.edu>
2018-11-20 17:12:12 -0700

    tweak fast rendering mode

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -908,6 +908,9 @@ void RayCaster::_load3rdPassUniforms(long castingMode,
     uniformLocation = glGetUniformLocation(_3rdPassShaderId, "clipPlanes");
     glUniform4fv(uniformLocation, 6, planes);
 
+    uniformLocation = glGetUniformLocation(_3rdPassShaderId, "fast");
+    glUniform1i(uniformLocation, int(fast));
+
     // Pass in textures
     GLuint textureUnit = 0;
     glActiveTexture(GL_TEXTURE0 + textureUnit);
