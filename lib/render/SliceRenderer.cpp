04cd4c83e5e09209d7412b1b88388b7db2c2130a
Scott Pearse <pearse@ucar.edu>
2018-12-03 15:31:17 -0700

    small update

--- lib/render/SliceRenderer.cpp
+++ lib/render/SliceRenderer.cpp
@@ -504,9 +504,6 @@ void SliceRenderer::_initializeState()
 
 void SliceRenderer::_resetState()
 {
-    glActiveTexture(GL_TEXTURE2);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, 0);
 
