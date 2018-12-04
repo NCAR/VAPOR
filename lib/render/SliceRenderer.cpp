05fc932a966d36cd1913c3e5c28325cc76982d49
Scott Pearse <pearse@ucar.edu>
2018-12-04 15:36:20 -0700

    Calling static method for unbinding shader in SliceRenderer

--- lib/render/SliceRenderer.cpp
+++ lib/render/SliceRenderer.cpp
@@ -520,7 +520,7 @@ void SliceRenderer::_resetState()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     ShaderProgram *s = _glManager->shaderManager->GetShader("Slice");
-    s->UnBind();
+    ShaderProgram::UnBind();
 
     glDisable(GL_BLEND);
     glDisable(GL_DEPTH_TEST);
