fe817d0eef3a287c449493e441f5e91d09e48f38
Samuel Li <shaomenglee@gmail.com>
2018-12-05 11:13:17 -0700

    tweak fast mode step size

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -937,7 +937,7 @@ void RayCaster::_load3rdPassUniforms(long castingMode,
         stepSize1D = std::sqrt(span[0] * span[0] + span[1] * span[1] + span[2] * span[2]) /
                      float(_userCoordinates.dims[3] * 2); // Use Nyquist frequency by default
     if (fast)
-        stepSize1D *= 4.0; // Quadruple step size when fast rendering
+        stepSize1D *= 8.0; // Increase step size, thus fewer steps, when fast rendering
     uniformLocation = glGetUniformLocation(_3rdPassShaderId, "stepSize1D");
     glUniform1f(uniformLocation, stepSize1D);
 
