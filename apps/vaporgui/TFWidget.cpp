b74b08cdf68930f0e4be9ee36de64fceda9ddd5c
Scott Pearse <pearse@ucar.edu>
2018-12-10 15:13:14 -0700

    Python debugging

--- apps/vaporgui/TFWidget.cpp
+++ apps/vaporgui/TFWidget.cpp
@@ -567,6 +567,8 @@ void TFWidget::checkForMainMapperRangeChanges() {
 	double newMin = mf->getMinMapValue();
 	double newMax = mf->getMaxMapValue();
 
+    cout << min << " " << max << " " << newMin << " " << newMax << endl;
+
 	if (min != newMin)
 		_mainHistoRangeChanged = true;
 	if (max != newMax)
@@ -948,6 +950,7 @@ MapperFunction* TFWidget::getMainMapperFunction() {
 	bool mainTF = true;
 	string varname = getTFVariableName(mainTF);
 	MapperFunction *mf = _rParams->GetMapperFunc(varname);
+    cout << "GetMapperFunc " << varname << endl;
 	assert(mf);
 	return mf;
 }
