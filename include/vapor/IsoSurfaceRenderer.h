d7d8719bbaee66d44a0294164204709439d32c4b
Samuel Li <shaomenglee@gmail.com>
2018-12-11 12:21:40 -0700

    fix names

--- include/vapor/IsoSurfaceRenderer.h
+++ include/vapor/IsoSurfaceRenderer.h
@@ -20,7 +20,7 @@ class RENDER_API IsoSurfaceRenderer : public RayCaster {
 
   protected:
     int _loadShaders();
-    void _3rdPassSpecialHandling(bool, long);
+    void _3rdPassSpecialHandling(bool fast, long castMode);
 };
 
 }; // namespace VAPoR
