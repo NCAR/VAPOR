2b589c609fb36124d66f7f10b19d7d63cfd56fda
Samuel Li <shaomenglee@gmail.com>
2018-12-11 14:56:33 -0700

    restore the original logic 2

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -854,6 +854,8 @@ void RayCaster::_load3rdPassUniforms( long               castingMode,
                                       const glm::mat4&   inversedMV,
                                       bool               fast ) const
 {
+    ShaderProgram *shader = _3rdPassShader;
+    
     glm::mat4 modelview  = _glManager->matrixManager->GetModelViewMatrix();
     glm::mat4 projection = _glManager->matrixManager->GetProjectionMatrix();
     
