3a6297bfaaf59e6709a73d169fbf0bc38f01dfb1
John Clyne <clyne@ucar.edu>
2018-12-11 20:07:45 -0700

    Increment

--- lib/vdc/Grid.cpp
+++ lib/vdc/Grid.cpp
@@ -607,7 +607,8 @@ template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator+=
 
     assert(offset >= 0);
 
-    vector<size_t> maxIndex(_dims3d.size());
+    static vector<size_t> maxIndex;
+    maxIndex.resize(_dims3d.size());
     for (int i = 0; i < _dims3d.size(); i++) maxIndex[i] = _dims3d[i] - 1;
 
     long maxIndexL = Wasp::LinearizeCoords(maxIndex, _dims3d);
