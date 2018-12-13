c8b9194add9de338ae442886392da9d72e13d9d2
Samuel Li <shaomenglee@gmail.com>
2018-12-13 15:07:10 -0700

    optimize matrix transpose

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -886,7 +886,7 @@ void RayCaster::_load3rdPassUniforms( long               castingMode,
     
     shader->SetUniform("MV", modelview);
     shader->SetUniform("Projection", projection);
-    shader->SetUniform("inversedMV", inversedMV);
+    shader->SetUniform("transposedInverseMV", glm::transpose(inversedMV));
 
     const float* cboxMin  = _userCoordinates.myGridMin;
     const float* cboxMax  = _userCoordinates.myGridMax;
