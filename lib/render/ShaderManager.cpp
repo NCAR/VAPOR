87dc939d8b653bf04f2c93d8b7b0eaa6652a4fc6
Stas Jaroszynski <stasj@ucar.edu>
2018-12-14 16:25:06 -0700

    libs building on windows

--- lib/render/ShaderManager.cpp
+++ lib/render/ShaderManager.cpp
@@ -15,7 +15,9 @@ using std::vector;
 
 #ifdef NDEBUG
     #if SHADER_AUTORELOAD
-        #warning Disabling shader autoreloading
+        #ifndef WIN32
+            #warning Disabling shader autoreloading
+        #endif
         #undef SHADER_AUTORELOAD
     #endif
 #endif
