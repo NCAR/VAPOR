57b22100474fc4d6af2c13f2ce6d554013327343
Scott Pearse <pearse@ucar.edu>
2018-12-17 09:06:16 -0700

    Removed console output

--- apps/vaporgui/TabManager.cpp
+++ apps/vaporgui/TabManager.cpp
@@ -271,11 +271,6 @@ void TabManager::_setActive(
 
 void TabManager::_newRenderer(
     string activeViz, string renderClass, string renderInst) {
-
-    std::clock_t start;
-    double duration;
-    start = std::clock();
-
     if (renderClass.empty() || renderInst.empty()) {
         HideRenderWidgets();
         return;
@@ -306,9 +301,6 @@ void TabManager::_newRenderer(
     w->setEnabled(true);
 
     er->updateTab();
-
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "_newRenderer() Time: " << duration << '\n';
 }
 
 vector<string> TabManager::GetInstalledTabNames(bool renderOnly) const {
