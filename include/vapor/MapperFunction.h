7d46be1bde7957ca85fada42b57a44514ed49bcf
Scott Pearse <pearse@ucar.edu>
2018-12-19 13:47:50 -0700

    removed stride as a param from MapperFunction

--- include/vapor/MapperFunction.h
+++ include/vapor/MapperFunction.h
@@ -136,19 +136,6 @@ public:
 	return(getMinMaxMapValue()[1]);
  };
 
- int getHistogramStride() {
-    return GetValueLong(_histogramStrideTag, 1);
- }
-
- void setHistogramStride(int stride) {
-    cout << "setting stride to " << stride << endl;
-    SetValueLong(
-        _histogramStrideTag, 
-        "Set histogram calculation stride", 
-        stride
-    );
- }
-
  //! Set both minimum and maximum mapping (histo) values
  //! \param[in] val1 minimum value
  //! \param[in] val2 maximum value
@@ -343,7 +330,6 @@ private:
  static const string _opacityMapTag;
  static const string _autoUpdateHistoTag;
  static const string _secondaryVarMapperTag;
- static const string _histogramStrideTag;
 
  //
  // Size of lookup table.  Always 1<<8 currently!
