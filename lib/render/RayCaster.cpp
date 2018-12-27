84254c2023fe4fe19ada0bb49b3ddb912513511f
Samuel Li <shaomeng@ucar.edu>
2018-12-26 17:36:27 -0700

    fine tune

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -957,7 +957,7 @@ void RayCaster::_load3rdPassUniforms(int castingMode,
         if (fast)
             stepSize1D *= 8.0f; //  Increase step size, thus fewer steps, when fast rendering
     } else
-        stepSize1D = _userCoordinates.baseStepSize * 0.9f;
+        stepSize1D = _userCoordinates.baseStepSize * 0.95f;
     shader->SetUniform("stepSize1D", stepSize1D);
 
     // Pass in textures
