c9817a3cf4178a3030fe8cd5da5cb39ec0cdd754
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-12-31 12:01:46 -0700

    Renamed printOpenGLError to CheckGLError because thats what it does

--- apps/vaporgui/VizWin.cpp
+++ apps/vaporgui/VizWin.cpp
@@ -266,7 +266,7 @@ void VizWin::resizeGL(int width, int height){
 		return;
 	}
 
-	int rc = printOpenGLErrorMsg("GLVizWindowResizeEvent");
+	int rc = CheckGLErrorMsg("GLVizWindowResizeEvent");
 	if (rc < 0) {
 		MSG_ERR("OpenGL error");
 	}
@@ -298,14 +298,14 @@ void VizWin::initializeGL()
     bool initialize = true;
     updateManip(initialize);
 
-	printOpenGLErrorMsg("GLVizWindowInitializeEvent");
+	CheckGLErrorMsg("GLVizWindowInitializeEvent");
 	int rc = _controlExec->InitializeViz(_winName, _glManager);
 	if (rc<0) {
 		MSG_FATAL("Failure to initialize Visualizer");
         return;
 	}
     _glManager->legacy->Initialize();
-	printOpenGLErrorMsg("GLVizWindowInitializeEvent");
+	CheckGLErrorMsg("GLVizWindowInitializeEvent");
 
 	ParamsMgr *paramsMgr = _controlExec->GetParamsMgr();
 	ViewpointParams* vParams = paramsMgr->GetViewpointParams(_winName);
@@ -618,7 +618,7 @@ void VizWin::Render(bool fast) {
 	
 	swapBuffers();
     
-	rc = printOpenGLErrorMsg("VizWindowPaintGL");
+	rc = CheckGLErrorMsg("VizWindowPaintGL");
 	if (rc < 0) {
 		MSG_ERR("OpenGL error");
 	}
