95d452394e3b7ac05d152a4b206390a5184453ba
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-02 21:49:26 -0700

    Fixed extents information for planar barbs

--- lib/render/BarbRenderer.cpp
+++ lib/render/BarbRenderer.cpp
@@ -272,6 +272,7 @@ int BarbRenderer::_getVarGrid(
 	return 0;
 }
 
+
 int BarbRenderer::_paintGL(bool) {
     int rc = 0;
     
@@ -612,14 +613,17 @@ void BarbRenderer::_reFormatExtents(
 	BarbParams* bParams = dynamic_cast<BarbParams*>(GetActiveParams());
 	assert(bParams);
 	vector<double> rMinExtents, rMaxExtents;
+    
 	bParams->GetBox()->GetExtents(rMinExtents, rMaxExtents);
 
+    bool planar = bParams->GetBox()->IsPlanar();
+    
 	rakeExts.push_back(rMinExtents[X]);
 	rakeExts.push_back(rMinExtents[Y]);
-	rakeExts.push_back(rMinExtents[Z]);
+    rakeExts.push_back(planar ? 0 : rMinExtents[Z]);
 	rakeExts.push_back(rMaxExtents[X]);
 	rakeExts.push_back(rMaxExtents[Y]);
-	rakeExts.push_back(rMaxExtents[Z]);
+    rakeExts.push_back(planar ? 0 : rMaxExtents[Z]);
 }
 
 void BarbRenderer::_makeRakeGrid(
