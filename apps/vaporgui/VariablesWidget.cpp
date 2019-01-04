653b6e7c33e1786950a7eba1eee3f41a6e142ce6
Scott Pearse <pearse@ucar.edu>
2019-01-04 15:04:37 -0700

    Fix for MPAS regression

--- apps/vaporgui/VariablesWidget.cpp
+++ apps/vaporgui/VariablesWidget.cpp
@@ -448,7 +448,8 @@ void VariablesWidget::updateCombos() {
 void VariablesWidget::updateDimCombo() {
     string varName = _rParams->GetVariableName();
     if (_dataMgr->VariableExists(_rParams->GetCurrentTimestep(), varName))
-        _activeDim = _dataMgr->GetNumDimensions(varName);
+        _activeDim = _dataMgr->GetVarTopologyDim(varName);
+    //_activeDim = _dataMgr->GetNumDimensions(varName);
 
     // Only update if we support multiple dimensions
     if (((_dimFlags & TWOD) && (_dimFlags & THREED))) {
