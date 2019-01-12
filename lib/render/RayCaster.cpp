df187c9412092fa93c6f2c89e043aa224ff76bac
Samuel Li <shaomeng@ucar.edu>
2019-01-11 22:32:27 -0700

    DVR mode 2 supports eye inside the volume too

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -667,9 +667,7 @@ int RayCaster::_initializeFramebufferTextures()
     return 0;
 }
 
-void RayCaster::_drawVolumeFaces(int whichPass, int castingMode,
-                                 // bool             insideACell,
-                                 const std::vector<size_t> &cameraCellIdx, const glm::mat4 &InversedMV, bool fast)
+void RayCaster::_drawVolumeFaces(int whichPass, int castingMode, const std::vector<size_t> &cameraCellIdx, const glm::mat4 &InversedMV, bool fast)
 {
     assert(cameraCellIdx.size() == 0 || cameraCellIdx.size() == 3);
     bool insideVolume = (cameraCellIdx.size() == 3);
@@ -719,7 +717,8 @@ void RayCaster::_drawVolumeFaces(int whichPass, int castingMode,
         _3rdPassShader->SetUniform("Projection", projection);
         _3rdPassShader->SetUniform("inversedMV", InversedMV);
         if (castingMode == CellTraversal) {
-            glm::ivec3 entryCellIdx(-1);
+            // Upload entryCellIdx, no matter inside or outside of the volume
+            glm::ivec3 entryCellIdx(0);
             if (insideVolume) {
                 entryCellIdx.x = int(cameraCellIdx[0]);
                 entryCellIdx.y = int(cameraCellIdx[1]);
