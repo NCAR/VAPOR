51b58746eb9c3410b58fc51451d4b336e41886bb
sgpearse <pearse@ucar.edu>
2019-01-17 11:24:31 -0700

    Issue 1214 (#1286) Fix multiple dataset domain
    * Initial commit
    
    * Incremental
    
    * incremental
    
    * Incremental
    
    * Cleanup time
    
    * Fixed problem with axis annotations and in-scene geometry
    
    * cleanup
    
    * Partial fix
    
    * Removed rewritten function
    
    * Fix for broken build
    
    * Incremental commit to address code review
    
    * Update for code review

--- include/vapor/AnnotationRenderer.h
+++ include/vapor/AnnotationRenderer.h
@@ -24,6 +24,7 @@
 #include <vapor/Renderer.h>
 #include <vapor/Transform.h>
 #include <vapor/DataMgrUtils.h>
+#include <vapor/GLManager.h>
 
 namespace VAPoR {
 
@@ -94,8 +95,17 @@ private:
     void _drawAxes(std::vector<double> min, std::vector<double> max, std::vector<double> origin, std::vector<double> color, double width);
     void _drawTic(double startPosn[], double endPosn[], double width, std::vector<double> color);
 
-    //! Render the domain fram
-    void drawDomainFrame(size_t ts) const;
+    void _makeTransformMatrix(const Transform *transform, glm::mat4 &transformMatrix) const;
+
+    void _applyDataMgrCornerToDomain(std::vector<double> *domainExtents, const glm::vec4 *dataMgrCorner, const glm::mat4 &transformMatrix) const;
+
+    void _getDataMgrCorner(const int cornerNumber, glm::vec4 *dataMgrCorner, const std::vector<double> &minDataMgrExtents, const std::vector<double> &maxDataMgrExtents) const;
+
+    void _applyDataMgrToDomainExtents(std::vector<double> &domainExtents, const std::vector<double> &dataMgrMinExts, const std::vector<double> &dataMgrMaxExts, const Transform *transform) const;
+
+    void _calculateDomainExtents(std::vector<double> &domainExtents) const;
+
+    void drawDomainFrame(const std::vector<double> corners) const;
 
     std::vector<double> getDomainExtents() const;
     AxisAnnotation *    getCurrentAxisAnnotation();
@@ -109,7 +119,9 @@ private:
 
     // Draw the axis lines, while building text labels.
     //
-    void       drawAxisTics(AxisAnnotation *aa = NULL);
+    void drawAxisTics(AxisAnnotation *aa = NULL);
+    void drawAxisTics(AxisAnnotation *aa, std::vector<double> minTic, std::vector<double> maxTic);
+
     void       applyTransform(Transform *t);
     void       renderText(double text, double coords[], AxisAnnotation *aa = NULL);
     Transform *getTransform(string dataMgr = "");
@@ -119,7 +131,7 @@ private:
 
     // Draw Axis arrows
     //
-    void drawAxisArrows(std::vector<double> minExts, std::vector<double> maxExts);
+    void drawAxisArrows(std::vector<double> minExts, std::vector<double> maxExts, Transform *transform);
 
 #ifdef VAPOR3_0_0_ALPHA
     //! Static method to convert axis coordinates between user and lat-lon
