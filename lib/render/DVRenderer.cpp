ae79d84f7c0c81335d8cc1bfa1f852951cfbe3da
Samuel Li <shaomenglee@gmail.com>
2019-01-17 15:44:30 -0700

    function signature change

--- lib/render/DVRenderer.cpp
+++ lib/render/DVRenderer.cpp
@@ -53,9 +53,10 @@ void DVRenderer::_3rdPassSpecialHandling( bool fast, int castingMode ) const
     //   though I don't know why...
 }
 
-void DVRenderer::_colormapSpecialHandling( RayCasterParams* params )
+void DVRenderer::_colormapSpecialHandling( )
 {
     // Get colormap for the primary variable
+    DVRParams*             params     = dynamic_cast<DVRParams*>( GetActiveParams() );
     VAPoR::MapperFunction* mapperFunc = params->GetMapperFunc();
     mapperFunc->makeLut( _colorMap );
     assert( _colorMap.size()  % 4 == 0 );
