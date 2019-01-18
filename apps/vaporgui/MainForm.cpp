9bc168543469273d7fc763405309b2df26670407
Scott Pearse <pearse@ucar.edu>
2019-01-18 08:43:51 -0700

    incremental

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -957,10 +957,20 @@ void MainForm::_createCaptureMenu() {
 		_captureSingleJpegCaptureAction, SIGNAL(triggered()),
 		this, SLOT (captureSingleJpeg())
 	);
+	connect (
+		_captureSinglePngCaptureAction, SIGNAL(triggered()),
+		this, SLOT (captureSinglePng())
+	);
+	connect (
+		_captureSingleTiffCaptureAction, SIGNAL(triggered()),
+		this, SLOT (captureSingleTiff())
+	);
+
 	connect( 
 		_captureStartJpegCaptureAction, SIGNAL( triggered() ),
 		this, SLOT( startAnimCapture() ) 
 	);
+
 	connect( 
 		_captureEndJpegCaptureAction, SIGNAL( triggered() ),
 		this, SLOT( endAnimCapture() ) 
@@ -2169,18 +2179,22 @@ void MainForm::enableAnimationWidgets(bool on) {
 	}
 }
 
+void MainForm::captureSingleJpeg() {
+    string filter = "JPG (*.jpg *.jpeg");
+}
 
 //Capture just one image
 //Launch a file save dialog to specify the names
 //
-void MainForm::captureSingleJpeg() {
+//void MainForm::captureSingleJpeg() {
+void MainForm::captureSingleImage(string filter) {
 	showCitationReminder();
 	SettingsParams *sP = GetSettingsParams();
 	string imageDir = sP->GetImageDir();
 	if (imageDir=="") imageDir = sP->GetDefaultImageDir();
     
     string fileDialogOptions;
-    ViewpointParams *VPP = _paramsMgr->GetViewpointParams(GetStateParams()->GetActiveVizName());
+    /*ViewpointParams *VPP = _paramsMgr->GetViewpointParams(GetStateParams()->GetActiveVizName());
     if (VPP->GetProjectionType() == ViewpointParams::MapOrthographic) {
         fileDialogOptions = ""
         "TIFF (*.tif *.tiff)\n"
@@ -2191,20 +2205,23 @@ void MainForm::captureSingleJpeg() {
         "PNG (*.png)\n"
         "JPG (*.jpg *.jpeg)\n"
         "TIFF (*.tif *.tiff)";
-    }
+    }*/
 
 	QFileDialog fileDialog(this,
 		"Specify single image capture file name",
 		imageDir.c_str(),
-		QString::fromStdString(fileDialogOptions));
+        QString::fromStdString(filter)
+    );
+		//QString::fromStdString(fileDialogOptions));
     
 	fileDialog.setAcceptMode(QFileDialog::AcceptSave);
 	fileDialog.move(pos());
 	fileDialog.resize(450,450);
     QStringList mimeTypeFilters;
-    mimeTypeFilters << "image/jpeg" // will show "JPEG image (*.jpeg *.jpg *.jpe)
+    /*mimeTypeFilters << "image/jpeg" // will show "JPEG image (*.jpeg *.jpg *.jpe)
     << "image/png"  // will show "PNG image (*.png)"
     << "application/octet-stream"; // will show "All files (*)"
+    */
     //fileDialog.setNameFilters(mimeTypeFilters);
 	if (fileDialog.exec() != QDialog::Accepted) return;
 	
