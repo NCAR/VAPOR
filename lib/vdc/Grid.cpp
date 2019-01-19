12a414b0b3aa3be6444964dd91d93d786382a47b
John Clyne <clyne@ucar.edu>
2019-01-18 18:39:06 -0700

    Fixed 1331

--- lib/vdc/Grid.cpp
+++ lib/vdc/Grid.cpp
@@ -60,13 +60,13 @@ Grid::Grid(
 }
 
 
-float Grid::AccessIndex(const std::vector <size_t> &indices) const {
+float Grid::AccessIndex(const size_t indices[3]) const {
 	float *fptr = AccessIndex(_blks, indices);
 	if (! fptr) return(GetMissingValue());
 	return (*fptr);
 }
 
-void Grid::SetValue(const std::vector <size_t> &indices, float v) {
+void Grid::SetValue(const size_t indices[3], float v) {
 	float *fptr = AccessIndex(_blks, indices);
 	if (! fptr) return;
 	*fptr = v;
@@ -74,18 +74,18 @@ void Grid::SetValue(const std::vector <size_t> &indices, float v) {
 
 float *Grid::AccessIndex(
 	const std::vector <float *> &blks,
-	const std::vector <size_t> &indices
+	const size_t indices[3]
 ) const {
 
-	std::vector <size_t> cIndices = indices;
-	ClampIndex(cIndices);
+	size_t cIndices[3];
+	ClampIndex(indices, cIndices);
 
 	size_t bs[] = {0,0,0};
 	size_t bdims[] = {0,0,0};
 
 	if (! blks.size()) return(NULL);
 
-	vector <size_t> dims = GetDimensions();
+	const vector <size_t> &dims = GetDimensions();
 	size_t ndim = dims.size();
 	for (int i=0; i<ndim; i++) {
 		bs[i] = _bs[i];
@@ -105,7 +105,7 @@ float *Grid::AccessIndex(
 }
 
 float Grid::AccessIJK(size_t i, size_t j, size_t k) const {
-    std::vector <size_t> indices = {i,j,k};
+    size_t indices[] = {i,j,k};
     return(AccessIndex(indices));
 }
 
