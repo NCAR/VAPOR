05f4cf2b05584431133b9dc53c3e3ff9400dbce6
Scott Pearse <pearse@ucar.edu>
2019-01-24 09:25:38 -0700

    cleanup

--- apps/vaporgui/GeometryWidget.cpp
+++ apps/vaporgui/GeometryWidget.cpp
@@ -123,17 +123,16 @@ void GeometryWidget::adjustLayoutToPlanarXY(bool reinit)
 
     if (!_rParams) return;
 
-    std::vector<double> minExt(3, 0);
-    std::vector<double> maxExt(3, 1);
-    Box *               box = _rParams->GetBox();
     if (reinit) {
-        cout << "reinitXY" << endl;
+        std::vector<double> minExt(3, 0);
+        std::vector<double> maxExt(3, 1);
         getFullExtents(minExt, maxExt);
         double average = (minExt[Z] + maxExt[Z]) / 2.f;
         _zSinglePoint->SetValue(average);
 
         minExt[Z] = average;
         maxExt[Z] = average;
+        Box *box = _rParams->GetBox();
         box->SetExtents(minExt, maxExt);
     }
 }
@@ -149,17 +148,17 @@ void GeometryWidget::adjustLayoutToPlanarXZ(bool reinit)
     _zPointFrame->hide();
 
     if (!_rParams) return;
-    std::vector<double> minExt(3, 0);
-    std::vector<double> maxExt(3, 1);
-    Box *               box = _rParams->GetBox();
+
     if (reinit) {
-        cout << "reinitXZ" << endl;
+        std::vector<double> minExt(3, 0);
+        std::vector<double> maxExt(3, 1);
         getFullExtents(minExt, maxExt);
         double average = (minExt[Y] + maxExt[Y]) / 2.f;
         _ySinglePoint->SetValue(average);
 
         minExt[Y] = average;
         maxExt[Y] = average;
+        Box *box = _rParams->GetBox();
         box->SetExtents(minExt, maxExt);
     }
 }
@@ -175,17 +174,16 @@ void GeometryWidget::adjustLayoutToPlanarYZ(bool reinit)
     _zPointFrame->hide();
 
     if (!_rParams) return;
-    std::vector<double> minExt(3, 0);
-    std::vector<double> maxExt(3, 1);
-    Box *               box = _rParams->GetBox();
     if (reinit) {
-        cout << "reinitYZ" << endl;
+        std::vector<double> minExt(3, 0);
+        std::vector<double> maxExt(3, 1);
         getFullExtents(minExt, maxExt);
         double average = (minExt[X] + maxExt[X]) / 2.f;
         _xSinglePoint->SetValue(average);
 
         minExt[X] = average;
         maxExt[X] = average;
+        Box *box = _rParams->GetBox();
         box->SetExtents(minExt, maxExt);
     }
 }
@@ -193,9 +191,6 @@ void GeometryWidget::adjustLayoutToPlanarYZ(bool reinit)
 void GeometryWidget::adjustLayoutTo2D()
 {
     _zFrame->hide();
-    //_minMaxContainerWidget->adjustSize();
-    //_minMaxTab->adjustSize();
-
     adjustSize();
 }
 
