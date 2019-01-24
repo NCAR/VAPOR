747330347b366af7efe92103257d24aab74aa8bf
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-24 11:08:17 -0700

    init

---
+++ apps/vaporgui/VolumeEventRouter.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <qobject.h>
+#include <vapor/MyBase.h>
+#include "GL/glew.h"
+#include "vapor/VolumeRenderer.h"
+#include "vapor/VolumeParams.h"
+#include "RenderEventRouter.h"
+#include "VariablesWidget.h"
+#include "VolumeSubtabs.h"
+
+QT_USE_NAMESPACE
+
+namespace VAPoR {
+class ControlExec;
+}
+
+class GLVolumeImageWindow;
+
+//!
+//! \class VolumeEventRouter
+//! \ingroup Public_GUI
+//! \brief An EventRouter subclass that handles the Volume tab in the GUI
+//! \author John Clyne
+//! \version 3.0
+//! \date  June 2018
+
+//!	The VolumeEventRouter class manages the Volume gui.
+//! There are three sub-tabs,
+//! for variables, geometry, and appearance.
+
+class VolumeEventRouter : public QTabWidget, public RenderEventRouter {
+
+    Q_OBJECT
+
+  public:
+    VolumeEventRouter(
+        QWidget *parent, VAPoR::ControlExec *ce);
+    ~VolumeEventRouter();
+
+    void GetWebHelp(
+        vector<pair<string, string>> &help) const;
+
+    //
+    static string GetClassType() {
+        return (VAPoR::VolumeRenderer::GetClassType());
+    }
+    string GetType() const { return GetClassType(); }
+
+  protected:
+    void _updateTab();
+    virtual string _getDescription() const;
+
+    virtual string _getSmallIconImagePath() const {
+        return ("WireFrame_small.png");
+    }
+    virtual string _getIconImagePath() const {
+        return ("WireFrame.png");
+    }
+
+  private:
+    VolumeEventRouter() {}
+
+    //! Override default wheel behavior on the tab.  This has the effect of
+    //! ignoring wheel events over the tab.  This is because wheel events will always
+    //! affect the combo boxes and other widgets in the tab, and it would be confusing
+    //! if wheel events also scrolled the tab itself
+    void wheelEvent(QWheelEvent *) {}
+
+    //! VariablesWidget is used as Variables tab
+    VolumeVariablesSubtab *_variables;
+    VolumeGeometrySubtab *_geometry;
+    GLVolumeImageWindow *_glVolumeImageWindow;
+    VolumeAppearanceSubtab *_appearance;
+    VolumeAnnotationSubtab *_annotation;
+};
---
+++ apps/vaporgui/VolumeSubtabs.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "ui_VolumeAppearanceGUI.h"
+#include "ui_VolumeVariablesGUI.h"
+#include "ui_VolumeGeometryGUI.h"
+#include "ui_VolumeAnnotationGUI.h"
+#include "Flags.h"
+
+namespace VAPoR {
+class ControlExec;
+class RenderParams;
+class ParamsMgr;
+class DataMgr;
+} // namespace VAPoR
+
+class VolumeVariablesSubtab : public QWidget, public Ui_VolumeVariablesGUI {
+
+    Q_OBJECT
+
+  public:
+    VolumeVariablesSubtab(QWidget *parent) {
+        setupUi(this);
+        _variablesWidget->Reinit(
+            (VariableFlags)(SCALAR | HEIGHT),
+            (DimFlags)(THREED | TWOD));
+    }
+
+    void Update(
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::RenderParams *rParams) {
+        _variablesWidget->Update(dataMgr, paramsMgr, rParams);
+    }
+};
+
+class VolumeAppearanceSubtab : public QWidget, public Ui_VolumeAppearanceGUI {
+
+    Q_OBJECT
+
+  public:
+    VolumeAppearanceSubtab(QWidget *parent) {
+        setupUi(this);
+        _TFWidget->Reinit((TFFlags)(CONSTANT_COLOR));
+    }
+
+    void Update(
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::RenderParams *rParams) {
+        _TFWidget->Update(dataMgr, paramsMgr, rParams);
+    }
+};
+
+class VolumeGeometrySubtab : public QWidget, public Ui_VolumeGeometryGUI {
+
+    Q_OBJECT
+
+  public:
+    VolumeGeometrySubtab(QWidget *parent) {
+        setupUi(this);
+        _geometryWidget->Reinit(
+            (DimFlags)THREED,
+            (VariableFlags)SCALAR);
+    }
+
+    void Update(
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::RenderParams *rParams) {
+        _geometryWidget->Update(paramsMgr, dataMgr, rParams);
+        _copyRegionWidget->Update(paramsMgr, rParams);
+        _transformTable->Update(rParams->GetTransform());
+    }
+};
+
+class VolumeAnnotationSubtab : public QWidget, public Ui_VolumeAnnotationGUI {
+
+    Q_OBJECT
+
+  public:
+    VolumeAnnotationSubtab(QWidget *parent) {
+        setupUi(this);
+    }
+
+    void Update(
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::RenderParams *rParams) {
+        _colorbarWidget->Update(dataMgr, paramsMgr, rParams);
+    }
+};
