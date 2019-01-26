3b9265d9f0f127544a7c57fb23508a1cf8bf28d0
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-26 00:01:12 -0700

    Fixed sampling position

--- lib/render/VolumeResampled.cpp
+++ lib/render/VolumeResampled.cpp
@@ -16,11 +16,11 @@ int VolumeResampled::LoadData(const Grid *grid) {
     grid->GetUserExtents(min, max);
 
     for (int z = 0; z < d; z++) {
-        const float zSamplePos = z / (float)d * (max[2] - min[2]) + min[2];
+        const float zSamplePos = (z + 0.5f) / (float)d * (max[2] - min[2]) + min[2];
         for (int y = 0; y < h; y++) {
-            const float ySamplePos = y / (float)h * (max[1] - min[1]) + min[1];
+            const float ySamplePos = (y + 0.5f) / (float)h * (max[1] - min[1]) + min[1];
             for (int x = 0; x < w; x++) {
-                const float xSamplePos = x / (float)w * (max[0] - min[0]) + min[0];
+                const float xSamplePos = (x + 0.5f) / (float)w * (max[0] - min[0]) + min[0];
                 data[z * w * h + y * w + x] = grid->GetValue(xSamplePos, ySamplePos, zSamplePos);
             }
         }
