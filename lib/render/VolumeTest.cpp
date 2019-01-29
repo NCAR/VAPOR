09299dfc373534a735dc3148e93a2b2b06b2d674
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-28 17:21:16 -0700

    inc

--- lib/render/VolumeTest.cpp
+++ lib/render/VolumeTest.cpp
@@ -7,19 +7,42 @@ using std::vector;
 
 using namespace VAPoR;
 
-int VolumeTest::LoadData(const Grid *grid) {
-    vector<size_t> dims = grid->GetDimensions();
-    float *data = new float[dims[0] * dims[1] * dims[2]];
+VolumeTest::VolumeTest() {
+    glGenTextures(1, &zCoordTexture);
+    glBindTexture(GL_TEXTURE_3D, zCoordTexture);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
-    size_t i = 0;
-    auto end = grid->cend();
-    for (auto it = grid->cbegin(); it != end; ++it, ++i) {
-        data[i] = *it;
+    glGenTextures(1, &xyCoordTexture);
+    glBindTexture(GL_TEXTURE_2D, xyCoordTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
+VolumeTest::~VolumeTest() {
+    if (zCoordTexture)
+        glDeleteTextures(1, &zCoordTexture);
+    if (xyCoordTexture)
+        glDeleteTextures(1, &xyCoordTexture);
+}
+
+int VolumeTest::LoadData(const Grid *grid) {
+    if (VolumeRegular::LoadData(grid) < 0)
+        return -1;
+
+    vector<size_t> dims = grid->GetDimensions();
+    float *xy = new float[dims[0] * dims[1]];
+
+    auto end = grid->ConstCoordEnd();
+    for (auto it = grid->ConstCoordBegin(); it != end; ++it) {
+        // it.
     }
 
-    glTexImage3D(GL_TEXTURE_3D, 0, GL_R32F, dims[0], dims[1], dims[2], 0, GL_RED, GL_FLOAT, data);
-
-    delete[] data;
     return 0;
 }
 
