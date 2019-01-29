4a7fe0bae5cd22e7a7f17b2c9a96705880707778
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-28 17:21:16 -0700

    inc

--- include/vapor/VolumeTest.h
+++ include/vapor/VolumeTest.h
@@ -1,13 +1,19 @@
 #pragma once
 
-#include <vapor/VolumeAlgorithm.h>
+#include <vapor/VolumeRegular.h>
 
 namespace VAPoR {
 
-class VolumeTest : public VolumeAlgorithm {
+class VolumeTest : public VolumeRegular {
   public:
+    VolumeTest();
+    ~VolumeTest();
     virtual int LoadData(const Grid *grid);
     virtual ShaderProgram *GetShader(ShaderManager *sm);
+
+  private:
+    unsigned int xyCoordTexture;
+    unsigned int zCoordTexture;
 };
 
 } // namespace VAPoR
