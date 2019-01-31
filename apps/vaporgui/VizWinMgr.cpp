7fb7e692e811011917fae2de83d1b2162c01f703
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-31 16:29:43 -0700

    Improved GL version checking

--- apps/vaporgui/VizWinMgr.cpp
+++ apps/vaporgui/VizWinMgr.cpp
@@ -105,8 +105,12 @@ void VizWinMgr::_attachVisualizer(string vizName) {
 
     QString qvizname = QString::fromStdString(vizName);
 
+    int major = 4, minor = 1;
+    if ((QGLFormat::openGLVersionFlags() & QGLFormat::OpenGL_Version_3_3) == 0)
+        major = minor = 1;
+
     QGLFormat glFormat;
-    glFormat.setVersion(4, 1);
+    glFormat.setVersion(major, minor);
     glFormat.setProfile(QGLFormat::CoreProfile);
 
     _vizWindow[vizName] = new VizWin(
