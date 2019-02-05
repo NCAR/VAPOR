2a408c0cbea1c21800620acdb6cb3fb75fd911e0
Samuel Li <shaomeng@ucar.edu>
2019-02-04 17:53:55 -0700

    Disabled GUI on Intel

--- apps/vaporgui/DVRSubtabs.h
+++ apps/vaporgui/DVRSubtabs.h
@@ -38,17 +38,27 @@ public:
     {
         _dvrParams = dynamic_cast<VAPoR::DVRParams*>( params );
         assert( _dvrParams );
-        long mode = _dvrParams->GetCastingMode();
-        _castingModeComboBox->setCurrentIndex( mode - 1 );
         _variablesWidget->Update(dataMgr, paramsMgr, params);
-        if( mode == 1 )
+
+        long mode = _dvrParams->GetCastingMode();
+        if(  mode == 10 )   // Intel GPU
         {
-            _sampleRateComboBox->setEnabled( true );
-            _sampleRateComboBox->setCurrentIndex( _dvrParams->GetSampleRateMultiplier() );
+            _castingModeComboBox->setCurrentIndex( 0 );
+            _castingModeComboBox->setEnabled( false );
+            _dvrParams->SetCastingMode( 1 );    // Set to fixed-step ray casting
         }
-        else 
+        else
         {
-            _sampleRateComboBox->setEnabled( false );
+            _castingModeComboBox->setCurrentIndex( mode - 1 );
+            if( mode == 1 )
+            {
+                _sampleRateComboBox->setEnabled( true );
+                _sampleRateComboBox->setCurrentIndex( _dvrParams->GetSampleRateMultiplier() );
+            }
+            else 
+            {
+                _sampleRateComboBox->setEnabled( false );
+            }
         }
     }
 
