5f04773fb6b1bafecc0195cd1beb0e22c8569876
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-02-04 20:14:41 -0700

    Finished two level bb

--- lib/render/VolumeCellTraversal.cpp
+++ lib/render/VolumeCellTraversal.cpp
@@ -355,7 +355,7 @@ int VolumeCellTraversal::LoadData(const Grid *grid)
     GL_ERR_BREAK();
     glBindTexture(GL_TEXTURE_2D, BBLevelDimTexture);
     GL_ERR_BREAK();
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32I, levels, 6, 0, GL_RG_INTEGER, GL_INT, mipDims);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32I, 6, levels, 0, GL_RG_INTEGER, GL_INT, mipDims);
     GL_ERR_BREAK();
 
     for (int level = 1; level < levels; level++) {
