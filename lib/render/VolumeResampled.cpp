85dda120cffb5c59e21a698bed4521606b2e2676
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-07 13:16:58 -0700

    Updated resampled renderer

--- lib/render/VolumeResampled.cpp
+++ lib/render/VolumeResampled.cpp
@@ -10,7 +10,8 @@ using namespace VAPoR;
 static VolumeAlgorithmRegistrar<VolumeResampled> registration;
 
 int VolumeResampled::LoadData(const Grid *grid) {
-#define S 5
+    VolumeRegular::LoadData(grid);
+#define S 1
     const vector<size_t> dims = grid->GetDimensions();
     const size_t w = dims[0] * S, h = dims[1] * S, d = dims[2] * S;
     float *data = new float[w * h * d];
@@ -31,6 +32,26 @@ int VolumeResampled::LoadData(const Grid *grid) {
     }
 
     glTexImage3D(GL_TEXTURE_3D, 0, GL_R32F, w, h, d, 0, GL_RED, GL_FLOAT, data);
+
+    hasMissingData = grid->HasMissingData();
+    if (hasMissingData) {
+        printf("Loading missing data...\n");
+        const float missingValue = grid->GetMissingValue();
+        unsigned char *missingMask = new unsigned char[w * h * d];
+        memset(missingMask, 0, w * h * d);
+
+        for (size_t i = 0; i < w * h * d; i++)
+            if (data[i] == missingValue)
+                missingMask[i] = 255;
+
+        glBindTexture(GL_TEXTURE_3D, missingTexture);
+        glTexImage3D(GL_TEXTURE_3D, 0, GL_R32F, 0, 0, 0, 0, GL_RED, GL_UNSIGNED_BYTE, NULL); // Fix driver bug with re-uploading large textures
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexImage3D(GL_TEXTURE_3D, 0, GL_R8, dims[0], dims[1], dims[2], 0, GL_RED, GL_UNSIGNED_BYTE, missingMask);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
+        delete[] missingMask;
+    }
 
     delete[] data;
     return 0;
