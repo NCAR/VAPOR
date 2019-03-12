f5c450e44365862a78f6a7283f09bfabeea58bd1
Scott Pearse <pearse@ucar.edu>
2019-03-12 14:38:51 -0600

    Updates to FlowSubtabs

---
+++ apps/vaporgui/FlowSubtabs.cpp
@@ -0,0 +1,147 @@
+#include "FlowSubtabs.h"
+#include "VariablesWidget.h"
+#include "TFWidget.h"
+#include "GeometryWidget.h"
+#include "CopyRegionWidget.h"
+#include "TransformTable.h"
+#include "ColorbarWidget.h"
+//#include "VaporWidgets.h"
+
+QVaporSubtab::QVaporSubtab(QWidget* parent) : QWidget(parent)
+{
+    _layout = new QVBoxLayout(this);
+    _layout->setContentsMargins(0,0,0,0);
+    _layout->insertSpacing(-1, 20);
+    setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Maximum);
+}
+
+FlowVariablesSubtab::FlowVariablesSubtab(QWidget* parent) : QVaporSubtab(parent)
+{
+    _variablesWidget = new VariablesWidget(this);
+    _variablesWidget->Reinit(   (VariableFlags)(VECTOR | COLOR),
+                                (DimFlags)(THREED) );
+
+    _layout->addWidget( _variablesWidget, 0, 0 );
+}
+
+void FlowVariablesSubtab::Update(   VAPoR::DataMgr *dataMgr,
+                                    VAPoR::ParamsMgr *paramsMgr,
+                                    VAPoR::RenderParams *rParams) 
+{
+    _variablesWidget->Update(dataMgr, paramsMgr, rParams);
+}
+
+FlowAppearanceSubtab::FlowAppearanceSubtab(QWidget* parent) : QVaporSubtab(parent)
+{
+    _TFWidget = new TFWidget(this);
+    _TFWidget->Reinit((TFFlags)(SAMPLING));
+
+    _layout->addWidget( _TFWidget, 0, 0 );
+
+    _params = NULL;
+}
+
+void FlowAppearanceSubtab::Update(  VAPoR::DataMgr *dataMgr,
+                                    VAPoR::ParamsMgr *paramsMgr,
+                                    VAPoR::RenderParams *rParams) 
+{
+    _params = dynamic_cast<VAPoR::FlowParams*>(rParams);
+    assert(_params);
+
+    _TFWidget->Update(dataMgr, paramsMgr, rParams);
+}
+
+FlowSeedingSubtab::FlowSeedingSubtab(QWidget* parent) : QVaporSubtab(parent)
+{
+    _geometryWidget   = new GeometryWidget(this);
+    _geometryWidget->Reinit( 
+        (DimFlags)THREED,
+        (VariableFlags)VECTOR
+        //(GeometryFlags)RAKE_HACK
+    );
+    _layout->addWidget( _geometryWidget );
+   
+/*    _pushTest = new VPushButton(this, "testLabel", "testButton"); 
+    connect( _pushTest, SIGNAL( _pressed() ),
+        this, SLOT( _pushTestPressed()));
+    _layout->addWidget( _pushTest );
+
+    _comboTest = new VComboBox(this);//, "testCombo");
+    _comboTest->AddOption( "foo" );
+    _comboTest->AddOption( "bar" );
+    _comboTest->AddOption( "baz" );
+    connect( _comboTest, SIGNAL( _indexChanged(int) ),
+        this, SLOT( _comboBoxSelected(int) ));
+    _layout->addWidget( _comboTest );
+
+    _checkboxTest = new VCheckBox(this, "testCheckbox");
+    connect( _checkboxTest, SIGNAL( _checkboxClicked() ),
+        this, SLOT( _checkBoxSelected() ) );
+    _layout->addWidget( _checkboxTest );*/
+}
+
+void FlowSeedingSubtab::Update(
+        VAPoR::DataMgr *dataMgr,
+        VAPoR::ParamsMgr *paramsMgr,
+        VAPoR::RenderParams *rParams
+    )
+{
+    _geometryWidget->Update(paramsMgr, dataMgr, rParams);
+}
+
+void FlowSeedingSubtab::_pushTestPressed() 
+{
+    cout << "Push button pressed" << endl;
+}
+
+void FlowSeedingSubtab::_comboBoxSelected( int index ) 
+{
+    string option = _comboTest->GetCurrentText();
+    cout << "Combo selected at index " << index << " for option " << option << endl;
+}
+
+void FlowSeedingSubtab::_checkBoxSelected() 
+{
+    bool checked = _checkboxTest->GetCheckState();
+    cout << "Checkbox is checked? " << checked << endl;
+}
+
+FlowGeometrySubtab::FlowGeometrySubtab(QWidget* parent) : QVaporSubtab(parent)
+{
+    _geometryWidget   = new GeometryWidget(this);
+    _copyRegionWidget = new CopyRegionWidget(this);
+    _transformTable   = new TransformTable(this);
+    _geometryWidget->Reinit( 
+        (DimFlags)THREED,
+        (VariableFlags)VECTOR
+    );
+
+    _layout->addWidget( _geometryWidget, 0 ,0 );
+    _layout->addWidget( _copyRegionWidget, 0 ,0 );
+    _layout->addWidget( _transformTable, 0 ,0 );
+
+    _params = NULL;
+}
+
+void FlowGeometrySubtab::Update( VAPoR::ParamsMgr *paramsMgr,
+                                 VAPoR::DataMgr *dataMgr,
+                                 VAPoR::RenderParams *rParams) 
+{
+    _geometryWidget->Update(paramsMgr, dataMgr, rParams);
+    _copyRegionWidget->Update(paramsMgr, rParams);
+    _transformTable->Update(rParams->GetTransform());
+}
+
+
+FlowAnnotationSubtab::FlowAnnotationSubtab(QWidget* parent) : QVaporSubtab(parent)
+{
+    _colorbarWidget = new ColorbarWidget(this);
+    _layout->addWidget(_colorbarWidget, 0, 0);
+}
+
+void FlowAnnotationSubtab::Update(  VAPoR::ParamsMgr *paramsMgr,
+                                    VAPoR::DataMgr *dataMgr,
+                                    VAPoR::RenderParams *rParams) 
+{
+    _colorbarWidget->Update(dataMgr, paramsMgr, rParams);
+}
---
+++ apps/vaporgui/FlowSubtabs.h
@@ -0,0 +1,136 @@
+#ifndef FLOWSUBTABS_H
+#define FLOWSUBTABS_H
+
+#include "Flags.h"
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <vapor/FlowParams.h>
+
+namespace VAPoR {
+	class ControlExec;
+	class RenderParams;
+	class ParamsMgr;
+	class DataMgr;
+}
+
+class VariablesWidget;
+class TFWidget;
+class GeometryWidget;
+class CopyRegionWidget;
+class TransformTable;
+class ColorbarWidget;
+class VPushButton;
+class VComboBox;
+class VCheckBox;
+
+class QVaporSubtab : public QWidget {
+    Q_OBJECT
+
+public:
+    QVaporSubtab(QWidget* parent);
+
+protected:
+    QVBoxLayout* _layout;
+};
+
+class FlowVariablesSubtab : public QVaporSubtab {
+
+	Q_OBJECT
+
+public:
+	FlowVariablesSubtab(QWidget* parent);
+
+	void Update(
+		VAPoR::DataMgr *dataMgr,
+		VAPoR::ParamsMgr *paramsMgr,
+		VAPoR::RenderParams *rParams
+	);
+
+private:
+    VAPoR::FlowParams*      _params;
+    VariablesWidget* _variablesWidget;
+};
+
+class FlowAppearanceSubtab : public QVaporSubtab {
+
+	Q_OBJECT
+
+public:
+	FlowAppearanceSubtab(QWidget* parent);
+
+	void Update(
+		VAPoR::DataMgr *dataMgr,
+		VAPoR::ParamsMgr *paramsMgr,
+		VAPoR::RenderParams *rParams
+	);
+
+private:
+    VAPoR::FlowParams* _params;
+    TFWidget*   _TFWidget;
+};
+
+class FlowSeedingSubtab : public QVaporSubtab {
+
+	Q_OBJECT
+
+public:
+	FlowSeedingSubtab(QWidget* parent);
+
+	void Update(
+		VAPoR::DataMgr *dataMgr,
+		VAPoR::ParamsMgr *paramsMgr,
+		VAPoR::RenderParams *rParams
+	);
+
+protected slots:
+    void _pushTestPressed();
+    void _comboBoxSelected( int index );
+    void _checkBoxSelected();
+
+private:
+    VAPoR::FlowParams*      _params;
+    GeometryWidget*         _geometryWidget;
+    VPushButton*            _pushTest;
+    VComboBox*              _comboTest;
+    VCheckBox*              _checkboxTest;
+};
+
+class FlowGeometrySubtab : public QVaporSubtab {
+
+	Q_OBJECT
+
+public:
+	FlowGeometrySubtab(QWidget* parent);
+	
+	void Update(
+		VAPoR::ParamsMgr *paramsMgr,
+		VAPoR::DataMgr *dataMgr,
+		VAPoR::RenderParams *rParams
+	); 
+
+private:
+    VAPoR::FlowParams*     _params;
+    GeometryWidget*         _geometryWidget;
+    CopyRegionWidget*       _copyRegionWidget;
+    TransformTable*         _transformTable;
+};
+
+class FlowAnnotationSubtab : public QVaporSubtab {
+
+	Q_OBJECT
+
+public:
+	FlowAnnotationSubtab(QWidget* parent);
+	
+	void Update(
+		VAPoR::ParamsMgr *paramsMgr,
+		VAPoR::DataMgr *dataMgr,
+		VAPoR::RenderParams *rParams
+	);
+
+private:
+    ColorbarWidget* _colorbarWidget;
+};
+
+#endif //FLOWSUBTABS_H
