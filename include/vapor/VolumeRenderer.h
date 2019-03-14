dab14a4c702eba039ed8454d140e8ad7aa8d2c63
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-13 21:20:07 -0600

    smarter fast render selection

--- include/vapor/VolumeRenderer.h
+++ include/vapor/VolumeRenderer.h
@@ -49,6 +49,8 @@ protected:
     unsigned int LUTTexture;
     unsigned int depthTexture;
     
+    double lastRenderTime;
+    
     struct Cache {
         string var = "";
         size_t ts = -1;
