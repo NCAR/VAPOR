20249734e5b433fffe62ff37977439393c919103
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-13 21:28:06 -0600

    constant color default

--- lib/params/VolumeParams.cpp
+++ lib/params/VolumeParams.cpp
@@ -128,4 +128,8 @@ void VolumeParams::Register(const std::string &name, Type type)
 }
 
 // Set everything to default values
-void VolumeParams::_init() { SetDiagMsg("VolumeParams::_init()"); }
+void VolumeParams::_init()
+{
+    SetDiagMsg("VolumeParams::_init()");
+    SetUseSingleColor(true);
+}
