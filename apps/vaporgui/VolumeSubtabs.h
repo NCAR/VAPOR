bb8c31d1997570ef17c10ad2378f4e50c759ace2
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-15 14:07:14 -0600

    remove constant color from DVR

--- apps/vaporgui/VolumeSubtabs.h
+++ apps/vaporgui/VolumeSubtabs.h
@@ -42,7 +42,7 @@ public:
     {
         setupUi(this);
         _TFWidget->SetOpacityIntegrated(true);
-        _TFWidget->Reinit((TFFlags)(CONSTANT_COLOR));
+        _TFWidget->Reinit((TFFlags)(0));
     }
 
     void Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr, VAPoR::RenderParams *rParams);
