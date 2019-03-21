bc11f6530da2e2b76fc6a6a765fddb2830eaffe1
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-21 16:32:17 -0600

    Depth

--- lib/render/VolumeRenderer.cpp
+++ lib/render/VolumeRenderer.cpp
@@ -247,6 +247,7 @@ int VolumeRenderer::_paintGL(bool fast) {
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     glDepthMask(GL_TRUE);
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_ALWAYS);
 
     void *start = GLManager::BeginTimer();
     if (algorithm->IsSlow()) {
@@ -262,6 +263,7 @@ int VolumeRenderer::_paintGL(bool fast) {
     lastRenderTime = GLManager::EndTimer(start);
     printf("Render time = %f\n", lastRenderTime);
 
+    glDepthFunc(GL_LESS);
     glDisable(GL_BLEND);
     GL_ERR_BREAK();
 
