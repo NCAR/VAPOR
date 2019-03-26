df21e1415422d1108e180ebb079c0a5f63298782
Samuel Li <shaomenglee@gmail.com>
2019-03-26 13:49:38 -0600

    WIP VaporField, move to fix compilation

--- include/vapor/VaporField.h
+++ include/vapor/VaporField.h
@@ -2,6 +2,8 @@
 #define VAPORFIELD_H
 
 #include "vapor/Field.h"
+#include "vapor/DataMgr.h"
+#include "vapor/FlowParams.h"
 
 namespace flow
 {
@@ -11,19 +13,47 @@ public:
     VaporField();
     virtual ~VaporField();
 
+    //
+    // Functions from class Field
+    //
     virtual bool InsideVolume( float time, const glm::vec3& pos ) const;
     virtual int  GetVelocity(  float time, const glm::vec3& pos,     // input 
                                glm::vec3& vel ) const;               // output
     virtual int  GetScalar(    float time, const glm::vec3& pos,     // input 
                                float& val) const ;                   // output
+    virtual int  GetNumberOfTimesteps() const;
 
-    virtual int GetNumberOfTimesteps() const;
+    //
+    // Functions for interaction with VAPOR components
+    //
+    void AssignDataManager( const VAPoR::DataMgr*    dmgr );
+    void UpdateParams(      const VAPoR::FlowParams* p );
+
+    //  
+    // Find one index whose timestamp is just below a given time
+    // I.e., _timestamps[floor] <= time 
+    //  
+    int LocateTimestamp( float   time,                        // Input
+                         size_t& floor ) const;               // Output
 
 
-protected:
-    
+
+private:
+   
+    // Member variables
+    std::vector<float>          _timestamps;    // in ascending order
+    const VAPoR::DataMgr*       _datamgr;   
+    const VAPoR::FlowParams*    _params;
 
 
+    // Member functions
+    template< typename T > 
+    size_t _binarySearch( const std::vector<T>& vec, T val, size_t begin, size_t end ) const;
+
+    // _getAGrid will use the cached params, _params, to generate grids. 
+    int  _getAGrid( int               timestep,         // Input
+                    std::string&      varName,          // Input
+                    Grid**            gridpp  ) const;  // Output
 };
 };
 
---
+++ lib/flow/VaporField.cpp
@@ -0,0 +1,114 @@
+#include "vapor/Field.h"
+
+using namespace flow;
+
+VaporField::VaporField()
+{
+    _datamgr = nullptr;
+}
+
+// Destructor
+VaporField::~VaporField()
+{ 
+}
+
+
+void
+VaporField::AssignDataManager( const VAPoR::DataMgr* dmgr )
+{
+    _datamgr = dmgr;
+
+    // Make a copy of the timestamps from the new data manager
+    const auto& timeCoords = dmgr->GetTimeCoordinates();
+    _timestamps.resize( timeCoords.size() );
+    for( size_t i = 0; i < timeCoords.size(); i++ )
+        _timestamps[i] = timeCoords[i];
+}
+
+
+void
+VaporField::UpdateParams( const VAPoR::FlowParams* p )
+{
+    _params = p;
+}
+
+
+int
+VaporField::LocateTimestamp( float time, size_t& floor ) const
+{
+    if( _timestamps.size() == 0 )
+        return TIME_ERROR;
+    if( _timestamps.size() == 1 )
+    {
+        if( _timestamps[0] != time )
+            return TIME_ERROR;
+        else
+        {
+            floor = 0;
+            return 0;
+        }
+    }
+
+    if( time < _timestamps.front() || time > _timestamps.back() )
+        return TIME_ERROR;
+    else
+    {
+        floor = _binarySearch( _timestamps, time, 0, _timestamps.size() - 1 );
+        return 0;
+    }
+}
+
+
+template<typename T> size_t
+VaporField::_binarySearch( const  std::vector<T>& vec, T val, 
+                           size_t begin, size_t end        ) const
+{
+    if( begin + 1 == end )
+    {
+        if( val == vec[end] )
+            return end;
+        else
+            return begin;
+    }
+
+    size_t middle = begin + (end - begin) / 2;
+    if( val == vec[middle] )
+        return middle;
+    else if( val < vec[middle] )
+        return _binarySearch( vec, val, begin, middle );
+    else
+        return _binarySearch( vec, val, middle, end );
+}
+
+
+int
+VaporField::GetNumberOfTimesteps() const
+{
+    return _timestamps.size();
+}
+
+
+int
+VaporField::_getAGrid( int               timestep,
+                       std::string&      varName,
+                       Grid**            gridpp  ) const
+{
+    std::vector<double>           extMin, extMax;
+    _params->GetBox()->GetExtents( extMin, extMax );
+    Grid* grid = _dataMgr->GetVariable( timestep,
+                                        varName,
+                                        _params->GetRefinementLevel(),
+                                        _params->GetCompressionLevel(),
+                                        extMin,
+                                        extMax );
+    if( grid == nullptr )
+    {
+        MyBase::SetErrMsg("Not able to get a grid!");
+        return GRID_ERROR;
+    }
+    else
+    {
+        *gridpp = grid;
+        return 0;
+    }
+}
