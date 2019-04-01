d69794c4a8e38e0f11b5eaa125479d20ae8d6e5b
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-04-01 10:55:13 -0600

    Cleaned up texture units

--- include/vapor/VolumeCellTraversal.h
+++ include/vapor/VolumeCellTraversal.h
@@ -15,7 +15,7 @@ class VolumeCellTraversal : public VolumeRegular {
 
     virtual int LoadData(const Grid *grid);
     virtual ShaderProgram *GetShader() const;
-    virtual void SetUniforms() const;
+    virtual void SetUniforms(int *nextTextureUnit) const;
 
   private:
     unsigned int minTexture;
@@ -43,7 +43,7 @@ class VolumeCellTraversalIso : public VolumeCellTraversal {
     static std::string GetName() { return "Iso Cell Traversal"; }
     static Type GetType() { return Type::Iso; }
     virtual ShaderProgram *GetShader() const;
-    virtual void SetUniforms() const;
+    virtual void SetUniforms(int *nextTextureUnit) const;
 };
 
 } // namespace VAPoR
--- include/vapor/VolumeRegular.h
+++ include/vapor/VolumeRegular.h
@@ -17,7 +17,7 @@ class VolumeRegular : public VolumeAlgorithm {
     virtual int LoadSecondaryData(const Grid *grid);
     virtual void DeleteSecondaryData();
     virtual ShaderProgram *GetShader() const;
-    virtual void SetUniforms() const;
+    virtual void SetUniforms(int *nextTextureUnit) const;
 
   protected:
     unsigned int _dataTexture;
@@ -41,7 +41,7 @@ class VolumeRegularIso : public VolumeRegular {
     static std::string GetName() { return "Iso Regular"; }
     static Type GetType() { return Type::Iso; }
     virtual ShaderProgram *GetShader() const;
-    virtual void SetUniforms() const;
+    virtual void SetUniforms(int *nextTextureUnit) const;
 };
 
 } // namespace VAPoR
