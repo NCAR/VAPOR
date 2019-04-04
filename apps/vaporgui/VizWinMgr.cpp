e4cfdfb61d025529252f1eaae2652398743120ee
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-04-04 11:44:26 -0600

    Removed qt gl version check which can give a false negative

--- apps/vaporgui/VizWinMgr.cpp
+++ apps/vaporgui/VizWinMgr.cpp
@@ -102,9 +102,7 @@ void VizWinMgr::_attachVisualizer(string vizName)
 
     QString qvizname = QString::fromStdString(vizName);
 
-    int major = 4, minor = 1;
-    if ((QGLFormat::openGLVersionFlags() & QGLFormat::OpenGL_Version_3_3) == 0) major = minor = 1;
-
+    int       major = 4, minor = 1;
     QGLFormat glFormat;
     glFormat.setVersion(major, minor);
     glFormat.setProfile(QGLFormat::CoreProfile);
