2015bc57b22ff348bc4987e3e02caaa072cf2cf5
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-04-29 21:51:19 -0600

    Added a Texture and Framebuffer class.

--- include/vapor/VolumeRegular.h
+++ include/vapor/VolumeRegular.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vapor/VolumeAlgorithm.h>
+#include <vapor/Texture.h>
 
 namespace VAPoR {
 
@@ -21,19 +22,18 @@ class VolumeRegular : public VolumeAlgorithm {
     virtual float GuestimateFastModeSpeedupFactor() const;
 
   protected:
-    unsigned int _dataTexture;
-    unsigned int _missingTexture;
+    Texture3D _data;
+    Texture3D _missing;
     bool _hasMissingData;
 
     std::vector<size_t> _dataDimensions;
 
     bool _hasSecondData;
-    unsigned int _dataTexture2;
-    unsigned int _missingTexture2;
+    Texture3D _data2;
+    Texture3D _missing2;
     bool _hasMissingData2;
 
-    static int _loadDataDirect(const Grid *grid, const unsigned int dataTexture, const unsigned int missingTexture, bool *hasMissingData);
-    static void _initializeTexture(unsigned int &texture);
+    int _loadDataDirect(const Grid *grid, Texture3D *dataTexture, Texture3D *missingTexture, bool *hasMissingData);
 };
 
 class VolumeRegularIso : public VolumeRegular {
