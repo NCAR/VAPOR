447aeb2f46e2eb24fb69605204ad879a20e86060
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-05-16 13:41:27 -0600

    Removed references to old DVR

--- lib/render/ControlExecutive.cpp
+++ lib/render/ControlExecutive.cpp
@@ -14,8 +14,6 @@
 #include <vapor/Visualizer.h>
 #include <vapor/DataStatus.h>
 
-#include <vapor/DVRenderer.h>
-#include <vapor/IsoSurfaceRenderer.h>
 #include <vapor/VolumeRenderer.h>
 #include <vapor/VolumeIsoRenderer.h>
 
@@ -194,9 +192,7 @@ int ControlExec::ActivateRender(
 
     rp->SetEnabled(on);
     v->MoveRendererToFront(renderType, renderName);
-    v->MoveRenderersOfTypeToFront(IsoSurfaceRenderer::GetClassType());
     v->MoveRenderersOfTypeToFront(VolumeIsoRenderer::GetClassType());
-    v->MoveRenderersOfTypeToFront(DVRenderer::GetClassType());
     v->MoveRenderersOfTypeToFront(VolumeRenderer::GetClassType());
 
     _paramsMgr->EndSaveStateGroup();
@@ -254,8 +250,8 @@ int ControlExec::ActivateRender(
 
     newRP->SetEnabled(on);
     v->MoveRendererToFront(renderType, renderName);
-    v->MoveRenderersOfTypeToFront(IsoSurfaceRenderer::GetClassType());
-    v->MoveRenderersOfTypeToFront(DVRenderer::GetClassType());
+    v->MoveRenderersOfTypeToFront(VolumeIsoRenderer::GetClassType());
+    v->MoveRenderersOfTypeToFront(VolumeRenderer::GetClassType());
 
     _paramsMgr->EndSaveStateGroup();
 
