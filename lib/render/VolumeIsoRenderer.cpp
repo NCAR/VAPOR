44e37139f63160e1990b28aef5a9e55bbbeed4b0
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-05-20 16:30:56 -0600

    Check if grid not null

--- lib/render/VolumeIsoRenderer.cpp
+++ lib/render/VolumeIsoRenderer.cpp
@@ -38,9 +38,13 @@ VolumeIsoRenderer::VolumeIsoRenderer( const ParamsMgr*    pm,
     if (_needToSetDefaultAlgorithm()) {
         VolumeParams *vp = (VolumeParams*)GetActiveParams();
         Grid *grid = _dataMgr->GetVariable(vp->GetCurrentTimestep(), vp->GetVariableName(), vp->GetRefinementLevel(), vp->GetCompressionLevel());
-        string algorithmName = _getDefaultAlgorithmForGrid(grid);
-        vp->SetAlgorithm(algorithmName);
-        delete grid;
+        if (grid) {
+            string algorithmName = _getDefaultAlgorithmForGrid(grid);
+            vp->SetAlgorithm(algorithmName);
+            delete grid;
+        } else {
+            vp->SetAlgorithm(VolumeRegularIso::GetName());
+        }
     }
 }
 
