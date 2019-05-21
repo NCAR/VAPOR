ec7ca9e4334558fc79957d5fcd6e7bd56481b0d9
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-05-21 16:08:09 -0600

    Added documentation

--- include/vapor/VolumeIsoRenderer.h
+++ include/vapor/VolumeIsoRenderer.h
@@ -9,6 +9,18 @@ using std::vector;
 
 namespace VAPoR {
 
+//! \class VolumeIsoRenderer
+//! \ingroup Public_Render
+//!
+//! \brief Isosurface renderer
+//!
+//! \author Stanislaw Jaroszynski
+//! \date Feburary, 2019
+//!
+//! Since Vapor requires a separate class for each renderer, this
+//! class is a copy of the VolumeRenderer class but it only gives access
+//! to the isosurface rendering algorithms
+
 class RENDER_API VolumeIsoRenderer : public VolumeRenderer {
 public:
     VolumeIsoRenderer(const ParamsMgr *pm, std::string &winName, std::string &dataSetName, std::string &instName, DataMgr *dataMgr);
