9f97f09848e9eda23eb9a86efa645c6a5ac35090
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-05-21 16:11:45 -0600

    Documentation

--- lib/render/VolumeRenderer.cpp
+++ lib/render/VolumeRenderer.cpp
@@ -307,7 +307,7 @@ void VolumeRenderer::_computeNewFramebufferRatio()
 
 bool VolumeRenderer::_shouldUseChunkedRender() const
 {
-    return _algorithm && _algorithm->IsSlow();
+    return _algorithm && _algorithm->RequiresChunkedRendering();
 }
 
 bool VolumeRenderer::_usingColorMapData() const
