ff9868a96808b481fbc97edbb059618fb04cb327
John Clyne <clyne@ucar.edu>
2019-05-21 19:20:56 -0600

    Incremental

--- lib/vdc/UnstructuredGrid.cpp
+++ lib/vdc/UnstructuredGrid.cpp
@@ -50,51 +50,65 @@ UnstructuredGrid::UnstructuredGrid(const std::vector<size_t> &vertexDims, const 
     _boundaryID = -2;
 }
 
-bool UnstructuredGrid::GetCellNodes(const std::vector<size_t> &cindices, std::vector<vector<size_t>> &nodes) const
+bool UnstructuredGrid::GetCellNodes(const size_t cindices[], size_t nodes[], int &n) const
 {
-    nodes.clear();
+    size_t cCindices[3];
+    ClampCellIndex(cindices, cCindices);
 
-    vector<size_t> cCindices = cindices;
-    ClampCellIndex(cCindices);
-
-    vector<size_t> cdims = GetCellDimensions();
+    const vector<size_t> &cdims = GetCellDimensions();
 
     // _vertexOnFace is dimensioned cdims[0] x _maxVertexPerFace
     //
     const int *ptr = _vertexOnFace + (_maxVertexPerFace * cCindices[0]);
     long       offset = GetNodeOffset();
 
+    n = 0;
     if (cdims.size() == 1) {
         for (int i = 0; i < _maxVertexPerFace; i++, ptr++) {
-            vector<size_t> indices;
             if (*ptr == GetMissingID() || *ptr + offset < 0) break;
             if (*ptr == GetBoundaryID()) continue;
 
-            indices.push_back(*ptr + offset);
-            nodes.push_back(indices);
+            nodes[n] = *ptr + offset;
+            n++;
         }
     } else {    // layered case
 
         for (int i = 0; i < _maxVertexPerFace; i++, ptr++) {
-            vector<size_t> indices;
             if (*ptr == GetMissingID() || *ptr + offset < 0) break;
             if (*ptr == GetBoundaryID()) continue;
 
-            indices.push_back(*ptr + offset);
-            indices.push_back(cCindices[1]);
-            nodes.push_back(indices);
+            nodes[2 * n + 0] = *ptr + offset;
+            nodes[2 * n + 1] = cCindices[1];
+            n++;
         }
 
         ptr = _vertexOnFace + (_maxVertexPerFace * cCindices[0]);
         for (int i = 0; i < _maxVertexPerFace; i++) {
-            vector<size_t> indices;
             if (*ptr == GetMissingID() || *ptr + offset < 0) break;
             if (*ptr == GetBoundaryID()) continue;
 
-            indices.push_back(*ptr + offset);
-            indices.push_back(cCindices[1] + 1);
-            nodes.push_back(indices);
+            nodes[2 * n + 0] = *ptr + offset;
+            nodes[2 * n + 1] = cCindices[1];
+            n++;
         }
+    }
+    return (true);
+}
+
+bool UnstructuredGrid::GetCellNodes(const std::vector<size_t> &cindices, std::vector<vector<size_t>> &nodes) const
+{
+    const vector<size_t> &cdims = GetCellDimensions();
+    size_t                nodes_a[_maxVertexPerFace * cdims.size()];
+    int                   n = 0;
+
+    bool ok = GetCellNodes(cindices.data(), nodes_a, n);
+    if (!ok) return (ok);
+
+    nodes.resize(n);
+    vector<size_t> indices(cdims.size(), 0);
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < cdims.size(); i++) { indices[i] = nodes_a[j * cdims.size() + i]; }
+        nodes[j] = indices;
     }
     return (true);
 }
