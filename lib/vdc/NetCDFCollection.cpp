50040ab9bbfe09b9b70dc5beedb98fcdc3c488fc
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-05-30 15:58:04 -0600

    Moved tvmap_cmp to correct namespace and changed to pass by reference

--- lib/vdc/NetCDFCollection.cpp
+++ lib/vdc/NetCDFCollection.cpp
@@ -40,12 +40,15 @@ bool readOK(vector <size_t> dims, size_t start[], size_t count[]) {
 	return(true);
 }
 
-  bool tvmap_cmp(
-    NetCDFCollection::TimeVaryingVar::tvmap_t a,
-    NetCDFCollection::TimeVaryingVar::tvmap_t b
-	) { return(a._time < b._time); };
-
 };
+
+bool VAPoR::tvmap_cmp(
+	NetCDFCollection::TimeVaryingVar::tvmap_t &a,
+	NetCDFCollection::TimeVaryingVar::tvmap_t &b
+)
+{
+	return(a._time < b._time);
+}
 
 NetCDFCollection::NetCDFCollection() {
 	_variableList.clear();
