3dd068676e7688b678b44771f62ffc3745f0fa3c
Samuel Li <shaomeng@ucar.edu>
2019-05-31 13:59:44 -0600

    :Revert "Moved tvmap_cmp to correct namespace and changed to pass by reference"
    This reverts commit 7d19939b98d6630d4cf22dca5f961eea2f6585ed.

--- lib/vdc/NetCDFCollection.cpp
+++ lib/vdc/NetCDFCollection.cpp
@@ -48,13 +48,11 @@ bool readOK(vector<size_t> dims, size_t start[], size_t count[]) {
     return (true);
 }
 
-}; // namespace
+bool tvmap_cmp(
+    NetCDFCollection::TimeVaryingVar::tvmap_t a,
+    NetCDFCollection::TimeVaryingVar::tvmap_t b) { return (a._time < b._time); };
 
-bool VAPoR::tvmap_cmp(
-    NetCDFCollection::TimeVaryingVar::tvmap_t &a,
-    NetCDFCollection::TimeVaryingVar::tvmap_t &b) {
-    return (a._time < b._time);
-}
+}; // namespace
 
 NetCDFCollection::NetCDFCollection() {
     _variableList.clear();
