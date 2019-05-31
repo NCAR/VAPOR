0590c567141ebeebce0537c427d24af037626d44
Samuel Li <shaomeng@ucar.edu>
2019-05-31 16:16:57 -0600

    fix compiler error

--- lib/common/VAssert.cpp
+++ lib/common/VAssert.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 #include <vapor/FileUtils.h>
 
 void Wasp::_VAssertFailed(const char *expr, const char *path, const unsigned int line) {
