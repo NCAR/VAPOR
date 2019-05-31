67c034b8a6c1b77a7c86e54417af3e510957c979
Samuel Li <shaomeng@ucar.edu>
2019-05-31 16:18:20 -0600

    fix memory leak

--- lib/common/VAssert.cpp
+++ lib/common/VAssert.cpp
@@ -7,9 +7,12 @@
 
 void Wasp::_VAssertFailed(const char *expr, const char *path, const unsigned int line)
 {
-    const char *fileName = strdup(FileUtils::Basename(std::string(path)).c_str());
+    char *fileName = strdup(FileUtils::Basename(std::string(path)).c_str());
     
     fprintf(stderr, "VAssert(%s) failed in %s, line %i\n", expr, fileName, line);
+
+    free( fileName );
+
 #ifdef NDEBUG
     exit(1);
 #else
