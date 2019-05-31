bf9b169ea6d3436173faafe2c1849b8b222d9efb
Samuel Li <shaomeng@ucar.edu>
2019-05-31 17:26:57 -0600

    fix warning

--- lib/render/Renderer.cpp
+++ lib/render/Renderer.cpp
@@ -487,7 +487,7 @@ void Renderer::renderColorbarText(ColorbarPbase *cbpb, float fbWidth, float fbHe
 
     // Corners in texture coordinates, to be derived later
     //
-    double Trx, Tlx, Tly, Tuy;
+    double Trx = 0.0, Tlx = 0.0, Tly = 0.0, Tuy = 0.0;
 
     _glManager->PixelCoordinateSystemPush();
 
