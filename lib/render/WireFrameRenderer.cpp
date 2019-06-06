780441cb01e0cc2bc77df3c7d6a0977fb1c45a83
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-06-06 11:51:28 -0600

    Enable depth testing for wireframe renderer

--- lib/render/WireFrameRenderer.cpp
+++ lib/render/WireFrameRenderer.cpp
@@ -294,7 +294,6 @@ int WireFrameRenderer::_buildCache() {
 }
 
 int WireFrameRenderer::_paintGL(bool fast) {
-
     int rc = 0;
     if (_isCacheDirty())
         rc = _buildCache();
@@ -307,7 +306,9 @@ int WireFrameRenderer::_paintGL(bool fast) {
     shader->SetUniform("MVP", _glManager->matrixManager->GetModelViewProjectionMatrix());
     glBindVertexArray(_VAO);
 
-    // glLineWidth(1);
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
     glDrawElements(GL_LINES, _nIndices, GL_UNSIGNED_INT, 0);
 
