767a2ba3da786fdf99b6d0b6db8f96da37906681
John Clyne <clyne@ucar.edu>
2019-06-10 20:47:15 -0600

    Fixed #1660

--- include/vapor/DataMgr.h
+++ include/vapor/DataMgr.h
@@ -443,6 +443,13 @@ public:
         return (GetDimLensAtLevel(varname, level, dims_at_level, dummy));
     }
 
+    //! Return a variable's array dimension lengths
+    //!
+    //! This method is equivalent to calling GetDimLensAtLevel() with \p level
+    //! equal to -1
+    //!
+    virtual int GetDimLens(string varname, std::vector<size_t> &dims) { return (GetDimLensAtLevel(varname, -1, dims)); }
+
     //! Unlock a floating-point region of memory
     //!
     //! Decrement the lock counter associatd with a
