b634f06298c9d127e8a9ac9e41802bc7803e989c
Scott Pearse <pearse@ucar.edu>
2019-06-11 12:05:13 -0600

    Merge with master

--- apps/vaporgui/TabManager.cpp
+++ apps/vaporgui/TabManager.cpp
@@ -545,7 +545,6 @@ void TabManager::_installWidgets() {
             QTabWidget *qtw = (QTabWidget *)_tabWidgets[tab];
             qtw->addTab(myScrollArea, QString::fromStdString(subTabName));
             myScrollArea->setWidget(_subTabWidgets[tab][j]);
-            myScrollArea->setWidgetResizable(true);
         }
     }
 
