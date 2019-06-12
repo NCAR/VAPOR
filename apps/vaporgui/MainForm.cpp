738a47132f576b799a3ad60f7a2656f5fdae3a50
Scott Pearse <pearse@ucar.edu>
2019-06-12 13:01:12 -0600

    Removed the Clear-undo/redo menu item

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -162,7 +162,6 @@ void MainForm::_initMembers() {
 	_navigationAction = NULL;
 	_editUndoAction = NULL;
 	_editRedoAction = NULL;
-	_editUndoRedoClearAction = NULL;
 	_timeStepEdit = NULL;
 	_timeStepEditValidator = NULL;
 
@@ -889,16 +888,9 @@ void MainForm::_createEditMenu() {
 	_editRedoAction->setToolTip("Redo the last undone session state change");
 	_editRedoAction->setEnabled(false);
 
-	_editUndoRedoClearAction = new QAction(this);
-	_editUndoRedoClearAction->setEnabled(true);
-	_editUndoRedoClearAction->setText( tr( "&Clear undo/redo" ) );
-	_editUndoRedoClearAction->setToolTip("Clear the undo/redo queue");
-	_editUndoRedoClearAction->setEnabled(true);
-
     _Edit = menuBar()->addMenu(tr("Edit"));
 	_Edit->addAction(_editUndoAction);
 	_Edit->addAction(_editRedoAction);
-	_Edit->addAction(_editUndoRedoClearAction);
 	_Edit->addSeparator();
 
 	connect(
@@ -909,11 +901,6 @@ void MainForm::_createEditMenu() {
 		_editRedoAction, SIGNAL(triggered()),
 		this, SLOT (redo())
 	);
-	connect(
-		_editUndoRedoClearAction, SIGNAL(triggered()),
-		this, SLOT(clear())
-	);
-
 }
 
 void MainForm::_createToolsMenu() {
@@ -1379,10 +1366,6 @@ void MainForm::undo() {
 void MainForm::redo() {
 	if (! _controlExec->RedoSize()) return;
 	undoRedoHelper(false);
-}
-
-void MainForm::clear(){
-	_controlExec->UndoRedoClear();
 }
 
 void MainForm::helpAbout()
