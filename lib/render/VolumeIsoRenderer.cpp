0a6386ed80eedc43ae20fe974c215de57d1a97df
Scott Pearse <pearse@ucar.edu>
2019-06-18 12:23:25 -0600

    Reverted diagnostic changes

--- lib/render/VolumeIsoRenderer.cpp
+++ lib/render/VolumeIsoRenderer.cpp
@@ -54,9 +54,7 @@ VolumeIsoRenderer::~VolumeIsoRenderer()
 
 bool VolumeIsoRenderer::_usingColorMapData() const
 {
-    bool rVal = !GetActiveParams()->UseSingleColor();
-    cout << "usingColorMapData() " << rVal << endl;
-    return rVal;
+    return !GetActiveParams()->UseSingleColor();
 }
 
 void VolumeIsoRenderer::_setShaderUniforms(const ShaderProgram *shader, const bool fast) const
