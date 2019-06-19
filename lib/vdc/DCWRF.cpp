6598030b99b51235eb89876ff6bfaad31ee69b76
John Clyne <clyne@ucar.edu>
2019-06-18 20:04:13 -0600

    Fixed #1711

--- lib/vdc/DCWRF.cpp
+++ lib/vdc/DCWRF.cpp
@@ -913,8 +913,8 @@ int DCWRF::_InitVerticalCoordinates(NetCDFCollection *ncdfc)
 {
     // Create 1D vertical coordinate variable for each "vertical" dimension
     //
-    // N.B. This only deals with the vertical dimensions we know about.
-    // Could be others.
+    // First do ones we know about. These require special treatment because
+    // they may be transformed to other units
     //
     string name = "bottom_top";
     if (_dimsMap.find(name) != _dimsMap.end()) { _derivedVars.push_back(_InitVerticalCoordinatesHelper(name, name)); }
@@ -922,8 +922,24 @@ int DCWRF::_InitVerticalCoordinates(NetCDFCollection *ncdfc)
     name = "bottom_top_stag";
     if (_dimsMap.find(name) != _dimsMap.end()) { _derivedVars.push_back(_InitVerticalCoordinatesHelper(name, name)); }
 
-    name = "soil_layers_stag";
-    if (_dimsMap.find(name) != _dimsMap.end()) { _derivedVars.push_back(_InitVerticalCoordinatesHelper(name, name)); }
+    // Now handle dimensions that are  not documented . I.e. everything
+    // else in the 3rd dimension position
+    //
+    vector<string> vars = ncdfc->GetVariableNames(3, true);
+
+    for (int i = 0; i < vars.size(); i++) {
+        vector<string> dimnames = ncdfc->GetSpatialDimNames(vars[i]);
+        assert(dimnames.size() == 3);
+        reverse(dimnames.begin(), dimnames.end());
+        name = dimnames[2];
+        if (_dimsMap.find(name) == _dimsMap.end()) continue;
+
+        // no duplicates
+        //
+        if (_coordVarsMap.find(name) != _coordVarsMap.end()) continue;
+
+        _derivedVars.push_back(_InitVerticalCoordinatesHelper(name, name));
+    }
 
     return (0);
 }
