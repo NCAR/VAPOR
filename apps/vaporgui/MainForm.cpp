04f342ecea6edf6a6b15b2c9fef1882dc95bcd6e
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-07-24 13:34:50 -0600

    Only allow the sidebar to be docked to the left or right of the viz window

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -286,8 +286,7 @@ MainForm::MainForm(
     //
     _tabDockWindow = new QDockWidget(this);
     addDockWidget(Qt::LeftDockWidgetArea, _tabDockWindow);
-    _tabDockWindow->setFeatures(
-        QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable);
+    _tabDockWindow->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
 
     // Register additional params with the ParamsMgr
     //
