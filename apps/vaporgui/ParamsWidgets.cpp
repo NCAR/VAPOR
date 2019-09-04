af3bdf963ac75cb36e164416dc5cc68c926b0101
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-09-04 10:21:47 -0600

    misc

--- apps/vaporgui/ParamsWidgets.cpp
+++ apps/vaporgui/ParamsWidgets.cpp
@@ -173,7 +173,6 @@ void ParamsWidgetDropdown::Update(VAPoR::ParamsBase *p)
 
 void ParamsWidgetDropdown::indexChangedSlot(int index)
 {
-    printf("INDEX CHANGE\n");
     if (_params)
         _params->SetValueLong(_tag, _tag, index);
 }
