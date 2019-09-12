e5de6f66a2bd554718ec5b8ae12e4cad9b277af6
clyne <clyne@ucar.edu>
2019-09-11 18:08:28 -0600

    Issue 855 (#1852)
    * Added floating point sanitation to DataMgr to ensure data returned are valid
    floating point numbers. i.e. no NaNs
    
    * Fixed #855
    
    The cf2vdc and wrf2vdc conversion utilities will immediately exit with an
    error if any variable contains NaNs. Two changes were made to address
    this problem:
    
    1. Upon encountering a NaN when processing a data variable the offending variable will be skipped and processing will continue. The application will exit with
    a non-zero status.
    
    2. Added a -xvars command line option to omit processing of bogus variables.

--- apps/wrf2vdc/wrf2vdc.cpp
+++ apps/wrf2vdc/wrf2vdc.cpp
@@ -17,6 +17,7 @@ struct opt_t {
     int nthreads;
     int numts;
     std::vector<string> vars;
+    std::vector<string> xvars;
     OptionParser::Boolean_T help;
 } opt;
 
@@ -26,9 +27,11 @@ OptionParser::OptDescRec_T set_opts[] = {
     {"numts", 1, "-1",
      "Number of timesteps to be included in the VDC. Default (-1) includes all timesteps."},
     {"vars", 1, "",
-     "Colon delimited list of 3D variable names (compressed) "
-     "to be included in "
-     "the VDC"},
+     "Colon delimited list of variable names "
+     "to be copied to the VDC"},
+    {"xvars", 1, "",
+     "Colon delimited list of variable names "
+     "to exclude from copying the VDC"},
     {"help", 0, "", "Print this message and exit"},
     {NULL}};
 
@@ -36,8 +39,22 @@ OptionParser::Option_T get_options[] = {
     {"nthreads", Wasp::CvtToInt, &opt.nthreads, sizeof(opt.nthreads)},
     {"numts", Wasp::CvtToInt, &opt.numts, sizeof(opt.numts)},
     {"vars", Wasp::CvtToStrVec, &opt.vars, sizeof(opt.vars)},
+    {"xvars", Wasp::CvtToStrVec, &opt.xvars, sizeof(opt.xvars)},
     {"help", Wasp::CvtToBoolean, &opt.help, sizeof(opt.help)},
     {NULL}};
+
+// Return a new vector containing elements of v1 with any elements from
+// v2 removed
+//
+vector<string> remove_vector(vector<string> v1, vector<string> v2) {
+    vector<string> newvec;
+    for (auto it = v1.begin(); it != v1.end(); ++it) {
+        if (find(v2.begin(), v2.end(), *it) == v2.end()) {
+            newvec.push_back(*it);
+        }
+    }
+    return (newvec);
+}
 
 string ProgName;
 
@@ -52,23 +69,23 @@ int main(int argc, char **argv) {
     ProgName = FileUtils::LegacyBasename(argv[0]);
 
     if (op.AppendOptions(set_opts) < 0) {
-        exit(1);
+        return (1);
     }
 
     if (op.ParseOptions(&argc, argv, get_options) < 0) {
-        exit(1);
+        return (1);
     }
 
     if (argc < 3) {
         cerr << "Usage: " << ProgName << " wrffiles... master.nc" << endl;
         op.PrintOptionHelp(stderr, 80, false);
-        exit(1);
+        return (1);
     }
 
     if (opt.help) {
         cerr << "Usage: " << ProgName << " master.nc" << endl;
         op.PrintOptionHelp(stderr, 80, false);
-        exit(0);
+        return (0);
     }
 
     argc--;
@@ -84,12 +101,12 @@ int main(int argc, char **argv) {
     vector<size_t> bs;
     int rc = vdc.Initialize(master, vector<string>(), VDC::A, bs, chunksize);
     if (rc < 0)
-        exit(1);
+        return (1);
 
     DCWRF dcwrf;
     rc = dcwrf.Initialize(wrffiles, vector<string>());
     if (rc < 0) {
-        exit(1);
+        return (1);
     }
 
     vector<string> varnames = dcwrf.GetCoordVarNames();
@@ -103,8 +120,10 @@ int main(int argc, char **argv) {
         for (int ts = 0; ts < nts; ts++) {
             cout << "  Time step " << ts << endl;
             int rc = vdc.CopyVar(dcwrf, ts, varnames[i], -1, -1);
-            if (rc < 0)
-                exit(1);
+            if (rc < 0) {
+                MyBase::SetErrMsg("Failed to copy variable %s", varnames[i].c_str());
+                return (1);
+            }
         }
     }
 
@@ -113,6 +132,10 @@ int main(int argc, char **argv) {
     } else {
         varnames = dcwrf.GetDataVarNames();
     }
+
+    varnames = remove_vector(varnames, opt.xvars);
+
+    int estatus = 0;
     for (int i = 0; i < varnames.size(); i++) {
         int nts = dcwrf.GetNumTimeSteps(varnames[i]);
         nts = opt.numts != -1 && nts > opt.numts ? opt.numts : nts;
@@ -125,10 +148,12 @@ int main(int argc, char **argv) {
             cout << "  Time step " << ts << endl;
 
             int rc = vdc.CopyVar(dcwrf, ts, varnames[i], -1, -1);
-            if (rc < 0)
-                exit(1);
+            if (rc < 0) {
+                MyBase::SetErrMsg("Failed to copy variable %s", varnames[i].c_str());
+                estatus = 1;
+            }
         }
     }
 
-    return (0);
+    return (1);
 }
