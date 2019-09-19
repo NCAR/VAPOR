0e23a2f28b1a31aac93f02636533badf4bf61095
clyne <clyne@ucar.edu>
2019-09-19 12:25:01 -0600

    Issue 855 (#1854)
    * Added floating point sanitation to DataMgr to ensure data returned are valid
    floating point numbers. i.e. no NaNs
    
    * Fixed #855
    
    The cf2vdc and wrf2vdc conversion utilities will immediately exit with an
    error if any variable contains NaNs. Two changes were made to address
    this problem:
    
    1. Upon encountering a NaN when processing a data variable the offending variable will be skipped and processing will continue. The application will exit with
    a non-zero status.
    
    2. Added a -xvars command line option to omit processing of bogus variables.
    
    * Replaced INFINITY constant with std::numeric_limits<T>::infinity();
    to address compiler warning

--- lib/vdc/DataMgr.cpp
+++ lib/vdc/DataMgr.cpp
@@ -476,7 +476,7 @@ bool is_int(std::string str)
 template<class T> void _sanitizeFloats(T *buffer, size_t n)
 {
     for (size_t i = 0; i < n; i++) {
-        if (isnan(buffer[i])) buffer[i] = INFINITY;
+        if (isnan(buffer[i])) buffer[i] = std::numeric_limits<T>::infinity();
     }
 }
 
