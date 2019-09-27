ce3f718edcda37eeb8dabc8b79c6d1a5faab2d57
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-09-27 14:58:35 -0600

    Removed old iso surface iso value editor

--- apps/vaporgui/VolumeIsoSubtabs.cpp
+++ apps/vaporgui/VolumeIsoSubtabs.cpp
@@ -55,27 +55,6 @@ VolumeIsoAppearanceSubtab::VolumeIsoAppearanceSubtab(QWidget* parent)
     _shininessWidget->SetLabel( QString::fromAscii("Shininess") );
     _shininessWidget->SetExtents( 1.0, 100.0 );
     _shininessWidget->SetIntType( true );
-
-    // Set up iso-value widgets
-    _isoWidget0->SetLabel( QString::fromAscii("Value 1") );
-    _isoWidget0->SetIntType( false );
-    _isoWidget0->SetDecimals( 4 );
-    _isoWidget0->setEnabled( true );
-
-    _isoWidget1->SetLabel( QString::fromAscii("Value 2") );
-    _isoWidget1->SetIntType( false );
-    _isoWidget1->SetDecimals( 4 );
-    _isoWidget1->setEnabled( false );
-
-    _isoWidget2->SetLabel( QString::fromAscii("Value 3") );
-    _isoWidget2->SetIntType( false );
-    _isoWidget2->SetDecimals( 4 );
-    _isoWidget2->setEnabled( false );
-
-    _isoWidget3->SetLabel( QString::fromAscii("Value 4") );
-    _isoWidget3->SetIntType( false );
-    _isoWidget3->SetDecimals( 4 );
-    _isoWidget3->setEnabled( false );
 }
 
 
@@ -117,54 +96,6 @@ void VolumeIsoAppearanceSubtab::Update( VAPoR::DataMgr      *dataMgr,
     _diffuseWidget->SetValue  (_params->GetPhongDiffuse());
     _specularWidget->SetValue (_params->GetPhongSpecular());
     _shininessWidget->SetValue(_params->GetPhongShininess());
-
-    /*
-     
-    // Get the value range
-    std::vector<double> valueRanged;
-	bool errEnabled = MyBase::EnableErrMsg(false);
-    int rc = dataMgr->GetDataRange( params->GetCurrentTimestep(),
-                           params->GetVariableName(),
-                           params->GetRefinementLevel(),
-                           params->GetCompressionLevel(), 1,
-                           valueRanged );
-	MyBase::EnableErrMsg(errEnabled);
-
-	if (rc<0)  return;
-
-    float valueRange[2] =  { float(valueRanged[0]), float(valueRanged[1]) };
-
-    std::vector<bool>   enableFlags = _params->GetEnabledIsoValues();
-    std::vector<double> isoValues   = _params->GetIsoValues();
-    _isoWidget0->setEnabled(        enableFlags[0] );  // update 1st widget
-    _isoValueCheckbox0->setChecked( enableFlags[0] );
-    if( enableFlags[0] )
-    {
-        _isoWidget0->SetExtents( valueRange[0], valueRange[1] );
-        _isoWidget0->SetValue  ( isoValues[0] );
-    }
-    _isoWidget1->setEnabled(        enableFlags[1] );  // update 2nd widget
-    _isoValueCheckbox1->setChecked( enableFlags[1] );
-    if( enableFlags[1] )
-    {
-        _isoWidget1->SetExtents( valueRange[0], valueRange[1] );
-        _isoWidget1->SetValue  ( isoValues[1] );
-    }
-    _isoWidget2->setEnabled(        enableFlags[2] );  // update 3rd widget
-    _isoValueCheckbox2->setChecked( enableFlags[2] );
-    if( enableFlags[2] )
-    {
-        _isoWidget2->SetExtents( valueRange[0], valueRange[1] );
-        _isoWidget2->SetValue  ( isoValues[2] );
-    }
-    _isoWidget3->setEnabled(        enableFlags[3] );  // update 4th widget
-    _isoValueCheckbox3->setChecked( enableFlags[3] );
-    if( enableFlags[3] )
-    {
-        _isoWidget3->SetExtents( valueRange[0], valueRange[1] );
-        _isoWidget3->SetValue  ( isoValues[3] );
-    }
-     */
 }
 
 void VolumeIsoAppearanceSubtab::on__castingModeComboBox_currentIndexChanged(const QString &text)
