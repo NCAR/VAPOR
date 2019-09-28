1be8486fcfc1d1e18286140caa3ea5f708021f04
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-09-28 10:38:29 -0600

    volume density slider

--- apps/vaporgui/VolumeSubtabs.cpp
+++ apps/vaporgui/VolumeSubtabs.cpp
@@ -1,5 +1,6 @@
 #include "VolumeSubtabs.h"
-#include <TFEditor.h>
+#include "TFEditor.h"
+#include "QSliderEdit.h"
 
 using namespace VAPoR;
 
@@ -17,6 +18,12 @@ VolumeAppearanceSubtab::VolumeAppearanceSubtab(QWidget* parent) {
     _TFWidget->SetOpacityIntegrated(true);
     _TFWidget->Reinit((TFFlags)(SAMPLING));
     verticalLayout->insertWidget(0, _tfe = new TFEditor);
+    
+    _densitySlider = new QSliderEdit();
+    _densitySlider->SetLabel("Volume Density");
+    _densitySlider->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+    connect(_densitySlider, SIGNAL(valueChanged(double)), this, SLOT(_densitySlider_valueChanged(double)));
+    _raytracingFrame->layout()->addWidget(_densitySlider);
     
     _params = nullptr;
     
@@ -83,6 +90,10 @@ void VolumeAppearanceSubtab::Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *p
     _samplingRateComboBox->blockSignals(false);
     
     
+    MapperFunction *tf = vp->GetMapperFunc(vp->GetVariableName());
+    float opacityValue = powf(tf->getOpacityScale(), 1/4.f);
+    _densitySlider->SetValue(opacityValue);
+    
     
     // ---------------------------
     // Lighting Parameters
@@ -93,6 +104,12 @@ void VolumeAppearanceSubtab::Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *p
     _diffuseWidget->SetValue  (_params->GetPhongDiffuse());
     _specularWidget->SetValue (_params->GetPhongSpecular());
     _shininessWidget->SetValue(_params->GetPhongShininess());
+}
+
+void VolumeAppearanceSubtab::_densitySlider_valueChanged(double v)
+{
+    MapperFunction *tf = _params->GetMapperFunc(_params->GetVariableName());
+    tf->setOpacityScale(powf(v, 4));
 }
 
 void VolumeAppearanceSubtab::on__castingModeComboBox_currentIndexChanged(const QString &text)
--- apps/vaporgui/VolumeSubtabs.h
+++ apps/vaporgui/VolumeSubtabs.h
@@ -16,6 +16,7 @@ namespace VAPoR {
 }
 
 class TFEditor;
+class QSliderEdit;
 
 class VolumeVariablesSubtab : public QWidget, public Ui_VolumeVariablesGUI {
 
@@ -50,6 +51,8 @@ public:
     static float GetSamplingRateForQString(const QString &str);
     
 private slots:
+    void _densitySlider_valueChanged(double v);
+    
     void on__castingModeComboBox_currentIndexChanged(const QString &text);
     void on__samplingRateComboBox_currentIndexChanged(const QString &text);
     void on__lightingCheckBox_toggled(bool checked);
@@ -62,6 +65,7 @@ private slots:
 private:
     VAPoR::VolumeParams *_params;
     TFEditor *_tfe;
+    QSliderEdit *_densitySlider;
 };
 
 class VolumeGeometrySubtab : public QWidget, public Ui_VolumeGeometryGUI {
