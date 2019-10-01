40d4068463a54e3e5165f2dea51db67da2119d79
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-10-01 15:04:26 -0600

    inc

--- lib/render/ModelRenderer.cpp
+++ lib/render/ModelRenderer.cpp
@@ -26,9 +26,14 @@
 #include <vapor/ShaderManager.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "vapor/GLManager.h"
+#include <vapor/GLManager.h>
+#include <vapor/FileUtils.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
 
 using namespace VAPoR;
+
+const aiScene *scene;
 
 static RendererRegistrar<ModelRenderer> registrar(
                                                     ModelRenderer::GetClassType(), ModelParams::GetClassType()
@@ -38,7 +43,16 @@ ModelRenderer::ModelRenderer(const ParamsMgr* pm, string winName,
                                  string dataSetName, string instName,
                                  DataMgr* dataMgr)
 : Renderer(pm, winName, dataSetName, ModelParams::GetClassType(),
-           ModelRenderer::GetClassType(), instName, dataMgr){}
+           ModelRenderer::GetClassType(), instName, dataMgr)
+{
+    Assimp::Importer importer;
+    const string path = "/Users/stas/Downloads/Windmill/nrel5MW-small.stl";
+    
+    scene = importer.ReadFile(path, 0);
+    if (!scene) {
+        printf("ERROR %s\n", importer.GetErrorString());
+    }
+}
 
 ModelRenderer::~ModelRenderer()
 {
