3b9c0c9e83d838b8d9e978f1eda0dce3d9b26b6b
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-10-02 14:57:32 -0600

    cleanup

--- apps/vaporgui/ParamsWidgets.h
+++ apps/vaporgui/ParamsWidgets.h
@@ -79,25 +79,6 @@ private slots:
 
 
 
-class QRangeSliderTextCombo;
-class ParamsWidgetRange : public ParamsWidget {
-    Q_OBJECT
-    
-    QRangeSliderTextCombo *_range = nullptr;
-    
-public:
-    ParamsWidgetRange(const std::string &tag, const std::string &label = "");
-    void Update(VAPoR::ParamsBase *p);
-    
-    ParamsWidgetRange *SetRange(float min, float max);
-    
-private slots:
-    void valueChangedSlot();
-};
-
-
-
-
 class ParamsWidgetDropdown : public ParamsWidget {
     Q_OBJECT
     
@@ -122,7 +103,6 @@ class QColorWidget;
 class ParamsWidgetColor : public ParamsWidget {
     Q_OBJECT
     
-//    QPushButton *_button = nullptr;
     QColorWidget *_color = nullptr;
     
 public:
@@ -134,7 +114,6 @@ public:
     
     
 private slots:
-//    void pressed();
     void colorChanged(QColor color);
 };
 
