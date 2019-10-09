58698de16bfd5b37e0b012fbc4acf8e09936552b
clyne <clyne@ucar.edu>
2019-10-09 08:44:06 -0600

    Issue 1755 (#1865)
    * Fixed 1755 - Vapor crashes with large MPAS wireframe renderer
    
    Reduced memory utilization by wireframerender by avoiding unnecessary
    redraws of the same cell edges
    
    * Performance tuning.
    
    * Rmove diagnostics and clean up code for better readability
    
    * Checked for overflow in precision reduction
    
    * Use std::swap for swap
    
    * Fixed regression in color mapping
    
    * Better handling of precision mismatch between Grid indices and OpenGL render
    buffers
    
    * Update include/vapor/WireFrameRenderer.h
    
    Co-Authored-By: Samuel Li <shaomeng@users.noreply.github.com>
    
    * Fixed munged GitHub merge

--- include/vapor/WireFrameRenderer.h
+++ include/vapor/WireFrameRenderer.h
@@ -17,6 +17,7 @@
     #include <GL/gl.h>
 #endif
 
+#include <utility>
 #include <vapor/DataMgr.h>
 #include <vapor/utils.h>
 #include <vapor/Renderer.h>
@@ -70,10 +71,54 @@ private:
 
     } _cacheParams;
 
+    // Helper class to keep track of which cell edges have been drawn so
+    // we can avoid duplicate draws.
+    //
+    class DrawList {
+    public:
+        // maxEntries is the maximum number of unique cell nodes.
+        // maxLinesPerVertex is the *expected* max valence (degree) of any
+        // node (vertex). If a node has more than maxLinesPerVertex edges
+        // only the first maxLinesPerVertex edges will be recorded in
+        // DrawList. Hence, queries to edges with DrawList::InList will
+        // return false once maxLinesPerVertex has been exceeded
+        //
+        DrawList(GLuint maxEntries, size_t maxLinesPerVertex)
+        : _drawList(maxEntries * maxLinesPerVertex, std::numeric_limits<GLuint>::max()), _maxEntries(maxEntries), _maxLinesPerVertex(maxLinesPerVertex)
+        {
+        }
+
+        bool InList(GLuint idx0, GLuint idx1)
+        {
+            VAssert(idx0 < _maxEntries);
+            VAssert(idx1 < _maxEntries);
+
+            if (idx1 < idx0) { std::swap(idx1, idx0); }
+
+            for (int i = 0; i < _maxLinesPerVertex; i++) {
+                if (_drawList[idx0 * _maxLinesPerVertex + i] == idx1) { return (true); }
+                if (_drawList[idx0 * _maxLinesPerVertex + i] == std::numeric_limits<GLuint>::max()) {
+                    _drawList[idx0 * _maxLinesPerVertex + i] = idx1;
+                    return (false);
+                }
+            }
+            return (false);
+        }
+
+    private:
+        vector<GLuint> _drawList;
+        const size_t   _maxEntries;
+        const size_t   _maxLinesPerVertex;
+    };
+
+    void _buildCacheVertices(const Grid *grid, const Grid *heightGrid, vector<GLuint> &nodeMap) const;
+
+    size_t _buildCacheConnectivity(const Grid *grid, const vector<GLuint> &nodeMap) const;
+
     int  _buildCache();
     bool _isCacheDirty() const;
     void _saveCacheParams();
-    void _drawCell(vector<VertexData> &vertices, vector<unsigned int> &indices, const float *verts, const float *colors, int n, bool layered);
+    void _drawCell(const GLuint *cellNodeIndices, int n, bool layered, const std::vector<GLuint> &nodeMap, GLuint invalidIndex, std::vector<unsigned int> &indices, DrawList &drawList) const;
 
     void _clearCache() { _cacheParams.varName.clear(); }
 };
