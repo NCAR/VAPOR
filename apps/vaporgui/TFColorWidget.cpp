d62fef4b40ec70cb04c0a81296eca6327330d1fc
Stanislaw Jaroszynski <Stanislaw Jaroszynski>
2019-10-31 11:51:13 -0600

    misc

--- apps/vaporgui/TFColorWidget.cpp
+++ apps/vaporgui/TFColorWidget.cpp
@@ -57,7 +57,7 @@ void TFColorMap::PopulateSettingsMenu(QMenu *menu) const
     for (int i = 0; i < fileNames.size(); i++) {
         string path = FileUtils::JoinPaths({builtinPath, fileNames[i]});
 
-        if (FileUtils::Extension(path) != ".tf3") continue;
+        if (FileUtils::Extension(path) != "tf3") continue;
 
         QAction *item = new ColorMapMenuItem(path);
         connect(item, SIGNAL(triggered(std::string)), this, SLOT(menuLoadBuiltin(std::string)));
