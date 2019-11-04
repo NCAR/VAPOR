e02362b2ad17e26da480503625fadcbc7bff72d6
Scott Pearse <pearse@ucar.edu>
2019-11-04 16:08:49 -0700

    removed diagnostics

--- apps/vaporgui/SettingsEventRouter.cpp
+++ apps/vaporgui/SettingsEventRouter.cpp
@@ -389,7 +389,6 @@ void SettingsEventRouter::_updateDirectoryPaths(){
 	_flowPathEdit->setText(QString::fromStdString(flowPath));
 
 	string pythonPath = sParams->GetPythonDir();
-	std::cout << "PythonPath " << pythonPath << endl;
 	_pythonPathEdit->setText(QString::fromStdString(pythonPath));
 }
 
