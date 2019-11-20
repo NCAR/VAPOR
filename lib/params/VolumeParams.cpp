c1ffd38198f474ce5e169a3ec87e9d7293be3177
StasJ <StasJ@users.noreply.github.com>
2019-11-19 17:01:14 -0700

    increase sampling rate options (#1932)

--- lib/params/VolumeParams.cpp
+++ lib/params/VolumeParams.cpp
@@ -71,7 +71,7 @@ bool VolumeParams::GetAlgorithmWasManuallySetByUser() const { return GetValueLon
 
 void VolumeParams::SetAlgorithmWasManuallySetByUser(bool v) { SetValueLong(_algorithmWasManuallySetByUserTag, "User manually changed the algorithm", v); }
 
-std::vector<float> VolumeParams::GetSamplingRateMultiples() { return {1, 2, 3, 4}; }
+std::vector<float> VolumeParams::GetSamplingRateMultiples() { return {1, 2, 4, 8, 16}; }
 
 float VolumeParams::GetSamplingMultiplier() const { return GetValueDouble(_samplingRateMultiplierTag, 1.0); }
 
