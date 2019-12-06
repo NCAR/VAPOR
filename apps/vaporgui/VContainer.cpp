ae959f970156ba7c54d5c77bf5d2661ae70017b3
Scott Pearse <pearse@ucar.edu>
2019-12-06 12:31:05 -0700

    Added new files

---
+++ apps/vaporgui/VContainer.cpp
@@ -0,0 +1,49 @@
+#include <QEvent>
+#include <QWidget>
+
+#include "VContainer.h"
+
+VContainer::VContainer( QWidget* containee ) 
+: QWidget() 
+{
+    QHBoxLayout* layout = new QHBoxLayout;
+    layout->setContentsMargins(
+        LEFT_MARGIN,
+        TOP_MARGIN,
+        RIGHT_MARGIN,
+        BOTTOM_MARGIN
+    );
+    setLayout( layout );
+
+    setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Preferred );
+}
+
+void VContainer::Hide() {
+    QWidget* parent = parentWidget();
+    if (parent != nullptr){
+        parent->hide();
+    }
+}
+
+void VContainer::Show() {
+    QWidget* parent = parentWidget();
+    if (parent != nullptr)
+        parent->show();
+}
+
+
+MouseWheelWidgetAdjustmentGuard::MouseWheelWidgetAdjustmentGuard(QObject *parent) : QObject(parent)
+{
+}
+
+bool MouseWheelWidgetAdjustmentGuard::eventFilter(QObject *o, QEvent *e)
+{
+    const QWidget* widget = static_cast<QWidget*>(o);
+    if (e->type() == QEvent::Wheel && widget && !widget->hasFocus())
+    {
+        e->ignore();
+        return true;
+    }
+
+    return QObject::eventFilter(o, e);
+}
