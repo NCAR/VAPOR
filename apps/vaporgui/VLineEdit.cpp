5a9a68efd799899f2a539ff325e931417bbeae77
Scott Pearse <pearse@ucar.edu>
2019-12-09 15:11:43 -0700

    Prototype for left-click options for VaporWidgets

--- apps/vaporgui/VLineEdit.cpp
+++ apps/vaporgui/VLineEdit.cpp
@@ -1,13 +1,18 @@
-
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <sstream>
+
+#include <QMenu>
 
 #include "VLineEdit.h"
 
 VLineEdit::VLineEdit( const std::string& value )
 : VContainer( this ),
   _value( value ),
-  _isDouble( false )
+  _isDouble( false ),
+  _scientific( false ),
+  _decDigits( 4 )
 {
     _lineEdit = new QLineEdit;
     SetValue( _value );
@@ -15,18 +20,48 @@ VLineEdit::VLineEdit( const std::string& value )
 
     connect( _lineEdit, SIGNAL( editingFinished() ),
         this, SLOT( emitLineEditChanged() ) );
+
+    // Qt is currently crashing if QMenu is given a parent
+    _menu = new QMenu();
+    
+    SpinBoxAction* decimalAction = new SpinBoxAction(tr("Decimal digits"), _decDigits);
+    connect( decimalAction, SIGNAL( editingFinished( int ) ),
+        this, SLOT( _decimalDigitsChanged( int ) ) );
+    _menu->addAction(decimalAction);
+
+    CheckBoxAction* checkBoxAction = new CheckBoxAction(tr("Scientific"), _scientific);
+    connect( checkBoxAction, SIGNAL( clicked( bool ) ),
+        this, SLOT( _scientificClicked( bool ) ) );
+    _menu->addAction(checkBoxAction);
+    
+    _lineEdit->setContextMenuPolicy( Qt::CustomContextMenu );
+    connect( _lineEdit, SIGNAL( customContextMenuRequested( const QPoint& ) ),
+        this, SLOT( ShowContextMenu( const QPoint& ) ) );
+}
+
+VLineEdit::~VLineEdit() {
+    // Qt is currently crashing if QMenu is given a parent, so delete it here
+    // 
+    if (_menu != nullptr) delete _menu;
 }
 
 void VLineEdit::SetValue( const std::string& value ) {
     // see if value is a valid double
     if (_isDouble) {
+        double dValue;
         try {
-            double dValue = std::stod( value );
-            _value = value;
+            dValue = std::stod( value );
         }
         catch (...) {
             std::cerr << "VLineEDit::SetValue failed to set value of " << value << std::endl;
         }
+
+        std::stringstream stream;
+        stream << std::fixed << std::setprecision( _decDigits );
+        if (_scientific)
+            stream << std::scientific;
+        stream << dValue << std::endl;
+        _value = stream.str();
     }
     else
         _value = value;
@@ -42,10 +77,31 @@ std::string VLineEdit::GetValue() const {
 
 void VLineEdit::SetIsDouble( bool isDouble ) {
     _isDouble = isDouble;
+
 }
 
 void VLineEdit::emitLineEditChanged() {
     std::string value = _lineEdit->text().toStdString();
     SetValue( value );
     emit ValueChanged( _value );
+}
+
+void VLineEdit::ShowContextMenu( const QPoint& pos ) {
+    QPoint globalPos = _lineEdit->mapToGlobal(pos);
+
+    QAction* selectedItem = _menu->exec(globalPos);
+    if (selectedItem)
+        std::cout << "Booga" << std::endl;
+    else
+        std::cout << "no Booga" << std::endl;
+}
+
+void VLineEdit::_decimalDigitsChanged( int value ) {
+    _decDigits = value;
+    SetValue( _value );
+}
+
+void VLineEdit::_scientificClicked( bool value ) {
+    _scientific = value;
+    SetValue( _value );
 }
--- apps/vaporgui/VLineEdit.h
+++ apps/vaporgui/VLineEdit.h
@@ -3,7 +3,11 @@
 #include <string>
 
 #include <QWidget>
+#include <QLabel>
 #include <QLineEdit>
+#include <QSpinBox>
+#include <QCheckBox>
+#include <QWidgetAction>
 
 #include "VContainer.h"
 
@@ -13,6 +17,8 @@ class VLineEdit : public VContainer {
 public:
     VLineEdit( const std::string& value = "");
 
+    ~VLineEdit();
+
     void SetValue( const std::string& value );
     std::string GetValue() const;
 
@@ -20,12 +26,95 @@ public:
 
 private:
     QLineEdit*  _lineEdit;
+    QMenu*      _menu;
     std::string _value;
     bool        _isDouble;
+    bool        _scientific;
+    int         _decDigits;
 
 public slots:
     void emitLineEditChanged();
 
+    void ShowContextMenu(const QPoint&);
+
+private slots:
+    void _decimalDigitsChanged( int value );
+    void _scientificClicked( bool value );
+
 signals:
     void ValueChanged( const std::string& value );
+};
+
+class SpinBoxAction : public QWidgetAction {
+    Q_OBJECT
+
+public:
+    SpinBoxAction (const QString& title, int value) : 
+      QWidgetAction (NULL) {
+        QWidget* widget = new QWidget (NULL);
+        QHBoxLayout* layout = new QHBoxLayout();
+        QLabel* label = new QLabel (title);  //bug fixed here, pointer was missing
+        _spinBox = new QSpinBox(NULL);
+
+        _spinBox->setValue( value );
+        layout->setContentsMargins(-1,0,-1,0);
+        layout->addWidget (label);
+        layout->addWidget (_spinBox);
+        widget->setLayout (layout);
+
+        //connect( _spinBox, SIGNAL( editingFinished() ),
+        //    this, SLOT( spinBoxChanged() ) );
+        connect( _spinBox, SIGNAL( valueChanged( int ) ),
+            this, SLOT( spinBoxChanged() ) );
+
+        setDefaultWidget(widget);
+    }
+
+private:
+    QSpinBox* _spinBox;
+
+private slots:
+    void spinBoxChanged() {
+        int value = _spinBox->value();
+        emit editingFinished( value );
+    }
+
+signals:
+    void editingFinished( int );
+};
+
+class CheckBoxAction : public QWidgetAction {
+    Q_OBJECT
+
+public:
+    CheckBoxAction (const QString& title, bool value) : QWidgetAction (NULL) 
+    {
+        QWidget* widget     = new QWidget (NULL);
+        QHBoxLayout* layout = new QHBoxLayout();
+        QLabel* label       = new QLabel (title);
+        _checkBox           = new QCheckBox(NULL);
+        _checkBox->setChecked( value );
+        
+        layout->setContentsMargins(-1,0,20,0);
+        layout->addWidget (label);
+        layout->addStretch();
+        layout->addWidget (_checkBox);
+        widget->setLayout (layout);
+
+        connect( _checkBox, SIGNAL( clicked( bool ) ),
+            this, SLOT( checkBoxChanged( bool ) ) );
+
+        setDefaultWidget(widget);
+    }
+
+private:
+    QCheckBox* _checkBox;
+
+private slots:
+    void checkBoxChanged( bool value ) {
+        emit clicked( value );
+    }
+
+signals:
+    void clicked( bool );
 };
