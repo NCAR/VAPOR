5cf7a788ff89b685d6753ff1e14795927b438e0e
sgpearse <pearse@ucar.edu>
2019-12-16 16:12:47 -0700

    fix for 1994 (#1995)

--- apps/vaporgui/FlowSubtabs.cpp
+++ apps/vaporgui/FlowSubtabs.cpp
@@ -147,7 +147,7 @@ void FlowSeedingSubtab::_createSeedingSection(QWidget *parent)
     _randomSeedsSliderEdit = new VSliderEdit(MIN_RANDOM_SEEDS, MAX_RANDOM_SEEDS);
     _randomSeedsSliderEdit->SetIntType(true);
     _randomSeedsFrame->addWidget(new VLineItem("Seed count", _randomSeedsSliderEdit));
-    connect(_randomSeedsSliderEdit, &VSliderEdit::ValueChanged, this, &FlowSeedingSubtab::_rakeNumOfSeedsChanged);
+    connect(_randomSeedsSliderEdit, &VSliderEdit::ValueChangedInt, this, &FlowSeedingSubtab::_rakeNumOfSeedsChanged);
 
     _biasWeightSliderEdit = new VSliderEdit(-1, 1, 0);
     _randomSeedsFrame->addWidget(new VLineItem("Bias weight", _biasWeightSliderEdit));
