b6ae349fb845a802e2bcd1cbcb26abdc1990f3bd
Stanislaw Jaroszynski <stas708@gmail.com>
2019-12-17 10:21:12 -0700

    doc

--- apps/vaporgui/VDoubleValidator.h
+++ apps/vaporgui/VDoubleValidator.h
@@ -7,6 +7,10 @@
 //! They behave the same except QDoubleValidator does not accept values outside
 //! the range while VDoubleValidator does not allow the user to input
 //! values outside of the range. This solves some quirks with QLineEdit
+//! With QDoubleValidator, the user can type a value outside the range in the text
+//! box but the text box will say the value is invalid and it will stop working
+//! VDoubleValidator, will not allow the user to type in a value outside the range
+//! omitting the above problem.
 
 class VDoubleValidator : public QDoubleValidator {
 public:
