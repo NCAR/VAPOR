94ba2de3af3cb7042db93c07ea20d983f52dfaca
Stanislaw Jaroszynski <stas708@gmail.com>
2019-12-28 01:00:30 -0700

    Apparently a 0 height extent breaks things in a 2D-only dataset so disable the previous commit for 2D-only datasets

--- lib/params/DataStatus.cpp
+++ lib/params/DataStatus.cpp
@@ -192,10 +192,14 @@ void DataStatus::_getExtents(
 			if (! status) continue;
             
             if (minVExts.size() == 2) {
-                double z = DataMgrUtils::Get2DRendererDefaultZ(dataMgr, ts, var.refLevel, var.compLevel);
-                minVExts.push_back(z);
-                maxVExts.push_back(z);
-                axes.push_back(2);
+                bool has3D = !dataMgr->GetDataVarNames(3).empty();
+                
+                if (has3D) {
+                    double z = DataMgrUtils::Get2DRendererDefaultZ(dataMgr, ts, var.refLevel, var.compLevel);
+                    minVExts.push_back(z);
+                    maxVExts.push_back(z);
+                    axes.push_back(2);
+                }
             }
 
 			print_extents(dataSetName, minVExts, maxVExts);
