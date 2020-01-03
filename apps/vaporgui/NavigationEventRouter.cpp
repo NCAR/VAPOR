725db05b26d09a480d4564463797eace6bd131b0
StasJ <StasJ@users.noreply.github.com>
2020-01-03 12:24:06 -0700

    Fix align "menu" (#2054)

--- apps/vaporgui/NavigationEventRouter.cpp
+++ apps/vaporgui/NavigationEventRouter.cpp
@@ -783,7 +783,7 @@ void NavigationEventRouter::AlignView(int axis) {
             dirvec[2] = -1.f;
             break;
         default:
-            VAssert(0);
+            return;
         }
     }
 
