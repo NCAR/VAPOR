10e26f5e7f11b410c0f5c31ca661aa4bba13c6ce
Scott Pearse <pearse@ucar.edu>
2020-01-09 15:06:46 -0700

    Revert "Simplest fix for #2059"
    This reverts commit 24f42a953aa1626a39a5727c56fbe853847cdcb2.

--- apps/vaporgui/FlowSubtabs.cpp
+++ apps/vaporgui/FlowSubtabs.cpp
@@ -69,8 +69,6 @@ void FlowVariablesSubtab::Update(VAPoR::DataMgr *dataMgr,
 
     _paramsMgr = paramsMgr;
 
-    _variablesWidget->Update(dataMgr, paramsMgr, rParams);
-
     GUIStateParams *gp = dynamic_cast<GUIStateParams *>(_paramsMgr->GetParams(GUIStateParams::GetClassType()));
     int nDims = gp->GetFlowDimensionality();
     bool no3DVars = dataMgr->GetDataVarNames(3).size() ? false : true;
@@ -96,6 +94,8 @@ void FlowVariablesSubtab::Update(VAPoR::DataMgr *dataMgr,
             _variablesWidget->Configure3DFieldVars();
         gp->SetFlowDimensionality(_variablesWidget->GetActiveDimension());
     }
+
+    _variablesWidget->Update(dataMgr, paramsMgr, rParams);
 }
 
 void FlowVariablesSubtab::_dimensionalityChanged(int nDims) const {
