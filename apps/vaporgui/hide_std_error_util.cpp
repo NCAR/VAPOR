6d6eb5c7820d5b3c8196b0c6faae7519ccc36511
Scott Pearse <pearse@ucar.edu>
2020-01-22 09:35:03 -0700

    Fix for #2142

--- apps/vaporgui/hide_std_error_util.cpp
+++ apps/vaporgui/hide_std_error_util.cpp
@@ -6,17 +6,28 @@
 #endif
 
 static int _savedSTDERR;
+static fpos_t pos;
 
 void HideSTDERR()
 {
 #ifndef WIN32
+    fflush( stderr );
+    fgetpos( stderr, &pos );
+
     _savedSTDERR = dup(STDERR_FILENO);
-    freopen("/dev/null", "w", stderr);
+
+    freopen( "/dev/null", "w", stderr );
 #endif
 }
 void RestoreSTDERR()
 {
 #ifndef WIN32
-    dup2(_savedSTDERR, STDERR_FILENO);
+    fflush( stderr );
+
+    dup2(  _savedSTDERR, STDERR_FILENO );
+    close( _savedSTDERR );
+
+    clearerr( stderr );
+    fsetpos( stderr, &pos );
 #endif
 }
