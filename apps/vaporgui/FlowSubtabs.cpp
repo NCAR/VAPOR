e70eaaf80790c277e28febdd0b0faf4072e5b8f5
sgpearse <pearse@ucar.edu>
2020-01-30 13:18:30 -0700

    Issue 2180 (#2190)
    * testing
    
    * incremental
    
    * removed commented code

--- apps/vaporgui/FlowSubtabs.cpp
+++ apps/vaporgui/FlowSubtabs.cpp
@@ -377,8 +377,8 @@ void FlowSeedingSubtab::Update( VAPoR::DataMgr      *dataMgr,
     }
 
     // Velocity multiplier
-    auto mltp = _params->GetVelocityMultiplier();
-    _velocityMultiplierLineEdit->SetValue( std::to_string( mltp ) );
+    double mltp = _params->GetVelocityMultiplier();
+    _velocityMultiplierLineEdit->SetValue( mltp );
 
     // Update seeding tab
     //
@@ -593,14 +593,23 @@ FlowSeedingSubtab::_velocityMultiplierChanged( const std::string& value )
 {
     double oldval = _params->GetVelocityMultiplier();
     double newval;
+    
     try
     {
-        newval = std::stod( value );
+        // stod trips up on the character \n, so remove it if needed
+        //
+        if (value.back() == '\n') {
+            std::string tmp = value;
+            tmp.pop_back();
+            newval = std::stod( tmp );
+        }
+        else 
+            newval = std::stod( value );
     }
     catch ( const std::invalid_argument& e )
     {
-        MSG_ERR( "Bad input: " + value );
-        _velocityMultiplierLineEdit->SetValue( std::to_string( oldval ) );
+        MSG_ERR( "Bad input to Velocity Multiplier: " + value );
+        _velocityMultiplierLineEdit->SetValue( oldval );
         return;
     }
 
@@ -608,13 +617,13 @@ FlowSeedingSubtab::_velocityMultiplierChanged( const std::string& value )
     {
         // std::stod() would convert "3.83aaa" without throwing an exception.
         // We set the correct text based on the number identified.
-        _velocityMultiplierLineEdit->SetValue( std::to_string(newval) ); 
+        _velocityMultiplierLineEdit->SetValue( newval );
         // Only write back to _params if newval is different from oldval 
         if( newval != oldval )
             _params->SetVelocityMultiplier( newval );
     }
     else
-        _velocityMultiplierLineEdit->SetValue( std::to_string( oldval ) );
+        _velocityMultiplierLineEdit->SetValue( oldval );
 }
 
 
--- apps/vaporgui/VLineEdit.cpp
+++ apps/vaporgui/VLineEdit.cpp
@@ -32,28 +32,25 @@ void VLineEdit::UseDoubleMenu() {
         this, &VLineEdit::ShowContextMenu );
 }
 
-void VLineEdit::SetValue( const std::string& value ) {
-    // see if value is a valid double
-    if (_isDouble) {
-        double dValue;
-        try {
-            dValue = std::stod( value );
-        }
-        catch (...) {
-            MSG_ERR("VLineEDit::SetValue failed to set value of " + value );
-        }
-
-        std::stringstream stream;
-        if (_menuEnabled) {
-            stream << std::fixed << std::setprecision( _decDigits );
-            if (_scientific)
-                stream << std::scientific;
-        }
-        stream << dValue << std::endl;
-        _value = stream.str();
+void VLineEdit::SetValue( double value ) {
+    VAssert( _isDouble );
+    
+    std::stringstream stream;
+    if (_menuEnabled) {
+        stream << std::fixed << std::setprecision( _decDigits );
+        if (_scientific)
+            stream << std::scientific;
     }
-    else
-        _value = value;
+    stream << value << std::endl;
+    _value = stream.str();
+    
+    _lineEdit->blockSignals(true);
+    _lineEdit->setText( QString::fromStdString(_value) );
+    _lineEdit->blockSignals(false);
+}
+
+void VLineEdit::SetValue( const std::string& value ) {
+    _value = value;
 
     _lineEdit->blockSignals(true);
     _lineEdit->setText( QString::fromStdString(_value) );
