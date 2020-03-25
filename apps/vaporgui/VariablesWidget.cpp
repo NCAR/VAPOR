28b7cf1438f1b1d5cf450e9eabc8d4187acd9213
John Clyne <clyne@ucar.edu>
2020-03-25 14:35:37 -0600

    Fix needed for renderers that support both 2D and 3D variables

--- apps/vaporgui/VariablesWidget.cpp
+++ apps/vaporgui/VariablesWidget.cpp
@@ -255,12 +255,14 @@ void VariablesWidget::setVariableDims(int index){
 	//_activeDim = index == 0 ? TWODIMS : THREEDIMS;
     if (index == 0) {
         _activeDim = TWODIMS;
+        _rParams->GetBox()->SetPlanar(true);
         _rParams->GetBox()->SetOrientation(VAPoR::Box::XY);
         //orientationFrame->show();
     }
     else {
         _activeDim = THREEDIMS;
         orientationFrame->hide();
+        _rParams->GetBox()->SetPlanar(false);
         _rParams->GetBox()->SetOrientation(VAPoR::Box::XYZ);
     }
 
