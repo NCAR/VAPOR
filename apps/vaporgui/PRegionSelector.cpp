c0ab9b6c91ada9de2fefb92c9b45406bb5e55d46
sgpearse <pearse@ucar.edu>
2020-04-14 17:40:44 -0600

    Fix PRegionSelector warning

--- apps/vaporgui/PRegionSelector.cpp
+++ apps/vaporgui/PRegionSelector.cpp
@@ -27,7 +27,7 @@ PRegionSelector::PRegionSelector(const std::string &tag, const std::string &labe
 
 void PRegionSelector::updateGUI() const
 {
-    bool on = getParams()->GetValueLong(getTag(), 0);
+    //bool on = getParams()->GetValueLong(getTag(), 0);
     //_qcheckbox->SetValue(on);
 }
 
