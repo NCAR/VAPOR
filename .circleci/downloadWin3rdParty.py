774283628518bcb54f56dc66f6568b57acf4366c
Scott Pearse <pearse@ucar.edu>
2020-04-17 08:38:26 -0600

    Update 3rd party libraries for Win and OSX executors

--- .circleci/downloadWin3rdParty.py
+++ .circleci/downloadWin3rdParty.py
@@ -1,5 +1,5 @@
 import gdown 
-url = "https://drive.google.com/a/ucar.edu/uc?id=1sRlE06jSVrCScrt546G4UtI3hj93xV2W"
+url = "https://drive.google.com/a/ucar.edu/uc?id=1NRMu4_g8ZXu9bILBVRDsuUKIGBiT201"
 output = "2019-Aug-Win32.zip"
 gdown.download(url, output, quiet=False)
 
