d906466eebe100b59f882394811ba03f49e8bfcc
Stanislaw Jaroszynski <stas708@gmail.com>
2020-05-29 19:27:52 -0600

    Made var private

--- apps/vaporgui/PGroup.h
+++ apps/vaporgui/PGroup.h
@@ -10,6 +10,7 @@
 class PGroup : public PWidget {
     Q_OBJECT
     
+    QWidget *_widget;
     std::vector<PWidget *> _children;
     
 public:
@@ -18,8 +19,6 @@ public:
     PGroup *Add(PWidget *pw);
 
 protected:
-    QWidget *_widget;
-    
     PGroup(QWidget *w);
     void updateGUI() const override;
 };
