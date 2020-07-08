a442ed052463acc716e43ea2a6c65274dcf444af
Scott Pearse <pearse@ucar.edu>
2020-07-08 09:00:55 -0600

    Fix unguarded if clauses

--- apps/vaporgui/VaporTable.cpp
+++ apps/vaporgui/VaporTable.cpp
@@ -101,10 +101,12 @@ void VaporTable::Update(int rows, int cols,
         _activeRow = -1;
         _activeCol = -1;
     }
-    if (_activeRow >= rows)
+    if (_activeRow >= rows) {
         _activeRow = rows-1;
-    if (_activeCol >= cols)
+    }
+    if (_activeCol >= cols) {
         _activeCol = cols-1;
+    }
 
 	if (_highlightFlags & ROWS) highlightActiveRow(_activeRow);
 	if (_highlightFlags & COLS) highlightActiveCol(_activeCol);
@@ -390,8 +392,9 @@ void VaporTable::setVerticalHeader(std::vector<std::string> header) {
 		_table->verticalHeader()->hide();
 		return;
 	}
-    else if ( _table->verticalHeader()->isHidden() )
+    else if ( _table->verticalHeader()->isHidden() ) {
         _table->verticalHeader()->show();
+    }
 	
 	QStringList list;
 	for (int i=0; i<size; i++) {
@@ -424,8 +427,9 @@ void VaporTable::SetCheckboxesInFinalColumn(bool enabled) {
 }
 
 void VaporTable::EnableDisableCheckboxes(bool enabled) {
-    if (!_lastRowIsCheckboxes && !_lastColIsCheckboxes)
+    if (!_lastRowIsCheckboxes && !_lastColIsCheckboxes) {
         return;
+    }
 
     _checkboxesEnabled = enabled;
 }
