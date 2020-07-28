f40c6ac95fdffd12076e21a43a461a24e2e5b795
Scott Pearse <pearse@ucar.edu>
2020-07-28 11:12:50 -0600

    test

--- share/gitHooks/setupHooks.sh
+++ share/gitHooks/setupHooks.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
-ln -s $(pwd)/pre-commit $HOOK_DIR/pre-commit
+ROOT_DIR=$(git rev-parse --show-toplevel)
+HOOK_DIR=$ROOT_DIR/.git/hooks
+rm $HOOK_DIR/pre-commit
+ln -s $ROOT_DIR/share/gitHooks/pre-commit $HOOK_DIR/pre-commit
