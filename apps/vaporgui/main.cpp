775b904bba65e511e9a4f2e71127eec475e2b960
Stanislaw Jaroszynski <stas708@gmail.com>
2020-08-04 12:54:05 -0600

    Address warning

--- apps/vaporgui/main.cpp
+++ apps/vaporgui/main.cpp
@@ -219,8 +219,9 @@ if (getenv("VAPOR_DEBUG"))
         estatus = mw->RenderAndExit(opt.renderRange.min, opt.renderRange.max, opt.outputPath, opt.resolution.nx, opt.resolution.ny);
     }
     
-    if (estatus == 0)
+    if (estatus == 0) {
         estatus = a.exec();
+    }
 
 	if (diagfp) fclose(diagfp);
 	if (errfp) fclose(errfp);
