ee491b9151b87a32dd27c4adb0d5b75fae1c7261
John Clyne <clyne@ucar.edu>
2020-10-22 08:17:48 -0600

    Fixed comments

--- apps/vaporgui/PShowIf.h
+++ apps/vaporgui/PShowIf.h
@@ -3,12 +3,12 @@
 #include "PWidgetWrapper.h"
 #include "PGroup.h"
 
-//! \class Pif
+//! \class PShowIf
 //! \brief Shows and hides PWidgets based on a logic test.
 //! \author Stas Jaroszynski
 //!
 //! For example:
-//! (new Pif("param_tag"))->Not()->Equals("Hi")->Then(
+//! (new PShowIf("param_tag"))->Not()->Equals("Hi")->Then(
 //!     PWidgets here will only show if param_tag != "Hi"
 //! )->Else(
 //!     PWidgets here will only show if param_tag == "Hi"
@@ -23,7 +23,7 @@ public:
     PShowIf (std::string tag);
     PShowIf *Equals(long l);
     PShowIf *Equals(std::string s);
-//    Pif *Or(Pif *);
+//    PShowIf *Or(PShowIf *);
     PShowIf *Not();
     PShowIf *Then(PWidget *p);
     PShowIf *Else(PWidget *p);
