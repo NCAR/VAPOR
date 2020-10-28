82181e7dd183e715f6b48657a6c8ef6b30a8b033
Samuel Li <shaomenglee@gmail.com>
2020-10-28 15:38:50 -0600

    continue fixing missing namespace

--- lib/flow/GrownGrid.cpp
+++ lib/flow/GrownGrid.cpp
@@ -34,7 +34,7 @@ float GrownGrid::GetMissingValue() const
     return _grid2d->GetMissingValue();
 }
 
-void GrownGrid::GetUserExtentsHelper( DblArr3 &minu, DblArr3 &maxu) const
+void GrownGrid::GetUserExtentsHelper( VAPoR::DblArr3 &minu, VAPoR::DblArr3 &maxu) const
 {
     _grid2d->GetUserExtents( minu, maxu );
 
@@ -44,24 +44,24 @@ void GrownGrid::GetUserExtentsHelper( DblArr3 &minu, DblArr3 &maxu) const
 	}
 }
     
-bool GrownGrid::InsideGrid(const DblArr3 &coords) const
+bool GrownGrid::InsideGrid(const VAPoR::DblArr3 &coords) const
 {
     // Note that we don't use defaultZ to decide if a position is inside of 
     // a grid or not.
     return ( _grid2d->InsideGrid( coords ) );
 }
 
-float GrownGrid::GetValue(const DblArr3 &coords) const
+float GrownGrid::GetValue(const VAPoR::DblArr3 &coords) const
 {
     return _grid2d->GetValue( coords );
 }
     
-float GrownGrid::GetValueNearestNeighbor(const DblArr3 &coords) const
+float GrownGrid::GetValueNearestNeighbor(const VAPoR::DblArr3 &coords) const
 {
     return _grid2d->GetValue( coords );
 }
     
-float GrownGrid::GetValueLinear(const DblArr3 &coords) const 
+float GrownGrid::GetValueLinear(const VAPoR::DblArr3 &coords) const 
 {
     return _grid2d->GetValue( coords );
 }
@@ -93,27 +93,27 @@ const std::vector<size_t>& GrownGrid::GetCellDimensions() const
     return( GetDimensions() );
 }
     
-bool GrownGrid::GetIndicesCell(const DblArr3 &coords,
-	Size_tArr3 &indices
+bool GrownGrid::GetIndicesCell(const VAPoR::DblArr3 &coords,
+	VAPoR::Size_tArr3 &indices
      ) const 
 {
     return false;
 }
     
 bool GrownGrid::GetCellNodes(
-	const Size_tArr3 &cindices, std::vector <Size_tArr3> &nodes) const 
+	const VAPoR::Size_tArr3 &cindices, std::vector <VAPoR::Size_tArr3> &nodes) const 
 {
     return false;
 }
     
 bool GrownGrid::GetCellNeighbors(
-    const Size_tArr3 &cindices, std::vector <Size_tArr3> &nodes) const 
+    const VAPoR::Size_tArr3 &cindices, std::vector <VAPoR::Size_tArr3> &nodes) const 
 {
     return false;
 }
     
 bool GrownGrid::GetNodeCells(
-    const Size_tArr3 &cindices, std::vector <Size_tArr3> &nodes) const 
+    const VAPoR::Size_tArr3 &cindices, std::vector <VAPoR::Size_tArr3> &nodes) const 
 {
     return false;
 }
