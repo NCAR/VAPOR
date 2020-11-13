9878391050f46fc82b645f327ae029cad3ad1b92
StasJ <StasJ@users.noreply.github.com>
2020-11-13 16:21:11 -0700

    #2419 Streamline data file name is hard to modify (#2432)
    * Change from open to save file
    
    * Typo

--- apps/vaporgui/FlowEventRouter.cpp
+++ apps/vaporgui/FlowEventRouter.cpp
@@ -65,7 +65,7 @@ FlowEventRouter::FlowEventRouter(QWidget *parent, ControlExec *ce)
         }),
         
         new PSection("Write Flowlines to File", {
-            new PFileOpenSelector(FP::_flowlineOutputFilenameTag, "Target file"),
+            new PFileSaveSelector(FP::_flowlineOutputFilenameTag, "Target file"),
             (new PButton("Write to file", [](ParamsBase *p){p->SetValueLong(FP::_needFlowlineOutputTag, "", true);}))->DisableUndo(),
             new PLabel("Specify variables to sample and output along the flowlines"),
             new PMultiVarSelector(FP::_flowOutputMoreVariablesTag)
