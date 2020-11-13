f228cb501e22c4ee2e31574bee8102d6ed1d1f42
Scott Pearse <pearse@ucar.edu>
2020-11-13 14:23:07 -0700

    update OSX deps

--- .circleci/getOSXDeps.sh
+++ .circleci/getOSXDeps.sh
@@ -6,7 +6,7 @@ pip3 install gdown
 sudo mkdir -p /usr/local/VAPOR-Deps
 sudo chmod 777 /usr/local/VAPOR-Deps
 cd /usr/local/VAPOR-Deps
-gdown https://drive.google.com/uc?id=1p47yeyjyUfxlc7-eglsXkjCGYJLqqtEs
+gdown https://drive.google.com/uc?id=1Q-IXlP_OgZSXsWKmT-smyrW9xnR-dUfg
 cd /usr/local/VAPOR-Deps
 tar xf 2019-Aug-Darwin.tar.xz -C /usr/local/VAPOR-Deps
 chmod -R 777 /usr/local/VAPOR-Deps
