55a6accb99aa7ffdf9ed00f83e8a6febab65c67c
StasJ <StasJ@users.noreply.github.com>
2020-11-17 20:47:39 -0700

    Fix #2451 (#2483)

--- lib/render/VolumeRenderer.cpp
+++ lib/render/VolumeRenderer.cpp
@@ -384,7 +384,7 @@ std::string VolumeRenderer::_getDefaultAlgorithmForGrid(const Grid *grid) const
     bool intel = GLManager::GetVendor() == GLManager::Vendor::Intel;
 
     if (dynamic_cast<const RegularGrid *>(grid)) return VolumeRegular ::GetName();
-    if (dynamic_cast<const StructuredGrid *>(grid)) return intel ? VolumeOSPRay::GetName() : VolumeCellTraversal::GetName();
+    if (dynamic_cast<const StructuredGrid *>(grid)) return intel ? VolumeRegular::GetName() : VolumeCellTraversal::GetName();
     if (dynamic_cast<const UnstructuredGrid *>(grid)) return VolumeOSPRay::GetName();
     MyBase::SetErrMsg("Unsupported grid type: %s", grid->GetType().c_str());
     return "";
