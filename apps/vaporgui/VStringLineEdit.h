844f02d78a96d6f522b15d4adf0c4e47560e373e
StasJ <StasJ@users.noreply.github.com>
2020-12-11 11:08:08 -0700

    Fix #2081 (#2567)

--- apps/vaporgui/VStringLineEdit.h
+++ apps/vaporgui/VStringLineEdit.h
@@ -33,6 +33,9 @@ class VStringLineEdit : public VHBoxWidget {
 
         //! Create a custom context menu for the QLineEdit
         void SetCustomContextMenu();
+    
+    void SetReadOnly(bool b) { _lineEdit->setReadOnly(b); }
+    void Clear() { SetValueString(""); }
 
     private:
         QLineEdit*  _lineEdit;
