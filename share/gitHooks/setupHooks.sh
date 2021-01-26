23721ab03e6d38286518d8f5b7e8646fccd15a03
Scott Pearse <pearse@ucar.edu>
2021-01-26 14:29:45 -0700

    force overwrite

--- share/gitHooks/setupHooks.sh
+++ share/gitHooks/setupHooks.sh
@@ -2,4 +2,4 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 HOOK_DIR=$ROOT_DIR/.git/hooks
-ln -s $ROOT_DIR/share/gitHooks/pre-commit $HOOK_DIR/pre-commit
+ln -sf $ROOT_DIR/share/gitHooks/pre-commit $HOOK_DIR/pre-commit
