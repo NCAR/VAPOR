43a889c2f84ca0004958a893cb0efecf8ebb5ad0
StasJ <stas708@gmail.com>
2021-02-23 14:26:26 -0700

    colorbar

---
+++ include/vapor/ColorbarRenderer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <functional>
+#include <string>
+
+namespace VAPoR {
+struct GLManager;
+class RenderParams;
+class MapperFunction;
+
+class ColorbarRenderer {
+public:
+    static void Render(GLManager *glm, RenderParams *rp);
+private:
+    static std::function<std::string(float)> makeFormatter(MapperFunction *mf, int sigFigs, bool scientific);
+};
+}
