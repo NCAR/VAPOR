b92ce4122ab9f7c491f74f994487c52525fbc263
StasJ <stas708@gmail.com>
2021-02-25 17:55:25 -0700

    clang-format pre-push hook

--- lib/render/ColorbarRenderer.cpp
+++ lib/render/ColorbarRenderer.cpp
@@ -210,7 +210,7 @@ void ColorbarRenderer::Render(GLManager *glm, RenderParams *rp)
 
     glDisable(GL_DEPTH_TEST);
     glDepthMask(GL_FALSE);
-    
+
     lgl->Color(foregroundColor);
     DrawRect(lgl, pos, size);
     lgl->Color(backgroundColor);
