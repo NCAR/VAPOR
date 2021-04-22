a43af37435ec078a8e27eb1a969364b4ca339be3
John Clyne <clyne@ucar.edu>
2021-04-22 14:59:13 -0600

    clang-format pre-push hook

--- test_apps/datamgr/test_datamgr.cpp
+++ test_apps/datamgr/test_datamgr.cpp
@@ -184,7 +184,7 @@ void test_get_value(Grid *g)
     size_t n = VProduct(g->GetDimensions());
 
     size_t ecount = 0;
-    int requested_num_threads = get_num_ompthreads();
+    int    requested_num_threads = get_num_ompthreads();
 #pragma omp parallel num_threads(requested_num_threads);
     {
         size_t my_ecount = 0;
