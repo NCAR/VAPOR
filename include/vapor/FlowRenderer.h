9f9d714d152e44ea66b887678125f8101e95e079
Samuel Li <shaomeng@cisl-vapor>
2021-05-10 13:41:18 -0600

    add a change

--- include/vapor/FlowRenderer.h
+++ include/vapor/FlowRenderer.h
@@ -20,7 +20,6 @@ class RENDER_API FlowRenderer final : public Renderer {
 public:
     FlowRenderer(const ParamsMgr *pm, std::string &winName, std::string &dataSetName, std::string &instName, DataMgr *dataMgr);
 
-    // Rule of five
     ~FlowRenderer();
     FlowRenderer(const FlowRenderer &) = delete;
     FlowRenderer(const FlowRenderer &&) = delete;
