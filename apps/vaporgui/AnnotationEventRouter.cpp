a78025625fc335f548685a0d3fb0ce88f50b7085
Scott Pearse <pearse@ucar.edu>
2021-05-25 08:59:32 -0600

    resolve Ubuntu warning

--- apps/vaporgui/AnnotationEventRouter.cpp
+++ apps/vaporgui/AnnotationEventRouter.cpp
@@ -56,25 +56,6 @@
 
 using namespace VAPoR;
 
-namespace {
-
-template<typename Out> void split(const std::string &s, char delim, Out result)
-{
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) { *(result++) = item; }
-}
-
-std::vector<std::string> split(const std::string &s, char delim)
-{
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
-
-}    // namespace
-
 AnnotationEventRouter::AnnotationEventRouter(QWidget *parent, ControlExec *ce) : QWidget(parent), EventRouter(ce, AnnotationParams::GetClassType())
 {
     setLayout(new QVBoxLayout);
