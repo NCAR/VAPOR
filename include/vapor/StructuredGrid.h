0bed95675b49755fc1b94985c324016710bedc51
Samuel Li <shaomeng@cisl-vapor>
2021-06-11 14:24:46 -0600

    work on StructuredGrid

--- include/vapor/StructuredGrid.h
+++ include/vapor/StructuredGrid.h
@@ -74,6 +74,8 @@ public:
     {
         auto tmp = GetDimensions();
         _nodeDimCache = {tmp[0], tmp[1], tmp[2]};
+        while( _nodeDimCache.back() == 1 )
+            _nodeDimCache.pop_back();
         return _nodeDimCache;
     }
 
