ca61c4d4ca0f896dc3e3a2442e29356a8e76f5dc
StasJ <stas708@gmail.com>
2021-07-07 16:36:46 -0600

    misc

--- apps/vaporgui/CheckForNotices.cpp
+++ apps/vaporgui/CheckForNotices.cpp
@@ -54,7 +54,7 @@ void CheckForGHNotices(std::function<void(const std::vector<Notice> &)> callback
                     Notice      notice;
                     notice.url = file["download_url"].toString().toStdString();
 
-                    if (!STLUtils::Contains(notice.url, "__example-notice")) continue;
+                    if (STLUtils::Contains(notice.url, "__example-notice")) continue;
 
                     _noticesToGet.push(notice);
                 }
