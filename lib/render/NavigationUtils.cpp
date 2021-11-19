2de6016c6aaf92f54982b00fb3a7554781d50d65
StasJ <StasJ@users.noreply.github.com>
2021-11-19 02:35:23 +0100

    Fix Align View dropdown is broken #2888 (#2909)
    * Fix #2888
    
    * clang-format pre-push hook

--- lib/render/NavigationUtils.cpp
+++ lib/render/NavigationUtils.cpp
@@ -85,6 +85,9 @@ void NavigationUtils::ViewAll(ControlExec *ce)
 
 void NavigationUtils::AlignView(ControlExec *ce, int axis)
 {
+    // This fixes a bug in this legacy function where the up dir is occationally wrong
+    if (axis != 3 && axis != 1) AlignView(ce, 3);
+
     float axes[3][3] = {{1.f, 0.f, 0.f}, {0.f, 1.f, 0.f}, {0.f, 0.f, 1.f}};
 
     double dirvec[3] = {0.0, 0.0, 0.0};
@@ -169,7 +172,7 @@ void NavigationUtils::AlignView(ControlExec *ce, int axis)
         curPosVec[i] = (curCenter[i] - dirvec[i]);
     }
 
-    SetAllCameras(ce, curPosVec, curViewDir, curUpVec, curCenter);
+    SetAllCameras(ce, curPosVec, dirvec, curUpVec, curCenter);
 }
 
 
