8103caf5f1f57110680a12c0f3146bb2d5ef6b35
Scott Pearse <pearse@ucar.edu>
2022-01-21 12:29:46 -0700

    document planeDescription

--- include/vapor/ArbitrarilyOrientedRegularGrid.h
+++ include/vapor/ArbitrarilyOrientedRegularGrid.h
@@ -8,6 +8,11 @@
 
 // clang-format off
 
+//! A struct that describes a 2D plane that will sample a 3D grid through a domain.
+//! The 2D plane will have an equal number of samples on its side, determined by sideSize.  Users
+//! will need to define an origin for the 2D plane, as well as its rotation in degrees on the X, Y,
+//! and Z axes.  The domainMin and domainMax values specify the 3D space that will be sampled, and
+//! samples that lie outside of boxMin and boxMax will be treated as missing.
 struct planeDescription {
     size_t sideSize;
     std::vector<double> origin;
@@ -23,8 +28,9 @@ namespace VAPoR {
 //! Create a 2D RegularGrid that is rotated along an arbitrary orientation.  This new grid
 //! samples a given 3D Grid object along a plane, through the entire domain.  The "planeDescription" 
 //! variable includes parameters that define the 2D plane's origin, rotation, and valid extents.  Points on the
-//! 2D RegularGrid that are outside of the valid extents will be assigned as
-//! missingValues.
+//! 2D RegularGrid that are outside of the valid extents will be assigned as missingValues.  This Grid class
+//! will always be valid, even if the the plane's description lies outside of the box.  In this case, the
+//! plane will contain all missing values.
 //!
 //! Note: The boxMin and boxMax values within the planeDescription should come from
 //! the current RenderParams' Box class.  The domainMin and domainMax values should
