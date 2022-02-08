NCAR/VAPOR

commit 76487dd7ff7bb87952c86dc7b2a7760c96092582

Fixed #2708 - Smoke tests failing on 1D and 2D grids (#2994)
* Fixed #2708 - Smoke tests failing on 1D and 2D grids

* clang-format

lib/vdc/CurvilinearGrid.cpp
@@ -46,7 +46,6 @@ CurvilinearGrid::CurvilinearGrid(const DimsType &dims, const DimsType &bs, const
                                  std::shared_ptr<const QuadTreeRectangleP> qtr)
 : StructuredGrid(dims, bs, blks)
 {
-
     // Only support 2D X & Y coordinates currently. I.e. only support
     // "layered" curvilinear grids
     //
@@ -275,7 +274,6 @@ CurvilinearGrid::ConstCoordItrCG::ConstCoordItrCG(const CurvilinearGrid *cg, boo
 {
     _cg = cg;
     auto dims = _cg->GetDimensions();
-    auto ndims = _cg->GetNumDimensions();
     _index = {0, 0, 0};
     _coords = {0.0, 0.0, 0.0};
     _terrainFollowing = _cg->_terrainFollowing;
@@ -288,22 +286,17 @@ CurvilinearGrid::ConstCoordItrCG::ConstCoordItrCG(const CurvilinearGrid *cg, boo
         _yCoordItr = _cg->_yrg.cend();
         if (_terrainFollowing) { _zCoordItr = _cg->_zrg.cend(); }
 
-        if (ndims < 1)
-            _index[0] = 1;    // edge case for 0D grids
-        else
-            _index[ndims - 1] = dims[ndims - 1];
+        _index = {0, 0, dims[dims.size() - 1]};
 
         return;
     }
     _coords[0] = *_xCoordItr;
     _coords[1] = *_yCoordItr;
 
-    if (ndims == 3) {
-        if (_terrainFollowing) {
-            _coords[2] = *_zCoordItr;
-        } else {
-            _coords[2] = _cg->_zcoords[0];
-        }
+    if (_terrainFollowing) {
+        _coords[2] = *_zCoordItr;
+    } else {
+        _coords[2] = _cg->_zcoords[0];
     }
 }
 
@@ -341,8 +334,6 @@ void CurvilinearGrid::ConstCoordItrCG::next()
         return;
     }
 
-    if (_cg->GetNumDimensions() <= 1) return;
-
     _index[0] = 0;
     _index[1]++;
 
@@ -352,8 +343,6 @@ void CurvilinearGrid::ConstCoordItrCG::next()
         if (_terrainFollowing) { _coords[2] = *_zCoordItr; }
         return;
     }
-
-    if (_cg->GetNumDimensions() == 2) return;
 
     _index[1] = 0;
     _index[2]++;
@@ -370,36 +359,29 @@ void CurvilinearGrid::ConstCoordItrCG::next()
         }
         return;
     }
+
+    _index = {0, 0, dims[dims.size() - 1]};    // last index
 }
 
 void CurvilinearGrid::ConstCoordItrCG::next(const long &offset)
 {
-
     auto dims = _cg->GetDimensions();
     auto ndims = _cg->GetNumDimensions();
 
     if (!ndims) return;
 
-    DimsType maxIndex = {0, 0, 0};
-
-    for (int i = 0; i < ndims; i++) {
-        VAssert(dims[i] > 0);
-        maxIndex[i] = dims[i] - 1;
-    }
-
-    long maxIndexL = Wasp::LinearizeCoords(maxIndex.data(), dims.data(), ndims);
-    long newIndexL = Wasp::LinearizeCoords(_index.data(), dims.data(), ndims) + offset;
+    long maxIndexL = Wasp::VProduct(dims.data(), dims.size()) - 1;
+    long newIndexL = Wasp::LinearizeCoords(_index.data(), dims.data(), dims.size()) + offset;
     if (newIndexL < 0) { newIndexL = 0; }
     if (newIndexL > maxIndexL) {
-        _index = {0, 0, 0};
-        _index[ndims - 1] = dims[ndims - 1];
+        _index = {0, 0, dims[dims.size() - 1]};
         return;
     }
 
     size_t index2DL = _index[1] * dims[0] + _index[0];
 
     _index = {0, 0, 0};
-    Wasp::VectorizeCoords(newIndexL, dims.data(), _index.data(), ndims);
+    Wasp::VectorizeCoords(newIndexL, dims.data(), _index.data(), dims.size());
 
     VAssert(_index[1] * dims[0] + _index[0] >= index2DL);
     size_t offset2D = (_index[1] * dims[0] + _index[0]) - index2DL;
@@ -409,8 +391,6 @@ void CurvilinearGrid::ConstCoordItrCG::next(const long &offset)
 
     _coords[0] = *_xCoordItr;
     _coords[1] = *_yCoordItr;
-
-    if (ndims == 2) return;
 
     if (_terrainFollowing) {
         _zCoordItr += offset;
@@ -735,8 +715,8 @@ bool CurvilinearGrid::_insideGrid(double x, double y, double z, size_t &i, size_
 
 std::shared_ptr<QuadTreeRectangleP> CurvilinearGrid::_makeQuadTreeRectangle() const
 {
-    const DimsType &      dims = GetCellDimensions();
-    size_t                reserve_size = dims[0] * dims[1];
+    const DimsType &dims = GetCellDimensions();
+    size_t          reserve_size = dims[0] * dims[1];
 
     CoordType minu, maxu;
     GetUserExtents(minu, maxu);

include/vapor/Grid.h
@@ -101,10 +101,10 @@ public:
 
     //! Return the useful number of dimensions of grid connectivity array
     //!
-    //! \param[out] dims The number of values of \p dims parameter provided to
+    //! \param[out] dims The number of non-unit components of \p dims parameter provided to
     //! the constructor.
     //!
-    size_t GetNumDimensions() const { return _nDims; }
+    size_t GetNumDimensions() const { return GetNumDimensions(_dims); }
 
     //! Return the number of non-unit dimensions
     //!
@@ -139,10 +139,10 @@ public:
     //
     virtual size_t GetGeometryDim() const = 0;
 
-    virtual const DimsType &           GetNodeDimensions() const = 0;
-    virtual const size_t               GetNumNodeDimensions() const = 0;
-    virtual const DimsType &           GetCellDimensions() const = 0;
-    virtual const size_t               GetNumCellDimensions() const = 0;
+    virtual const DimsType &GetNodeDimensions() const = 0;
+    virtual const size_t    GetNumNodeDimensions() const = 0;
+    virtual const DimsType &GetCellDimensions() const = 0;
+    virtual const size_t    GetNumCellDimensions() const = 0;
 
     //! Return the ijk dimensions of grid in blocks
     //!
@@ -982,7 +982,6 @@ public:
 
     protected:
         DimsType _dims;
-        size_t   _nDims;
         DimsType _index;
         DimsType _lastIndex;
     };
@@ -1057,7 +1056,6 @@ public:
 
     protected:
         DimsType _dims;
-        size_t   _nDims;
         DimsType _index;
         DimsType _lastIndex;
     };
@@ -1146,14 +1144,13 @@ public:
         ForwardIterator<T> &operator=(ForwardIterator<T> rhs);
         ForwardIterator<T> &operator=(ForwardIterator<T> &rhs) = delete;
 
-        bool operator==(const ForwardIterator<T> &rhs) const { return (_indexL == rhs._indexL); }
-        bool operator!=(const ForwardIterator<T> &rhs) { return (_indexL != rhs._indexL); }
+        bool operator==(const ForwardIterator<T> &rhs) const { return (_index == rhs._index); }
+        bool operator!=(const ForwardIterator<T> &rhs) { return (_index != rhs._index); }
 
         const ConstCoordItr &GetCoordItr() { return (_coordItr); }
 
         friend void swap(Grid::ForwardIterator<T> &a, Grid::ForwardIterator<T> &b)
         {
-            std::swap(a._ndims, b._ndims);
             std::swap(a._blks, b._blks);
             std::swap(a._dims3d, b._dims3d);
             std::swap(a._bdims3d, b._bdims3d);
@@ -1161,25 +1158,22 @@ public:
             std::swap(a._blocksize, b._blocksize);
             std::swap(a._coordItr, b._coordItr);
             std::swap(a._index, b._index);
-            std::swap(a._indexL, b._indexL);
-            std::swap(a._end_indexL, b._end_indexL);
+            std::swap(a._lastIndex, b._lastIndex);
             std::swap(a._xb, b._xb);
             std::swap(a._itr, b._itr);
             std::swap(a._pred, b._pred);
         }
 
     private:
-        size_t               _ndims;
         std::vector<float *> _blks;
         DimsType             _dims3d;
         DimsType             _bdims3d;
         DimsType             _bs3d;
         size_t               _blocksize;
         ConstCoordItr        _coordItr;
-        DimsType             _index;         // current index into grid
-        size_t               _indexL;        // current index into grid
-        size_t               _end_indexL;    // Last valid index
-        size_t               _xb;            // x index within a block
+        DimsType             _index;        // current index into grid
+        DimsType             _lastIndex;    // Last valid index
+        size_t               _xb;           // x index within a block
         float *              _itr;
         InsideBox            _pred;
     };
@@ -1270,8 +1264,7 @@ protected:
     }
 
 private:
-    DimsType             _dims;    // dimensions of grid arrays
-    size_t               _nDims;
+    DimsType             _dims;                   // dimensions of grid arrays
     DimsType             _bs = {{1, 1, 1}};       // dimensions of each block
     DimsType             _bdims = {{1, 1, 1}};    // dimensions (specified in blocks) of ROI
     std::vector<size_t>  _bsDeprecated;           // legacy API
@@ -1289,8 +1282,6 @@ private:
     mutable CoordType    _maxuCache = {{std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()}};
 
     void _grid(const DimsType &dims, const DimsType &bs, const std::vector<float *> &blks, size_t topology_dimension);
-
-    virtual void _getUserCoordinatesHelper(const std::vector<double> &coords, double &x, double &y, double &z) const;
 };
 
 template void Grid::CopyToArr3<size_t>(const std::vector<size_t> &src, std::array<size_t, 3> &dst);

include/vapor/RegularGrid.h
@@ -99,7 +99,6 @@ public:
     private:
         DimsType  _index;
         DimsType  _dims;
-        size_t    _nDims;
         CoordType _minu;
         CoordType _delta;
         CoordType _coords;
@@ -122,10 +121,10 @@ protected:
 private:
     void _regularGrid(const CoordType &minu, const CoordType &maxu);
 
-    CoordType           _minu = {{0.0, 0.0, 0.0}};
-    CoordType           _maxu = {{0.0, 0.0, 0.0}};
-    size_t              _geometryDim;
-    CoordType           _delta;    // increment between grid points in user coords
+    CoordType _minu = {{0.0, 0.0, 0.0}};
+    CoordType _maxu = {{0.0, 0.0, 0.0}};
+    size_t    _geometryDim;
+    CoordType _delta;    // increment between grid points in user coords
 };
 };    // namespace VAPoR
 #endif

lib/vdc/Grid.cpp
@@ -21,11 +21,7 @@
 using namespace std;
 using namespace VAPoR;
 
-Grid::Grid()
-{
-    _dims = {1, 1, 1};
-    _nDims = 0;
-}
+Grid::Grid() { _dims = {1, 1, 1}; }
 
 void Grid::_grid(const DimsType &dims, const DimsType &bs, const std::vector<float *> &blks, size_t topology_dimension)
 {
@@ -42,7 +38,6 @@ void Grid::_grid(const DimsType &dims, const DimsType &bs, const std::vector<flo
             blks.size() == std::accumulate(_bdims.begin(), _bdims.end(), 1, std::multiplies<size_t>()));
 
     _dims = dims;
-    _nDims = GetNumDimensions(dims);
     _periodic = vector<bool>(topology_dimension, false);
     _topologyDimension = topology_dimension;
     _missingValue = INFINITY;
@@ -179,23 +174,9 @@ void Grid::GetRange(const DimsType &min, const DimsType &max, float range[2]) co
 
     range[0] = range[1] = mv;
 
-    size_t jmin = 0;
-    size_t jmax = 0;
-    if (GetNumDimensions() > 1) {
-        jmin = cMin[1];
-        jmax = cMax[1];
-    }
-
-    size_t kmin = 0;
-    size_t kmax = 0;
-    if (GetNumDimensions() > 2) {
-        kmin = cMin[2];
-        kmax = cMax[2];
-    }
-
     bool first = true;
-    for (size_t k = kmin; k <= kmax; k++) {
-        for (size_t j = jmin; j <= jmax; j++) {
+    for (size_t k = cMin[2]; k <= cMax[2]; k++) {
+        for (size_t j = cMin[1]; j <= cMax[1]; j++) {
             for (size_t i = cMin[0]; i <= cMax[0]; i++) {
                 float v = AccessIJK(i, j, k);
                 if (first && v != mv) {
@@ -236,12 +217,6 @@ float Grid::GetValue(const CoordType &coords) const
     }
 }
 
-void Grid::_getUserCoordinatesHelper(const vector<double> &coords, double &x, double &y, double &z) const
-{
-    if (GetNumDimensions() >= 1) { x = coords[0]; }
-    if (GetNumDimensions() >= 2) { y = coords[1]; }
-    if (GetNumDimensions() >= 3) { z = coords[2]; }
-}
 
 void Grid::GetUserCoordinates(size_t i, double &x, double &y, double &z) const
 {
@@ -249,7 +224,9 @@ void Grid::GetUserCoordinates(size_t i, double &x, double &y, double &z) const
     vector<size_t> indices = {i};
     vector<double> coords;
     GetUserCoordinates(indices, coords);
-    _getUserCoordinatesHelper(coords, x, y, z);
+    x = coords[0];
+    y = coords[1];
+    z = coords[2];
 }
 
 void Grid::GetUserCoordinates(size_t i, size_t j, double &x, double &y, double &z) const
@@ -258,7 +235,9 @@ void Grid::GetUserCoordinates(size_t i, size_t j, double &x, double &y, double &
     vector<size_t> indices = {i, j};
     vector<double> coords;
     GetUserCoordinates(indices, coords);
-    _getUserCoordinatesHelper(coords, x, y, z);
+    x = coords[0];
+    y = coords[1];
+    z = coords[2];
 }
 
 void Grid::GetUserCoordinates(size_t i, size_t j, size_t k, double &x, double &y, double &z) const
@@ -267,7 +246,9 @@ void Grid::GetUserCoordinates(size_t i, size_t j, size_t k, double &x, double &y
     vector<size_t> indices = {i, j, k};
     vector<double> coords;
     GetUserCoordinates(indices, coords);
-    _getUserCoordinatesHelper(coords, x, y, z);
+    x = coords[0];
+    y = coords[1];
+    z = coords[2];
 }
 
 void Grid::SetInterpolationOrder(int order)
@@ -285,14 +266,8 @@ void Grid::SetInterpolationOrder(int order)
 Grid::ConstNodeIteratorSG::ConstNodeIteratorSG(const Grid *g, bool begin) : ConstNodeIteratorAbstract()
 {
     _dims = g->GetNodeDimensions();
-    _nDims = g->GetNumNodeDimensions();
     _index = {0, 0, 0};
-    _lastIndex = {0, 0, 0};
-
-    if (_nDims < 1)
-        _lastIndex[0] = 1;    // edge case for 0D grids
-    else
-        _lastIndex[_nDims - 1] = _dims[_nDims - 1];
+    _lastIndex = {0, 0, _dims[_dims.size() - 1]};
 
     if (!begin) { _index = _lastIndex; }
 }
@@ -300,7 +275,6 @@ Grid::ConstNodeIteratorSG::ConstNodeIteratorSG(const Grid *g, bool begin) : Cons
 Grid::ConstNodeIteratorSG::ConstNodeIteratorSG(const ConstNodeIteratorSG &rhs) : ConstNodeIteratorAbstract()
 {
     _dims = rhs._dims;
-    _nDims = rhs._nDims;
     _index = rhs._index;
     _lastIndex = rhs._lastIndex;
 }
@@ -308,7 +282,6 @@ Grid::ConstNodeIteratorSG::ConstNodeIteratorSG(const ConstNodeIteratorSG &rhs) :
 Grid::ConstNodeIteratorSG::ConstNodeIteratorSG() : ConstNodeIteratorAbstract()
 {
     _dims = {1, 1, 1};
-    _nDims = 0;
     _index = {0, 0, 0};
     _lastIndex = {0, 0, 0};
 }
@@ -316,16 +289,17 @@ Grid::ConstNodeIteratorSG::ConstNodeIteratorSG() : ConstNodeIteratorAbstract()
 void Grid::ConstNodeIteratorSG::next()
 {
     _index[0]++;
-    if (_index[0] < _dims[0] || _nDims <= 1) { return; }
+    if (_index[0] < _dims[0]) { return; }
 
     _index[0] = 0;
     _index[1]++;
-    if (_index[1] < _dims[1] || _nDims == 2) { return; }
+    if (_index[1] < _dims[1]) { return; }
 
     _index[1] = 0;
     _index[2]++;
-    if (_index[2] < _dims[2] || _nDims == 3) { return; }
-    VAssert(0 && "Invalid state");
+    if (_index[2] < _dims[2]) { return; }
+
+    _index[2] = _dims[2];    // last index;
 }
 
 void Grid::ConstNodeIteratorSG::next(const long &offset)
@@ -381,14 +355,8 @@ void Grid::ConstNodeIteratorBoxSG::next(const long &offset)
 Grid::ConstCellIteratorSG::ConstCellIteratorSG(const Grid *g, bool begin) : ConstCellIteratorAbstract()
 {
     _dims = g->GetCellDimensions();
-    _nDims = g->GetNumCellDimensions();
     _index = {0, 0, 0};
-    _lastIndex = _index;
-
-    if (_nDims < 1)
-        _lastIndex[0] = 1;    // edge case for 0D grids
-    else
-        _lastIndex[_nDims - 1] = _dims[_nDims - 1];
+    _lastIndex = {0, 0, _dims[_dims.size() - 1]};
 
     if (!begin) { _index = _lastIndex; }
 }
@@ -396,7 +364,6 @@ Grid::ConstCellIteratorSG::ConstCellIteratorSG(const Grid *g, bool begin) : Cons
 Grid::ConstCellIteratorSG::ConstCellIteratorSG(const ConstCellIteratorSG &rhs) : ConstCellIteratorAbstract()
 {
     _dims = rhs._dims;
-    _nDims = rhs._nDims;
     _index = rhs._index;
     _lastIndex = rhs._lastIndex;
 }
@@ -404,7 +371,6 @@ Grid::ConstCellIteratorSG::ConstCellIteratorSG(const ConstCellIteratorSG &rhs) :
 Grid::ConstCellIteratorSG::ConstCellIteratorSG() : ConstCellIteratorAbstract()
 {
     _dims = {1, 1, 1};
-    _nDims = 0;
     _index = {0, 0, 0};
     _lastIndex = {0, 0, 0};
 }
@@ -412,16 +378,17 @@ Grid::ConstCellIteratorSG::ConstCellIteratorSG() : ConstCellIteratorAbstract()
 void Grid::ConstCellIteratorSG::next()
 {
     _index[0]++;
-    if (_index[0] < (_dims[0]) || _nDims <= 1) { return; }
+    if (_index[0] < (_dims[0])) { return; }
 
     _index[0] = 0;
     _index[1]++;
-    if (_index[1] < (_dims[1]) || _nDims == 2) { return; }
+    if (_index[1] < (_dims[1])) { return; }
 
     _index[1] = 0;
     _index[2]++;
-    if (_index[2] < (_dims[2]) || _nDims == 3) { return; }
-    VAssert(0 && "Invalid state");
+    if (_index[2] < (_dims[2])) { return; }
+
+    _index[2] = _dims[2];    // last index;
 }
 
 void Grid::ConstCellIteratorSG::next(const long &offset)
@@ -527,28 +494,21 @@ void Grid::ConstCellIteratorBoxSG::next(const long &offset)
 
 template<class T> Grid::ForwardIterator<T>::ForwardIterator(T *rg, bool begin, const CoordType &minu, const CoordType &maxu) : _pred(minu, maxu)
 {
-    _ndims = rg->GetNumDimensions();
-
     _blks = rg->GetBlks();
 
     _dims3d = rg->GetDimensions();
+    _bdims3d = {1, 1, 1};
+    _bs3d = {1, 1, 1};
     CopyToArr3(rg->GetDimensionInBlks(), _bdims3d);
     CopyToArr3(rg->GetBlockSize(), _bs3d);
-    for (int i = _ndims; i < 3; i++) {
-        _dims3d[i] = 1;
-        _bdims3d[i] = 1;
-        _bs3d[i] = 1;
-    }
+
     _blocksize = Wasp::VProduct(_bs3d.data(), _bs3d.size());
 
     _index = {0, 0, 0};
-    _indexL = 0;
+    _lastIndex = {0, 0, _dims3d[_dims3d.size() - 1]};
 
-    _end_indexL = 0;
-
-    _end_indexL = Wasp::VProduct(_dims3d.data(), _dims3d.size());
     if (!begin || !_blks.size()) {
-        _indexL = _end_indexL;
+        _index = _lastIndex;
         return;
     }
 
@@ -561,7 +521,6 @@ template<class T> Grid::ForwardIterator<T>::ForwardIterator(T *rg, bool begin, c
 
 template<class T> Grid::ForwardIterator<T>::ForwardIterator(ForwardIterator<T> &&rhs)
 {
-    _ndims = rhs._ndims;
     _blks = rhs._blks;
     _dims3d = rhs._dims3d;
     _bdims3d = rhs._bdims3d;
@@ -569,8 +528,6 @@ template<class T> Grid::ForwardIterator<T>::ForwardIterator(ForwardIterator<T> &
     _blocksize = rhs._blocksize;
     _coordItr = std::move(rhs._coordItr);
     _index = rhs._index;
-    _indexL = rhs._indexL;
-    _end_indexL = rhs._end_indexL;
     _xb = rhs._xb;
     _itr = rhs._itr;
     rhs._itr = nullptr;
@@ -579,19 +536,14 @@ template<class T> Grid::ForwardIterator<T>::ForwardIterator(ForwardIterator<T> &
 
 template<class T> Grid::ForwardIterator<T>::ForwardIterator()
 {
-    _ndims = 0;
     _blks.clear();
     _dims3d = {1, 1, 1};
     _bdims3d = {1, 1, 1};
     _bs3d = {1, 1, 1};
     _blocksize = 1;
-    //_coordItr = xx;
     _index = {0, 0, 0};
-    _indexL = 0;
-    _end_indexL = 0;
     _xb = 0;
     _itr = nullptr;
-    //_pred = xx;
 }
 
 template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator=(ForwardIterator<T> rhs)
@@ -614,7 +566,6 @@ template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator++
         _xb++;
         _itr++;
         _index[0]++;
-        _indexL++;
         if (_pred.Enabled()) ++_coordItr;
 
         if (_xb < _bs3d[0] && _index[0] < _dims3d[0]) {
@@ -625,22 +576,16 @@ template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator++
 
         _xb = 0;
         if (_index[0] >= _dims3d[0]) {
-            if (_indexL == _end_indexL) {
-                return (*this);    // last element
-            }
             _index[0] = _xb = 0;
             _index[1]++;
         }
 
         if (_index[1] >= _dims3d[1]) {
-            if (_indexL == _end_indexL) {
-                return (*this);    // last element
-            }
             _index[1] = 0;
             _index[2]++;
         }
 
-        if (_indexL == _end_indexL) {
+        if (_index == _lastIndex) {
             return (*this);    // last element
         }
 
@@ -654,7 +599,7 @@ template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator++
         float *blk = _blks[zb * _bdims3d[0] * _bdims3d[1] + yb * _bdims3d[0] + xb];
         _itr = &blk[z * _bs3d[0] * _bs3d[1] + y * _bs3d[0] + x];
 
-    } while (_indexL != _end_indexL && !_pred(*_coordItr));
+    } while (_index != _lastIndex && !_pred(*_coordItr));
 
     return (*this);
 }
@@ -672,16 +617,18 @@ template<class T> Grid::ForwardIterator<T> &Grid::ForwardIterator<T>::operator+=
 
     if (!_blks.size()) return (*this);
 
-    _indexL = Wasp::LinearizeCoords(_index.data(), _dims3d.data(), _ndims) + offset;
+    long maxIndexL = Wasp::VProduct(_dims3d.data(), _dims3d.size()) - 1;
+    long indexL = Wasp::LinearizeCoords(_index.data(), _dims3d.data(), _index.size()) + offset;
 
     // Check for overflow
     //
-    if (_indexL >= _end_indexL) {
-        _indexL = _end_indexL;
+    if (indexL < 0) { indexL = 0; }
+    if (indexL > maxIndexL) {
+        _index = _lastIndex;
         return (*this);
     }
 
-    Wasp::VectorizeCoords(_indexL, _dims3d.data(), _index.data(), _ndims);
+    Wasp::VectorizeCoords(indexL, _dims3d.data(), _index.data(), _index.size());
     if (_pred.Enabled()) _coordItr += offset;
 
     size_t x = _index[0] % _bs3d[0];

lib/vdc/RegularGrid.cpp
@@ -140,14 +140,12 @@ float RegularGrid::GetValueLinear(const CoordType &coords) const
 
     if (_delta[0] != 0.0) { i = (size_t)floor((cCoords[0] - _minu[0]) / _delta[0]); }
     if (_delta[1] != 0.0) { j = (size_t)floor((cCoords[1] - _minu[1]) / _delta[1]); }
-
-    if (GetGeometryDim() == 3 && _delta[2] != 0.0) { k = (size_t)floor((cCoords[2] - _minu[2]) / _delta[2]); }
+    if (_delta[2] != 0.0) { k = (size_t)floor((cCoords[2] - _minu[2]) / _delta[2]); }
 
     auto dims = GetDimensions();
     VAssert(i < dims[0]);
     VAssert(j < dims[1]);
-
-    if (GetNumDimensions() == 3) { VAssert(k < dims[2]); }
+    VAssert(k < dims[2]);
 
     double iwgt = 0.0;
     double jwgt = 0.0;
@@ -155,8 +153,7 @@ float RegularGrid::GetValueLinear(const CoordType &coords) const
 
     if (_delta[0] != 0.0) { iwgt = ((cCoords[0] - _minu[0]) - (i * _delta[0])) / _delta[0]; }
     if (_delta[1] != 0.0) { jwgt = ((cCoords[1] - _minu[1]) - (j * _delta[1])) / _delta[1]; }
-
-    if (GetGeometryDim() == 3 && _delta[2] != 0.0) { kwgt = ((cCoords[2] - _minu[2]) - (k * _delta[2])) / _delta[2]; }
+    if (_delta[2] != 0.0) { kwgt = ((cCoords[2] - _minu[2]) - (k * _delta[2])) / _delta[2]; }
 
     float  missingValue = GetMissingValue();
     double p0, p1, p2, p3, p4, p5, p6, p7;
@@ -239,7 +236,7 @@ void RegularGrid::GetUserCoordinates(const DimsType &indices, CoordType &coords)
 
     auto dims = GetDimensions();
 
-    for (int i = 0; i < GetNumDimensions(); i++) {
+    for (int i = 0; i < dims.size(); i++) {
         size_t index = cIndices[i];
         VAssert(dims[i] > 0);
 
@@ -295,26 +292,19 @@ bool RegularGrid::InsideGrid(const CoordType &coords) const
 RegularGrid::ConstCoordItrRG::ConstCoordItrRG(const RegularGrid *rg, bool begin) : ConstCoordItrAbstract()
 {
     _dims = rg->GetDimensions();
-    _nDims = rg->GetNumDimensions();
     _delta = rg->_delta;
     _minu = rg->_minu;
     _coords = rg->_minu;
     _index = {0, 0, 0};
 
 
-    if (!begin) {
-        if (_nDims < 1)
-            _index[0] = 1;    // edge case for 0D grids
-        else
-            _index[_nDims - 1] = _dims[_nDims - 1];
-    }
+    if (!begin) { _index = {0, 0, _dims[_dims.size() - 1]}; }
 }
 
 RegularGrid::ConstCoordItrRG::ConstCoordItrRG(const ConstCoordItrRG &rhs) : ConstCoordItrAbstract()
 {
     _index = rhs._index;
     _dims = rhs._dims;
-    _nDims = rhs._nDims;
     _minu = rhs._minu;
     _delta = rhs._delta;
     _coords = rhs._coords;
@@ -335,8 +325,6 @@ void RegularGrid::ConstCoordItrRG::next()
     _coords[0] += _delta[0];
     if (_index[0] < _dims[0]) { return; }
 
-    if (_nDims <= 1) { return; }
-
     _index[0] = 0;
     _coords[0] = _minu[0];
     _index[1]++;
@@ -344,28 +332,23 @@ void RegularGrid::ConstCoordItrRG::next()
 
     if (_index[1] < _dims[1]) { return; }
 
-    if (_nDims == 2) { return; }
-
     _index[1] = 0;
     _coords[1] = _minu[1];
     _index[2]++;
     _coords[2] += _delta[2];
+
+    if (_index[2] < _dims[2]) { return; }
+
+    _index[2] = _dims[2];    // last index;
 }
 
 void RegularGrid::ConstCoordItrRG::next(const long &offset)
 {
-    if (!_nDims) return;
-
-    static DimsType maxIndex = {0, 0, 0};
-    ;
-    for (int i = 0; i < _nDims; i++) maxIndex[i] = _dims[i] - 1;
-
-    long maxIndexL = Wasp::LinearizeCoords(maxIndex.data(), _dims.data(), _dims.size());
+    long maxIndexL = Wasp::VProduct(_dims.data(), _dims.size()) - 1;
     long newIndexL = Wasp::LinearizeCoords(_index.data(), _dims.data(), _dims.size()) + offset;
     if (newIndexL < 0) { newIndexL = 0; }
     if (newIndexL > maxIndexL) {
-        _index = {0, 0, 0};
-        _index[_nDims - 1] = _dims[_nDims - 1];
+        _index = {0, 0, _dims[_dims.size() - 1]};
         return;
     }
 

commit 379ff97afe8bd90b3e52ded40f7740500de158ba

Fixed #2899 - Problems switching barb or flow renderer between 2D and 3D variables (#2998)
* Fixed #2899 - Problems switching barb or flow renderer between 2D and 3D variables

* Fixed regression from PR #2994

* clang-format pre-push hook

include/vapor/DataMgr.h
@@ -484,6 +484,10 @@ public:
     //!
     size_t GetVarTopologyDim(string varname) const;
 
+    //! \copydoc DC:GetVarGeometryDim()
+    //!
+    size_t GetVarGeometryDim(string varname) const;
+
     //! Clear the memory cache
     //!
     //! This method clears the internal memory cache of all entries
@@ -584,7 +588,7 @@ public:
 
         void Insert(const DimsType &bcoord, const CoordType &min, const CoordType &max);
 
-        bool Intersect(const CoordType &min, const CoordType &max, DimsType &bmin, DimsType &bmax) const;
+        bool Intersect(const CoordType &min, const CoordType &max, DimsType &bmin, DimsType &bmax, int nCoords = 3) const;
 
         friend std::ostream &operator<<(std::ostream &o, const BlkExts &b);
 

lib/vdc/CurvilinearGrid.cpp
@@ -296,7 +296,7 @@ CurvilinearGrid::ConstCoordItrCG::ConstCoordItrCG(const CurvilinearGrid *cg, boo
     if (_terrainFollowing) {
         _coords[2] = *_zCoordItr;
     } else {
-        _coords[2] = _cg->_zcoords[0];
+        if (_cg->_zcoords.size()) _coords[2] = _cg->_zcoords[0];
     }
 }
 

lib/vdc/DataMgr.cpp
@@ -1743,6 +1743,23 @@ size_t DataMgr::GetVarTopologyDim(string varname) const
     return (mesh.GetTopologyDim());
 }
 
+size_t DataMgr::GetVarGeometryDim(string varname) const
+{
+    VAssert(_dc);
+
+    DC::DataVar var;
+    bool        status = GetDataVarInfo(varname, var);
+    if (!status) return (0);
+
+    string mname = var.GetMeshName();
+
+    DC::Mesh mesh;
+    status = GetMesh(mname, mesh);
+    if (!status) return (0);
+
+    return (mesh.GetGeometryDim());
+}
+
 template<typename T> T *DataMgr::_get_region_from_cache(size_t ts, string varname, int level, int lod, const DimsType &bmin, const DimsType &bmax, bool lock)
 {
     list<region_t>::iterator itr;
@@ -2350,7 +2367,7 @@ void DataMgr::BlkExts::Insert(const DimsType &bcoord, const CoordType &min, cons
     _maxs[offset] = max;
 }
 
-bool DataMgr::BlkExts::Intersect(const CoordType &min, const CoordType &max, DimsType &bmin, DimsType &bmax) const
+bool DataMgr::BlkExts::Intersect(const CoordType &min, const CoordType &max, DimsType &bmin, DimsType &bmax, int nCoords) const
 {
     bmin = _bmax;
     bmax = _bmin;
@@ -2362,7 +2379,7 @@ bool DataMgr::BlkExts::Intersect(const CoordType &min, const CoordType &max, Dim
     //
     for (size_t offset = 0; offset < _mins.size(); offset++) {
         bool overlap = true;
-        for (int j = 0; j < min.size(); j++) {
+        for (int j = 0; j < nCoords; j++) {
             if (_maxs[offset][j] < min[j] || _mins[offset][j] > max[j]) {
                 overlap = false;
                 break;
@@ -2379,7 +2396,7 @@ bool DataMgr::BlkExts::Intersect(const CoordType &min, const CoordType &max, Dim
             DimsType coord;
             Wasp::VectorizeCoords(offset, _bmin.data(), _bmax.data(), coord.data(), coord.size());
 
-            for (int i = 0; i < coord.size(); i++) {
+            for (int i = 0; i < nCoords; i++) {
                 if (coord[i] < bmin[i]) bmin[i] = coord[i];
                 if (coord[i] > bmax[i]) bmax[i] = coord[i];
             }
@@ -2762,10 +2779,12 @@ int DataMgr::_find_bounding_grid(size_t ts, string varname, int level, int lod, 
 
     const BlkExts &blkexts = itr->second;
 
+
+
     // Find block coordinates of region that contains the bounding volume
     //
     DimsType bmin, bmax;
-    ok = blkexts.Intersect(min, max, bmin, bmax);
+    ok = blkexts.Intersect(min, max, bmin, bmax, GetVarGeometryDim(varname));
     if (!ok) { return (1); }
 
     // Finally, map from block to voxel coordinates