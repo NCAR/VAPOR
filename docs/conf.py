631487620420e0fe8281025bdcde25ac90bfe710
Scott Pearse <pearse@ucar.edu>
2022-02-24 17:18:32 -0700

    update for 3.6

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ author = 'John Clyne, Scott Pearse, Samuel Li, Stanislaw Jaroszynski'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.5.0'
+release = '3.6.0'
 
 
 #breathe_projects = { "myproject": "/Users/pearse/vapor2/targets/common/doc/library/xml" }
