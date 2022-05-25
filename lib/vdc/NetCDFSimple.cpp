c3210017e4802bb45b29913c9688f029115a21c2
Scott Pearse <pearse@ucar.edu>
2022-05-25 11:04:09 -0600

    Update new 3rd party libraries for 3.7

--- lib/vdc/NetCDFSimple.cpp
+++ lib/vdc/NetCDFSimple.cpp
@@ -200,7 +200,10 @@ int NetCDFSimple::Read(const size_t start[], const size_t count[], float *data, 
 
     int rc = nc_get_vara_float(_ncid, varid, start, count, data);
     if (rc != 0) {
-        SetErrMsg("nc_get_vara_float(%d, %d) : %s", _ncid, varid, nc_strerror(rc));
+        std::string error = nc_strerror(rc);
+        if (error.find("NetCDF: Filter error: unimplemented filter encountered") != std::string::npos)
+            SetErrMsg("\nHave you set the environment variable HDF5_PLUGIN_PATH to Vapor's /lib directory?\n");
+        SetErrMsg("nc_get_vara_float(%d, %d) : %s", _ncid, varid, error.c_str());
         return (-1);
     }
     return (0);
