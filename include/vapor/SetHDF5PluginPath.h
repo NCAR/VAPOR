bf3e985b8d6403cb3d957bc1b5dd4fd12c769eaf
Scott Pearse <pearse@ucar.edu>
2022-08-16 09:06:22 -0600

    forgot to add the important file

---
+++ include/vapor/SetHDF5PluginPath.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vapor/ResourcePath.h"
+
+#ifndef WIN32
+    #include <H5PLpublic.h>
+#endif
+
+namespace VAPoR {
+VDF_API void SetHDF5PluginPath() {
+    string plugins = Wasp::GetSharePath("plugins");
+
+    #ifndef WIN32
+        H5PLreplace(plugins.c_str(), 0);
+    #else
+        plugins = "HDF5_PLUGIN_PATH=" + plugins;
+        int rc=_putenv(plugins.c_str());
+        if (rc != 0) MyBase::SetErrMsg("Unable to set environtment variable %s", plugins.c_str());
+    #endif
+}
+};    // namespace VAPoR
