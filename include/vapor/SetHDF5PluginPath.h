c837fd22976c2187d365a1f6feba0210346f2045
Scott Pearse <pearse@ucar.edu>
2023-01-19 12:51:01 -0700

    fix 3306

--- include/vapor/SetHDF5PluginPath.h
+++ include/vapor/SetHDF5PluginPath.h
@@ -3,20 +3,16 @@
 #include "vapor/MyBase.h"
 #include "vapor/ResourcePath.h"
 
-#ifndef WIN32
-    #include <H5PLpublic.h>
-#endif
-
 namespace VAPoR {
 void SetHDF5PluginPath() {
-    string plugins = Wasp::GetSharePath("plugins");
-
-    #ifndef WIN32
-        H5PLreplace(plugins.c_str(), 0);
+    int rc;
+    std::string plugins = "HDF5_PLUGIN_PATH=" + Wasp::GetSharePath("plugins");
+    #ifdef WIN32
+        rc=_putenv(plugins.c_str());
     #else
-        plugins = "HDF5_PLUGIN_PATH=" + plugins;
-        int rc=_putenv(plugins.c_str());
-        if (rc != 0) Wasp::MyBase::SetErrMsg("Unable to set environtment variable %s", plugins.c_str());
+        rc = setenv("HDF5_PLUGIN_PATH", Wasp::GetSharePath("plugins").c_str(), 1);
     #endif
+
+    if (rc != 0) Wasp::MyBase::SetErrMsg("Unable to set environtment variable s", plugins.c_str());
 }
 };    // namespace VAPoR
