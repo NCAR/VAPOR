fc93e061a39bb6fa5b4ebaa8361eba75fb6d54d6
StasJ <StasJ@users.noreply.github.com>
2023-04-17 14:36:07 -0600

    Fix jupyter-widget and move to own repo (#3371)
    * Fix jupyter-widget and move to own repo
    
    * Fix separate widget (#3378)
    
    * test
    
    * Test
    
    * test
    
    ---------
    
    Co-authored-by: sgpearse <pearse@ucar.edu>

--- apps/pythonapi/jupyter-vapor-widget/js/lib/index.js
+++
@@ -1,6 +0,0 @@
-// Export widget models and views, and the npm package version number.
-
-export {HelloModel, HelloView} from './example';
-export {VaporVisualizerModel, VaporVisualizerView} from './VaporVisualizerWidget';
-export {version} from '../package.json';
-
--- apps/pythonapi/jupyter-vapor-widget/jupyter_vapor_widget/_version.py
+++
@@ -1,4 +0,0 @@
-# Module version
-__version__ = '0.1.0a0'
-
-NPM_PACKAGE_RANGE='^1.0'
