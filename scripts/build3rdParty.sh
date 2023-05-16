55004ace1e36240256b323ac914250168d3dd0d3
sgpearse <pearse@ucar.edu>
2023-05-16 14:36:30 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -650,32 +650,32 @@ elif [ "$OS" == "Windows" ]; then
     windowsPrerequisites
 fi
 
-openssl
-pythonVapor
-zlib
-libpng
-assimp
-szip
-hdf5
-netcdf
-expat
-udunits
-freetype
-jpeg
-tiff
-sqlite
-proj
-geotiff
-#geotiff2
-if [ "$OS" == "Ubuntu" ] ; then
-   xinerama
-fi         
-ospray
-glm
-gte
-images
+#openssl
+#pythonVapor
+#zlib
+#libpng
+#assimp
+#szip
+#hdf5
+#netcdf
+#expat
+#udunits
+#freetype
+#jpeg
+#tiff
+#sqlite
+#proj
+#geotiff
+##geotiff2
+#if [ "$OS" == "Ubuntu" ] ; then
+#   xinerama
+#fi         
+#ospray
+#glm
+#gte
+#images
 qt
-if [ "$OS" == "OSX" ] || [ "$OS" == "M1" ]; then
-   $installDir/bin/python3.9.vapor /Users/distiller/project/buildutils/OSX_PostBuild.py
-fi         
-renameAndCompress
+#if [ "$OS" == "OSX" ] || [ "$OS" == "M1" ]; then
+#   $installDir/bin/python3.9.vapor /Users/distiller/project/buildutils/OSX_PostBuild.py
+#fi         
+#renameAndCompress
