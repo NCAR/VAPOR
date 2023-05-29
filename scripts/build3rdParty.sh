95a6bb232512f557d39b92b8f31a5d370ba97b07
Scott Pearse <pearse@ucar.edu>
2023-05-29 12:33:57 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -60,6 +60,8 @@ macOSPrerequisites() {
     brew install xz zlib openssl
     brew install python@3.9
     brew install pkg-config openssl@1.1 xz gdbm tcl-tk # https://devguide.python.org/getting-started/setup-building/index.html#macos-and-os-x
+    brew install gettext
+    brew link gettext --force
 }
 
 ubuntuPrerequisites() {
