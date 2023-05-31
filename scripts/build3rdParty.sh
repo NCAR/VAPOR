b700f6c6ae3949e98f9294209cb09dc3e5569cc3
Scott Pearse <pearse@ucar.edu>
2023-05-31 07:36:11 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -798,32 +798,32 @@ elif [ "$OS" == "Windows" ]; then
     windowsPrerequisites
 fi
 
-#openssl
-#pythonVapor
-#zlib
-#libpng
-#assimp
-#szip
-#hdf5
-#netcdf
-#expat
-#udunits
-#freetype
-#jpeg
-#tiff
-#sqlite
-#proj
-#geotiff
-###geotiff2
-#if [ "$OS" == "Ubuntu" ] ; then
-#   xinerama
-#fi         
-#ospray
-#glm
-#gte
-#images
-#qt
-#if [ "$OS" == "macOSx86" ] || [ "$OS" == "M1" ]; then
-#    add_rpath
-#fi         
-#renameAndCompress
+openssl
+pythonVapor
+zlib
+libpng
+assimp
+szip
+hdf5
+netcdf
+expat
+udunits
+freetype
+jpeg
+tiff
+sqlite
+proj
+geotiff
+##geotiff2
+if [ "$OS" == "Ubuntu" ] ; then
+   xinerama
+fi         
+ospray
+glm
+gte
+images
+qt
+if [ "$OS" == "macOSx86" ] || [ "$OS" == "M1" ]; then
+    add_rpath
+fi         
+renameAndCompress
