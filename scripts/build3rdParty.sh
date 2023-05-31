3af4bea8454a25f955ed0f2ce600c6b5c928d1c6
Scott Pearse <pearse@ucar.edu>
2023-05-31 11:10:39 -0600

    x86nogettext

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -807,28 +807,28 @@ fi
 openssl
 zlib
 pythonVapor
-#libpng
-#assimp
-#szip
-#hdf5
-#netcdf
-#expat
-#udunits
-#freetype
-#jpeg
-#tiff
-#sqlite
-#proj
-#geotiff
-###geotiff2
-#if [ "$OS" == "Ubuntu" ] ; then
-#   xinerama
-#fi         
-#ospray
-#glm
-#gte
-#images
-#qt
+libpng
+assimp
+szip
+hdf5
+netcdf
+expat
+udunits
+freetype
+jpeg
+tiff
+sqlite
+proj
+geotiff
+##geotiff2
+if [ "$OS" == "Ubuntu" ] ; then
+   xinerama
+fi         
+ospray
+glm
+gte
+images
+qt
 if [ "$OS" == "macOSx86" ] || [ "$OS" == "M1" ]; then
     add_rpath
 fi         
