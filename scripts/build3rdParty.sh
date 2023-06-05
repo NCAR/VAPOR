5add75436565e0073ee7254345ed4af07794c450
Scott Pearse <pearse@ucar.edu>
2023-06-05 17:02:11 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -82,7 +82,7 @@ ubuntuPrerequisites() {
 
     # all for cmake
     apt-get update
-    apt-get install -y gpg wget
+    apt-get install -y gpg wget libssl1.1
     wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
     echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
     DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
