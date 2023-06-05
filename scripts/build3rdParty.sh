133f54f59768afe9f1a4732c134105ed44c41057
Scott Pearse <pearse@ucar.edu>
2023-06-05 16:49:12 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -86,7 +86,7 @@ ubuntuPrerequisites() {
     wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
     echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
     DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
-    apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+    apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ focal main'
     apt install -y cmake --allow-unauthenticated
 
     apt install -y \
