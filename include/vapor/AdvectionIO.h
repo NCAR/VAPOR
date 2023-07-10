2ecf47661943d536e66d980cbd426fedd1fc91fb
Samuel Li <shaomenglee@gmail.com>
2023-07-10 12:54:48 -0600

    fix #3361

--- include/vapor/AdvectionIO.h
+++ include/vapor/AdvectionIO.h
@@ -12,18 +12,24 @@
 
 namespace flow {
 // Output a certain number of steps from an advection.
-// When `append == false`, a header will also be output.
-// Otherwise, only trajectories are output.
-FLOW_API auto OutputFlowlinesNumSteps(const Advection *adv, const char *filename, size_t numStep, const std::string &proj4string, bool append) -> int;
+// When `append == false`, a header will also be output; otherwise, only trajectories are output.
+FLOW_API auto OutputFlowlinesNumSteps(const Advection* adv,
+                                      const char* filename,
+                                      size_t numStep,
+                                      const std::string& proj4string,
+                                      bool append) -> int;
 
 // Output trajectory to a maximum time.
-// When `append == false`, a header will also be output.
-// Otherwise, only trajectories are output.
-FLOW_API auto OutputFlowlinesMaxTime(const Advection *adv, const char *filename, double maxTime, const std::string &proj4string, bool append) -> int;
+// When `append == false`, a header will also be output; otherwise, only trajectories are output.
+FLOW_API auto OutputFlowlinesMaxTime(const Advection* adv,
+                                     const char* filename,
+                                     double maxTime,
+                                     const std::string& proj4string,
+                                     bool append) -> int;
 
 // Input a list of seeds from lines of CSVs.
 // In case of any error occurs, it returns an empty list.
-FLOW_API auto InputSeedsCSV(const std::string &filename) -> std::vector<flow::Particle>;
+FLOW_API auto InputSeedsCSV(const std::string& filename) -> std::vector<flow::Particle>;
 
-};    // namespace flow
+};  // namespace flow
 #endif
