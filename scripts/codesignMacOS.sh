13b4c678b33189e0beb6f6a77a52fcf5a2fe1b5e
sgpearse <pearse@ucar.edu>
2024-06-07 12:31:05 -0600

    test app working

--- scripts/codesignMacOS.sh
+++ scripts/codesignMacOS.sh
@@ -171,6 +171,16 @@ for file in $(find $binDir -name "*"); do
     $codesignSignature $file
 done
 
+#baseDir="/Applications/vapor.app"
+#binDir="${baseDir}/Contents/MacOS"
+#libDir="${baseDir}/Contents/Frameworks"
+#pluginDir="${baseDir}/Contents/PlugIns"
+#pyDir="${libDir}/python3.9"
+##codesignSignature="sudo codesign --force --verbose=2 --sign DQ4ZFL4KLF --options runtime --timestamp"
+#declare codesignSignature="sudo codesign --verbose=2 --sign DQ4ZFL4KLF --options runtime --timestamp"
+#declare removeCodeSignature="sudo codesign --remove-signature"
+#declare verifyCodeSignature="codesign --verify --deep --strict --verbose=2"
+
 # Codesign the bundle
 printf "Codesign the bundle"
 $codesignSignature --force $baseDir
@@ -180,22 +190,72 @@ version=$($baseDir/Contents/MacOS/vaporversion)
 version=3.9.1
 hdiutil create -ov -srcFolder $baseDir -o VAPOR3-$version-M1signed.dmg
 
+# try to sign the bundle
+#sudo codesign --force --verbose=2 --sign DQ4ZFL4KLF --options runtime --timestamp --force /Applications/testApp.app
+
+# Codesign unsigned frameworks that macdeployqt complains about
+#codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app/Contents/Frameworks/QtUiPlugin.framework
+#codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app
+
 # app-specific passwords: https://support.apple.com/en-us/102654
-xcrun notarytool store-credentials "notarytool-password" --apple-id pearse@ucar.edu --team-id DQ4ZFL4KLF --password vvnd-oaaz-rxge-ukma
+xcrun notarytool store-credentials "testApp-password" --apple-id pearse@ucar.edu --team-id DQ4ZFL4KLF --password ggjp-lbdh-ziib-ruqg
+#xcrun notarytool store-credentials "notarytool-password" --apple-id pearse@ucar.edu --team-id DQ4ZFL4KLF --password vvnd-oaaz-rxge-ukma
+
+codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app/Contents/MacOS/platforms/libqcocoa.dylib
+codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app/Contents/MacOS/styles/libqmacstyle.dylib
+
+# run macdeployqt
+#/usr/local/VAPOR-Deps/current/bin/macdeployqt /Applications/testApp.app -dmg -sign-for-notarization="Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF), pearse@ucar.com, ggjp-lbdh-ziib-ruqg"
+#/usr/local/VAPOR-Deps/current/bin/macdeployqt /Applications/testApp.app -timestamp -dmg -sign-for-notarization="Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)"
+/usr/local/VAPOR-Deps/current/bin/macdeployqt /Applications/testApp.app -timestamp -sign-for-notarization="Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)"
+
+# create and sign the .dmg
+hdiutil create -volname "testApp" -srcfolder /Applications/testApp.dmg -ov -format UDZO /Applications/testApp.app
+codesign --force --verify --verbose --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" /Applications/testApp.dmg
 
 # notarize
-xcrun -v notarytool submit VAPOR3-3.9.1-M1signed.dmg --keychain-profile "notarytool-password" --wait
+xcrun -v notarytool submit /Applications/testApp.dmg --keychain-profile "testApp-password" --wait
+#xcrun -v notarytool submit testApp-M1signed.dmg --keychain-profile "testApp-password" --wait
+#xcrun -v notarytool submit VAPOR3-3.9.1-M1signed.dmg --keychain-profile "notarytool-password" --wait
 
 # Fetch the notorization log
+xcrun notarytool log --keychain-profile "testApp-password" 246f94c0-9efa-4f2f-9513-2da8a696573b
 #xcrun -v notarytool log f34e7964-93ab-4693-941d-c562cf7062a6
 # % xcrun notarytool log CREDENTIALS REQUEST_UUID
 # where:
 # CREDENTIALS is your notary service credentials, the same credentials you used to submit your request
 # REQUEST_UUID is the UUID printed by notarytool when you submitted your request.
 
+xcrun stapler staple /Applications/testApp.dmg
+xcrun stapler validate /Applications/testApp.dmg
 
 
 
+# Must install gcc@11 via homebrew to be compliant with the third-party-library build
+# > /opt/homebrew/bin/brew install gcc@11
+# > rm /opt/homebrew/opt/gcc
+# > ln -s /opt/homebrew/Cellar/gcc@11/11.4.0 /opt/homebrew/opt/gcc
+#for file in /Applications/testApp.app/Contents/Frameworks/libhdf5*.dylib; do
+for file in /Applications/testApp.app/Contents/Frameworks/*.dylib; do
+    # Check if the file exists to avoid printing the pattern itself when no files match
+    if [ -e "$file" ]; then
+        codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose $file
+    fi
+done
+
+codesign --verify --deep --strict --verbose=2
+
+codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app/Contents/MacOS/platforms/libqcocoa.dylib \
+&& codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose /Applications/testApp.app/Contents/MacOS/styles/libqmacstyle.dylib \
+&& /usr/local/VAPOR-Deps/current/bin/macdeployqt /Applications/testApp.app -timestamp -sign-for-notarization="Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" \
+
+&& codesign --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" --force --deep --verbose --options runtime /Applications/testApp.app/Contents/MacOS/testApp \
+
+&& hdiutil create -volname "testApp" -srcfolder /Applications/testApp.app -ov -format UDZO /Applications/testApp.dmg \
+&& codesign --force --verify --verbose --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" /Applications/testApp.dmg \
+&& xcrun -v notarytool submit /Applications/testApp.dmg --keychain-profile "testApp-password" --wait \
+&& xcrun stapler staple /Applications/testApp.dmg \
+&& xcrun stapler validate /Applications/testApp.dmg
 
 
 # NEED TO CONFIRM:
