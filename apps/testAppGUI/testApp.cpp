4861b0406433a26edac26140d9cd01cb15c271da
sgpearse <pearse@ucar.edu>
2024-06-20 10:57:26 -0600

    test

--- apps/testAppGUI/testApp.cpp
+++ apps/testAppGUI/testApp.cpp
@@ -6,7 +6,8 @@
 
 //#include <openssl/opensslv.h>
 //#include <openssl/crypto.h>
-#include <png.h>
+#include <hdf5.h>
+/*#include <png.h>
 #include <jpeglib.h>
 #include <tiffio.h>
 #include <sqlite3.h>
@@ -21,12 +22,11 @@
 #include <udunits2.h>
 #include <freetype2/ft2build.h>
 #include FT_FREETYPE_H
+#include <netcdf.h>*/
 //#include <ospray.h>
 
-#include <hdf5.h>
-#include <netcdf.h>
 
-//#include <vapor/MyPython.h>
+/*//#include <vapor/MyPython.h>
 //#include <vapor/CFuncs.h>
 #include <vapor/FileUtils.h> //common
 //#include <vapor/ResourcePath.h>
@@ -35,16 +35,13 @@
 #include <vapor/DataStatus.h> // params
 #include <vapor/Advection.h> //flow
 #include <vapor/GLContextProvider.h> //osgl
+*/
 
 #include <QApplication>
 #include <QLabel>
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    QLabel label("testApp Label");
-    label.show();
-
     //const char* version = OpenSSL_version(OPENSSL_VERSION);
     //std::cout << "OpenSSL library version: " << version << std::endl; 
 
@@ -52,7 +49,7 @@ int main(int argc, char **argv)
     H5get_libversion(&majnum, &minnum, &relnum);
     std::cout << "THIRD PARTY LIBRARIES" << std::endl;
     std::cout << "    HDF5 library version: " << majnum << "." << minnum << "." << relnum << std::endl;
-    std::cout << "    NetCDF library version: " << nc_inq_libvers() << std::endl;
+/*    std::cout << "    NetCDF library version: " << nc_inq_libvers() << std::endl;
 
     std::cout << png_get_header_version(nullptr) << std::endl;
     std::cout << "    libjpeg version: " << JPEG_LIB_VERSION << std::endl;
@@ -69,8 +66,9 @@ int main(int argc, char **argv)
     std::cout << "    udunits test: " << UT_SUCCESS << std::endl;
     FT_Library  library; int error = FT_Init_FreeType( &library ); std::cout << "    freetype test " << error << std::endl;
     //std::cout << "    ospray test " << ospGetCurrentDevice() << std::endl;
+*/
 
-    std::cout << std::endl << std::endl << "VAPOR LIBRARIES" << std::endl;
+    /*std::cout << std::endl << std::endl << "VAPOR LIBRARIES" << std::endl;
     //std::cout << "    python test " <<  Wasp::MyPython::Instance()->Initialize() << std::endl;
 
     std::cout << "    common test " <<  Wasp::FileUtils::HomeDir() << std::endl;
@@ -91,7 +89,11 @@ int main(int argc, char **argv)
     std::cout << "    flow test " <<  a->GetNumberOfStreams() << std::endl;
 
     GLContext* c = GLContextProvider::CreateContext();
-    std::cout << "    osgl test " << c->GetVersion() << std::endl;
+    std::cout << "    osgl test " << c->GetVersion() << std::endl;*/
+
+    QApplication app(argc, argv);
+    QLabel label("testApp Label");
+    label.show();
 
     return app.exec();
 }
