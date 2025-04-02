0206f4a1dbacfb9bd4b05d13cb26cb7e445e303b
sgpearse <pearse@ucar.edu>
2025-04-01 18:13:11 -0600

    windows test

--- apps/vaporgui/PImportDataWidget.cpp
+++ apps/vaporgui/PImportDataWidget.cpp
@@ -8,14 +8,10 @@
 #include "vapor/ControlExecutive.h"
 #include "vapor/GUIStateParams.h"
 
-PImportDataWidget::PImportDataWidget(VAPoR::ControlExec* ce, MainForm* mf) : PWidget("", _group = new PGroup()), _ce(ce), _mf(mf) {
+PImportDataWidget::PImportDataWidget(VAPoR::ControlExec* ce, MainForm* mf) : PGroup() {
     std::vector<std::string> types = GetDatasetTypeDescriptions();
     PRadioButtons* prb = new PRadioButtons(GUIStateParams::ImportDataTypeTag, types);
-    _group->Add(prb);
+    Add(prb);
     
-    _group->Add(new PImportDataButton(ce, mf));
-}
-
-void PImportDataWidget::updateGUI() const {
-    _group->Update(getParams());
+    Add(new PImportDataButton(ce, mf));
 }
--- apps/vaporgui/PImportDataWidget.h
+++ apps/vaporgui/PImportDataWidget.h
@@ -1,32 +1,17 @@
 #pragma once
 
+#include "PGroup.h"
 #include "PSection.h"
 #include "PLineItem.h"
 
 namespace VAPoR {
     class ControlExec;
 }
-class VGroup;
-class PGroup;
-class QRadioButton;
-class PFilesOpenSelector;
 class MainForm;
-class VHBoxWidget;
-class VLabel;
 
-class PImportDataWidget : public PWidget {
+class PImportDataWidget : public PGroup {
     Q_OBJECT
-
-    PGroup*             _group;
-    VSection*           _section;
-    PFilesOpenSelector* _selector;
-    VHBoxWidget*        _hBox;
-    VAPoR::ControlExec* _ce;
-    MainForm* _mf;
 
 public:
     PImportDataWidget(VAPoR::ControlExec* ce, MainForm *mf);
-
-protected:
-    void updateGUI() const override;
 };
