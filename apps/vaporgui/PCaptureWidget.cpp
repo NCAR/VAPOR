ad0b25f21fb7d6f3039244547a53ad99498cf980
sgpearse <pearse@ucar.edu>
2025-08-06 08:20:41 -0600

    inc

--- apps/vaporgui/PCaptureWidget.cpp
+++ apps/vaporgui/PCaptureWidget.cpp
@@ -15,14 +15,9 @@
 #include "vapor/FileUtils.h"
 #include "vapor/NavigationUtils.h"
 
+#include <regex>
 #include <QFileDialog>
 #include <QHBoxLayout>
-
-const std::string TiffStrings::FileFilter = "TIFF (*.tif)";
-const std::string TiffStrings::FileSuffix = "tif";
-
-const std::string PngStrings::FileFilter = "PNG (*.png)";
-const std::string PngStrings::FileSuffix = "png";
 
 PCaptureHBox::PCaptureHBox(VAPoR::ControlExec *ce, CaptureController *captureController)
     : PWidget("", _hBox = new VHBoxWidget()), _ce(ce), _captureController(captureController)
@@ -34,11 +29,10 @@ PCaptureHBox::PCaptureHBox(VAPoR::ControlExec *ce, CaptureController *captureCon
     _fileLabel = new VLabel("");
     _fileLabel->MakeSelectable();
 
-    //_captureButton = new PButton("Export", [this](VAPoR::ParamsBase*){CaptureUtils::CaptureSingleImage(_ce);});
     _captureButton = new PButton("Export", [this](VAPoR::ParamsBase*){_captureController->CaptureSingleImage();});
     _captureButton->ShowBasedOnParam(AnimationParams::CaptureModeTag, AnimationParams::SingleImage);
 
-    _captureTimeSeriesButton = new PButton("Export", [this](VAPoR::ParamsBase*){_captureTimeSeries();});
+    _captureTimeSeriesButton = new PButton("Export", [this](VAPoR::ParamsBase*){_captureController->EnableAnimationCapture();});
     _captureTimeSeriesButton->ShowBasedOnParam(AnimationParams::CaptureModeTag, AnimationParams::TimeSeries);
 
     QHBoxLayout* layout = qobject_cast<QHBoxLayout*>(_hBox->layout());
@@ -70,8 +64,8 @@ void PCaptureHBox::updateGUI() const {
         file = ap->GetValueString(AnimationParams::CaptureTimeSeriesFileNameTag, "");
         if (file != "") {
             file = FileUtils::Basename(ap->GetValueString(AnimationParams::CaptureTimeSeriesFileNameTag, ""));
-            size_t lastDot = file.find_last_of(".");
-            file = file.substr(0, lastDot) + "####" + file.substr(lastDot);
+            size_t pos = file.rfind("0000");
+            if (pos != std::string::npos) file.replace(pos, 4, "####"); // Replace last instance of 0000 with #### for file label
             time = ap->GetValueString(AnimationParams::CaptureTimeSeriesTimeTag, "");
         }
     }
@@ -80,18 +74,6 @@ void PCaptureHBox::updateGUI() const {
 
     _captureButton->Update(ap);
     _captureTimeSeriesButton->Update(ap);
-}
-
-void PCaptureHBox::_captureTimeSeries() {
-    // User may cancel to prevent file overwrite, so capture return code
-    //bool rc = CaptureUtils::EnableAnimationCapture(_ce);
-    bool rc = _captureController->EnableAnimationCapture();
-    if (rc) {
-        //AnimationParams* ap = (AnimationParams*)_ce->GetParamsMgr()->GetParams(AnimationParams::GetClassType());
-    //    std::cout << "Setting to timestep " << ap->GetStartTimestep() << std::endl;
-        //NavigationUtils::SetTimestep(_ce, ap->GetValueLong(AnimationParams::CaptureStartTag, ap->GetStartTimestep()));
-        //ap->SetValueLong(AnimationParams::AnimationStartedTag, "Enable tag", true);
-    }
 }
 
 PCaptureWidget::PCaptureWidget(VAPoR::ControlExec *ce, CaptureController *captureController)
